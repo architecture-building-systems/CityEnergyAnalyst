123b9d2738c814f3c4b27a6259a96b112722adeb
daren-thomas <dthomas.ch@gmail.com>
2016-03-31 17:16:29 +0200

    DemandTool works, but demand_calculation does not
    there is a bug around line 102 (`df` is not set if
    `len(prop_RC_model.index) == 0`

--- cea/functions.py
+++ cea/functions.py
@@ -134,7 +134,7 @@ def CalcIncidentRadiation(AllProperties, Radiation_Shading2):
          #transform all the points of solar radiation into Wh
         Radiation_Shading2['T'+str(Column)] = Radiation_Shading2['T'+str(Column)]*Radiation_Shading2['AreaExposed']
 
-    Radiation_Shading2.to_csv(r'C:\Users\Jimeno\Desktop\test.csv')
+    #Radiation_Shading2.to_csv(r'C:\Users\Jimeno\Desktop\test.csv')
     #Do pivot table to sum up the irradiation in every surface to the building 
     #and merge the result with the table allProperties
     PivotTable3 = pd.pivot_table(Radiation_Shading2, rows='Name', margins='Add all row')
