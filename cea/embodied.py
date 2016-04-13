64a19afe3a4ffc3dcab53059d39104259a813b44
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-04-13 17:49:47 +0800

    new changes

--- cea/embodied.py
+++ cea/embodied.py
@@ -69,7 +69,13 @@ def lca_embodied(path_LCA_embodied_energy, path_LCA_embodied_emissions, path_age
                                                         right_index=True).merge(architecture_df,
                                                                                 left_index=True,
                                                                                 right_index=True)
-    cat_df['facade'] =
+    # calculate building geometry
+    cat_df['windows_ag'] = cat_df['win_wall']*cat_df['perimeter']*cat_df['height_ag']
+    cat_df['area_walls_ext_ag'] = cat_df['perimeter']*cat_df['height_ag'] - cat_df['windows_ag']
+    cat_df['area_walls_ext_bg'] = cat_df['perimeter'] * cat_df['height_bg']
+    cat_df['floor_area_ag'] = cat_df['footprint'] * cat_df['floors_ag']
+    cat_df['floor_area_bg'] = cat_df['footprint'] * cat_df['floors_bg']
+    cat_df['total_area'] = cat_df['floor_area_ag'] + cat_df['floor_area_bg']
 
     # get categories for each year of construction/retrofit
     cat_df['cat_built'] = cat_df.apply(lambda x: calc_category_construction(x['mainuse'],x['built']), axis=1)
@@ -82,7 +88,7 @@ def lca_embodied(path_LCA_embodied_energy, path_LCA_embodied_emissions, path_age
     result = []
     for archetype in list_of_archetypes:
         database_df = pd.read_csv(archetype)
-
+        counter = 0
         # merge databases acording to category
         built_df = cat_df.merge(database_df, left_on='cat_built', right_on='Code')
         envelope_df = cat_df.merge(database_df, left_on='cat_envelope', right_on='Code')
@@ -93,201 +99,79 @@ def lca_embodied(path_LCA_embodied_energy, path_LCA_embodied_emissions, path_age
         HVAC_df = cat_df.merge(database_df, left_on='cat_HVAC', right_on='Code')
 
         # contributions due to construction
-        [Wall_ext_ag, Wall_ext_bg, Floor_int, Wall_int_sup, Wall_int_nosup, Roof, Floor_g, Services, Win_ext, Excavation]
+        built_df['delta_year'] =  (built_df['envelope']-yearcalc)*-1
+        built_df['confirm'] = built_df.apply(lambda x: calc_if_existing(x['delta_year'], gv.sl_materials), axis=1)
+        built_df['contrib'] = ((built_df['Wall_ext_ag']*built_df['area_walls_ext_ag'])+
+                               (built_df['Roof']*built_df['footprint'])+
+                               (built_df['windows_ag']*built_df['Win_ext']) +
+                               (built_df['floor_area_ag']*built_df['Floor_int']+
+                                built_df['floor_area_ag']*built_df['Wall_int_sup']*gv.fwratio +
+                                built_df['footprint'] * built_df['Wall_int_nosup']*gv.fwratio)+
+                               (basement_df['footprint'] * basement_df['Floor_g'] +
+                                basement_df['Wall_ext_bg'] * basement_df['area_walls_ext_bg']) +
+                               (built_df['footprint'] * built_df['Excavation']))/gv.sl_materials + \
+                               ((HVAC_df['floor_area_ag']+HVAC_df['footprint']) * HVAC_df['Services')/gv.sl_services
 
         # contributions due to envelope retrofit
-        [Wall_ext_ag]
-        envelope_df['contrib'] = envelope_df['Wall_ext_ag']*envelope_df['Wall_ext_ag']
+        envelope_df['delta_year'] =  (envelope_df['envelope']-yearcalc)*-1
+        envelope_df['confirm'] = envelope_df.apply(lambda x: calc_if_existing(x['delta_year'],gv.sl_materials), axis=1)
+        envelope_df['contrib'] = (envelope_df['Wall_ext_ag']*envelope_df['area_walls_ext_ag'])*envelope_df['confirm']/(gv.sl_materials)
 
         # contributions due to roof retrofit
-        [Roof]
+        roof_df['delta_year'] =  (roof_df['roof']-yearcalc)*-1
+        roof_df['confirm'] = roof_df.apply(lambda x: calc_if_existing(x['delta_year'],gv.sl_materials), axis=1)
+        roof_df['contrib'] = roof_df['Roof']*roof_df['footprint']*roof_df['confirm']/gv.sl_materials
 
         # contributions due to windows retrofit
-        [Win_ext]
+        windows_df['delta_year'] =  (windows_df['windows']-yearcalc)*-1
+        windows_df['confirm'] = windows_df.apply(lambda x: calc_if_existing(x['delta_year'],gv.sl_materials), axis=1)
+        windows_df['contrib'] = windows_df['windows_ag']*windows_df['Win_ext']*windows_df['confirm']/gv.sl_materials
 
         # contributions due to partitions retrofit
-        [[Floor_int, Wall_int_sup, Wall_int_nosup]]
+        partitions_df['delta_year'] =  (partitions_df['partitions']-yearcalc)*-1
+        partitions_df['confirm'] = partitions_df.apply(lambda x: calc_if_existing(x['delta_year'],gv.sl_materials), axis=1)
+        partitions_df['contrib'] = (partitions_df['floor_area_ag']*partitions_df['Floor_int']+
+                                    partitions_df['floor_area_ag']*partitions_df['Wall_int_sup']*gv.fwratio +
+                                    partitions_df['footprint'] * partitions_df['Wall_int_nosup']*gv.fwratio) * \
+                                    partitions_df['confirm']/gv.sl_materials
 
         # contributions due to basement_df
-        [[Floor_g, Wall_ext_bg]]
+        basement_df['delta_year'] = (basement_df['basement'] - yearcalc) * -1
+        basement_df['confirm'] = basement_df.apply(lambda x: calc_if_existing(x['delta_year'], gv.sl_materials),
+                                                       axis=1)
+        basement_df['contrib'] = (basement_df['footprint'] * basement_df['Floor_g'] +
+                                  basement_df['Wall_ext_bg'] * basement_df['area_walls_ext_bg'])* \
+                                  basement_df['confirm']/gv.sl_materials
 
         # contributions due to HVAC_df
-        [[Services]]
+        HVAC_df['delta_year'] = (HVAC_df['basement'] - yearcalc) * -1
+        HVAC_df['confirm'] = HVAC_df.apply(lambda x: calc_if_existing(x['delta_year'], gv.sl_materials),axis=1)
+        HVAC_df['contrib'] = ((HVAC_df['floor_area_ag']+HVAC_df['footprint']) * HVAC_df['Services'])*HVAC_df['confirm'] \
+                             /gv.sl_services
+        if counter = 0:
+            built_df['GEN_GJ'] = (HVAC_df['contrib']+basement_df['contrib']+ partitions_df['contrib']+built_df['contrib']+
+                                  roof_df['contrib'] + envelope_df['contrib'] + windows_df['contrib'])/1000
+            built_df['GEN_MJm2'] = built_df['GEN_GJ']*1000/built_df['total_area']
 
-
-        # building construction properties to array
-        fp = df3['footprint'].values
-        floors = df3['floors'].values
-        af = fp*floors*df3['Es'].values
-        yearcons = df3['year_built'].values
-        yearretro = df3['year_retrofit'].values
-        height = df3['height'].values
-        fwindow = df3['fwindow'].values
-        perimeter = df3['perimeter'].values
-        area = df3['built_area'].values
-        PFloor = df3['PFloor'].values
-        windows = perimeter*height*fwindow
-
-        # computing factors to array of construction
-        win_ext_factor = df3['Win_ext'].values
-        excavation_factor = df3['Excavation'].values
-        wall_int_avg_factor = (
-            (df3['Wall_int_nosup'] +
-             df3['Wall_int_sup']) *
-            gv.fwratio /
-            2).values
-        walls_ext_bg_factor = df3['Wall_ext_bg'].values
-        walls_ext_ag_factor = df3['Wall_ext_ag'].values
-        floor_int_factor = df3['Floor_int'].values
-        services_factor = df3['Services'].values
-        floor_basement_factor = df3['Floor_g'].values
-        roof_factor = df3['Roof'].values
-
-        # computing factors to array of retrofit
-        win_ext_factor2 = df3['Win_ext_y'].values
-        wall_int_avg_factor2 = (
-            (df3['Wall_int_nosup_y'] +
-             df3['Wall_int_sup_y']) *
-            gv.fwratio /
-            2).values
-        walls_ext_ag_factor2 = df3['Wall_ext_ag_y'].values
-        floor_int_factor2 = df3['Floor_int_y'].values
-        services_factor2 = df3['Services_y'].values
-        floor_basement_factor2 = df3['Floor_g_y'].values
-        roof_factor2 = df3['Roof_y'].values
-
-        # computing factor out vectorization
-        # windows
-        win_ext = win_ext_factor*windows
-        win_ext2 = win_ext_factor2*windows
-        # external walls
-        walls_ext_ag = walls_ext_ag_factor*perimeter*height*(1-fwindow)*PFloor
-        walls_ext_ag2 = walls_ext_ag_factor2 * \
-            perimeter*height*(1-fwindow)*PFloor
-        # external walls basement
-        walls_ext_bg = walls_ext_bg_factor*perimeter*gv.hf
-        # technical instalations
-        services = services_factor*af
-        services2 = services_factor2*af
-        # floor basement
-        floor_basement = floor_basement_factor*fp
-        floor_basement2 = floor_basement_factor2*fp
-        # roof
-        roof = roof_factor*fp
-        roof2 = roof_factor2*fp
-        # excavation
-        excavation = excavation_factor*fp
-        # computing individual shares per building component
-        result.append(
-            np.vectorize(query_embodied)(
-                fp,
-                floors,
-                yearcons,
-                yearretro,
-                PFloor,
-                walls_ext_ag,
-                walls_ext_ag2,
-                yearcalc,
-                windows,
-                win_ext,
-                win_ext2,
-                excavation,
-                wall_int_avg_factor,
-                wall_int_avg_factor2,
-                walls_ext_bg,
-                floor_int_factor,
-                floor_int_factor2,
-                services,
-                services2,
-                floor_basement,
-                floor_basement2,
-                roof,
-                roof2,
-                ''.join(str(int(b)) for b in (
-                    retrofit_roof,
-                    retrofit_walls,
-                    retrofit_partitions,
-                    retrofit_windows,
-                    retrofit_int_floors,
-                    retrofit_installations,
-                    retrofit_basement_floor)),
-                area,
-                gv))
-
-    
-    df3['GEN_MJm2'] = 0
-    df3['CO2_kgm2'] = 0
-    for x in range(df3.Name.count()):
-        if af[x] <= 0:
-            df3.loc[x,'GEN_MJm2'] = result[0][x]/(fp[x]*floors[x]*0.9)
-            df3.loc[x,'CO2_kgm2'] = result[1][x]/(fp[x]*floors[x]*0.9)
+            result[0] = built_df[['GEN_GJ','GEN_MJm2']]
         else:
-            df3.loc[x,'GEN_MJm2'] = result[0][x]/(af[x])
-            df3.loc[x,'CO2_kgm2'] = result[1][x]/(af[x])
-            
-    
-    pd.DataFrame({'Name':df3['Name'],
-                    'GEN_GJ': result[0] / 1000,
-                    'GEN_MJm2': df3['GEN_MJm2'],
-                    'CO2_ton': result[1] / 1000,
-                    'CO2_kgm2': df3['CO2_kgm2']}).to_csv(
-    os.path.join(path_results, 'Total_LCA_embodied.csv'),
-                            index=False, float_format='%.2f')
+            built_df['CO2_ton'] = (HVAC_df['contrib'] + basement_df['contrib'] + partitions_df['contrib'] + built_df['contrib'] +
+                                  roof_df['contrib'] + envelope_df['contrib'] + windows_df['contrib']) / 1000
+            built_df['CO2_kgm2'] = built_df['CO2_ton'] * 1000 / built_df['total_area']
 
-def query_embodied(fp, floors, yearcons, yearretro, PFloor, walls_ext_ag, walls_ext_ag2, yearcalc, windows, win_ext,
-                   win_ext2, excavation, wall_int_avg_factor, wall_int_avg_factor2, walls_ext_bg, floor_int_factor,
-                   floor_int_factor2, services, services2, floor_basement, floor_basement2, roof, roof2, retrofit,
-                   area, gv):
-    flagroof, flag_ext_wall, flag_int_wall, flag_windows, flag_int, flag_services, flagbasement = [bool(int(c)) for c in retrofit]  # noqa
-    # internal walls
-    # it means that part is a parking lot or storage in the building so
-    # internal partitions are considered and services od storage
-    if PFloor < 1:
-        walls_int = wall_int_avg_factor*area*PFloor
-        walls_int2 = wall_int_avg_factor2*area*PFloor
-    else:
-        walls_int = wall_int_avg_factor*floors*fp
-        walls_int2 = wall_int_avg_factor2*floors*fp
-    # internal floors
-    if floors > 1:
-        floor_int = (floors-1)*fp*floor_int_factor
-        floor_int2 = (floors-1)*fp*floor_int_factor2
-    else:
-        floor_int = 0
-        floor_int2 = 0
-    # first it is assumed like the building was built
-    construction = (
-        1 / gv.sl_materials) * (
-        floor_basement + roof + walls_ext_bg + walls_ext_ag + walls_int
-        + win_ext + floor_int + excavation) + services / gv.sl_installations
-    if yearretro > yearcons:
-        period = yearcalc - yearretro
-        if period > gv.sl_materials:
-            result = 0
-        else:
-            retrofit = (
-                (1/gv.sl_materials)*(
-                    floor_basement2*flagbasement +
-                    roof2*flagroof +
-                    walls_ext_ag2*flag_ext_wall +
-                    walls_int2*flag_int_wall +
-                    win_ext2*flag_windows +
-                    floor_int2*flag_int) +
-                services2 * flag_services / gv.sl_installations)
-            if yearcalc-yearcons < gv.sl_materials:
-                result = retrofit + construction
-            else:
-                result = retrofit
-    else:
-        period = yearcalc - yearcons
-        # if building is older thatn gv.sl_materials years it already offset
-        # all its emissions.
-        if period > gv.sl_materials:
-            result = 0
-        else:
-            result = construction
-    return result
+            result[1] = built_df[['CO2_ton', 'CO2_kgm2']]
+        counter += 1
 
+    pd.DataFrame({'Name':result[0].ix,'GEN_GJ': result[0].GEN_GJ, 'GEN_MJm2': result[0].GEN_MJm2,
+                  'CO2_ton': result[1].CO2_ton, 'CO2_kgm2': result[1].CO2_kgm2}).to_csv(os.path.join(path_results, 'Total_LCA_embodied.csv'),
+                   index=False, float_format='%.2f')
+    print 'done'
+
+def calc_if_existing(x, y):
+    if x <= y:
+        return 1
+    else:
+        return 0
 
 def calc_category_construction(a, x):
     if 0 <= x <= 1920:
@@ -344,29 +228,12 @@ def test_lca_embodied():
     path_results = r'C:\CEA_FS2015_EXERCISE02\01_Scenario one\103_final output\emissions'  # noqa
     path_LCA_embodied_energy = os.path.join(os.path.dirname(__file__), 'db', 'Archetypes', 'Archetypes_embodied_energy.csv')  # noqa
     path_LCA_embodied_emissions = os.path.join(os.path.dirname(__file__), 'db', 'Archetypes', 'Archetypes_embodied_emissions.csv')  # noqa
-    path_properties = r'C:\CEA_FS2015_EXERCISE02\01_Scenario one\102_intermediate output\building properties\properties.xls'  # noqa
+    path_age_shp =
+    path_occupancy_shp =
     yearcalc = 2050
-    retrofit_windows = True
-    retrofit_roof = True
-    retrofit_walls = True
-    retrofit_partitions = True
-    retrofit_int_floors = True
-    retrofit_installations = True
-    retrofit_basement_floor = True
-    lca_embodied(
-        path_LCA_embodied_energy,
-        path_LCA_embodied_emissions,
-        path_properties,
-        path_results,
-        yearcalc,
-        retrofit_windows,
-        retrofit_roof,
-        retrofit_walls,
-        retrofit_partitions,
-        retrofit_int_floors,
-        retrofit_installations,
-        retrofit_basement_floor,
-        gv)
+
+    lca_embodied(path_LCA_embodied_energy, path_LCA_embodied_emissions, path_age_shp, path_occupancy_shp,
+                 path_geometry_shp, path_architecture_shp, path_results, yearcalc, gv
     print 'done!'
 
 if __name__ == '__main__':
