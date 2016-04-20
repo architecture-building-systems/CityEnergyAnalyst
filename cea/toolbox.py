07025608e258164496ddb26252797c4d4878b820
daren-thomas <dthomas.ch@gmail.com>
2016-04-20 17:29:46 +0200

    updated tool for emissions script to use `InputLocator`

--- cea/toolbox.py
+++ cea/toolbox.py
@@ -151,38 +151,78 @@ class EmissionsTool(object):
         self.canRunInBackground = False
 
     def getParameterInfo(self):
-        path_total_demand = arcpy.Parameter(
-            displayName="Energy demand (Total_demand.csv)",
-            name="path_total_demand",
-            datatype="DEFile",
-            parameterType="Required",
-            direction="Input")
-        path_total_demand.filter.list = ['csv']
-
-        path_LCA_operation = arcpy.Parameter(
-            displayName="LCA operation data (LCA_operation.xls)",
-            name="path_LCA_operation",
-            datatype="DEFile",
-            parameterType="Required",
-            direction="Input")
-        path_LCA_operation.filter.list = ['xls']
-
-        path_properties = arcpy.Parameter(
-            displayName="Properties File Path (properties.xls)",
-            name="path_properties",
-            datatype="DEFile",
-            parameterType="Required",
-            direction="Input")
-        path_properties.filter.list = ['xls']
-
-        path_results = arcpy.Parameter(
-            displayName="Results folder",
-            name="path_results",
+        scenario_path = arcpy.Parameter(
+            displayName="Path to the scenario",
+            name="scenario_path",
             datatype="DEFolder",
             parameterType="Required",
             direction="Input")
-        return [path_total_demand, path_LCA_operation,
-                path_properties, path_results]
+        Qww_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to hot water consumption.",
+            name="Qww_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Qww_flag.value = True
+        Qhs_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to space heating.",
+            name="Qhs_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Qhs_flag.value = True
+        Qcs_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to space cooling.",
+            name="Qcs_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Qcs_flag.value = True
+        Qcdata_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to servers cooling.",
+            name="Qcdata_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Qcdata_flag.value = True
+        Qcrefri_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to refrigeration.",
+            name="Qcrefri_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Qcrefri_flag.value = True
+        Eal_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to appliances and lighting.",
+            name="Eal_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Eal_flag.value = True
+        Eaux_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to auxiliary electricity.",
+            name="Eaux_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Eaux_flag.value = True
+        Epro_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to electricity in industrial processes.",
+            name="Epro_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Epro_flag.value = True
+        Edata_flag = arcpy.Parameter(
+            displayName="Create a separate file with emissions due to electricity consumption in data centers.",
+            name="Edata_flag",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        Edata_flag.value = True
+
+        return [scenario_path, Qww_flag, Qhs_flag, Qcs_flag, Qcdata_flag, Qcrefri_flag, Eal_flag, Eaux_flag, Epro_flag,
+                Edata_flag]
 
     def isLicensed(self):
         return True
@@ -195,10 +235,21 @@ class EmissionsTool(object):
 
     def execute(self, parameters, messages):
         from cea.emissions import lca_operation
-        lca_operation(path_total_demand=parameters[0].valueAsText,
-                      path_LCA_operation=parameters[1].valueAsText,
-                      path_properties=parameters[2].valueAsText,
-                      path_results=parameters[3].valueAsText)
+        import inputlocator
+        scenario_path = parameters[0].valueAsText
+        locator = inputlocator.InputLocator(scenario_path)
+        Qww_flag = parameters[1].value
+        Qhs_flag = parameters[2].value
+        Qcs_flag = parameters[3].value
+        Qcdata_flag = parameters[4].value
+        Qcrefri_flag = parameters[5].value
+        Eal_flag = parameters[6].value
+        Eaux_flag = parameters[7].value
+        Epro_flag = parameters[8].value
+        Edata_flag = parameters[9].value
+        lca_operation(locator=locator, Qww_flag=Qww_flag, Qhs_flag=Qhs_flag, Qcs_flag=Qcs_flag, Qcdata_flag=Qcdata_flag,
+                      Qcrefri_flag=Qcrefri_flag, Eal_flag=Eal_flag, Eaux_flag=Eaux_flag, Epro_flag=Epro_flag,
+                      Edata_flag=Edata_flag)
 
 
 class GraphsDemandTool(object):
