558f760aa734c4b7ee0ecaf8ad0e12b77ebf0ccf
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-04-26 15:35:32 +0800

    input files created for control system
    All inputs in both, the reference case and the core have been changed to
    accomodate for different types of control systems

--- cea/demand.py
+++ cea/demand.py
@@ -59,45 +59,45 @@ def demand_calculation(locator, gv):
     prop_geometry['footprint'] = prop_geometry.area
     prop_geometry['perimeter'] = prop_geometry.length
     prop_geometry = prop_geometry.drop('geometry', axis=1).set_index('Name')
-    prop_HVAC = gpdf.from_file(locator.get_building_hvac()).drop('geometry', axis=1).set_index('Name')
+    prop_HVAC = gpdf.from_file(locator.get_building_hvac()).drop('geometry', axis=1)
     prop_thermal = gpdf.from_file(locator.get_building_thermal()).drop('geometry', axis=1).set_index('Name')
     prop_occupancy = gpdf.from_file(locator.get_building_occupancy()).drop('geometry', axis=1).set_index('Name')
     prop_architecture = gpdf.from_file(locator.get_building_architecture()).drop('geometry', axis=1).set_index('Name')
     prop_age = gpdf.from_file(locator.get_building_age()).drop('geometry', axis=1).set_index('Name')
 
+    # get temperatures of operation
+    prop_HVAC_result= get_temperatures(locator, prop_HVAC).set_index('Name')
     # weather conditions
     T_ext = np.array(weather_data.te)
     RH_ext = np.array(weather_data.RH)
     T_ext_max = T_ext.max()
     T_ext_min = T_ext.min()
 
-    # obtain schedules per building
-    rows = len(list_uses)
-    profiles = list(range(rows))
-    for row in range(rows):
-        profiles[row] = pd.read_csv(locator.get_schedule(list_uses[row]), nrows=8760)
+    # get schedules
+    schedules = get_schedules(locator, list_uses)
 
-    # calculate file with thermal properties for RC model
-    prop_RC_model = f.get_prop_RC_model(prop_occupancy, prop_architecture, prop_thermal, prop_geometry, prop_HVAC,
+    # get thermal properties for RC model
+    prop_RC_model = f.get_prop_RC_model(prop_occupancy, prop_architecture, prop_thermal, prop_geometry, prop_HVAC_result,
                                         radiation_file, gv)
 
-    # calculate clean file of radiation - @ daren: this is a A BOTTLE NECK
+    # get solar insolation @ daren: this is a A BOTTLE NECK
     Solar = f.CalcIncidentRadiation(radiation_file)
 
-    # compute demand
+    # get timeseries of demand
     num_buildings = len(prop_RC_model.index)
     counter = 0
     for building in prop_RC_model.index:
         f.CalcThermalLoads(building, prop_occupancy.ix[building], prop_architecture.ix[building],
                            prop_thermal.ix[building],
-                           prop_geometry.ix[building], prop_HVAC.ix[building], prop_RC_model.ix[building],
-                           prop_age.ix[building], Solar.ix[building], locator.get_demand_results_folder(), profiles,
+                           prop_geometry.ix[building], prop_HVAC_result.ix[building], prop_RC_model.ix[building],
+                           prop_age.ix[building], Solar.ix[building], locator.get_demand_results_folder(), schedules,
                            list_uses, T_ext, T_ext_max, RH_ext, T_ext_min, locator.get_temporary_folder(), gv, 0, 0)
-        message = 'Building No. ' + str(counter + 1) + ' completed out of ' + str(num_buildings)
-        arcpy.AddMessage(message)
+        print 'Building No. ' + str(counter + 1) + ' completed out of ' + str(num_buildings)
         counter += 1
-    # put together all rows of the total file
-    arcpy.AddMessage('len(prop_RC_model.index): %i' % len(prop_RC_model.index))
+
+    # get total file
+
+
     counter = 0
     for name in prop_RC_model.index:
         temporary_file = locator.get_temporary_file('%(name)sT.csv' % locals())
@@ -112,6 +112,29 @@ def demand_calculation(locator, gv):
 
     print 'finished'
 
+
+def get_schedules(locator, list_uses):
+    rows = len(list_uses)
+    result = list(range(rows))
+    for row in range(rows):
+        result[row] = pd.read_csv(locator.get_schedule(list_uses[row]), nrows=8760)
+    return result
+
+def get_temperatures(locator, prop_HVAC):
+    prop_emission_heating = pd.read_excel(locator.get_technical_emission_systems(), 'heating')
+    prop_emission_cooling = pd.read_excel(locator.get_technical_emission_systems(), 'cooling')
+    prop_emission_dhw = pd.read_excel(locator.get_technical_emission_systems(), 'dhw')
+
+    df = prop_HVAC.merge(prop_emission_heating, left_on='type_hs', right_on='code')
+    df2 = prop_HVAC.merge(prop_emission_cooling, left_on='type_cs', right_on='code')
+    df3 = prop_HVAC.merge(prop_emission_dhw, left_on='type_dhw', right_on='code')
+
+    fields = ['Name', 'type_hs', 'type_cs', 'type_dhw', 'Tshs0_C', 'dThs0_C', 'Qhsmax_Wm2']
+    fields2 = ['Name', 'Tscs0_C', 'dTcs0_C', 'Qcsmax_Wm2']
+    fields3 = ['Name', 'Tsww0_C', 'dTww0_C', 'Qwwmax_Wm2']
+
+    result = df[fields].merge(df2[fields2], on='Name').merge(df3[fields3], on='Name')
+    return result
 
 def test_demand():
     locator = inputlocator.InputLocator(scenario_path=r'C:\reference-case\baseline')
