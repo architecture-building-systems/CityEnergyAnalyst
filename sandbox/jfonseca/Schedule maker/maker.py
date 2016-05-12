0c4a1c9a1a0e028bb344d628967001c99cb8ee1a
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-05-12 10:19:22 +0800

    first attempt

--- sandbox/jfonseca/Schedule maker/maker.py
+++ sandbox/jfonseca/Schedule maker/maker.py
@@ -1,42 +1,63 @@
 import pandas as pd
-import numpy as np
-
-    x = pd.read_excel(r'C:\Users\Jimeno\Desktop\BOOK_1.xlsx', 'ADMIN').T
 
 
 
+def get_schedules(x):
+    schedules = []
+    for use in list_uses():
+        # read lists of every daily profile
+        occ_schedules, el_schedules, dhw_schedules, pro_schedules, month = read_schedules(use, x)
 
-def get_schedules(x)
-    weekday_occ = x['Weekday_1'].values
-    saturday_occ = x['Saturday_1'].values
-    sunday_occ = x['Sunday_1'].values
-    weekday_el = x['Weekday_2'].values
-    saturday_el = x['Saturday_2'].values
-    sunday_el = x['Sunday_2'].values
-    weekday_dhw = x['Weekday_3'].values
-    saturday_dhw = x['Saturday_3'].values
-    sunday_dhw = x['Sunday_3'].values
-    month_value = x['Month'].values
-    date = pd.date_range('1/1/2016', periods=8760, freq='H')
-    dates = len(date)
+        # get yearly vectors
+        schedule = get_yearly_vectors(date, occ_schedules, el_schedules, dhw_schedules, pro_schedules, month)
+
+        schedules.append(schedule)
+
+    return schedules
+
+def read_schedules(use, x):
+
+    occ = [x['Weekday_1'].values,x['Saturday_1'].values,x['Sunday_1'].values]
+    el = [x['Weekday_2'].values, x['Saturday_2'].values, x['Sunday_2'].values]
+    dhw = [x['Weekday_3'].values, x['Saturday_3'].values, x['Sunday_3'].values]
+    month = x['month'].values
+
+    if use is "INDUSTRIAL":
+        pro = [x['Weekday_4'].values, x['Saturday_4'].values, x['Sunday_4'].values]
+    else:
+        pro = [[]*24, []*24,[]*24]
+
+    return occ, el, dhw, pro, month
+
+def get_yearly_vectors(date, occ_schedules, el_schedules, dhw_schedules, pro_schedules, month)
     occ = []
     el = []
     dhw = []
-    for hour in range(dates):
+    pro = []
+    for hour in range(8760):
         month_year = date[hour].month-1
         hour_day = date[hour].hour
         if date[hour].dayofweek is 0 or 1 or 2 or 3 or 4: #weekday
-            occ.append(weekday_occ[hour_day]*month_value[month_year])
-            el.append(weekday_el[hour_day]*month_value[month_year])
-            dhw.append(weekday_dhw[hour_day]*month_value[month_year])
+            occ.append(occ_schedules[0][hour_day]*month[month_year])
+            el.append(el_schedules[0][hour_day]*month[month_year])
+            dhw.append(dhw_schedules[0][hour_day]*month[month_year])
+            pro.append(pro_schedules[0][hour_day] * month[month_year])
         elif date[hour].dayofweek is 5: #saturday
-            occ.append(saturday_occ[hour_day] * month_value[month_year])
-            el.append(saturday_el[hour_day] * month_value[month_year])
-            dhw.append(saturday_dhw[hour_day] * month_value[month_year])
+            occ.append(occ_schedules[1][hour_day] * month[month_year])
+            el.append(el_schedules[1][hour_day] * month[month_year])
+            dhw.append(dhw_schedules[1][hour_day] * month[month_year])
+            pro.append(pro_schedules[1][hour_day] * month[month_year])
         else: # sunday
-            occ.append(sunday_occ[hour_day] * month_value[month_year])
-            el.append(sunday_el[hour_day] * month_value[month_year])
-            dhw.append(sunday_dhw[hour_day] * month_value[month_year])
+            occ.append(occ_schedules[2][hour_day] * month[month_year])
+            el.append(el_schedules[2]l[hour_day] * month[month_year])
+            dhw.append(dhw_schedules[2][hour_day] * month[month_year])
+            pro.append(pro_schedules[2][hour_day] * month[month_year])
 
-    return occ, el, dwh
+    schedule = pd.DataFrame('DATE': date)
 
+    return schedule
+
+x = pd.read_excel(r'C:\Users\JF\Desktop\book_1.xlsx', 'OFFICE').T
+x.to_csv(r'C:\Users\JF\Desktop\book_1.csv')
+schedules = get_schedules(x)
+print occ
