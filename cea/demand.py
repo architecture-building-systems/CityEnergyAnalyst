24add487931a8ce5fbd9ee47b0036306534b2bcb
Daren Thomas <thomas@arch.ethz.ch>
2016-05-29 17:37:12 +0200

    refactoring + credits g. happle

--- cea/demand.py
+++ cea/demand.py
@@ -7,6 +7,7 @@ J. Fonseca  script development          24.08.15
 D. Thomas   formatting, refactoring, debugging and cleaning
 D. Thomas   integration in toolbox
 J. Fonseca  refactoring to new properties file   22.03.16
+G. Happle   BuildingPropsThermalLoads   27.05.2016
 """
 from __future__ import division
 import pandas as pd
@@ -17,7 +18,6 @@ import epwreader
 import globalvar
 from geopandas import GeoDataFrame as gpdf
 import inputlocator
-import functioninputs as fi
 
 reload(f)
 reload(globalvar)
@@ -66,19 +66,20 @@ def demand_calculation(locator, weather_path, gv):
     prop_HVAC = gpdf.from_file(locator.get_building_hvac()).drop('geometry', axis=1)
     prop_thermal = gpdf.from_file(locator.get_building_thermal()).drop('geometry', axis=1).set_index('Name')
     prop_occupancy_df = gpdf.from_file(locator.get_building_occupancy()).drop('geometry', axis=1).set_index('Name')
-    prop_occupancy = prop_occupancy_df.loc[:, (prop_occupancy_df != 0).any(axis=0)] # trick to erase occupancies that are not being used (it speeds up the code)
+    prop_occupancy = prop_occupancy_df.loc[:, (prop_occupancy_df != 0).any(
+        axis=0)]  # trick to erase occupancies that are not being used (it speeds up the code)
     prop_architecture = gpdf.from_file(locator.get_building_architecture()).drop('geometry', axis=1).set_index('Name')
     prop_age = gpdf.from_file(locator.get_building_age()).drop('geometry', axis=1).set_index('Name')
     prop_comfort = gpdf.from_file(locator.get_building_comfort()).drop('geometry', axis=1).set_index('Name')
     prop_internal_loads = gpdf.from_file(locator.get_building_internal()).drop('geometry', axis=1).set_index('Name')
     # get temperatures of operation
-    prop_HVAC_result= get_temperatures(locator, prop_HVAC).set_index('Name')
+    prop_HVAC_result = get_temperatures(locator, prop_HVAC).set_index('Name')
     # weather conditions
     # T_ext = np.array(weather_data.drybulb_C)
     # RH_ext = np.array(weather_data.relhum_percent)
-    #get list of uses
+    # get list of uses
     list_uses = list(prop_occupancy.drop('PFloor', axis=1).columns)
-    #get date
+    # get date
     date = pd.date_range(gv.date_start, periods=8760, freq='H')
     gv.log('done')
 
@@ -95,16 +96,12 @@ def demand_calculation(locator, weather_path, gv):
     gv.log("done")
 
     # construct function input object
-    building_props_thermal_loads = fi.BuildingPropsThermalLoads()
-    building_props_thermal_loads.prop_geometry = prop_geometry
-    building_props_thermal_loads.prop_occupancy = prop_occupancy
-    building_props_thermal_loads.prop_architecture = prop_architecture
-    building_props_thermal_loads.prop_age = prop_age
-    building_props_thermal_loads.prop_comfort = prop_comfort
-    building_props_thermal_loads.prop_internal_loads = prop_internal_loads
-    building_props_thermal_loads.prop_HVAC_result = prop_HVAC_result
-    building_props_thermal_loads.prop_RC_model = prop_RC_model
-    building_props_thermal_loads.solar = solar
+    building_properties = BuildingProperties(prop_geometry=prop_geometry, prop_occupancy=prop_occupancy,
+                                                      prop_architecture=prop_architecture, prop_age=prop_age,
+                                                      prop_comfort=prop_comfort,
+                                                      prop_internal_loads=prop_internal_loads,
+                                                      prop_HVAC_result=prop_HVAC_result,
+                                                      prop_RC_model=prop_RC_model, solar=solar)
 
     # construct weather dict for input to function
     weather = {'temp_ext': np.array(weather_data.drybulb_C),
@@ -119,7 +116,7 @@ def demand_calculation(locator, weather_path, gv):
     num_buildings = len(prop_RC_model.index)
     counter = 0
     for building in prop_RC_model.index:
-        gv.models['calc-thermal-loads'](building, building_props_thermal_loads, weather, usage_schedules, date, gv,
+        gv.models['calc-thermal-loads'](building, building_properties, weather, usage_schedules, date, gv,
                                         locator.get_demand_results_folder(), locator.get_temporary_folder())
         gv.log('Building No. %(bno)i completed out of %(btot)i', bno=counter + 1, btot=num_buildings)
         counter += 1
@@ -140,6 +137,7 @@ def demand_calculation(locator, weather_path, gv):
 
     gv.log('finished')
 
+
 def get_temperatures(locator, prop_HVAC):
     prop_emission_heating = pd.read_excel(locator.get_technical_emission_systems(), 'heating')
     prop_emission_cooling = pd.read_excel(locator.get_technical_emission_systems(), 'cooling')
@@ -149,12 +147,75 @@ def get_temperatures(locator, prop_HVAC):
     df2 = prop_HVAC.merge(prop_emission_cooling, left_on='type_cs', right_on='code')
     df3 = prop_HVAC.merge(prop_emission_dhw, left_on='type_dhw', right_on='code')
 
-    fields = ['Name', 'type_hs', 'type_cs', 'type_dhw',  'type_ctrl', 'Tshs0_C', 'dThs0_C', 'Qhsmax_Wm2']
+    fields = ['Name', 'type_hs', 'type_cs', 'type_dhw', 'type_ctrl', 'Tshs0_C', 'dThs0_C', 'Qhsmax_Wm2']
     fields2 = ['Name', 'Tscs0_C', 'dTcs0_C', 'Qcsmax_Wm2']
     fields3 = ['Name', 'Tsww0_C', 'dTww0_C', 'Qwwmax_Wm2']
 
     result = df[fields].merge(df2[fields2], on='Name').merge(df3[fields3], on='Name')
     return result
+
+
+class BuildingProperties(object):
+    """
+    Groups building properties used for the calc-thermal-loads functions. Stores the full DataFrame for each of the
+    building properties and provides methods for indexing them by name.
+
+    G. Happle   BuildingPropsThermalLoads   27.05.2016
+    """
+    def __init__(self, prop_geometry=None, prop_architecture=None, prop_occupancy=None, prop_HVAC_result=None,
+                 prop_RC_model=None, prop_comfort=None, prop_internal_loads=None, prop_age=None, solar=None,
+                 prop_windows=None):
+        self._prop_geometry = prop_geometry
+        self._prop_architecture = prop_architecture
+        self._prop_occupancy = prop_occupancy
+        self._prop_HVAC_result = prop_HVAC_result
+        self._prop_RC_model = prop_RC_model
+        self._prop_comfort = prop_comfort
+        self._prop_internal_loads = prop_internal_loads
+        self._prop_age = prop_age
+        self._solar = solar
+        self._prop_windows = prop_windows
+
+    def get_prop_geometry(self, name_building):
+        """get geometry of a building by name"""
+        return self._prop_geometry.ix[name_building]
+
+    def get_prop_architecture(self, name_building):
+        """get the architecture properties of a building by name"""
+        return self._prop_architecture.ix[name_building]
+
+    def get_prop_occupancy(self, name_building):
+        """get the occupancy properties of a building by name"""
+        return self._prop_occupancy.ix[name_building]
+
+    def get_prop_hvac(self, name_building):
+        """get HVAC properties of a building by name"""
+        return self._prop_HVAC_result.ix[name_building]
+
+    def get_prop_rc_model(self, name_building):
+        """get RC-model properties of a building by name"""
+        return self._prop_RC_model.ix[name_building]
+
+    def get_prop_comfort(self, name_building):
+        """get comfort properties of a building by name"""
+        return self._prop_comfort.ix[name_building]
+
+    def get_prop_internal_loads(self, name_building):
+        """get internal loads properties of a building by name"""
+        return self._prop_internal_loads.ix[name_building]
+
+    def get_prop_age(self, name_building):
+        """get age properties of a building by name"""
+        return self._prop_age.ix[name_building]
+
+    def get_solar(self, name_building):
+        """get solar properties of a building by name"""
+        return self._solar.ix[name_building]
+
+    def get_prop_windows(self, name_building):
+        """get windows and their properties of a building by name"""
+        return self._prop_windows.loc[self._prop_windows['name_building'] == name_building].to_dict('list')
+
 
 def test_demand():
     locator = inputlocator.InputLocator(scenario_path=r'C:\reference-case\baseline')
