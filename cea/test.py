8211d328990e9f87af341fe4e2d5d6b8f0c9f07b
daren-thomas <dthomas.ch@gmail.com>
2016-06-08 09:01:41 +0200

    small fix to the radiation test script

--- cea/test.py
+++ cea/test.py
@@ -16,6 +16,7 @@ __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
 __credits__ = ["Daren Thomas"]
 __license__ = "MIT"
 __version__ = "0.1"
+
 __maintainer__ = "Daren Thomas"
 __email__ = "thomas@arch.ethz.ch"
 __status__ = "Production"
