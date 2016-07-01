a05c732af557ba76bd9408282fa1298a8352f38d
Daren Thomas <thomas@arch.ethz.ch>
2016-07-01 16:17:14 +0200

    add test results to test case

--- tests/test_calc_thermal_loads_new_ventilation.py
+++ tests/test_calc_thermal_loads_new_ventilation.py
@@ -15,12 +15,12 @@ class TestCalcThermalLoadsNewVentilation(TestCase):
 
         locator = InputLocator(r'C:\reference-case\baseline')
         gv = GlobalVariables()
-        building_properties = BuildingProperties(locator, gv)
 
         weather_path = locator.get_default_weather()
         weather_data = epwreader.epw_reader(weather_path)[['drybulb_C', 'relhum_percent', 'windspd_ms']]
 
-        list_uses = list(building_properties._prop_occupancy.drop('PFloor', axis=1).columns)
+        self.building_properties = BuildingProperties(locator, gv)
+        list_uses = list(self.building_properties._prop_occupancy.drop('PFloor', axis=1).columns)
 
         # FIXME: the usage_schedules bit needs to be fixed!!
         date = pd.date_range(gv.date_start, periods=8760, freq='H')
@@ -28,8 +28,8 @@ class TestCalcThermalLoadsNewVentilation(TestCase):
         usage_schedules = {'list_uses': list_uses,
                            'schedules': schedules}
 
-        result = calc_thermal_loads_new_ventilation('B140577', building_properties, weather_data, usage_schedules, date,
-                                                    gv, locator.get_temporary_folder(),
+        result = calc_thermal_loads_new_ventilation('B140577', self.building_properties, weather_data, usage_schedules,
+                                                    date, gv, locator.get_temporary_folder(),
                                                     locator.get_temporary_folder())
         self.assertIsNone(result)
         self.assertTrue(os.path.exists(locator.get_temporary_file('B140577.csv')), 'Building csv not produced')
@@ -47,4 +47,14 @@ class TestCalcThermalLoadsNewVentilation(TestCase):
         self.assertEqual(set(expected_columns), set(df.columns), 'Column list of building csv does not match')
         self.assertEqual(df.shape[0], 8760, 'Expected one row per hour in the year')
 
-
+        value_columns = [u'Ealf_kWh', u'Eauxf_kWh', u'Edataf_kWh', u'Ef_kWh', u'Epro_kWh', u'QCf_kWh', u'QHf_kWh',
+                        u'Qcdataf_kWh', u'Qcref_kWh', u'Qcs_kWh', u'Qcsf_kWh', u'Qhs_kWh', u'Qhsf_kWh', u'Qww_kWh',
+                        u'Qww_tankloss_kWh', u'Qwwf_kWh', u'Trcs_C', u'Trhs_C', u'Trww_C', u'Tscs_C', u'Tshs_C',
+                        u'Tsww_C', u'Tww_tank_C', u'Vw_m3', u'mcpcs_kWC', u'mcphs_kWC', u'mcpww_kWC', u'occ_pax']
+        values = [2335522.029999916, 51283.209999999264, 0.0, 2386803.5299999011, 0.0, 1600579.3699999989,
+                  10583959.84999999, 0.0, 0.0, 969342.64000000141, 1600579.3699999989, 917820.5499999997,
+                  1598211.1199999996, 8964857.8500000071, 9976.2799999999643, 8985748.6999999825, 16116, 52842, 99500.0,
+                  11376, 63853, 525600, 525426.29999996931, 476000.19000000018, 319634, 148190, 184502.95000000024,
+                  6827181.0]
+        for i, column in enumerate(value_columns):
+            self.assertAlmostEqual(values[i], df[column].sum(), 'Sum of column %s differs' % column)
