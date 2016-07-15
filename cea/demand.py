d3e9b347c7baabeb0ebd55ccab9adae819d4a3a0
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-07-15 14:52:53 +0800

    Move totals to new multiprocessing algorithm

--- cea/demand.py
+++ cea/demand.py
@@ -2,24 +2,16 @@
 ===========================
 Analytical energy demand model algorithm
 ===========================
-File history and credits:
-J. Fonseca  script development          24.08.15
-D. Thomas   formatting, refactoring, debugging and cleaning
-D. Thomas   integration in toolbox
-J. Fonseca  refactoring to new properties file   22.03.16
-G. Happle   BuildingPropsThermalLoads   27.05.2016
+
 """
 from __future__ import division
-
 import multiprocessing as mp
-
 import pandas as pd
-
-import epwreader
 import functions as f
 import globalvar
 import inputlocator
 import maker as m
+from cea.utils import epwreader
 from cea import thermal_loads
 from cea.thermal_loads import BuildingProperties
 
@@ -79,50 +71,25 @@ def demand_calculation(locator, weather_path, gv):
     B153767T.csv: csv file for every building with hourly demand data
     Total_demand.csv: csv file of yearly demand data per buidling.
     """
-    # local variables
 
-
-    # initialize function inputs
-    weather_data = epwreader.epw_reader(weather_path)[['drybulb_C', 'relhum_percent', 'windspd_ms']]
-    building_properties = BuildingProperties(locator, gv)
-    # get list of uses
-    list_uses = list(building_properties._prop_occupancy.drop('PFloor', axis=1).columns)
-    # get date
+    # starting date
     date = pd.date_range(gv.date_start, periods=8760, freq='H')
 
-    gv.log("reading occupancy schedules")
-    # get schedules
+    # weather model
+    weather_data = epwreader.epw_reader(weather_path)[['drybulb_C', 'relhum_percent', 'windspd_ms']]
+
+    # building properties model
+    building_properties = BuildingProperties(locator, gv)
+
+    # schedules model
+    list_uses = list(building_properties._prop_occupancy.drop('PFloor', axis=1).columns)
     schedules = m.schedule_maker(date, locator, list_uses)
-    gv.log("done")
+    schedules_dict = {'list_uses': list_uses, 'schedules': schedules}
 
-    # construct schedules dict for input to function
-    usage_schedules = {'list_uses': list_uses,
-                       'schedules': schedules}
-
-
-
-    # demand calculation for each building
+    # demand model
     num_buildings = len(building_properties)
-
-    thermal_loads_all_buildings_multiprocessing(building_properties, date, gv, locator, num_buildings, usage_schedules,
+    thermal_loads_all_buildings_multiprocessing(building_properties, date, gv, locator, num_buildings, schedules_dict,
                                                 weather_data)
-
-    # get total file
-
-    counter = 0
-    for name in building_properties.list_building_names():
-        temporary_file = locator.get_temporary_file('%(name)sT.csv' % locals())
-        # TODO: check this logic
-        if counter == 0:
-            df = pd.read_csv(temporary_file)
-            counter += 1
-        else:
-            df2 = pd.read_csv(temporary_file)
-            df = df.append(df2, ignore_index=True)
-    df.to_csv(locator.get_total_demand(), index=False, float_format='%.2f')
-
-    gv.log('finished')
-
 
 def thermal_loads_all_buildings(building_properties, date, gv, locator, num_buildings, usage_schedules,
                                 weather_data):
@@ -149,6 +116,18 @@ def thermal_loads_all_buildings_multiprocessing(building_properties, date, gv, l
     for i, job in enumerate(joblist):
         job.get(60)
         gv.log('Building No. %(bno)i completed out of %(num_buildings)i', bno=i + 1, num_buildings=num_buildings)
+
+    counter = 0
+    for name in building_properties.list_building_names():
+        temporary_file = locator.get_temporary_file('%(name)sT.csv' % locals())
+        if counter == 0:
+            df = pd.read_csv(temporary_file)
+            counter += 1
+        else:
+            df2 = pd.read_csv(temporary_file)
+            df = df.append(df2, ignore_index=True)
+    df.to_csv(locator.get_total_demand(), index=False, float_format='%.2f')
+    gv.log('done')
 
 
 def test_demand():
