5e77e92a6ab0e501368925af52eb73969adc201d
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-07-18 11:31:03 +0800

    Physics functions created

--- cea/hvac_kaempf.py
+++ cea/hvac_kaempf.py
@@ -12,8 +12,8 @@
 """
 
 from __future__ import division
-import math
 import numpy as np
+from utils.physics import calc_h, calc_w
 
 def calc_hvac(rhum_1, temp_1, temp_zone_set, qv_req, qe_sen, temp_5_prev, wint, gv, timestep):
     """
@@ -325,55 +325,3 @@ def calc_w3_cooling_case(t5, w2, t3, w5):
         w3 = min(w2, calc_w(t3, 100))
 
     return w3
-
-
-def calc_w(t, RH):
-    """
-    Moisture content in kg/kg of dry air
-
-    Parameters
-    ----------
-    t : temperature of air in (°C)
-    RH : relative humidity of air in (%)
-
-    Returns
-    -------
-    w : moisture content of air in (kg/kg dry air)
-    """
-
-    # TODO: add documentation and source of formula
-
-    Pa = 100000  # Pa
-    Ps = 610.78 * math.exp(t / (t + 238.3) * 17.2694)
-    Pv = RH / 100 * Ps
-    w = 0.62 * Pv / (Pa - Pv)
-
-    return w
-
-
-def calc_h(t, w):
-    """
-    calculates enthalpy of moist air in kJ/kg
-
-    source: thesis Kaempf Eq.(4.30) extended to temperatures below -10°
-
-    Parameters
-    ----------
-    t : air temperature in (°C)
-    w : moisture content of air in (kg/kg dry air)
-
-    Returns
-    -------
-    h : enthalpy of moist air in (kJ/kg)
-    """
-
-    if 0 < t < 60:  # temperature above zero
-        h = (1.007 * t - 0.026) + w * (2501 + 1.84 * t)
-    elif -100 < t <= 0: # temperature below zero
-        h = (1.005 * t) + w * (2501 + 1.84 * t)
-    else:
-        h = np.nan
-        print('Warning: Temperature out of bounds (>60°C or <-100°C)')
-        print(t)
-
-    return h
