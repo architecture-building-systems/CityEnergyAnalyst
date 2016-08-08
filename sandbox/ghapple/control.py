5a58896f72ff57b08fdd51f4e185080f9f9c69bc
Gabriel Happle <happle@arch.ethz.ch>
2016-08-08 15:05:26 +0800

    update sandbox happle

--- sandbox/ghapple/control.py
+++ sandbox/ghapple/control.py
@@ -15,14 +15,254 @@ __email__ = "thomas@arch.ethz.ch"
 __status__ = "Production"
 
 
-def night_flushing_status(hoy, tsd, bpr, weather_data):
+
+def has_heating_system(bpr):
+    """
+    determines whether a building has a heating system installed or not
+
+    Parameters
+    ----------
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if bpr.hvac['type_hs'] in {'T1', 'T2', 'T3', 'T4'}:
+        return True
+    elif bpr.hvac['type_hs'] in {'T0'}:
+        return False
+    else:
+        print('Error: Unknown heating system')
+        return False
+
+
+def heating_system_is_ac(bpr):
+    """
+    determines whether a building's heating system is ac or not
+
+    Parameters
+    ----------
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if bpr.hvac['type_hs'] in {'T3'}:  # central ac
+        return True
+    elif bpr.hvac['type_hs'] in {'T0', 'T1', 'T2', 'T4'}:
+        return False
+    else:
+        print('Error: Unknown heating system')
+        return False
+
+
+def heating_system_is_radiative(bpr):
+    """
+    determines whether a building's heating system is radiative (radiators/floor heating) or not
+
+    Parameters
+    ----------
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if bpr.hvac['type_hs'] in {'T1', 'T2', 'T4'}:
+        return True
+    elif bpr.hvac['type_hs'] in {'T0', 'T3'}:
+        return False
+    else:
+        print('Error: Unknown heating system')
+        return False
+
+
+def has_cooling_system(bpr):
+    """
+    determines whether a building has a cooling system installed or not
+
+    Parameters
+    ----------
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if bpr.havc['type_cs'] in {'T1', 'T2', 'T3'}:
+        return True
+    elif bpr.hvac['type_cs'] in {'T0'}:
+        return False
+    else:
+        print('Error: Unknown cooling system')
+        return False
+
+
+def cooling_system_is_ac(bpr):
+    """
+    determines whether a building's heating system is ac or not
+
+    Parameters
+    ----------
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if bpr.hvac['type_cs'] in {'T2', 'T3'}:  # mini-split ac and central ac
+        return True
+    elif bpr.hvac['type_cs'] in {'T0', 'T1'}:
+        return False
+    else:
+        print('Error: Unknown cooling system')
+        return False
+
+
+def cooling_system_is_radiative(bpr):
+    """
+    determines whether a building's heating system is radiative or not
+
+    Parameters
+    ----------
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if bpr.hvac['type_cs'] in {'T1'}:
+        return True
+    elif bpr.hvac['type_cs'] in {'T0', 'T2', 'T3'}:
+        return False
+    else:
+        print('Error: Unknown cooling system')
+        return False
+
+
+def is_heating_active(hoy, bpr):
+    """
+    determines whether the heating system of a building is active or not at a certain hour of the year
+
+    Parameters
+    ----------
+    hoy : hour of the year
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if has_heating_system(bpr):
+
+        if h.is_heatingseason_hoy(hoy):
+            return True
+        else:
+            return False
+    else:
+        False
+
+
+def is_cooling_active(hoy, bpr):
+    """
+    determines whether the cooling system of a building is active or not at a certain hour of the year
+
+    Parameters
+    ----------
+    hoy : hour of the year
+    bpr : building properties row object
+
+    Returns
+    -------
+    bool
+
+    """
+
+    if has_cooling_system(bpr):
+        if h.is_coolingseason_hoy(hoy):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def has_mech_ventilation():
+
+    # check building properties
+    # TODO: code
+    return False
+
+def has_night_flushing():
+
+    # check building properties
+    # TODO: code
+    return False
+
+def has_window_ventilation():
+
+    # check building properties
+    # TODO: code
+    return False
+
+def has_hex():
+
+    # check building properties
+    # TODO: code
+    return False
+
+
+def is_mech_ventilation_active():
+
+    if has_mech_ventilation():
+        # check for clock time, season, temperature and occupancy constraints
+        # TODO: code
+        return False
+
+    elif not has_mech_ventilation():
+        return False
+
+
+def is_night_flushing_active(hoy, tsd, bpr, weather_data):
+    """
+    determines system status (active/inactive) of night flushing system
+
+    Parameters
+    ----------
+    hoy : hour of year
+    tsd : data frame with thermal loads simulation results
+    bpr : building properties row object
+    weather_data : weather data
+
+    Returns
+    -------
+    bool
+
+    """
 
     # TODO: get night flushing building property (bpr)
-    night_flushing = True
+    # night_flushing = True
 
     # TODO: get control temperatures from ? (gv?)
-    if night_flushing:
+    if has_night_flushing():
 
+        # check for clock time, season, temperature and occupancy constraints
         if h.is_nighttime_hoy(hoy) and h.is_coolingseason_hoy(hoy):  # clocktime condition
 
             temp_zone_control = 28  # TODO: make dynamic
@@ -35,7 +275,32 @@ def night_flushing_status(hoy, tsd, bpr, weather_data):
                 return False
         else:
             return False
-    else:
+
+    elif not has_night_flushing():
         return False
 
 
+
+
+def is_window_ventilation_active():
+
+    if has_window_ventilation():
+
+        # TODO: code
+        # check for clock time, season, temperature and occupancy constraints
+        return False
+
+    elif not has_window_ventilation():
+        return False
+
+
+def is_hex_active():
+
+    if has_hex():
+
+        # check for clock time, season, temperature and occupancy constraints
+        # TODO: code
+        return False
+
+    elif not has_hex():
+        return False
