9cbe143b8c34ecc5fd3b121e649db39ee4e42e4e
Daren Thomas <thomas@arch.ethz.ch>
2016-08-16 15:26:01 +0200

    update unittest results to reflect numba changes

--- cea/technologies/radiators.py
+++ cea/technologies/radiators.py
@@ -7,7 +7,7 @@ heating radiators
 """
 from __future__ import division
 from scipy.optimize import newton as opt_newton
-import scipy
+import math
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
@@ -28,11 +28,11 @@ def calc_radiator(Qh, tair, Qh0, tair0, tsh0, trh0):
         trh0 = trh0 + 273
         mCw0 = Qh0 / (tsh0 - trh0)
         # minimum
-        LMRT = (tsh0 - trh0) / scipy.log((tsh0 - tair0) / (trh0 - tair0))
+        LMRT = (tsh0 - trh0) / math.log((tsh0 - tair0) / (trh0 - tair0))
         k1 = 1 / mCw0
 
         def fh(x):
-            Eq = mCw0 * k2 - Qh0 * (k2 / (scipy.log((x + k2 - tair) / (x - tair)) * LMRT)) ** (nh + 1)
+            Eq = mCw0 * k2 - Qh0 * (k2 / (math.log((x + k2 - tair) / (x - tair)) * LMRT)) ** (nh + 1)
             return Eq
 
         k2 = Qh * k1
