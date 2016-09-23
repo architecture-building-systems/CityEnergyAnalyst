370fac121ec83ba925308e79e2345fb987f51eb2
Daren Thomas <thomas@arch.ethz.ch>
2016-09-23 14:25:06 +0200

    logging in, downloading, unziping

--- tests/dodo.py
+++ tests/dodo.py
@@ -5,17 +5,44 @@ The reference cases can be found here: https://github.com/architecture-building-
 """
 import requests
 import os
-
+import shutil
+import zipfile
 
 
 def task_download_reference_case_zug():
     """Download the (current) state of the reference-case-zug"""
+
     def download_reference_case_zug():
-        r = requests.get(requests.get("https://github.com/architecture-building-systems/cea-reference-case/archive/master.zip", auth=('daren-thomas', 'blahblahblah')))
-        with open(os.path.expandvars(r'%TEMP%\cea-reference-case.zip')) as f:
-            f.write(r.content, 'wb')
+        dest_folder = os.path.expandvars(r'%TEMP%\cea-reference-case')
+        dest_file = os.path.expandvars(r'%TEMP%\cea-reference-case.zip')
+
+        # get the username / password for github
+        with open(os.path.expanduser('~/github.auth')) as f:
+            user, password = map(str.strip, f.readlines())
+
+        # download the zip file
+        if os.path.exists(dest_file):
+            os.remove(dest_file)
+        r = requests.get("https://github.com/architecture-building-systems/cea-reference-case/archive/master.zip",
+                         auth=(user, password))
+        assert r.ok, 'could not download the reference case'
+        with open(dest_file, 'wb') as f:
+            f.write(r.content)
+
+        # extract the reference cases to the temp folder
+        if os.path.exists(dest_folder):
+            shutil.rmtree(dest_folder)
+        archive = zipfile.ZipFile(dest_file)
+        archive.extractall(dest_folder)
+
 
     return {
         'actions': [download_reference_case_zug],
         'targets': [r'c:\reference-case-zug\baseline\input']
     }
+
+
+if __name__ == '__main__':
+    import doit
+
+    doit.run(globals())
