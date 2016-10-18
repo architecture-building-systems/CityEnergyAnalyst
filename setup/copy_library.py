38a701c3a79c07a4fb01b7453bafd3469d1b093a
Daren Thomas <thomas@arch.ethz.ch>
2016-10-18 18:09:53 +0200

    don't forget to save your work before going home...

--- setup/copy_library.py
+++ setup/copy_library.py
@@ -14,16 +14,23 @@ def main(env):
     _, env_dir = map(str.strip, env_parts)
 
     assert os.path.exists(env_dir)
+    print 'found environment', env_dir
+
     env_lib = os.path.join(env_dir, 'Lib', 'site-packages')
     destination = os.path.join(os.path.dirname(__file__), 'site-packages')
     if os.path.exists(destination):
+        print 'removing old setup/site-packages'
         shutil.rmtree(destination)
+    print 'copying from env/site-packages', env_lib
     shutil.copytree(env_lib, destination)
 
     # delete some libraries already installed by ArcGIS
-    libraries_to_delete = ['numpy', 'scipy', 'matplotlib', 'pandas', 'xlrd', 'xlwt', 'evernote']
+    libraries_to_delete = ['numpy', 'scipy', 'matplotlib', 'pandas', 'xlrd', 'xlwt', 'evernote', 'snakeviz']
     for library in libraries_to_delete:
-        shutil.rmtree(os.path.join(destination, library))
+        folder_to_delete = os.path.join(destination, library)
+        if os.path.exists(folder_to_delete):
+            print 'removing library', folder_to_delete
+            shutil.rmtree(folder_to_delete)
 
 
 if __name__ == '__main__':
