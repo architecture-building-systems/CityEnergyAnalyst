264694929be0ecf5838e2f81e56d7fa61ea1c20a
Daren Thomas <thomas@arch.ethz.ch>
2016-10-19 10:22:16 +0200

    updated tests and edge case `T0` based on comments on #358

--- tests/test_sensible_loads.py
+++ tests/test_sensible_loads.py
@@ -8,3 +8,8 @@ class TestCorrectionFactorForHeatingAndCoolingSetpoints(unittest.TestCase):
         self.assertRaises(ValueError, calc_t_em_ls, heating_system='T1', cooling_system='T1', control_system=None)
         self.assertRaises(ValueError, calc_t_em_ls, heating_system='T1', cooling_system='XYZ', control_system='T1')
         self.assertRaises(ValueError, calc_t_em_ls, heating_system='T1', cooling_system=None, control_system='T1')
+
+    def test_calc_t_em_ls_T0(self):
+        from cea.demand.sensible_loads import calc_t_em_ls
+        self.assertEqual(calc_t_em_ls('T1', 'T0', 'T1'),  (2.65, 0.0))
+        self.assertEqual(calc_t_em_ls('T0', 'T3', 'T1'), (0.0, -2.0))
