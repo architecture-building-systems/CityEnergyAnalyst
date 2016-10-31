e16f55893ab469e0989012cf94ee5be2bdd6ae5d
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-10-31 14:23:07 +0800

    ministeps

--- cea/demand/calibration/clustering.py
+++ cea/demand/calibration/clustering.py
@@ -12,6 +12,7 @@ from __future__ import division
 
 import pandas as pd
 import numpy as np
+import time
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2016, Architecture and Building Systems - ETH Zurich"
@@ -23,14 +24,19 @@ __email__ = "thomas@arch.ethz.ch"
 __status__ = "Production"
 
 
-def clustering(locator, building_name, building_load, how):
-
+def clustering(locator, gv, building_name, building_load):
+    t0 = time.clock()
     # import data
-    data = pd.read_csv(locator.get_demand_results_file(building_name))[building_load]
-    print data
-   if how is 'daily': # cluster daily
+    data = pd.read_csv(locator.get_demand_results_file(building_name), usecols=['DATE', building_load], index_col='DATE')
+    data.set_index(pd.to_datetime(data.index), inplace=True)
+
+    # transform into arrays of 24h
+    groups = data.groupby(data.index.dayofyear)
+    arrays = [group[1][building_load] for group in groups]
+    
 
 
+    gv.log('done - time elapsed: %(time_elapsed).2f seconds', time_elapsed=time.clock() - t0)
 
 def run_as_script():
     import cea.globalvar as gv
@@ -38,7 +44,7 @@ def run_as_script():
     gv = gv.GlobalVariables()
     scenario_path = gv.scenario_reference
     locator = inputlocator.InputLocator(scenario_path=scenario_path)
-    clustering(locator, building_name = 'B01', building_load = 'Qhsf_kWh', how='daily')
+    clustering(locator=locator, gv=gv, building_name = 'B01', building_load = 'Qhsf_kWh')
 
 if __name__ == '__main__':
     run_as_script()
