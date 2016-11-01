76a57e1284eeefd30ce778d4a83631eae2e73ffe
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-11-01 10:31:59 +0800

    changes in days

--- cea/demand/calibration/clustering.py
+++ cea/demand/calibration/clustering.py
@@ -1,7 +1,8 @@
 """
 ===========================
 Clustering
-This script clusters typical days for a building and for a
+This script clusters typical days for a building
+using SAX method for timeseries.
 
 ===========================
 J. Fonseca  script development          27.10.16
@@ -13,6 +14,7 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2016, Architecture and Building Systems - ETH Zurich"
@@ -33,7 +35,10 @@ def clustering(locator, gv, building_name, building_load):
     # transform into arrays of 24h
     groups = data.groupby(data.index.dayofyear)
     arrays = [group[1][building_load] for group in groups]
-    
+    data['day'] = data.index.dayofweeky
+    print data.loc[data.day <= 4].loc[(data != 0).any(1)][building_load].hist(bins=10, figsize=(15, 5))
+    #data2.plot()
+    #plt.show()
 
 
     gv.log('done - time elapsed: %(time_elapsed).2f seconds', time_elapsed=time.clock() - t0)
@@ -44,7 +49,7 @@ def run_as_script():
     gv = gv.GlobalVariables()
     scenario_path = gv.scenario_reference
     locator = inputlocator.InputLocator(scenario_path=scenario_path)
-    clustering(locator=locator, gv=gv, building_name = 'B01', building_load = 'Qhsf_kWh')
+    clustering(locator=locator, gv=gv, building_name = 'B01', building_load = 'Ef_kWh')
 
 if __name__ == '__main__':
     run_as_script()
