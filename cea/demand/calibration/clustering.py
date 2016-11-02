d7d61acb7a25add054328aa4bf3db14689db8886
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-11-02 18:21:35 +0800

    Mileston sax onboard now it needs to be optimized

--- cea/demand/calibration/clustering.py
+++ cea/demand/calibration/clustering.py
@@ -11,6 +11,7 @@ J. Fonseca  script development          27.10.16
 """
 from __future__ import division
 
+from cea.demand.calibration.sax import SAX
 import pandas as pd
 import numpy as np
 import time
@@ -26,29 +27,36 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
-def clustering(locator, gv, building_name, building_load):
+def clustering(locator, gv, wordSize, alphabetSize, building_name, building_load):
     t0 = time.clock()
+
     # import data
     data = pd.read_csv(locator.get_demand_results_file(building_name), usecols=['DATE', building_load], index_col='DATE')
     data.set_index(pd.to_datetime(data.index), inplace=True)
 
-    # transform into arrays of 24h
+    # transform into dicts where key = day and value = 24 h array
     groups = data.groupby(data.index.dayofyear)
-    arrays = [group[1][building_load] for group in groups]
-    data['day'] = data.index.dayofweek
-    data.loc[data.day <= 4].loc[(data != 0).any(1)][building_load][:300].plot()
-    plt.show()
+    arrays = [group[1][building_load].values  for group in groups]
 
+    # calculate and store into string dictionary
+    s = SAX(wordSize, alphabetSize)
+    pieces = [s.to_letter_rep(array)[0] for array in arrays]
+    #data = pd.DataFrame((x + '_MWhyr', [tsd[x].sum() / 1000000) for piece in pieces)
+    print pieces[100:]
+
+    plt.show()
+    print
 
     gv.log('done - time elapsed: %(time_elapsed).2f seconds', time_elapsed=time.clock() - t0)
 
 def run_as_script():
+    """"""
     import cea.globalvar as gv
     import cea.inputlocator as inputlocator
     gv = gv.GlobalVariables()
     scenario_path = gv.scenario_reference
     locator = inputlocator.InputLocator(scenario_path=scenario_path)
-    clustering(locator=locator, gv=gv, building_name = 'B01', building_load = 'Qhsf_kWh')
+    clustering(locator=locator, gv=gv, wordSize=8, alphabetSize=7, building_name = 'B01', building_load = 'Qhsf_kWh')
 
 if __name__ == '__main__':
     run_as_script()
---
+++ cea/demand/calibration/sax.py
@@ -0,0 +1,172 @@
+"""
+===========================
+Symbolic Aggregate approXimation in python.
+Based on the paper A Symbolic Representation of Time Series, with Implications for Streaming Algorithms
+Adapted from work og N. Hoffman published under MIT license.
+===========================
+
+"""
+from __future__ import division
+
+import numpy as np
+import math
+import scipy.stats as stats
+
+__author__ = "Nathan Hoffman"
+__copyright__ = "Copyright (c) 2013 Nathan Hoffman"
+__credits__ = ["Nathan Hoffman", "Jimeno A. Fonseca"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
+
+
+class SAX(object):
+    """
+    This class is for computing common things with the Symbolic
+    Aggregate approXimation method.  In short, this translates
+    a series of data to a string, which can then be compared with other
+    such strings using a lookup table.
+    """
+
+    def __init__(self, wordSize = 8, alphabetSize = 7, epsilon = 1e-6):
+
+        if alphabetSize < 3:
+            raise "do not do that"
+        self.aOffset = ord('a')
+        self.wordSize = wordSize
+        self.alphabetSize = alphabetSize
+        self.eps = epsilon
+        self.beta = list(stats.norm().ppf(np.linspace(0.01, 0.99, self.alphabetSize+1))[1:-1])
+        self.build_letter_compare_dict()
+        self.scalingFactor = 1
+
+    def to_letter_rep(self, x):
+        """
+        Function takes a series of data, x, and transforms it to a string representation
+        """
+        (paaX, indices) = self.to_PAA(self.normalize(x))
+        self.scalingFactor = np.sqrt((len(x) * 1.0) / (self.wordSize * 1.0))
+        return self.alphabetize(paaX), indices
+
+    def normalize(self, x):
+        """
+        Function will normalize an array (give it a mean of 0, and a
+        standard deviation of 1) unless it's standard deviation is below
+        epsilon, in which case it returns an array of zeros the length
+        of the original array.
+        """
+        X = np.asanyarray(x)
+        if X.std() < self.eps:
+            return [0 for entry in X]
+        return (X-X.mean())/X.std()
+
+    def to_PAA(self, x):
+        """
+        Function performs Piecewise Aggregate Approximation on data set, reducing
+        the dimension of the dataset x to w discrete levels. returns the reduced
+        dimension data set, as well as the indicies corresponding to the original
+        data for each reduced dimension
+        """
+        n = len(x)
+        stepFloat = n/float(self.wordSize)
+        step = int(math.ceil(stepFloat))
+        frameStart = 0
+        approximation = []
+        indices = []
+        i = 0
+        while frameStart <= n-step:
+            thisFrame = np.array(x[frameStart:int(frameStart + step)])
+            approximation.append(np.mean(thisFrame))
+            indices.append((frameStart, int(frameStart + step)))
+            i += 1
+            frameStart = int(i*stepFloat)
+        return (np.array(approximation), indices)
+
+    def alphabetize(self,paaX):
+        """
+        Converts the Piecewise Aggregate Approximation of x to a series of letters.
+        """
+        alphabetizedX = ''
+        for i in range(0, len(paaX)):
+            letterFound = False
+            for j in range(0, len(self.beta)):
+                if paaX[i] < self.beta[j]:
+                    alphabetizedX += chr(self.aOffset + j)
+                    letterFound = True
+                    break
+            if not letterFound:
+                alphabetizedX += chr(self.aOffset + len(self.beta))
+        return alphabetizedX
+
+    def compare_strings(self, sA, sB):
+        """
+        Compares two strings based on individual letter distance
+        Requires that both strings are the same length
+        """
+        if len(sA) != len(sB):
+            raise StringsAreDifferentLength()
+        list_letters_a = [x for x in sA]
+        list_letters_b = [x for x in sB]
+        mindist = 0.0
+        for i in range(0, len(list_letters_a)):
+            mindist += self.compare_letters(list_letters_a[i], list_letters_b[i])**2
+        mindist = self.scalingFactor* np.sqrt(mindist)
+        return mindist
+
+    def compare_letters(self, la, lb):
+        """
+        Compare two letters based on letter distance return distance between
+        """
+        return self.compareDict[la+lb]
+
+    def build_letter_compare_dict(self):
+        """
+        Builds up the lookup table to determine numeric distance between two letters
+        given an alphabet size.  Entries for both 'ab' and 'ba' will be created
+        and will have identical values.
+        """
+
+        number_rep = range(0,self.alphabetSize)
+        letters = [chr(x + self.aOffset) for x in number_rep]
+        self.compareDict = {}
+        for i in range(0, len(letters)):
+            for j in range(0, len(letters)):
+                if np.abs(number_rep[i]-number_rep[j]) <=1:
+                    self.compareDict[letters[i]+letters[j]] = 0
+                else:
+                    high_num = np.max([number_rep[i], number_rep[j]])-1
+                    low_num = np.min([number_rep[i], number_rep[j]])
+                    self.compareDict[letters[i]+letters[j]] = self.beta[high_num] - self.beta[low_num]
+
+    def sliding_window(self, x, numSubsequences = None, overlappingFraction = None):
+        if not numSubsequences:
+            numSubsequences = 20
+        self.windowSize = int(len(x)/numSubsequences)
+        if not overlappingFraction:
+            overlappingFraction = 0.9
+        overlap = self.windowSize*overlappingFraction
+        moveSize = int(self.windowSize - overlap)
+        if moveSize < 1:
+            raise OverlapSpecifiedIsNotSmallerThanWindowSize()
+        ptr = 0
+        n = len(x)
+        windowIndices = []
+        stringRep = []
+        while ptr < n-self.windowSize+1:
+            thisSubRange = x[ptr:ptr+self.windowSize]
+            (thisStringRep,indices) = self.to_letter_rep(thisSubRange)
+            stringRep.append(thisStringRep)
+            windowIndices.append((ptr, ptr+self.windowSize))
+            ptr += moveSize
+        return (stringRep,windowIndices)
+
+    def batch_compare(self, xStrings, refString):
+        return [self.compare_strings(x, refString) for x in xStrings]
+
+    def set_scaling_factor(self, scalingFactor):
+        self.scalingFactor = scalingFactor
+
+    def set_window_size(self, windowSize):
+        self.windowSize = windowSize
