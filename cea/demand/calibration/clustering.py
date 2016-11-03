809ee29e14a14acbb0acda50f76e4df91d999e6b
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-11-03 18:39:00 +0800

    Clustering evaluation almost ready

--- cea/demand/calibration/clustering.py
+++ cea/demand/calibration/clustering.py
@@ -15,6 +15,8 @@ from cea.demand.calibration.sax import SAX
 import pandas as pd
 import numpy as np
 import time
+from sklearn.metrics import silhouette_score
+from sklearn import metrics
 import matplotlib.pyplot as plt
 
 __author__ = "Jimeno A. Fonseca"
@@ -33,19 +35,49 @@ def clustering(locator, gv, wordSize, alphabetSize, building_name, building_load
     # import data
     data = pd.read_csv(locator.get_demand_results_file(building_name), usecols=['DATE', building_load], index_col='DATE')
     data.set_index(pd.to_datetime(data.index), inplace=True)
+    data['day'] = data.index.dayofyear
 
     # transform into dicts where key = day and value = 24 h array
-    groups = data.groupby(data.index.dayofyear)
-    arrays = [group[1][building_load].values  for group in groups]
+    groups = data.groupby(data.day)
+    arrays = [group[1][building_load].values for group in groups]#[[group[0], group[1][building_load].values] for group in groups]
 
-    # calculate and store into string dictionary
-    s = SAX(wordSize, alphabetSize)
-    pieces = [s.to_letter_rep(array)[0] for array in arrays]
-    #data = pd.DataFrame((x + '_MWhyr', [tsd[x].sum() / 1000000) for piece in pieces)
-    print pieces[100:]
+    # calculate sax alphabet per every day
+    alphabets = range(3,30)
+    for alphabetSize in alphabets:
+        s = SAX(wordSize, alphabetSize)
+        sax = [s.to_letter_rep(array)[0] for array in arrays]
+        silhouette_avg = silhouette_score(arrays, sax)
+        calinski = metrics.calinski_harabaz_score(arrays, sax)
+        print alphabetSize, silhouette_avg, calinski
 
-    plt.show()
-    print
+    wordsizes = [3, 6, 8, 12]
+    alphabetSize = 4
+    for wordSize in wordsizes:
+        s = SAX(wordSize, alphabetSize)
+        sax = [s.to_letter_rep(array)[0] for array in arrays]
+        silhouette_avg = silhouette_score(arrays, sax)
+        calinski = metrics.calinski_harabaz_score(arrays, sax)
+        print wordSize, silhouette_avg, calinski
+
+    # calculate dict with data per hour for the whole year and create groups per pattern
+    # array_hours = range(24)
+    # array_days = range(365)
+    # arrays_T = [list(x) for x in zip(*arrays)]
+    # dict_data = dict((group,x) for group, x in zip(array_hours, arrays_T))
+    # dict_data.update({'sax': sax, 'day': array_days})
+    #
+    # # create group by pattern
+    # grouped_sax = pd.DataFrame(dict_data).groupby('sax')
+    #
+    # for name, group in grouped_sax:
+    #     transpose = group.T.drop(['sax', 'day'], axis=0)
+    #     transpose.plot()
+    #     plt.show()
+
+
+    # print grouped_sax.describe()
+
+
 
     gv.log('done - time elapsed: %(time_elapsed).2f seconds', time_elapsed=time.clock() - t0)
 
@@ -56,7 +88,7 @@ def run_as_script():
     gv = gv.GlobalVariables()
     scenario_path = gv.scenario_reference
     locator = inputlocator.InputLocator(scenario_path=scenario_path)
-    clustering(locator=locator, gv=gv, wordSize=8, alphabetSize=7, building_name = 'B01', building_load = 'Qhsf_kWh')
+    clustering(locator=locator, gv=gv, wordSize=6, alphabetSize=4, building_name = 'B01', building_load = 'Qhsf_kWh')
 
 if __name__ == '__main__':
     run_as_script()
