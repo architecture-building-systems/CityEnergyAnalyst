fab723127fdce4968777de5ac42123b1f70dd087
Daren Thomas <thomas@arch.ethz.ch>
2016-11-03 11:05:36 +0100

    adding parameters for the different methods

--- cea/analysis/sensitivity/sensitivity_demand_samples.py
+++ cea/analysis/sensitivity/sensitivity_demand_samples.py
@@ -10,17 +10,22 @@ from SALib.sample.morris import sample as sampler_morris
 from cea.inputlocator import InputLocator
 
 
-def create_demand_samples(method='morris', num_samples=1000, variable_groups=['THERMAL']):
+def create_demand_samples(method='morris', num_samples=1000, variable_groups=('THERMAL'), sampler_params={}):
     """
 
+    :param sampler_params: additional, sampler-specific parameters. For `method='morris'` these are: [grid_jump,
+                           num_levels], for `method='sobol'` these are: [calc_second_order]
     :param output_folder: Folder to place the output file 'samples.npy' (FIXME: should this be part of the
                           InputLocator?)
-    :param method:
-    :param num_samples:
+    :param method: The method to use. Valid values are 'morris' (default) and 'sobol'.
+    :param num_samples: The number of samples `N` to make
     :param variable_groups: list of names of groups of variables to analyse. Possible values are:
         'THERMAL', 'ARCHITECTURE', 'INDOOR_COMFORT', 'INTERNAL_LOADS'. This list links to the probability density
-        functions of the variables contained in locator.get_uncertainty_db().
-    :return:
+        functions of the variables contained in locator.get_uncertainty_db() and refers to the Excel worksheet name.
+    :return: (samples, problem) - samples is a list of configurations for each simulation to run, a configuration being
+        a list of values for each variable in the problem. The problem is a dictionary with the keys 'num_vars',
+        'names' and 'bounds' and describes the variables being sampled: 'names' is list of variable names of length
+        'num_vars' and 'bounds' is a list of tuples(lower-bound, upper-bound) for each of these variables.
     """
     locator = InputLocator(None)
 
@@ -36,30 +41,42 @@ def create_demand_samples(method='morris', num_samples=1000, variable_groups=['T
     problem = {'num_vars': num_vars, 'names': names, 'bounds': bounds, 'groups': None}
 
     # create samples (combinations of variables)
-    def sampler():
+    def sampler(**kwargs):
         if method is 'sobol':
-            second_order = False
-            return sampler_sobol(problem, N=num_samples, calc_second_order=second_order)
+            return sampler_sobol(problem, N=num_samples, **kwargs)
         else:
-            grid = 2
-            levels = 4
-            return sampler_morris(problem, N=num_samples, grid_jump=grid, num_levels=levels)
+            return sampler_morris(problem, N=num_samples, **kwargs)
 
     return sampler(), problem
 
 
 if __name__ == '__main__':
     import argparse
+    import pickle
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--method', help='Method to use {morris, sobol}', default='morris')
     parser.add_argument('-n', '--num-samples', help='number of samples (generally 1000 or until it converges',
                         default=1000)
-    parser.add_argument('-o', '--output-folder', help='folder to place the output file (samples.npy) in',
-                        default='.')
+    parser.add_argument('--calc-second-order', help='(sobol) calc_second_order parameter',
+                        default=False)
+    parser.add_argument('--grid-jump', help='(morris) grid_jump parameter',
+                        default=2)
+    parser.add_argument('--num-levels', help='(morris) num_levels parameter',
+                        default=4)
+    parser.add_argument('-o', '--output-folder', default='.',
+                        help='folder to place the output files (samples.npy, problem.pickle) in')
     args = parser.parse_args()
+
+    sampler_params = {}
+    if args.method == 'morris':
+        sampler_params['grid_jump'] = args.grid_jump
+        sampler_params['num_levels'] = args.num_levels
+    elif args.method == 'sobol':
+        sampler_params['calc_second_order'] = args.calc_second_order
 
     samples, problem = create_demand_samples(method=args.method, num_samples=args.num_samples)
 
     # save out to disk
     np.save(os.path.join(args.output_folder, 'samples.npy'), samples)
+    pickle.dump(os.path.join(args.output_folder, 'problem.pickle', problem))
