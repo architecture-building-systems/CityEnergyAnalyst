cc53c4323f0fb522ffbd17e4f52420e62f202b2d
Daren Thomas <thomas@arch.ethz.ch>
2016-11-04 11:50:54 +0100

    create samples script

---
+++ euler/create-samples.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# parameters
+N=1
+SAMPLES_FOLDER=/cluster/scratch/username/samples
+SIMULATION_FOLDER=$TMPDIR
+
+
+# create the samples
+python cea\analysis\sensitivity\sensitivity_demand_samples.py --samples-folder $SAMPLES_FOLDER -n $N \
+   --simulation-folder $SIMULATION_FOLDER
+
