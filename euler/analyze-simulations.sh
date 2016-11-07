c0432b461026602ed6d0c4efc2bbd8b7921164c5
daren-thomas <thomas@arch.ethz.ch>
2016-11-07 16:22:00 +0100

    ran analysis of trivial case on euler

---
+++ euler/analyze-simulations.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# parameters
+N=1
+SAMPLES_FOLDER=$SCRATCH/samples_morris_$N
+SIMULATION_FOLDER=$TMPDIR
+
+mkdir -p $SAMPLES_FOLDER
+
+# create the samples
+python -m cea.analysis.sensitivity.sensitivity_demand_analyze --samples-folder $SAMPLES_FOLDER
