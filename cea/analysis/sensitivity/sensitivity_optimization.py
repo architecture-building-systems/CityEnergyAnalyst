b72a3e2802ba7341dba656ba62b0a778bbf10969
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-11-17 11:37:59 +0800

    rearranging folders for better comprenhension of dependencies

--- cea/analysis/sensitivity/sensitivity_optimization.py
+++ cea/analysis/sensitivity/sensitivity_optimization.py
@@ -5,15 +5,15 @@ Sensitivity analysis
 
 """
 from __future__ import division
-import cea.globalVar as glob
+
 import os
 
+import cea.globalVar as glob
 import cea.optimization.evolAlgo.evaluateInd as eI
 import numpy as np
 from deap import base
 
-import cea.globalVar as glob
-import cea.optimization.master.ea.selection as sel
+import cea.optimization.conversion_storage.master.ea.selection as sel
 import cea.optimization.supportFn as sFn
 
 __author__ = "Tim Vollrath"
