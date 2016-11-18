07a290abd438861771b61f89844a0bd74d5a7803
Daren Thomas <thomas@arch.ethz.ch>
2016-11-18 16:35:15 +0100

    updated documentation of `sensitivity_demand_count.py`

--- cea/analysis/sensitivity/sensitivity_demand_count.py
+++ cea/analysis/sensitivity/sensitivity_demand_count.py
@@ -1,14 +1,29 @@
 """
 Return the count a list of samples in a specified folder as input for the demand sensitivity analysis.
-This reads in the `samples.npy` file and prints out the number of samples contained. This can be used for scripting
-the demand simulations with a load sharing facility system like the Euler cluster.
+
+This reads in the `samples.npy` file produced by the script `sensitivity_demand_samples.py` and prints out the number
+of samples contained. This can be used for scripting the demand simulations with a load sharing facility system like
+the Euler cluster.
 """
+
 import os
 import numpy as np
+
+__author__ = "Jimeno A. Fonseca; Daren Thomas"
+__copyright__ = "Copyright 2016, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Jimeno A. Fonseca", "Daren Thomas"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "thomas@arch.ethz.ch"
+__status__ = "Production"
 
 
 def count_samples(samples_path):
     """
+    Read in the `samples.npy` numpy array from disk in the `samples_path` and report the row count (each row
+    in the array is a sample to simulate for either the morris or the sobol method.
+
     :param samples_path: path to folder with the samples - see sensitivity_demand_samples.py
     :type samples_path: str
     :return: number of samples in the samples folder.
