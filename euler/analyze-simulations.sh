0502096de03711b1977d959fca2379a54f869bf8
Daren Thomas <thomas@arch.ethz.ch>
2016-11-21 16:56:16 +0100

    documented the `analyze-simulations.sh` script

--- euler/analyze-simulations.sh
+++ euler/analyze-simulations.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env sh
 
+# Runs the script `cea/analysis/sensitivity/sensitivity_demand_analyze.py`.
+# Provides a handy interface using environment variables and default values tailored to the Euler cluster.
+
+# Each parameter is set to the environment variable of the same name or the default value if not set.
+# The samples folder is created if it doesn't yet exist.
+# This is the mapping of environment variables to argument names of the `sensitivity_demand_analyze.py` script:
+#
+# environment variable | corresponding argument
+# N                    | (not used, except for calculating the default $SAMPLES_FOLDER value)
+# METHOD               | --method
+# SAMPLES_FOLDER       | --samples-folder
+# CALC_SECOND_ORDER    | --calc-second-order
+# GRID_JUMP            | --grid-jump
+# NUM_LEVELS           | --num-levels
+# VARIABLE_GROUPS      | FIXME: not implemented yet
+
 # parameters
 N=${N:-1}
 METHOD=${METHOD:-morris}
