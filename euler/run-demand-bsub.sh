cf8b43d70fd4d582e6ca558a47256b70db904626
Daren Thomas <thomas@arch.ethz.ch>
2016-11-22 11:22:09 +0100

    added YAML frontmatter-formatted credits to the shell scripts

--- euler/run-demand-bsub.sh
+++ euler/run-demand-bsub.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env sh
 # run just the bsub portion of the demand (for a single batch)
+# The reason that the call to `sensitivity_demand_simulate` was extracted to it's own script is because the
+# variable `$TMPDIR` is not set at the time `bsub` is called, only when the job is actually running on a node.
+# (it might be possible to escape the variable somehow, I just haven't figured out how...)
+
+# ---
+# author: Daren Thomas
+# copyright: Copyright 2016, Architecture and Building Systems - ETH Zurich
+# credits: Daren Thomas
+# license: MIT
+# version:  0.1
+# maintainer: Daren Thomas
+# email: thomas@arch.ethz.ch
+# status: Production
+# ---
 
 echo "Simulating batch $i with size $NUM_SIMULATIONS"
 
