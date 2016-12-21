3f6c7beab185ad70b0c120ae1c491fee660011f7
Daren Thomas <thomas@arch.ethz.ch>
2016-12-21 09:56:53 +0100

    created `create_function_graph.py` file

---
+++ cea/utilities/create_function_graph.py
@@ -0,0 +1,5 @@
+"""
+Create a graph of function calls as an overview of the CEA software.
+
+In a first step, this only runs on the demand script and just logs which functions call which other functions.
+"""
