6ec674e860213e9862cd3cc4dcf267bff9394664
Daren Thomas <thomas@arch.ethz.ch>
2017-01-03 15:33:03 +0100

    added batch file to create the function graph too...

--- bin/create_demand_function_graph.bat
+++ bin/create_demand_function_graph.bat
@@ -1,7 +1,7 @@
-:: create_demand_module_overview.bat
+:: create_demand_function_graph.bat
 ::
 :: Use the `create_function_graph.py` script to create an overview of the demand calculation tracing calls from
-:: module to module
+:: function to function.
 ::
 :: NOTE: This script expects GraphViz to be installed and accessible from your PATH environment variable.
 :: You can install GraphViz from here: http://www.graphviz.org/Download_windows.php
@@ -18,11 +18,11 @@
 :: ---
 
 REM generate the dot file by running the demand script on the nine cubes test case
-REM (the dot file is placed in the ..\docs\demand folder under the name "demand-module-overview.gv")
+REM (the dot file is placed in the ..\docs\demand folder under the name "demand-function-graph.gv")
 REM %~dp0 resolves to the full path of the folder in which the batch script resides.
 if not exist "%~dp0..\docs\demand" mkdir %~dp0..\docs\demand
-call python create_function_graph.py --module-overview --output %~dp0..\docs\demand\demand-module-overview.gv
+call python create_function_graph.py --output %~dp0..\docs\demand\demand-function-graph.gv
 
 REM generate the .pdf file by running the `dot` program
-REM (the resulting pdf is placed in the ..\docs\demand folder under the name "demand-module-overview.gv.pdf")
-call dot -Tpdf -O %~dp0..\docs\demand\demand-module-overview.gv
+REM (the resulting pdf is placed in the ..\docs\demand folder under the name "demand-function-graph.gv.pdf")
+call dot -Tpdf -O %~dp0..\docs\demand\demand-function-graph.gv
