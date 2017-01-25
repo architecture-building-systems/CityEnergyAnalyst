7ae9301b1874b5b3592c49155e0c1fabc1af4d3f
Daren Thomas <thomas@arch.ethz.ch>
2017-01-25 09:34:07 +0100

    first version with TimeStepDataT (time elapsed: 32.47 seconds)

---
+++ cea/demand/time_step_data_t.py
@@ -0,0 +1,14 @@
+class TimeStepDataT(object):
+    def __init__(self, tsd, t):
+        self.m_ve_mech = tsd['m_ve_mech'][t]
+        self.m_ve_window = tsd['m_ve_window'][t]
+        self.m_ve_inf_simple = tsd['m_ve_inf_simple'][t]
+        self.Elf = tsd['Elf'][t]
+        self.Eaf = tsd['Eaf'][t]
+        self.Qcdataf = tsd['Qcdataf'][t]
+        self.Qcref = tsd['Qcref'][t]
+        self.people = tsd['people'][t]
+        self.I_sol = tsd['I_sol'][t]
+        self.T_ext = tsd['T_ext'][t]
+        self.theta_ve_mech = tsd['theta_ve_mech'][t]
+
