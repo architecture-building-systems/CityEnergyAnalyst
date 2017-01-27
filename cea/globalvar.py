76216c063853cb4d771bde049ed7f6b795c339e7
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-01-27 11:43:39 +0800

    a

--- cea/globalvar.py
+++ cea/globalvar.py
@@ -19,14 +19,14 @@ __status__ = "Production"
 
 class GlobalVariables(object):
     def __init__(self):
-        self.scenario_reference = r'c:\reference-case-open\baseline'
+        self.scenario_reference = r'c:\reference-case-zug\baseline'
         self.print_partial = 'hourly' # hourly or monthly for the deamnd script
         self.print_totals = True # print yearly values
         self.print_yearly_peak = True # print peak values
         self.simulate_building_list = None # fill it with a list of names of buildings in case not all the data set needs to be run
         self.date_start = '2016-01-01'  # format: yyyy-mm-dd
         self.seasonhours = [3216, 6192]
-        self.multiprocessing = False  # use multiprocessing / parallel execution if possible
+        self.multiprocessing = True  # use multiprocessing / parallel execution if possible
         self.Z = 3  # height of basement for every building in m
         self.Bf = 0.7  # it calculates the coefficient of reduction in transmittance for surfaces in contact with the ground according to values of SIA 380/1
         self.his = 3.45  # heat transfer coefficient between air and the surfacein W/(m2K)
@@ -78,7 +78,7 @@ class GlobalVariables(object):
         # Commands for the evolutionary algorithm
 
         self.initialInd = 3  # number of initial individuals
-        self.NGEN = 3000  # number of total generations
+        self.NGEN = 5  # number of total generations
         self.fCheckPoint = 1  # frequency for the saving of checkpoints
         self.maxTime = 7 * 24 * 3600  # maximum computional time [seconds]
 
