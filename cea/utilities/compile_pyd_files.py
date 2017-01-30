79b18d155c99a17c8788f2fc4cf7e45a0a35641a
Daren Thomas <thomas@arch.ethz.ch>
2017-01-30 11:13:49 +0100

    numba cc calc_phi_c

--- cea/utilities/compile_pyd_files.py
+++ cea/utilities/compile_pyd_files.py
@@ -48,6 +48,7 @@ def compile_rc_model_sia():
     # cc.export('calc_h_ea', "f8(f8, f8, f8)")(cea.demand.rc_model_SIA.calc_h_ea)
     # cc.export('calc_f_sc', "f8(f8, f8, f8, f8)")(cea.demand.rc_model_SIA.calc_f_sc)
     cc.export('calc_phi_m', "f8(f8, f8, f8, f8, f8, f8, f8)")(cea.demand.rc_model_SIA.calc_phi_m)
+    cc.export('calc_phi_c', "f8(f8, f8, f8, f8, f8, f8, f8)")(cea.demand.rc_model_SIA.calc_phi_c)
     cc.compile()
 
 
