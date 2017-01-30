4fe9f9378a072115a46244dc552ef3fa4a502be4
Daren Thomas <thomas@arch.ethz.ch>
2017-01-30 11:13:49 +0100

    numba cc calc_phi_c

--- cea/demand/rc_model_SIA.py
+++ cea/demand/rc_model_SIA.py
@@ -617,8 +617,7 @@ def _calc_rc_model_temperatures(Eaf, Elf, Htr_op, Htr_w, I_sol, Qcdataf, Qcref, 
     h_1 = calc_h_1(h_ea=h_ea, h_ac=h_ac)
     phi_a = calc_phi_a(phi_hc_cv=phi_hc_cv, phi_i_l=phi_i_l, phi_i_a=phi_i_a, phi_i_p=phi_i_p, I_sol=I_sol)
     phi_m = calc_phi_m(phi_hc_r, phi_i_l, phi_i_a, phi_i_p, I_sol, f_im, f_sm)
-    phi_c = calc_phi_c(phi_hc_r=phi_hc_r, phi_i_l=phi_i_l, phi_i_a=phi_i_a, phi_i_p=phi_i_p, I_sol=I_sol, f_ic=f_ic,
-                       f_sc=f_sc)
+    phi_c = calc_phi_c(phi_hc_r, phi_i_l, phi_i_a, phi_i_p, I_sol, f_ic, f_sc)
     theta_ea = calc_theta_ea(m_ve_mech=m_ve_mech, m_ve_window=m_ve_window, m_ve_inf_simple=m_ve_inf_simple,
                              theta_ve_mech=theta_ve_mech, T_ext=T_ext)
     theta_em = calc_theta_em(T_ext=T_ext)
@@ -739,7 +738,7 @@ def lookup_f_hc_cv_cooling(bpr):
 # use the optimized (numba_cc) versions of the functions in this module if available
 try:
     # import Numba AOT versions of the functions above, overwriting them
-    from rc_model_sia_cc import calc_phi_m
+    from rc_model_sia_cc import calc_phi_m, calc_phi_c
 except ImportError:
     # fall back to using the python version
     print('failed to import from rc_model_sia_cc.pyd, falling back to pure python functions')
