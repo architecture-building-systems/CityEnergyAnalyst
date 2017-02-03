5c85d36433f003ec1aeab0d2b2545eb3f4439d6b
martin-mosteiro <mosteiro@arch.ethz.ch>
2017-02-03 09:45:14 +0100

    Fixed nomenclature for consistency

--- cea/technologies/thermal_network_matrix.py
+++ cea/technologies/thermal_network_matrix.py
@@ -134,12 +134,12 @@ def thermal_network_main(locator, gv, network, source):
     # skip calculation, import csv TODO: get rid of this after testing
     T_supply_nodes_list = pd.read_csv(locator.get_optimization_network_layout_supply_temperature_file(network))
     T_return_nodes_list = pd.read_csv(locator.get_optimization_network_layout_return_temperature_file(network))
-    plant_heat_requirements = pd.read_csv(locator.get_optimization_network_layout_plant_heat_requirement_file(network))
+    # plant_heat_requirements = pd.read_csv(locator.get_optimization_network_layout_plant_heat_requirement_file(network))
 
 
     # calculate pressure at each node and pressure drop throughout the entire network
     pressure_nodes_supply, pressure_nodes_return, pressure_loss_system = calc_pressure_nodes(edge_node_df,
-                                                    pipe_properties_df[:]['DN':'DN'].values, pipe_length_df.values,
+                                                    pipe_properties_df[:]['Pipe_DN':'Pipe_DN'].values, pipe_length_df.values,
                                                     edge_mass_flow_df.values, T_supply_nodes_list, T_return_nodes_list, gv)
     pd.DataFrame(pressure_nodes_supply, columns=edge_node_df.index). \
             to_csv(locator.get_optimization_network_layout_supply_pressure_file(network), index=False, float_format='%.3f')
@@ -242,7 +242,7 @@ def assign_pipes_to_edges(mass_flow_df, locator, gv):
 
     return pipe_properties_df
 
-def calc_pressure_nodes(edge_node_df, pipe_diameter, pipe_length, mass_flow_rate, T_supply_node, T_return_node, gv, locator):
+def calc_pressure_nodes(edge_node_df, pipe_diameter, pipe_length, mass_flow_rate, T_supply_node, T_return_node, gv):
     ''' calculates the pressure at each node based on Eq. 1 in Todini & Pilati (1987) "A gradient method for the analysis
 of pipe networks," in Computer Applications in Water Supply Volume 1 - Systems Analysis and Simulation. Since the
 pressure is calculated after the mass flow rate (rather than concurrently) this is only a first step towards implementing
