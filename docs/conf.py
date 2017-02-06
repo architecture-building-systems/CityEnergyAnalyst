2c91bd610233a7bba9cfe16bb9b8bf04415251b7
Daren Thomas <thomas@arch.ethz.ch>
2017-02-06 13:20:50 +0100

    fixing paths for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 # import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../cea'))
+sys.path.insert(0, os.path.abspath('..'))
 
 print 'conf.py:', sys.path
 print 'currdir:', os.path.abspath(os.curdir)
