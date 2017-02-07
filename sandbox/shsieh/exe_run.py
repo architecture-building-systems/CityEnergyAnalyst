3248e59a0b31141fa42ca2d01fcb9922cbeb7d74
Daren Thomas <thomas@arch.ethz.ch>
2017-02-07 13:36:45 +0100

    removing sandbox from docs branch

--- sandbox/shsieh/exe_run.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-#command = "Studio.exe"
-#os.system(command)
-
-#subprocess.call('dir', shell=True)
-output = subprocess.check_output('dir', shell = True)
-print(output)
