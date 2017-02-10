c62ba514d090078575f254b6a1c0cccfc02b8c30
martin-mosteiro <mosteiro@arch.ethz.ch>
2017-02-10 15:47:32 +0100

    Solving conflicts
    Also removed the function `get_data_mobility`, which is obsolete.

--- cea/inputlocator.py
+++ cea/inputlocator.py
@@ -231,16 +231,8 @@ class InputLocator(object):
         return os.path.join(self.db_path, 'Systems',  'thermal_networks.xls')
 
     def get_data_benchmark(self):
-        """db/Benchmarks/benchmark_targets.xls"""
-        return os.path.join(self.db_path, 'Benchmarks', 'benchmark_targets.xls')
-
-    def get_data_benchmark_today(self):
-        """db/Benchmarks/benchmark_today.xls"""
-        return os.path.join(self.db_path, 'Benchmarks', 'benchmark_today.xls')
-
-    def get_data_mobility(self):
-        """db/Benchmarks/mobility.xls"""
-        return os.path.join(self.db_path, 'Benchmarks', 'mobility.xls')
+        """databases/CH/Benchmarks/benchmark_targets.xls"""
+        return os.path.join(self.db_path, 'Benchmarks', 'benchmark_2000W.xls')
 
     def get_uncertainty_db(self):
         """db/Uncertainty/uncertainty_distributions.xls"""
