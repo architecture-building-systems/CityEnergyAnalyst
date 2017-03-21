e63ed46646507090f309dd85387bf65179fd14dd
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-03-21 17:37:11 +0800

    changes to saving format

--- cea/globalvar.py
+++ cea/globalvar.py
@@ -77,8 +77,8 @@ class GlobalVariables(object):
         # Commands for the evolutionary algorithm
 
 
-        self.initialInd = 4  # number of initial individuals
-        self.NGEN = 5  # number of total generations
+        self.initialInd = 50  # number of initial individuals
+        self.NGEN = 50  # number of total generations
         self.fCheckPoint = 1  # frequency for the saving of checkpoints
         self.maxTime = 7 * 24 * 3600  # maximum computational time [seconds]
 
