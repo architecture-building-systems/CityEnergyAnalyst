a368a12f4da1c92ac6acf6bee10b122dc5b1ab7d
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-03-24 16:36:41 +0800

    modifications to facilitate changing of parameter values

--- cea/Individual_Evaluation.py
+++ cea/Individual_Evaluation.py
@@ -1,186 +1,89 @@
-def configDesign(generation):
-    import cea.globalvar
-    import cea.inputlocator
-    import csv
-    import matplotlib
-    import matplotlib.cm as cmx
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    import os
-    import re
-
-    gv = cea.globalvar.GlobalVariables()
-    scenario_path = gv.scenario_reference
-    locator = cea.inputlocator.InputLocator(scenario_path)
-    os.chdir(locator.get_optimization_master_results_folder())
-    with open("CheckPoint" + str(generation), "rb") as csv_file:
-        reader = csv.reader(csv_file)
-        mydict = dict(reader)
-        pop = mydict['population']
-        m = re.findall(r"\[.*?\]", pop)
-        for i in xrange(len(m)):
-            m[i] = re.findall(r'\d+(?:\.\d+)?', m[i])
-            m[i] = [float(j) for j in m[i]]
-    ind = m[0]
-    fig = plt.figure(figsize=(6,4))
-    fig.suptitle('Design of the centralized heating hub')
-
-    # Central heating plant
-    subplot1 = fig.add_subplot(221, adjustable = 'box', aspect = 1)
-
-    def NGorBG(value):
-        if value%2 == 1:
-            gas = 'NG'
-        else:
-            gas = 'BG'
-        return gas
-
-    labels = ['CC '+NGorBG(ind[0]), 'Boiler Base '+NGorBG(ind[2]), 'Boiler peak '+NGorBG(ind[4]), 'HP Lake', 'HP Sew', 'GHP']
-    fracs = [ ind[2*i + 1] for i in range(6) ]
-    colors = ['LimeGreen', 'LightSalmon', 'Crimson', 'RoyalBlue', 'MidnightBlue', 'Gray']
-
-    zipper = [ (l,f,c) for (l,f,c) in zip(labels,fracs,colors) if f > 0.01 ]
-    labelsPlot, fracsPlot, colorsPlot = map( list, zip(*zipper) )
-    subplot1.pie(fracsPlot, labels = labelsPlot, colors = colorsPlot, startangle = 90, autopct='%1.1f%%', pctdistance = 0.5)
-
-    # Solar total area
-    subplot2 = fig.add_subplot(222, adjustable = 'box', aspect = 1)
-    labels = ['Solar covered area', 'Uncovered area']
-    fracs = [ ind[20], 1 - ind[20] ]
-    colors = ['Gold', 'Gray']
-    subplot2.pie(fracs, labels = labels, startangle = 90, colors = colors, autopct='%1.1f%%', pctdistance = 0.5)
-
-    # Solar system distribution
-    subplot3 = fig.add_subplot(223, adjustable = 'box', aspect = 1)
-    labels = ['PV', 'PVT', 'SC']
-    fracs = [ ind[15], ind[17], ind[19] ]
-    colors = ['Yellow', 'Orange', 'OrangeRed']
-
-    zipper = [ (l,f,c) for (l,f,c) in zip(labels,fracs,colors) if f > 0.01 ]
-    labelsPlot, fracsPlot, colorsPlot = map( list, zip(*zipper) )
-    subplot3.pie(fracsPlot, labels = labelsPlot, colors = colorsPlot, startangle = 90, autopct='%1.1f%%', pctdistance = 0.5)
-
-    # Connected buildings
-    connectedBuild = ind[21:].count(1) / len(ind[21:])
-    subplot4 = fig.add_subplot(224, adjustable = 'box', aspect = 1)
-    labels = ['Connected buildings', 'Disconnected buildings',]
-    fracs = [ connectedBuild, 1 - connectedBuild]
-    colors = ['Chocolate', 'Gray']
-    subplot4.pie(fracs, labels = labels, startangle = 90, colors = colors, autopct='%1.1f%%', pctdistance = 0.5)
-    plt.rcParams.update({'font.size':10})
-    plt.show()
 
 def test_graphs_optimization(generation):
     import cea.globalvar
     import cea.inputlocator
     import csv
+    import pandas as pd
+    import cea.optimization.distribution.network_opt_main as network_opt
+    import cea.optimization.master.master_main as master
+    import cea.optimization.master.evaluation as evaluation
+
     import matplotlib
     import matplotlib.cm as cmx
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
     import os
     import re
+    from cea.optimization.preprocessing.preprocessing_main import preproccessing
 
     gv = cea.globalvar.GlobalVariables()
     scenario_path = gv.scenario_reference
     locator = cea.inputlocator.InputLocator(scenario_path)
+    weather_file = locator.get_default_weather()
     os.chdir(locator.get_optimization_master_results_folder())
+
     pareto = []
     xs = []
     ys = []
     zs = []
-    if generation is 'all':
-        for i in xrange(gv.NGEN):
-            with open("CheckPoint" + str(i + 1), "rb") as csv_file:
-                reader = csv.reader(csv_file)
-                mydict = dict(reader)
-                objective_function = mydict['objective_function_values']
-                objective_function = re.findall(r'\d+\.\d+', objective_function)
-                for j in xrange(gv.initialInd):
-                    pareto_intermediate = [objective_function[3 * j], objective_function[3 * j + 1],
-                                           objective_function[3 * j + 2]]
-                    pareto.append(pareto_intermediate)
-                    xs.append(float(objective_function[3 * j]))
-                    ys.append(float(objective_function[3 * j + 1]))
-                    zs.append(float(objective_function[3 * j + 2]))
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(xs, ys, zs, c='r', marker='o')
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
-        os.chdir(locator.get_optimization_plots_folder())
-        plt.savefig("Generation" + str(generation) + "Pareto_Front_3D.png")
-        plt.show()
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        cm = plt.get_cmap('jet')
-        cNorm = matplotlib.colors.Normalize(vmin=min(zs), vmax=max(zs))
-        scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
-        ax.scatter(xs, ys, c=scalarMap.to_rgba(zs), s=50, alpha=0.8)
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
+    with open("CheckPoint" + str(generation), "rb") as csv_file:
+        pop = []
+        popfloat = []
+        reader = csv.reader(csv_file)
+        mydict = dict(reader)
+        population = mydict['population']
+        population = re.findall(r'\d+(?:\.\d+)?', population)
+        print (population)
+        popfloat = [float(x) for x in population]
 
-        scalarMap.set_array(zs)
-        fig.colorbar(scalarMap, label='Z Label')
-        plt.grid(True)
-        plt.rcParams['figure.figsize'] = (6, 4)
-        plt.rcParams.update({'font.size': 12})
-        plt.gcf().subplots_adjust(bottom=0.15)
-        plt.savefig("Generation" + str(generation) + "Pareto_Front_2D.png")
-        plt.show()
-        plt.clf()
-
-    else:
-        with open("CheckPoint" + str(generation), "rb") as csv_file:
-            pareto = []
-            xs = []
-            ys = []
-            zs = []
-            reader = csv.reader(csv_file)
-            mydict = dict(reader)
-            objective_function = mydict['objective_function_values']
-            objective_function = re.findall(r'\d+\.\d+', objective_function)
-            for i in xrange(gv.initialInd):
-                pareto_intermediate = [objective_function[3*i], objective_function[3*i + 1], objective_function[3*i + 2]]
-                pareto.append(pareto_intermediate)
-                xs.append(float(objective_function[3*i]))
-                ys.append(float(objective_function[3*i + 1]))
-                zs.append(float(objective_function[3*i + 2]))
-
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            ax.scatter(xs, ys, zs, c='r', marker='o')
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
-            os.chdir(locator.get_optimization_plots_folder())
-            plt.savefig("Generation" + str(generation) + "Pareto_Front_3D.png")
-            # plt.show()
-
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-            cm = plt.get_cmap('jet')
-            cNorm = matplotlib.colors.Normalize(vmin=min(zs), vmax=max(zs))
-            scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
-            ax.scatter(xs, ys, c=scalarMap.to_rgba(zs), s=50, alpha=0.8)
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-
-            scalarMap.set_array(zs)
-            fig.colorbar(scalarMap, label='Z Label')
-            plt.grid(True)
-            plt.rcParams['figure.figsize'] = (6, 4)
-            plt.rcParams.update({'font.size': 12})
-            plt.gcf().subplots_adjust(bottom=0.15)
-            plt.savefig("Generation" + str(generation) + "Pareto_Front_2D.png")
-            # plt.show()
-            plt.clf()
+        for i in xrange(len(popfloat)):
+            if popfloat[i] - int(popfloat[i]) == 0:
+                popfloat[i] = int(popfloat[i])
 
 
+        for i in xrange(len(popfloat)/45):
+            pop.append(popfloat[i*45:(((i+1)*45)-1)])
+
+    print (len(pop))
+    total_demand = pd.read_csv(locator.get_total_demand())
+    building_names = total_demand.Name.values
+    gv.num_tot_buildings = total_demand.Name.count()
+
+    extra_costs, extra_CO2, extra_primary_energy, solarFeat = preproccessing(locator, total_demand,
+                                                                                     building_names,
+                                                                                     weather_file, gv)
+
+
+
+    network_features = network_opt.network_opt_main()
+
+    print (pop[1])
+    print (type(pop[0]))
+
+
+    def objective_function(ind):
+        (costs, CO2, prim) = evaluation.evaluation_main(ind, building_names, locator, extra_costs, extra_CO2, extra_primary_energy, solarFeat,
+                                                        network_features, gv)
+        print (costs, CO2, prim)
+        return (costs, CO2, prim)
+
+    nBuildings = len(building_names)
+    ntwList = ["1" * nBuildings]
+    fitness = []
+    for i in xrange(1):
+        print (i)
+        print (pop[i])
+        evaluation.checkNtw(pop[i], ntwList, locator, gv)
+        fitness.append(objective_function(pop[i]))
+
+    os.chdir(locator.get_optimization_master_results_folder())
+    with open("CheckPointTesting", "wb") as csv_file:
+        writer = csv.writer(csv_file)
+        cp = dict(population=pop, generation=generation, objective_function_values=fitness)
+        for key, value in cp.items():
+            writer.writerow([key, value])
 
 if __name__ == '__main__':
-    generation = 'all'
-    # configDesign(generation)
+    generation = 50
     test_graphs_optimization(generation)
