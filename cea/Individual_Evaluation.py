b6698b96cbe9305acd362d1a2a35f3e02c44c1c7
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-03-29 13:48:46 +0800

    changes to the level in which uncertainty is present

--- cea/Individual_Evaluation.py
+++ cea/Individual_Evaluation.py
@@ -98,6 +98,6 @@ def test_graphs_optimization(generation, level):
 
 if __name__ == '__main__':
     generation = 50
-    level = 19
+    level = 99
 
     test_graphs_optimization(generation, level)
