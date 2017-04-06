59da30f9fe7f3a18f47a6127af86e06a0eb3c3d2
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-04-06 17:56:32 +0800

    saving as json files

--- cea/globalvar.py
+++ cea/globalvar.py
@@ -80,8 +80,8 @@ class GlobalVariables(object):
         # Commands for the evolutionary algorithm
 
 
-        self.initialInd = 4  # number of initial individuals
-        self.NGEN = 2  # number of total generations
+        self.initialInd = 1  # number of initial individuals
+        self.NGEN = 3  # number of total generations
         self.fCheckPoint = 1  # frequency for the saving of checkpoints
         self.maxTime = 7 * 24 * 3600  # maximum computational time [seconds]
 
