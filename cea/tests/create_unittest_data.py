9ebece673eeda41ae6be0f2a770495edbc7df738
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-04-13 17:10:59 +0800

    chaning unitest data erasing dict -changed to list

--- cea/tests/create_unittest_data.py
+++ cea/tests/create_unittest_data.py
@@ -58,17 +58,10 @@ def main():
 
     print("data for test_calc_thermal_loads_other_buildings:")
     # randomly selected except for B302006716, which has `Af == 0`
-    buildings = {'B01': (81124.39400, 150471.05200),
-                 'B03': (81255.09200, 150520.01000),
-                 'B02': (82176.15300, 150604.85100),
-                 'B05': (84058.72400, 150841.56200),
-                 'B04': (82356.22600, 150598.43400),
-                 'B07': (81052.19000, 150490.94800),
-                 'B06': (83108.45600, 150657.24900),
-                 'B09': (84491.58100, 150853.54000),
-                 'B08': (88572.59000, 151020.09300), }
+    buildings = ['B01', 'B03', 'B02', 'B05', 'B04','B07','B06','B09',
+                 'B08']
 
-    for building in buildings.keys():
+    for building in buildings:
         bpr = building_properties[building]
         b, qcf_kwh, qhf_kwh = run_for_single_building(building, bpr, weather_data, usage_schedules,
                                                       date, gv, locator)
