4e3c0645d9f1bb28e37d5bc45ec4db15bbb4512f
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-04-19 16:27:56 +0800

    updates to remove hardcoding

--- cea/analysis/uncertainty/Individual_Evaluation.py
+++ cea/analysis/uncertainty/Individual_Evaluation.py
@@ -12,7 +12,6 @@ import cea.inputlocator
 import pandas as pd
 import cea.optimization.distribution.network_opt_main as network_opt
 import cea.optimization.master.evaluation as evaluation
-import os
 import json
 import csv
 
@@ -25,7 +24,7 @@ __maintainer__ = "Daren Thomas"
 __email__ = "thomas@arch.ethz.ch"
 __status__ = "Production"
 
-def individual_evaluation(generation, level, size):
+def individual_evaluation(generation, level, size, variable_groups):
     """
     :param generation: Generation of the optimization in which the individual evaluation is to be done
     :type generation: int
@@ -41,9 +40,8 @@ def individual_evaluation(generation, level, size):
     scenario_path = gv.scenario_reference
     locator = cea.inputlocator.InputLocator(scenario_path)
     weather_file = locator.get_default_weather()
-    os.chdir(locator.get_optimization_master_results_folder())
 
-    with open("CheckPoint" + str(generation), "rb") as fp:
+    with open(locator.get_optimization_master_results_folder() + "\CheckPoint_" + str(generation), "rb") as fp:
         data = json.load(fp)
 
     pop = data['population']
@@ -51,49 +49,15 @@ def individual_evaluation(generation, level, size):
 
     # # Uncertainty Part
     row = []
-    with open('uncertainty.csv') as f:
+    with open(locator.get_uncertainty_results_folder() + '\uncertainty.csv') as f:
         reader = csv.reader(f)
         for i in xrange(size+1):
             row.append(next(reader))
 
     j = level + 1
-    # Uncertainty Parameters
-    HP_n = float(row[j][1])  # lifetime [years] default 20
-    GHP_nHP = float(row[j][2])  # for the geothermal heat pump default 20
-    Boiler_n = float(row[j][3])  # lifetime, after A+W, default 20
-    CC_n = float(row[j][4])  # lifetime default 25
-    FC_n = float(row[j][5])  # years of operation default 10
-    PVT_n = float(row[j][6])  # years of operation default 20
-    SC_n = PVT_n  # years of operation default 20
-    CT_a = (float(row[j][7]))  # annuity factor default 0.15
-    Subst_n = float(row[j][8])  # Lifetime after A+W default 20
-    ELEC_PRICE = float(row[j][9]) * gv.EURO_TO_CHF / 1000.0  # default 0.2
-    cPump = ELEC_PRICE * 24. * 365.  # coupled to electricity cost
-    NG_PRICE = float(row[j][10]) * gv.EURO_TO_CHF / 1000.0  # [CHF / wh] # default 0.068
-    BG_PRICE = float(row[j][11]) * gv.EURO_TO_CHF / 1000.0  # [CHF / wh] # default 0.076
-    Subst_i = float(row[j][12])/100 # default 0.05
-    FC_i = float(row[j][13]) /100 # interest rate default 0.05
-    HP_i = float(row[j][14])/100  # interest rate default 0.05
-    Boiler_i = float(row[j][15])/100  # interest rate default 0.05
 
-    # changing the parameters based on the uncertainty distribution
-    setattr(gv, 'HP_n', HP_n)
-    setattr(gv, 'GHP_nHP', GHP_nHP)
-    setattr(gv, 'Boiler_n', Boiler_n)
-    setattr(gv, 'CC_n', CC_n)
-    setattr(gv, 'FC_n', FC_n)
-    setattr(gv, 'PVT_n', PVT_n)
-    setattr(gv, 'SC_n', SC_n)
-    setattr(gv, 'CT_a', CT_a)
-    setattr(gv, 'Subst_n', Subst_n)
-    setattr(gv, 'ELEC_PRICE', ELEC_PRICE)
-    setattr(gv, 'cPump', cPump)
-    setattr(gv, 'NG_PRICE', NG_PRICE)
-    setattr(gv, 'BG_PRICE', BG_PRICE)
-    setattr(gv, 'Subst_i', Subst_i)
-    setattr(gv, 'FC_i', FC_i)
-    setattr(gv, 'HP_i', HP_i)
-    setattr(gv, 'Boiler_i', Boiler_i)
+    for i in xrange(len(row[0])-1):
+        setattr(gv, row[0][i+1], float(row[j][i+1]))
 
     total_demand = pd.read_csv(locator.get_total_demand())
     building_names = total_demand.Name.values
@@ -118,51 +82,9 @@ def individual_evaluation(generation, level, size):
         cp = dict(population=pop, uncertainty_level=level, population_fitness=fitness)
         json.dump(cp, fp)
 
-    x = pd.read_excel(locator.get_uncertainty_db(), 'ECONOMIC')
-
-    HP_n = x[x['name'] == 'HP_n']['default'].max()  # lifetime [years] default 20
-    GHP_nHP = x[x['name'] == 'GHP_nHP']['default'].max()  # for the geothermal heat pump default 20
-    Boiler_n = x[x['name'] == 'Boiler_n']['default'].max()  # lifetime, after A+W, default 20
-    CC_n = x[x['name'] == 'CC_n']['default'].max()  # lifetime default 25
-    FC_n = x[x['name'] == 'FC_n']['default'].max()  # years of operation default 10
-    PVT_n = x[x['name'] == 'PVT_n']['default'].max()  # years of operation default 20
-    SC_n = PVT_n  # years of operation default 20
-    CT_a = x[x['name'] == 'CT_a']['default'].max()  # annuity factor default 0.15
-    Subst_n = x[x['name'] == 'Subst_n']['default'].max()  # Lifetime after A+W default 20
-    ELEC_PRICE = x[x['name'] == 'ELEC_PRICE']['default'].max() * gv.EURO_TO_CHF / 1000.0  # default 0.2
-    cPump = ELEC_PRICE * 24. * 365.  # coupled to electricity cost
-    NG_PRICE = x[x['name'] == 'NG_PRICE'][
-                        'default'].max() * gv.EURO_TO_CHF / 1000.0  # [CHF / wh] # default 0.068
-    BG_PRICE = x[x['name'] == 'BG_PRICE'][
-                        'default'].max() * gv.EURO_TO_CHF / 1000.0  # [CHF / wh] # default 0.076
-    Subst_i = x[x['name'] == 'Subst_i']['default'].max() / 100  # default 0.05
-    FC_i = x[x['name'] == 'FC_i']['default'].max() / 100  # interest rate default 0.05
-    HP_i = x[x['name'] == 'HP_i']['default'].max() / 100  # interest rate default 0.05
-    Boiler_i = x[x['name'] == 'Boiler_i']['default'].max() / 100  # interest rate default 0.05
-
-    # defaulting the parameters
-    setattr(gv, 'HP_n', HP_n)
-    setattr(gv, 'GHP_nHP', GHP_nHP)
-    setattr(gv, 'Boiler_n', Boiler_n)
-    setattr(gv, 'CC_n', CC_n)
-    setattr(gv, 'FC_n', FC_n)
-    setattr(gv, 'PVT_n', PVT_n)
-    setattr(gv, 'SC_n', SC_n)
-    setattr(gv, 'CT_a', CT_a)
-    setattr(gv, 'Subst_n', Subst_n)
-    setattr(gv, 'ELEC_PRICE', ELEC_PRICE)
-    setattr(gv, 'cPump', cPump)
-    setattr(gv, 'NG_PRICE', NG_PRICE)
-    setattr(gv, 'BG_PRICE', BG_PRICE)
-    setattr(gv, 'Subst_i', Subst_i)
-    setattr(gv, 'FC_i', FC_i)
-    setattr(gv, 'HP_i', HP_i)
-    setattr(gv, 'Boiler_i', Boiler_i)
-
-
 if __name__ == '__main__':
     generation = 50  # generation which you are interested in testing
-    size = 1  # number of uncertain scenarios being tested
+    size = 1000  # number of uncertain scenarios being tested
 
     for i in xrange(size):
-        individual_evaluation(generation, i, size)
+        individual_evaluation(generation, i, size, variable_groups=('ECONOMIC',))
--- cea/analysis/uncertainty/Uncertainty_parameters.py
+++ cea/analysis/uncertainty/Uncertainty_parameters.py
@@ -14,263 +14,44 @@ __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
-def variable_uncertainty_distribution_assignment(locator, variable_groups):
+def variable_uncertainty_distribution_assignment(locator, number_of_uncertain_scenarios, variable_groups):
 
     # get probability density functions (pdf) of all variable_groups from the uncertainty database
     pdf = pd.concat([pd.read_excel(locator.get_uncertainty_db(), group, axis=1) for group in variable_groups])
-    # a list of tupples containing the lower-bound and upper-bound of each variable
-    names = list(zip(pdf['name']))
-    for i in names:
-        print (i)
-        print (type(i))
-    return 0
 
-def run_as_script(scenario_path):
-
-
-    # Importing the excel sheet containing the uncertainty distributions
-    locator = cea.inputlocator.InputLocator(scenario_path)
-    variable_uncertainty_distribution_assignment(locator, variable_groups=('ECONOMIC',))
-    uncertainty_db = pd.read_excel(locator.get_uncertainty_db(), 'ECONOMIC')
+    names = pdf['name']
 
     # Fetching the distribution type and the corresponding variables from the excel sheet
-    HP_n_distribution = uncertainty_db[uncertainty_db['name'] == 'HP_n']['distribution'].max()  # lifetime [years] distribution 20
-    GHP_nHP_distribution = uncertainty_db[uncertainty_db['name'] == 'GHP_nHP']['distribution'].max()  # for the geothermal heat pump default 20
-    Boiler_n_distribution = uncertainty_db[uncertainty_db['name'] == 'Boiler_n']['distribution'].max()  # lifetime, after A+W, default 20
-    CC_n_distribution = uncertainty_db[uncertainty_db['name'] == 'CC_n']['distribution'].max()  # lifetime default 25
-    FC_n_distribution = uncertainty_db[uncertainty_db['name'] == 'FC_n']['distribution'].max()  # years of operation default 10
-    PVT_n_distribution = uncertainty_db[uncertainty_db['name'] == 'PVT_n']['distribution'].max()  # years of operation default 20
-    CT_a_distribution = uncertainty_db[uncertainty_db['name'] == 'CT_a']['distribution'].max()  # annuity factor default 0.15
-    Subst_n_distribution = uncertainty_db[uncertainty_db['name'] == 'Subst_n']['distribution'].max()  # Lifetime after A+W default 20
-    ELEC_PRICE_distribution = uncertainty_db[uncertainty_db['name'] == 'ELEC_PRICE']['distribution'].max()  # default 0.2
-    NG_PRICE_distribution = uncertainty_db[uncertainty_db['name'] == 'NG_PRICE']['distribution'].max()  # [CHF / wh] # default 0.068
-    BG_PRICE_distribution = uncertainty_db[uncertainty_db['name'] == 'BG_PRICE']['distribution'].max()   # [CHF / wh] # default 0.076
-    Subst_i_distribution = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['distribution'].max()  # default 0.05
-    FC_i_distribution = uncertainty_db[uncertainty_db['name'] == 'FC_i']['distribution'].max()  # interest rate default 0.05
-    HP_i_distribution = uncertainty_db[uncertainty_db['name'] == 'HP_i']['distribution'].max()  # interest rate default 0.05
-    Boiler_i_distribution = uncertainty_db[uncertainty_db['name'] == 'Boiler_i']['distribution'].max()  # interest rate default 0.05
+    distribution = []
+    mu_c = []
+    stdv_alpha = []
+    beta = []
+    for i in names:
+        distribution.append(pdf[pdf['name'] == i]['distribution'].max())
+        mu_c.append(pdf[pdf['name'] == i]['mu_c'].max())
+        stdv_alpha.append(pdf[pdf['name'] == i]['stdv_alpha'].max())
+        beta.append(pdf[pdf['name'] == i]['beta'].max())
 
-    if HP_n_distribution == 'Beta':
-        HP_n_c = uncertainty_db[uncertainty_db['name'] == 'HP_n']['mu/c'].max()
-        HP_n_alpha = uncertainty_db[uncertainty_db['name'] == 'HP_n']['stdv/alpha'].max()
-        HP_n_beta = uncertainty_db[uncertainty_db['name'] == 'HP_n']['beta'].max()
-    elif HP_n_distribution == 'Normal':
-        HP_n_mu = uncertainty_db[uncertainty_db['name'] == 'HP_n']['mu/c'].max()
-        HP_n_stdv = uncertainty_db[uncertainty_db['name'] == 'HP_n']['stdv/alpha'].max()
+    df = pd.DataFrame()
+    for i in xrange(len(names)):
+        if distribution[i] == 'Beta':
+            a = []
+            for j in xrange(number_of_uncertain_scenarios):
+                a.append(mu_c[i] * np.random.beta(stdv_alpha[i], beta[i], size=None))
+            df[names[i]] = a
+        elif distribution[i] == 'Normal':
+            a = []
+            for j in xrange(number_of_uncertain_scenarios):
+                a.append(np.random.normal(loc=mu_c[i], scale=stdv_alpha[i]))
+            df[names[i]] = a
+    df.to_csv(locator.get_uncertainty_results_folder()+ "\uncertainty.csv")
 
-    if GHP_nHP_distribution == 'Beta':
-        GHP_nHP_c = uncertainty_db[uncertainty_db['name'] == 'GHP_nHP']['mu/c'].max()
-        GHP_nHP_alpha = uncertainty_db[uncertainty_db['name'] == 'GHP_nHP']['stdv/alpha'].max()
-        GHP_nHP_beta = uncertainty_db[uncertainty_db['name'] == 'GHP_nHP']['beta'].max()
-    elif GHP_nHP_distribution == 'Normal':
-        GHP_nHP_mu = uncertainty_db[uncertainty_db['name'] == 'GHP_nHP']['mu/c'].max()
-        GHP_nHP_stdv = uncertainty_db[uncertainty_db['name'] == 'GHP_nHP']['stdv/alpha'].max()
 
-    if Boiler_n_distribution == 'Beta':
-        Boiler_n_c = uncertainty_db[uncertainty_db['name'] == 'Boiler_n']['mu/c'].max()
-        Boiler_n_alpha = uncertainty_db[uncertainty_db['name'] == 'Boiler_n']['stdv/alpha'].max()
-        Boiler_n_beta = uncertainty_db[uncertainty_db['name'] == 'Boiler_n']['beta'].max()
-    elif Boiler_n_distribution == 'Normal':
-        Boiler_n_mu = uncertainty_db[uncertainty_db['name'] == 'Boiler_n']['mu/c'].max()
-        Boiler_n_stdv = uncertainty_db[uncertainty_db['name'] == 'Boiler_n']['stdv/alpha'].max()
+def run_as_script(scenario_path, number_of_uncertain_scenarios):
 
-    if CC_n_distribution == 'Beta':
-        CC_n_c = uncertainty_db[uncertainty_db['name'] == 'CC_n']['mu/c'].max()
-        CC_n_alpha = uncertainty_db[uncertainty_db['name'] == 'CC_n']['stdv/alpha'].max()
-        CC_n_beta = uncertainty_db[uncertainty_db['name'] == 'CC_n']['beta'].max()
-    elif CC_n_distribution == 'Normal':
-        CC_n_mu = uncertainty_db[uncertainty_db['name'] == 'CC_n']['mu/c'].max()
-        CC_n_stdv = uncertainty_db[uncertainty_db['name'] == 'CC_n']['stdv/alpha'].max()
-
-    if FC_n_distribution == 'Beta':
-        FC_n_c = uncertainty_db[uncertainty_db['name'] == 'FC_n']['mu/c'].max()
-        FC_n_alpha = uncertainty_db[uncertainty_db['name'] == 'FC_n']['stdv/alpha'].max()
-        FC_n_beta = uncertainty_db[uncertainty_db['name'] == 'FC_n']['beta'].max()
-    elif FC_n_distribution == 'Normal':
-        FC_n_mu = uncertainty_db[uncertainty_db['name'] == 'FC_n']['mu/c'].max()
-        FC_n_stdv = uncertainty_db[uncertainty_db['name'] == 'FC_n']['stdv/alpha'].max()
-
-    if PVT_n_distribution == 'Beta':
-        PVT_n_c = uncertainty_db[uncertainty_db['name'] == 'PVT_n']['mu/c'].max()
-        PVT_n_alpha = uncertainty_db[uncertainty_db['name'] == 'PVT_n']['stdv/alpha'].max()
-        PVT_n_beta = uncertainty_db[uncertainty_db['name'] == 'PVT_n']['beta'].max()
-    elif PVT_n_distribution == 'Normal':
-        PVT_n_mu = uncertainty_db[uncertainty_db['name'] == 'PVT_n']['mu/c'].max()
-        PVT_n_stdv = uncertainty_db[uncertainty_db['name'] == 'PVT_n']['stdv/alpha'].max()
-
-    if CT_a_distribution == 'Beta':
-        CT_a_c = uncertainty_db[uncertainty_db['name'] == 'CT_a']['mu/c'].max()
-        CT_a_alpha = uncertainty_db[uncertainty_db['name'] == 'CT_a']['stdv/alpha'].max()
-        CT_a_beta = uncertainty_db[uncertainty_db['name'] == 'CT_a']['beta'].max()
-    elif CT_a_distribution == 'Normal':
-        CT_a_mu = uncertainty_db[uncertainty_db['name'] == 'CT_a']['mu/c'].max()
-        CT_a_stdv = uncertainty_db[uncertainty_db['name'] == 'CT_a']['stdv/alpha'].max()
-
-    if Subst_n_distribution == 'Beta':
-        Subst_n_c = uncertainty_db[uncertainty_db['name'] == 'Subst_n']['mu/c'].max()
-        Subst_n_alpha = uncertainty_db[uncertainty_db['name'] == 'Subst_n']['stdv/alpha'].max()
-        Subst_n_beta = uncertainty_db[uncertainty_db['name'] == 'Subst_n']['beta'].max()
-    elif Subst_n_distribution == 'Normal':
-        Subst_n_mu = uncertainty_db[uncertainty_db['name'] == 'Subst_n']['mu/c'].max()
-        Subst_n_stdv = uncertainty_db[uncertainty_db['name'] == 'Subst_n']['stdv/alpha'].max()
-
-    if ELEC_PRICE_distribution == 'Beta':
-        ELEC_PRICE_c = uncertainty_db[uncertainty_db['name'] == 'ELEC_PRICE']['mu/c'].max()
-        ELEC_PRICE_alpha = uncertainty_db[uncertainty_db['name'] == 'ELEC_PRICE']['stdv/alpha'].max()
-        ELEC_PRICE_beta = uncertainty_db[uncertainty_db['name'] == 'ELEC_PRICE']['beta'].max()
-    elif ELEC_PRICE_distribution == 'Normal':
-        ELEC_PRICE_mu = uncertainty_db[uncertainty_db['name'] == 'ELEC_PRICE']['mu/c'].max()
-        ELEC_PRICE_stdv = uncertainty_db[uncertainty_db['name'] == 'ELEC_PRICE']['stdv/alpha'].max()
-
-    if NG_PRICE_distribution == 'Beta':
-        NG_PRICE_c = uncertainty_db[uncertainty_db['name'] == 'NG_PRICE']['mu/c'].max()
-        NG_PRICE_alpha = uncertainty_db[uncertainty_db['name'] == 'NG_PRICE']['stdv/alpha'].max()
-        NG_PRICE_beta = uncertainty_db[uncertainty_db['name'] == 'NG_PRICE']['beta'].max()
-    elif NG_PRICE_distribution == 'Normal':
-        NG_PRICE_mu = uncertainty_db[uncertainty_db['name'] == 'NG_PRICE']['mu/c'].max()
-        NG_PRICE_stdv = uncertainty_db[uncertainty_db['name'] == 'NG_PRICE']['stdv/alpha'].max()
-
-    if BG_PRICE_distribution == 'Beta':
-        BG_PRICE_c = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['mu/c'].max()
-        BG_PRICE_alpha = uncertainty_db[uncertainty_db['name'] == 'BG_PRICE']['stdv/alpha'].max()
-        BG_PRICE_beta = uncertainty_db[uncertainty_db['name'] == 'BG_PRICE']['beta'].max()
-    elif BG_PRICE_distribution == 'Normal':
-        BG_PRICE_mu = uncertainty_db[uncertainty_db['name'] == 'BG_PRICE']['mu/c'].max()
-        BG_PRICE_stdv = uncertainty_db[uncertainty_db['name'] == 'BG_PRICE']['stdv/alpha'].max()
-
-    if Subst_i_distribution == 'Beta':
-        Subst_i_c = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['mu/c'].max()
-        Subst_i_alpha = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['stdv/alpha'].max()
-        Subst_i_beta = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['beta'].max()
-    elif Subst_i_distribution == 'Normal':
-        Subst_i_mu = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['mu/c'].max()
-        Subst_i_stdv = uncertainty_db[uncertainty_db['name'] == 'Subst_i']['stdv/alpha'].max()
-
-    if FC_i_distribution == 'Beta':
-        FC_i_c = uncertainty_db[uncertainty_db['name'] == 'FC_i']['mu/c'].max()
-        FC_i_alpha = uncertainty_db[uncertainty_db['name'] == 'FC_i']['stdv/alpha'].max()
-        FC_i_beta = uncertainty_db[uncertainty_db['name'] == 'FC_i']['beta'].max()
-    elif FC_i_distribution == 'Normal':
-        FC_i_mu = uncertainty_db[uncertainty_db['name'] == 'FC_i']['mu/c'].max()
-        FC_i_stdv = uncertainty_db[uncertainty_db['name'] == 'FC_i']['stdv/alpha'].max()
-
-    if HP_i_distribution == 'Beta':
-        HP_i_c = uncertainty_db[uncertainty_db['name'] == 'HP_i']['mu/c'].max()
-        HP_i_alpha = uncertainty_db[uncertainty_db['name'] == 'HP_i']['stdv/alpha'].max()
-        HP_i_beta = uncertainty_db[uncertainty_db['name'] == 'HP_i']['beta'].max()
-    elif HP_i_distribution == 'Normal':
-        HP_i_mu = uncertainty_db[uncertainty_db['name'] == 'HP_i']['mu/c'].max()
-        HP_i_stdv = uncertainty_db[uncertainty_db['name'] == 'HP_i']['stdv/alpha'].max()
-
-    if Boiler_i_distribution == 'Beta':
-        Boiler_i_c = uncertainty_db[uncertainty_db['name'] == 'Boiler_i']['mu/c'].max()
-        Boiler_i_alpha = uncertainty_db[uncertainty_db['name'] == 'Boiler_i']['stdv/alpha'].max()
-        Boiler_i_beta = uncertainty_db[uncertainty_db['name'] == 'Boiler_i']['beta'].max()
-    elif Boiler_i_distribution == 'Normal':
-        Boiler_i_mu = uncertainty_db[uncertainty_db['name'] == 'Boiler_i']['mu/c'].max()
-        Boiler_i_stdv = uncertainty_db[uncertainty_db['name'] == 'Boiler_i']['stdv/alpha'].max()
-
-    HP_n = []
-    GHP_nHP = []
-    Boiler_n = []
-    CC_n = []
-    FC_n = []
-    PVT_n = []
-    CT_a = []
-    Subst_n = []
-    ELEC_PRICE = []
-    NG_PRICE = []
-    BG_PRICE = []
-    Subst_i = []
-    FC_i = []
-    HP_i = []
-    Boiler_i = []
-
-    for i in xrange(500):
-        if HP_n_distribution == 'Beta':
-            HP_n.append(HP_n_c * np.random.beta(HP_n_alpha, HP_n_beta, size = None))
-        elif HP_n_distribution == 'Normal':
-            HP_n.append(np.random.normal(loc=HP_n_mu, scale=HP_n_stdv))
-
-        if GHP_nHP_distribution == 'Beta':
-            GHP_nHP.append(GHP_nHP_c * np.random.beta(GHP_nHP_alpha, GHP_nHP_beta, size = None))
-        elif GHP_nHP_distribution == 'Normal':
-            GHP_nHP.append(np.random.normal(loc=GHP_nHP_mu, scale=GHP_nHP_stdv))
-
-        if Boiler_n_distribution == 'Beta':
-            Boiler_n.append(Boiler_n_c * np.random.beta(Boiler_n_alpha, Boiler_n_beta, size = None))
-        elif Boiler_n_distribution == 'Normal':
-            Boiler_n.append(np.random.normal(loc=Boiler_n_mu, scale=Boiler_n_stdv))
-
-        if CC_n_distribution == 'Beta':
-            CC_n.append(CC_n_c * np.random.beta(CC_n_alpha, CC_n_beta, size = None))
-        elif CC_n_distribution == 'Normal':
-            CC_n.append(np.random.normal(loc=CC_n_mu, scale=CC_n_stdv))
-
-        if FC_n_distribution == 'Beta':
-            FC_n.append(FC_n_c * np.random.beta(FC_n_alpha, FC_n_beta, size = None))
-        elif FC_n_distribution == 'Normal':
-            FC_n.append(np.random.normal(loc=FC_n_mu, scale=FC_n_stdv))
-
-        if PVT_n_distribution == 'Beta':
-            PVT_n.append(PVT_n_c * np.random.beta(PVT_n_alpha, PVT_n_beta, size = None))
-        elif PVT_n_distribution == 'Normal':
-            PVT_n.append(np.random.normal(loc=PVT_n_mu, scale=PVT_n_stdv))
-
-        if CT_a_distribution == 'Beta':
-            CT_a.append(CT_a_c * np.random.beta(CT_a_alpha, CT_a_beta, size = None))
-        elif CT_a_distribution == 'Normal':
-            CT_a.append(np.random.normal(loc=CT_a_mu, scale=CT_a_stdv))
-
-        if Subst_n_distribution == 'Beta':
-            Subst_n.append(Subst_n_c * np.random.beta(Subst_n_alpha, Subst_n_beta, size = None))
-        elif Subst_n_distribution == 'Normal':
-            Subst_n.append(np.random.normal(loc=Subst_n_mu, scale=Subst_n_stdv))
-
-        if ELEC_PRICE_distribution == 'Beta':
-            ELEC_PRICE.append(ELEC_PRICE_c * np.random.beta(ELEC_PRICE_alpha, ELEC_PRICE_beta, size = None))
-        elif ELEC_PRICE_distribution == 'Normal':
-            ELEC_PRICE.append(np.random.normal(loc=ELEC_PRICE_mu, scale=ELEC_PRICE_stdv))
-
-        if NG_PRICE_distribution == 'Beta':
-            NG_PRICE.append(NG_PRICE_c * np.random.beta(NG_PRICE_alpha, NG_PRICE_beta, size = None))
-        elif NG_PRICE_distribution == 'Normal':
-            NG_PRICE.append(np.random.normal(loc=NG_PRICE_mu, scale=NG_PRICE_stdv))
-
-        if BG_PRICE_distribution == 'Beta':
-            BG_PRICE.append(BG_PRICE_c * np.random.beta(BG_PRICE_alpha, BG_PRICE_beta, size = None))
-        elif BG_PRICE_distribution == 'Normal':
-            BG_PRICE.append(np.random.normal(loc=BG_PRICE_mu, scale=BG_PRICE_stdv))
-
-        if Subst_i_distribution == 'Beta':
-            Subst_i.append(Subst_i_c * np.random.beta(Subst_i_alpha, Subst_i_beta, size = None))
-        elif Subst_i_distribution == 'Normal':
-            Subst_i.append(np.random.normal(loc=Subst_i_mu, scale=Subst_i_stdv))
-
-        if FC_i_distribution == 'Beta':
-            FC_i.append(FC_i_c * np.random.beta(FC_i_alpha, FC_i_beta, size=None))
-        elif FC_i_distribution == 'Normal':
-            FC_i.append(np.random.normal(loc=FC_i_mu, scale=FC_i_stdv))
-
-        if HP_i_distribution == 'Beta':
-            HP_i.append(HP_i_c * np.random.beta(HP_i_alpha, HP_i_beta, size=None))
-        elif HP_i_distribution == 'Normal':
-            HP_i.append(np.random.normal(loc=HP_i_mu, scale=HP_i_stdv))
-
-        if Boiler_i_distribution == 'Beta':
-            Boiler_i.append(Boiler_i_c * np.random.beta(Boiler_i_alpha, Boiler_i_beta, size=None))
-        elif Boiler_i_distribution == 'Normal':
-            Boiler_i.append(np.random.normal(loc=Boiler_i_mu, scale=Boiler_i_stdv))
-
-    table = [HP_n, GHP_nHP, Boiler_n, CC_n, FC_n, PVT_n, CT_a, Subst_n, ELEC_PRICE, NG_PRICE, BG_PRICE, Subst_i, FC_i, HP_i, Boiler_i]
-    df = pd.DataFrame(table)
-    df = df.transpose()
-    cols = ['WHP', 'GHP', 'Boiler', 'CC', 'FC', 'PVT', 'CT', 'Substation', 'EP', 'NG', 'BG', 'Subst_IR', 'FC_IR', 'HP_IR', 'Boiler_IR']
-    df.columns = cols
-    print (df)
-    locator.get_uncertainty_parameters()
-    df.to_csv(locator.get_optimization_master_results_folder() + "\uncertainty.csv")
-
+   # Importing the excel sheet containing the uncertainty distributions
+    locator = cea.inputlocator.InputLocator(scenario_path)
+    variable_uncertainty_distribution_assignment(locator, number_of_uncertain_scenarios, variable_groups=('ECONOMIC',) )
 
     print 'Uncertain Parameters have been generated for the given economic scenarios'
 
@@ -278,4 +59,5 @@ if __name__ == '__main__':
     import cea.globalvar
     gv = cea.globalvar.GlobalVariables()
     scenario_path = gv.scenario_reference
-    run_as_script(scenario_path)
+    number_of_uncertain_scenarios = 1000  # The total number of scenarios to be generated
+    run_as_script(scenario_path, number_of_uncertain_scenarios)
