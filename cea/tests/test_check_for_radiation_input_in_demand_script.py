8bf5c8866cc3a00fadd9fde6c455fab3eb299807
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-04-19 16:31:50 +0800

    fixing unitest path

--- cea/tests/test_check_for_radiation_input_in_demand_script.py
+++ cea/tests/test_check_for_radiation_input_in_demand_script.py
@@ -19,9 +19,9 @@ class TestCheckForRadiationInputInDemandScript(unittest.TestCase):
         in the `../examples` folder as a zip file (`ninecubes.zip`) and has to be extracted first.
         """
         import zipfile
-        import examples
+        import cea.examples
         import tempfile
-        archive = zipfile.ZipFile(os.path.join(os.path.dirname(examples.__file__), 'reference-case-open.zip'))
+        archive = zipfile.ZipFile(os.path.join(os.path.dirname(cea.examples.__file__), 'reference-case-open.zip'))
         archive.extractall(tempfile.gettempdir())
         cls.reference_case = os.path.join(tempfile.gettempdir(), 'reference-case-open', 'baseline')
 
