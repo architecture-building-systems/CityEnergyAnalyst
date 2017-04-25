fd08c2ca99c980d3445dbfe72bb4f600adbcf783
Daren Thomas <thomas@arch.ethz.ch>
2017-04-25 09:52:13 +0200

    removing the env creation from the batch file as it can't ever work.

--- install-develop.bat
+++ install-develop.bat
@@ -1,5 +1,3 @@
-conda create -n cea python=2.7
-activate cea
 conda install -c conda-forge -y geopandas ephem
 conda install -c dlr-sc tbb freeimageplus gl2ps
 conda install --c oce -c pythonocc -y pythonocc-core=0.17.3
