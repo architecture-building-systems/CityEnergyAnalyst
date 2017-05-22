48e1db4d87a780934e5ea91ba4c4e4d5c638bc73
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-05-22 17:27:12 +0800

    chanign settings to one single point in the radiation

--- cea/resources/radiation_daysim/settings.py
+++ cea/resources/radiation_daysim/settings.py
@@ -23,8 +23,8 @@ RAD_PARMS = {
 }
 # Daysim Sensor parameters
 SEN_PARMS = {
-'X_DIM': 100,
-'Y_DIM': 100,
+'X_DIM': 100, # aximum so there is only one point per surface
+'Y_DIM': 100, # maximum so there is only one point per surface
 'MIN_Z_DIR': -0.85,
 'MAX_Z_DIR': 0.05,
 }
@@ -32,4 +32,4 @@ SEN_PARMS = {
 TERRAIN_PARAMS = {'e_terrain': 0.8}
 
 # simulation parameters
-SIMUL_PARAMS = {'n_build_in_chunk':5} # min number of buildings for multiprocessing # limit the number if running out of memory
+SIMUL_PARAMS = {'n_build_in_chunk':50} # min number of buildings for multiprocessing # limit the number if running out of memory
