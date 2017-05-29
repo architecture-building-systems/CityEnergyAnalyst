6d521b317efca3bae28ec090d5542155f1a3ee70
martin-mosteiro <mosteiro@arch.ethz.ch>
2017-05-29 18:22:51 +0200

    Creating unit tests

--- cea/demand/preprocessing/properties.py
+++ cea/demand/preprocessing/properties.py
@@ -15,13 +15,12 @@ import cea.inputlocator
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
-__credits__ = ["Jimeno A. Fonseca", "Daren Thomas"]
+__credits__ = ["Jimeno A. Fonseca", "Daren Thomas", "Martin Mosteiro"]
 __license__ = "MIT"
 __version__ = "0.1"
 __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
-
 
 def properties(locator, prop_architecture_flag, prop_hvac_flag, prop_comfort_flag, prop_internal_loads_flag):
     """
@@ -179,7 +178,7 @@ def calc_category(archetype_DB, age):
                                          (archetype_DB['standard'] == 'C')].Code.values[0])
     return category
 
-def correct_archetype_areas(prop_architecture_df, architecture_DB, list_uses, categories_df):
+def correct_archetype_areas(prop_architecture_df, architecture_DB, list_uses):
     """
         Corrects the heated area 'Hs' for buildings with multiple uses.
 
--- cea/tests/test_schedules.py
+++ cea/tests/test_schedules.py
@@ -1,6 +1,33 @@
 import unittest
 
 
+class TestBuildingPreprocessing(unittest.TestCase):
+    def test_mixed_use_archetype_values(self):
+        # test if a sample mixed use building gets standard results
+        from cea.demand.preprocessing.properties import calculate_average_multiuse
+        from cea.demand.preprocessing.properties import correct_archetype_areas
+        import pandas as pd
+
+        locator = cea.inputlocator.InputLocator(scenario_path=cea.globalvar.GlobalVariables().scenario_reference)
+
+        self.assert(pd.DataFrame(data=np.array(['B1', 0.5, 0.5, 208.94736, 12.9],['B2', 0.75, 0.25, 14.4, 164.48275],
+                                               columns=['Name', 'OFFICE', 'GYM', 'X_ghp', 'El_Wm2']),
+                                 calculate_average_multiuse,
+                                 properties_df = pd.DataFrame(data=np.array(['B1', 0.5, 0.5, 0, 0],
+                                                                            ['B2', 0.25, 0.75, 0, 0]),
+                                                              columns=['Name','OFFICE', 'GYM', 'X_ghp', 'El_Wm2']),
+                                 occupant_densities = {'OFFICE': 0.071428571,'GYM': 0.2}, list_uses = ['OFFICE', 'GYM'],
+                                 properties_DB = get_database(locator.get_archetypes_properties(), 'INTERNAL_LOADS')
+
+        self.assert(pd.DataFrame(data=np.array(['B1', 0.5, 0.5, 0.5],['B2', 0.75, 0.25, 0.75],
+                                               columns=['Name', 'SERVERROOM', 'PARKING', 'Hs']),
+                                 correct_archetype_areas,
+                                 prop_architecture_df = pd.DataFrame(
+                                     data=np.array(['B1', 0.5, 0.5, 0.5], ['B2', 0.25, 0.75, 0.25]),
+                                     columns=['Name','SERVERROOM', 'PARKING', 'Hs']),
+                                 architecture_DB = get_database(locator.get_archetypes_properties(), 'ARCHITECTURE'),
+                                 list_uses = ['OFFICE', 'GYM']))
+
 class TestScheduleCreation(unittest.TestCase):
     def test_mixed_use_schedules(self):
         from cea.demand.occupancy_model import calc_schedules
