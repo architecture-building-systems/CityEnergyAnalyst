41d43500e9aae4e1107de9fe7cd08cffdc4186fd
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-05-29 18:13:50 +0800

    clean and running

--- cea/analysis/retrofit/retrofit_potential.py
+++ cea/analysis/retrofit/retrofit_potential.py
@@ -51,38 +51,37 @@ def retrofit_main(locator_baseline, name_new_scenario, age_retrofit, age_crit, e
     selection_names =[] #list to store names of selected buildings to retrofit
     #CASE 1
     for criteria in age_crit:
-        if criteria[0]:
-            age_difference = age_retrofit - criteria[1]
+        if criteria[1][0]:
+            age_difference = age_retrofit - criteria[1][1]
             age_df = dbfreader.dbf2df(locator_baseline.get_building_age())
-            selection_names.append(("Crit_age",age_filter_HVAC(age_df, age_difference)))
+            selection_names.append(("Crit_"+criteria[0],age_filter_HVAC(age_df, age_difference)))
 
     #CASE 2
     for criteria in eui_crit:
-        if criteria[0]:
+        if criteria[1][0]:
             demand_totals = pd.read_csv(locator_baseline.get_total_demand())
-            selection_names.append(("Crit_eui_"+criteria[1], eui_filter_HVAC(demand_totals, criteria[1],
-                                                                             criteria[2])))
+            selection_names.append(("Crit_eui_"+criteria[0], eui_filter_HVAC(demand_totals, criteria[0], criteria[1][1])))
 
     #CASE 3
     for criteria in op_costs_crit:
-        if criteria[0]:
-            costs_totals = pd.read_csv(locator_baseline.get_costs_operation_file(criteria[1]))
-            selection_names.append(("Crit_cost_"+criteria[1],emissions_filter_HVAC(costs_totals, criteria[1]+"_cost_m2",
-                                                                           criteria[2])))
+        if criteria[1][0]:
+            costs_totals = pd.read_csv(locator_baseline.get_costs_operation_file(criteria[0]))
+            selection_names.append(("Crit_cost_"+criteria[0],emissions_filter_HVAC(costs_totals, criteria[0]+"_cost_m2",
+                                                                                   criteria[1][1])))
 
     # CASE 4
     for criteria in losses_crit:
-        if criteria[0]:
+        if criteria[2][0]:
             demand_totals = pd.read_csv(locator_baseline.get_total_demand())
-            selection_names.append(("Crit_loss_"+criteria[1], losses_filter_HVAC(demand_totals, criteria[1],
-                                                                         criteria[2],
-                                                                         criteria[3])))
+            selection_names.append(("Crit_loss_"+criteria[0], losses_filter_HVAC(demand_totals, criteria[0],
+                                                                         criteria[1],
+                                                                         criteria[2][1])))
     #CASE 5
     for criteria in LCA_crit:
-        if criteria[0]:
+        if criteria[1][0]:
             emissions_totals = pd.read_csv(locator_baseline.get_lca_operation())
-            selection_names.append(("Crit_ghg_tot",emissions_filter_HVAC(emissions_totals, criteria[1],
-                                                                         criteria[2])))
+            selection_names.append(("Crit_"+criteria[0],emissions_filter_HVAC(emissions_totals, criteria[0],
+                                                                              criteria[1][1])))
 
     #appending all the resutls
     counter = 0
@@ -97,7 +96,8 @@ def retrofit_main(locator_baseline, name_new_scenario, age_retrofit, age_crit, e
         counter+=1
 
     # fill with FALSE for those buildings that do not comply the criteria
-    data.fillna(value="FALSE").to_csv(locator_baseline.get_retrofit_filters(name_new_scenario))
+    data.fillna(value="FALSE", inplace=True)
+     #   .to_csv(locator_baseline.get_retrofit_filters(name_new_scenario))
 
     # Create a retrofit case with the buildings that pass the criteria
     retrofit_scenario_creator(locator_baseline.scenario_path, os.path.join(locator_baseline.case_study_path,name_new_scenario),
@@ -110,23 +110,36 @@ def retrofit_scenario_creator(src, dst, data, symlinks=False, ignore=None):
     :return:
     """
 
-    #Create new folder and trow eeror if already existing,
+    #Create new folder and trow eeror if already existing
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
     shutil.copytree(src, dst, symlinks, ignore)
 
     #Get locator
     locator_new_scenario = cea.inputlocator.InputLocator(scenario_path=dst)
 
-    #Import properties buidlings
+    #Import properties buidlings and export just selectred buildings + criteria
+
     geometry = gdf.from_file(locator_new_scenario.get_building_geometry())
     geometry.merge(data, on='Name').to_file(locator_new_scenario.get_building_geometry())
-    # age = locator_new_scenario.get_building_age()
-    # architecture = locator_new_scenario.get_building_architecture()
-    # comfort = locator_new_scenario.get_building_comfort()
-    # internal_loads = locator_new_scenario.get_building_internal()
-    # hvac = locator_new_scenario.get_building_hvac()
-    # supply = locator_new_scenario.get_supply_systems_database()
 
+    age = dbfreader.dbf2df(locator_new_scenario.get_building_age())
+    dbfreader.df2dbf(age.merge(data, on='Name'), locator_new_scenario.get_building_age())
 
+    architecture = dbfreader.dbf2df(locator_new_scenario.get_building_architecture())
+    dbfreader.df2dbf(architecture.merge(data, on='Name'), locator_new_scenario.get_building_architecture())
+
+    comfort = dbfreader.dbf2df(locator_new_scenario.get_building_comfort())
+    dbfreader.df2dbf(comfort.merge(data, on='Name'), locator_new_scenario.get_building_comfort())
+
+    internal_loads = dbfreader.dbf2df(locator_new_scenario.get_building_internal())
+    dbfreader.df2dbf(internal_loads.merge(data, on='Name'), locator_new_scenario.get_building_internal())
+
+    hvac = dbfreader.dbf2df(locator_new_scenario.get_building_hvac())
+    dbfreader.df2dbf(hvac.merge(data, on='Name'), locator_new_scenario.get_building_hvac())
+
+    supply = dbfreader.dbf2df(locator_new_scenario.get_building_supply())
+    dbfreader.df2dbf(supply.merge(data, on='Name'), locator_new_scenario.get_building_supply())
 
 
 def run_as_script(scenario_path=None):
@@ -144,32 +157,41 @@ def run_as_script(scenario_path=None):
     age_criteria = [True, 15] #[true or false, threshold]
 
     #CRITERIA UNERGY USE INTENSITY
-    eui_heating_criteria = [True, "Qhsf_MWhyr", 50] #[true or false, load to verify, threshold]
-    eui_hotwater_criteria = [True, "Qwwf_MWhyr", 50]  # [true or false, load to verify, threshold]
-    eui_cooling_criteria = [True, "Qcsf_MWhyr", 4]  # [true or false, load to verify, threshold]
-    eui_electricity_criteria = [False, "Ef_MWhyr", 20]  # [true or false, load to verify, threshold]
+    eui_heating_criteria = [True,  50] #[true or false, load to verify, threshold]
+    eui_hotwater_criteria = [False, 50]  # [true or false, load to verify, threshold]
+    eui_cooling_criteria = [False, 4]  # [true or false, load to verify, threshold]
+    eui_electricity_criteria = [False, 20]  # [true or false, load to verify, threshold]
 
     #CRITERIA EMISSIONS
-    emissions_operation_criteria = [False, "O_ghg_ton", 30] #[true or false, threshold]
+    emissions_operation_criteria = [False, 30] #[true or false, threshold]
 
     #CRITERIA COSTS
-    heating_costs_criteria = [True, "Qhsf", 2] #[true or false, threshold]
-    hotwater_costs_criteria = [True, "Qwwf", 2]  # [true or false, threshold]
-    cooling_costs_criteria = [True, "Qcsf", 2]  # [true or false, threshold]
-    electricity_costs_criteria = [False, "Ef", 2]  # [true or false, threshold]
+    heating_costs_criteria = [True,  2] #[true or false, threshold]
+    hotwater_costs_criteria = [False,  2]  # [true or false, threshold]
+    cooling_costs_criteria = [False,  2]  # [true or false, threshold]
+    electricity_costs_criteria = [False, 2]  # [true or false, threshold]
 
     #CASE OF THERMAL LOSSES
-    heating_losses_criteria = [True, "Qhsf_MWhyr", "Qhs_MWhyr", 15]  # [true or false, threshold]
-    hotwater_losses_criteria = [True, "Qwwf_MWhyr", "Qww_MWhyr", 15]  # [true or false, threshold]
-    cooling_losses_criteria = [True, "Qcsf_MWhyr", "Qcs_MWhyr", 15]   # [true or false, threshold]
+    heating_losses_criteria = [True, 15]  # [true or false, threshold]
+    hotwater_losses_criteria = [False, 15]  # [true or false, threshold]
+    cooling_losses_criteria = [False, 15]   # [true or false, threshold]
 
     ##PROCESS
-    age_crit = [age_criteria]
-    eui_crit = [eui_heating_criteria, eui_hotwater_criteria, eui_cooling_criteria, eui_electricity_criteria]
-    LCA_crit = [emissions_operation_criteria]
-    op_costs_crit = [heating_costs_criteria, hotwater_costs_criteria, cooling_costs_criteria, electricity_costs_criteria]
-    losses_crit = [heating_losses_criteria, hotwater_losses_criteria, cooling_losses_criteria]
-    retrofit_main(locator_baseline=locator_baseline, name_new_scenario=name_new_scenario, age_retrofit=age_retrofit, age_crit=age_crit, eui_crit=eui_crit,
+    age_crit = [["age", age_criteria]]
+    eui_crit = [["Qhsf_MWhyr", eui_heating_criteria],
+                ["Qwwf_MWhyr", eui_hotwater_criteria],
+                ["Qcsf_MWhyr", eui_cooling_criteria],
+                ["Ef_MWhyr", eui_electricity_criteria]]
+    LCA_crit = [["O_ghg_ton", emissions_operation_criteria]]
+    op_costs_crit = [["Qhsf", heating_costs_criteria],
+                     ["Qwwf", hotwater_costs_criteria],
+                     ["Qcsf", cooling_costs_criteria],
+                     ["Ef", electricity_costs_criteria]]
+    losses_crit = [["Qhsf_MWhyr", "Qhs_MWhyr", heating_losses_criteria],
+                   ["Qwwf_MWhyr", "Qww_MWhyr", hotwater_losses_criteria],
+                   ["Qcsf_MWhyr", "Qcs_MWhyr", cooling_losses_criteria]]
+    retrofit_main(locator_baseline=locator_baseline, name_new_scenario=name_new_scenario, age_retrofit=age_retrofit,
+                  age_crit=age_crit, eui_crit=eui_crit,
                   LCA_crit=LCA_crit,
                   op_costs_crit=op_costs_crit, losses_crit=losses_crit)
 
