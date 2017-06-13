52182b3c36b6980bf5c9031a0a247d570058921d
Daren Thomas <thomas@arch.ethz.ch>
2017-06-13 16:00:10 +0200

    CLI now supports option to demand: --use-dynamic-infiltration-calculation

--- cea/cli.py
+++ cea/cli.py
@@ -26,7 +26,9 @@ def demand(args):
             args.weather = cea.inputlocator.InputLocator(None).get_weather(args.weather)
         except:
             pass
-    cea.demand.demand_main.run_as_script(scenario_path=args.scenario, weather_path=args.weather)
+    print 'use_dynamic_infiltration_calculation:', args.use_dynamic_infiltration_calculation
+    cea.demand.demand_main.run_as_script(scenario_path=args.scenario, weather_path=args.weather,
+                                         use_dynamic_infiltration_calculation=args.use_dynamic_infiltration_calculation)
 
 
 def data_helper(args):
@@ -255,6 +257,8 @@ def main():
     demand_parser = subparsers.add_parser('demand',
                                           formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     demand_parser.add_argument('-w', '--weather', help='Path to the weather file')
+    demand_parser.add_argument('--use-dynamic-infiltration-calculation', action='store_true',
+                               help='Use the dynamic infiltration calculation instead of default')
     demand_parser.set_defaults(func=demand)
 
     data_helper_parser = subparsers.add_parser('data-helper',
