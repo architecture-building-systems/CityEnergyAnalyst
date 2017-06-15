b5d529004c32966df220e5eb0c4854dba372a6ee
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-06-15 11:34:31 +0800

    standard locator for all files

--- cea/inputlocator.py
+++ cea/inputlocator.py
@@ -368,11 +368,8 @@ class InputLocator(object):
         return os.path.join(self.get_solar_radiation_folder(), '%s_insolation_Whm2.json' %building_name)
 
     def get_radiation_metadata(self, building_name):
-        """scenario/outputs/data/solar-radiation/radiation.csv"""
-        solar_radiation_folder = os.path.join(self.scenario_path, 'outputs', 'data', 'solar-radiation')
-        if not os.path.exists(solar_radiation_folder):
-            os.makedirs(solar_radiation_folder)
-        return os.path.join(solar_radiation_folder, '%s_geometry.csv' %building_name)
+        """scenario/outputs/data/solar-radiation/{building_name}_geometrgy.csv"""
+        return os.path.join(self.get_solar_radiation_folder(), '%s_geometry.csv' %building_name)
 
     def get_building_list(self):
         """scenario/outputs/data/solar-radiation/radiation.csv"""
