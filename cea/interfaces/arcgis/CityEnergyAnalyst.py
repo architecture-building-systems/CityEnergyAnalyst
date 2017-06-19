b0cf7158810439c78599c3eae6747e60d95ae995
Daren Thomas <thomas@arch.ethz.ch>
2017-06-19 17:12:24 +0200

    implemented arcgis parameter config for emissions operation

--- cea/interfaces/arcgis/CityEnergyAnalyst.py
+++ cea/interfaces/arcgis/CityEnergyAnalyst.py
@@ -87,11 +87,11 @@ class DemandTool(object):
             for p in parameters[1:]:
                 p.enabled = True
             parameters = {p.name: p for p in parameters}
-            flags = ConfigurationStore().read(scenario_path, 'demand')
-            if flags:
-                for key in flags.keys():
+            previous_run = ConfigurationStore().read(scenario_path, 'demand')
+            if previous_run:
+                for key in previous_run.keys():
                     p = parameters[key]
-                    p.value = flags[key]
+                    p.value = previous_run[key]
             parameters['weather_path'].enabled = parameters['weather_name'].value == '<choose path from below>'
         else:
             parameters = {p.name: p for p in parameters}
@@ -286,82 +286,88 @@ class EmissionsTool(object):
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Qww_flag.value = True
         Qhs_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to space heating.",
             name="Qhs_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Qhs_flag.value = True
         Qcs_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to space cooling.",
             name="Qcs_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Qcs_flag.value = True
         Qcdata_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to servers cooling.",
             name="Qcdata_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Qcdata_flag.value = True
         Qcrefri_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to refrigeration.",
             name="Qcrefri_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Qcrefri_flag.value = True
         Eal_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to appliances and lighting.",
             name="Eal_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Eal_flag.value = True
         Eaux_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to auxiliary electricity.",
             name="Eaux_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Eaux_flag.value = True
         Epro_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to electricity in industrial processes.",
             name="Epro_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Epro_flag.value = True
         Edata_flag = arcpy.Parameter(
             displayName="Create a separate file with emissions due to electricity consumption in data centers.",
             name="Edata_flag",
             datatype="GPBoolean",
             parameterType="Required",
             direction="Input")
-        Edata_flag.value = True
 
         return [scenario_path, Qww_flag, Qhs_flag, Qcs_flag, Qcdata_flag, Qcrefri_flag, Eal_flag, Eaux_flag, Epro_flag,
                 Edata_flag]
 
-    def execute(self, parameters, _):
+    def updateParameters(self, parameters):
         scenario_path = parameters[0].valueAsText
-        flags = {
-            'Qww': parameters[1].value,
-            'Qhs': parameters[2].value,
-            'Qcs': parameters[3].value,
-            'Qcdata': parameters[4].value,
-            'Qcrefri': parameters[5].value,
-            'Eal': parameters[6].value,
-            'Eaux': parameters[7].value,
-            'Epro': parameters[8].value,
-            'Edata': parameters[9].value,
-        }
+        if scenario_path is None:
+            for p in parameters[1:]:
+                p.enabled = False
+            return
+        if not os.path.exists(scenario_path):
+            for p in parameters[1:]:
+                p.enabled = False
+            parameters[0].setErrorMessage('Scenario folder not found: %s' % scenario_path)
+            return
+        if not parameters[1].enabled:
+            for p in parameters[1:]:
+                p.enabled = True
+            parameters = {p.name: p for p in parameters}
+            previous_run = ConfigurationStore().read(scenario_path, 'emissions')
+            if previous_run:
+                for key in previous_run.keys():
+                    p = parameters[key + '_flag']
+                    p.value = previous_run[key]
+
+
+    def execute(self, parameters, _):
+        parameters = {p.name: p for p in parameters}
+        scenario_path = parameters['scenario_path'].valueAsText
+        flags = {key.split('_')[0]: parameter.value for key, parameter in parameters.items()
+                 if not key == 'scenario_path'}
         extra_files_to_create = [key for key in flags if flags[key]]
         run_cli(scenario_path, 'emissions', '--extra-files-to-create', *extra_files_to_create)
+        ConfigurationStore().write(scenario_path, 'emissions', flags)
 
 
 class EmbodiedEnergyTool(object):
