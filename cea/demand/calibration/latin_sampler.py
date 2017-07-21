60ba2617aaa83798f4c6734fab2e5f0353dac0a0
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-07-21 10:59:56 +0800

    sampler ready

--- cea/demand/calibration/latin_sampler.py
+++ cea/demand/calibration/latin_sampler.py
@@ -11,21 +11,22 @@ def latin_sampler(locator, num_samples, variables, variable_groups = ('ENVELOPE'
     # get probability density function PDF of variables of interest
     database = pd.concat([pd.read_excel(locator.get_uncertainty_db(), group, axis=1)
                                                 for group in variable_groups])
-    pdf_list = database[database['name'].isin(variables)].reset_index()
+    pdf_list = database[database['name'].isin(variables)].set_index('name')
 
     # get number of variables
     num_vars = pdf_list.shape[0] #alternatively use len(variables)
 
     # get design of experiments
     design = lhs(num_vars, samples=num_samples)
-    for i, pdf in enumerate(pdf_list.itertuples()):
-        min = pdf.min
-        max = pdf.max
-        mu = pdf.mu
-        stdv = pdf.stdv
-        if pdf.distribution == 'triangular':
+    for i, variable in enumerate(variables):
+        distribution = pdf_list.loc[variable, 'distribution']
+        min = pdf_list.loc[variable,'min']
+        max = pdf_list.loc[variable,'min']
+        mu = pdf_list.loc[variable,'mu']
+        stdv = pdf_list.loc[variable,'stdv']
+        if distribution == 'triangular':
             design[:, i] = triang(loc=min, c=mu, scale=max).ppf(design[:, i])
-        elif pdf.distribution == 'normal':
+        elif distribution == 'normal':
             design[:, i] = norm(loc=mu, scale=stdv).ppf(design[:, i])
         else: # assume it is uniform
             design[:, i] = uniform(loc=min, scale=max).ppf(design[:, i])
