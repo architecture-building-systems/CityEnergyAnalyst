05b380886c93b13e00722a0b3fbd529747e4a5de
martin-mosteiro <mosteiro@arch.ethz.ch>
2017-07-28 17:48:43 +0200

    Create `run_as_script` function

--- cea/analysis/sensitivity/sensitivity_demand_samples.py
+++ cea/analysis/sensitivity/sensitivity_demand_samples.py
@@ -113,6 +113,19 @@ def sampler(method, problem, num_samples, sampler_parameters):
     else:
         raise ValueError("Sampler method unknown: %s" % method)
 
+
+def run_as_script(method, num_samples, variable_groups, sampler_parameters, samples_folder):
+
+    samples, problem_dict = create_demand_samples(method=method, num_samples=num_samples,
+                                                  variable_groups=variable_groups,
+                                                  sampler_parameters=sampler_parameters)
+
+    # save `samples.npy` and `problem.pickle` to the samples folder
+    np.save(os.path.join(samples_folder, 'samples.npy'), samples)
+    with open(os.path.join(samples_folder, 'problem.pickle'), 'w') as f:
+        pickle.dump(problem_dict, f)
+    print('created %i samples in %s' % (samples.shape[0], samples_folder))
+
 if __name__ == '__main__':
     import argparse
     import pickle
@@ -144,12 +157,4 @@ if __name__ == '__main__':
     elif args.method == 'sobol':
         sampler_params['calc_second_order'] = args.calc_second_order
 
-    samples, problem_dict = create_demand_samples(method=args.method, num_samples=args.num_samples,
-                                                  variable_groups=args.variable_groups,
-                                                  sampler_parameters=sampler_params)
-
-    # save `samples.npy` and `problem.pickle` to the samples folder
-    np.save(os.path.join(args.samples_folder, 'samples.npy'), samples)
-    with open(os.path.join(args.samples_folder, 'problem.pickle'), 'w') as f:
-        pickle.dump(problem_dict, f)
-    print('created %i samples in %s' % (samples.shape[0], args.samples_folder))
+    run_as_script(args.method, args.num_samples, args.variable_groups, sampler_params, args.samples_folder)
