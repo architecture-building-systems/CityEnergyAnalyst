9f6bb8a05ee97890bc6dba13218c469ee320ed9b
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-08-18 11:54:13 +0800

    extracting sampler

--- cea/demand/calibration/calibration_sampling.py
+++ cea/demand/calibration/calibration_sampling.py
@@ -1,13 +1,11 @@
 from __future__ import division
 import pandas as pd
-from scipy.stats import triang
-from scipy.stats import norm
-from scipy.stats import uniform
-from pyDOE import lhs
 import cea
 import numpy as np
 from cea.demand import demand_main
+from cea.demand.calibration import latin_sampler
 from geopandas import GeoDataFrame as Gdf
+
 import pickle
 import json
 
@@ -50,6 +48,7 @@ def simulate_demand_sample(locator, building_name, output_parameters):
 
     return cv_rmse, rmse
 
+
 def calc_cv_rmse(prediction, target):
     """
     This function calculates the covariance of the root square mean error between two vectors.
@@ -74,49 +73,6 @@ def calc_cv_rmse(prediction, target):
     return round(CVrmse,3), round(rmse,3) #keep only 3 significant digits
 
 
-def latin_sampler(locator, num_samples, variables):
-    """
-    This script creates a matrix of m x n samples using the latin hypercube sampler.
-    for this, it uses the database of probability distribtutions stored in locator.get_uncertainty_db()
-
-    :param locator: pointer to locator of files of CEA
-    :param num_samples: number of samples to do
-    :param variables: list of variables to sample
-    :return:
-        1. design: a matrix m x n with the samples
-        2. pdf_list: a dataframe with properties of the probability density functions used in the excercise.
-    """
-
-
-    # get probability density function PDF of variables of interest
-    variable_groups = ('ENVELOPE', 'INDOOR_COMFORT', 'INTERNAL_LOADS')
-    database = pd.concat([pd.read_excel(locator.get_uncertainty_db(), group, axis=1)
-                                                for group in variable_groups])
-    pdf_list = database[database['name'].isin(variables)].set_index('name')
-
-    # get number of variables
-    num_vars = pdf_list.shape[0] #alternatively use len(variables)
-
-    # get design of experiments
-    design = lhs(num_vars, samples=num_samples)
-    for i, variable in enumerate(variables):
-        distribution = pdf_list.loc[variable, 'distribution']
-        min = pdf_list.loc[variable,'min']
-        max = pdf_list.loc[variable,'max']
-        mu = pdf_list.loc[variable,'mu']
-        stdv = pdf_list.loc[variable,'stdv']
-        if distribution == 'triangular':
-            loc = min
-            scale = max - min
-            c = (mu - min) / (max - min)
-            design[:, i] = triang(loc=loc, c=c, scale=scale).ppf(design[:, i])
-        elif distribution == 'normal':
-            design[:, i] = norm(loc=mu, scale=stdv).ppf(design[:, i])
-        else: # assume it is uniform
-            design[:, i] = uniform(loc=min, scale=max).ppf(design[:, i])
-
-    return design, pdf_list
-
 def sampling_main(locator, variables, building_name, building_load):
     """
     This script creates samples using a lating Hypercube sample of 5 variables of interest.
@@ -140,7 +96,7 @@ def sampling_main(locator, variables, building_name, building_load):
     """
 
     # create list of samples with a LHC sampler and save to disk
-    samples, pdf_list = latin_sampler(locator, number_samples, variables)
+    samples, pdf_list = latin_sampler.latin_sampler(locator, number_samples, variables)
     np.save(locator.get_calibration_samples(building_name), samples)
 
     # create problem and save to disk as json
