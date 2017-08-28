44d7fb9148aea4547f358e297db0845da545693a
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-08-28 15:29:00 +0800

    normalizing inputs and outputs

--- cea/demand/calibration/latin_sampler.py
+++ cea/demand/calibration/latin_sampler.py
@@ -2,6 +2,7 @@ from __future__ import division
 from scipy.stats import triang
 from scipy.stats import norm
 from scipy.stats import uniform
+from sklearn import preprocessing
 from pyDOE import lhs
 import pandas as pd
 
@@ -14,45 +15,68 @@ __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
+
 def latin_sampler(locator, num_samples, variables):
     """
     This script creates a matrix of m x n samples using the latin hypercube sampler.
     for this, it uses the database of probability distribtutions stored in locator.get_uncertainty_db()
 
+
     :param locator: pointer to locator of files of CEA
     :param num_samples: number of samples to do
     :param variables: list of variables to sample
     :return:
-        1. design: a matrix m x n with the samples
+        1. design: a matrix m x n with the samples where each feature is normalized from [0,1]
+        2. design_norm: a matrix m x n with the samples where each feature is normalized from [0,1]
         2. pdf_list: a dataframe with properties of the probability density functions used in the excercise.
     """
-
 
     # get probability density function PDF of variables of interest
     variable_groups = ('ENVELOPE', 'INDOOR_COMFORT', 'INTERNAL_LOADS')
     database = pd.concat([pd.read_excel(locator.get_uncertainty_db(), group, axis=1)
-                                                for group in variable_groups])
+                          for group in variable_groups])
     pdf_list = database[database['name'].isin(variables)].set_index('name')
 
     # get number of variables
-    num_vars = pdf_list.shape[0] #alternatively use len(variables)
+    num_vars = pdf_list.shape[0]  # alternatively use len(variables)
 
     # get design of experiments
-    design = lhs(num_vars, samples=num_samples)
+    samples = lhs(num_vars, samples=num_samples)
+    samples_norm = samples.copy()
     for i, variable in enumerate(variables):
+        # normalization of data:
         distribution = pdf_list.loc[variable, 'distribution']
-        min = pdf_list.loc[variable,'min']
-        max = pdf_list.loc[variable,'max']
-        mu = pdf_list.loc[variable,'mu']
-        stdv = pdf_list.loc[variable,'stdv']
+        #sampling into lhs
+        min = pdf_list.loc[variable, 'min']
+        max = pdf_list.loc[variable, 'max']
+        mu = pdf_list.loc[variable, 'mu']
+        stdv = pdf_list.loc[variable, 'stdv']
         if distribution == 'triangular':
             loc = min
             scale = max - min
             c = (mu - min) / (max - min)
-            design[:, i] = triang(loc=loc, c=c, scale=scale).ppf(design[:, i])
+            samples[:, i] = triang(loc=loc, c=c, scale=scale).ppf(samples[:, i])
         elif distribution == 'normal':
-            design[:, i] = norm(loc=mu, scale=stdv).ppf(design[:, i])
-        else: # assume it is uniform
-            design[:, i] = uniform(loc=min, scale=max).ppf(design[:, i])
+            samples[:, i] = norm(loc=mu, scale=stdv).ppf(samples[:, i])
+        else:  # assume it is uniform
+            samples[:, i] = uniform(loc=min, scale=max).ppf(samples[:, i])
 
-    return design, pdf_list
+        #sampling into lhs normalized
+        arguments = [min, max, mu,stdv]
+        min_max_scaler = preprocessing.MinMaxScaler(copy=True, feature_range=(0, 1))
+        arguments_norm = min_max_scaler.fit_transform(arguments)
+        min = arguments_norm[0]
+        max = arguments_norm[1]
+        mu = arguments_norm[2]
+        stdv = arguments_norm[3]
+        if distribution == 'triangular':
+            loc = min
+            scale = max - min
+            c = (mu - min) / (max - min)
+            samples_norm[:, i] = triang(loc=loc, c=c, scale=scale).ppf(samples_norm[:, i])
+        elif distribution == 'normal':
+            samples_norm[:, i] = norm(loc=mu, scale=stdv).ppf(samples_norm[:, i])
+        else:  # assume it is uniform
+            samples_norm[:, i] = uniform(loc=min, scale=max).ppf(samples_norm[:, i])
+
+    return samples, samples_norm, pdf_list
