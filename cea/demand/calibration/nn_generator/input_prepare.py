50659f50fdf70018531466c4a8bfb5155eb40d44
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-09-21 11:08:29 +0800

    update

--- cea/demand/calibration/nn_generator/input_prepare.py
+++ cea/demand/calibration/nn_generator/input_prepare.py
@@ -21,7 +21,36 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
+def input_prepare_main(list_building_names, locator, target_parameters):
+    '''
+    this function prepares the final inputs and targets to be fed into the NN
+    :param list_building_names:
+    :param locator:
+    :param target_parameters:
+    :return:
+    '''
+    for counter, building_name in enumerate(list_building_names):
+        raw_nn_targets = get_cea_outputs(building_name, locator, target_parameters)
+        raw_nn_inputs = get_cea_inputs(building_name)
+        NN_input_ready, NN_target_ready = prep_NN_delay(raw_nn_inputs, raw_nn_targets, nn_delay)
+        if counter == 0:
+            urban_input_matrix = NN_input_ready
+            urban_taget_matrix = NN_target_ready
+        else:
+            urban_input_matrix = np.concatenate((urban_input_matrix, NN_input_ready))
+            urban_taget_matrix = np.concatenate((urban_taget_matrix, NN_target_ready))
+
+        print (counter)
+    return urban_input_matrix, urban_taget_matrix
+
 def prep_NN_delay(NN_input,NN_target,NN_delays):
+    '''
+    This function adds delay to the inputs
+    :param NN_input:
+    :param NN_target:
+    :param NN_delays:
+    :return:
+    '''
     input1=NN_input
     target1=NN_target
     nS, nF = input1.shape
@@ -54,15 +83,18 @@ def prep_NN_delay(NN_input,NN_target,NN_delays):
 
     return NN_input_ready , NN_target_ready
 
-def get_cea_outputs(building_name,locator):
-    target_parameters=['Qhsf', 'Qcsf', 'theta_a']
-    file_path = os.path.join(locator.get_demand_results_folder(), "%(building_name)s.xls" % locals())
-    calcs_outputs_xls = pd.read_excel(file_path)
-    temp_file = os.path.join(locator.get_temporary_folder(), "%(building_name)s.csv" % locals())
-    calcs_outputs_xls.to_csv(temp_file, index=False, header=True, float_format='%.3f', decimal='.')
-    raw_nn_targets = pd.read_csv(temp_file, usecols=target_parameters)
-    raw_nn_targets = np.array(raw_nn_targets)
 
+
+def get_cea_outputs(building_name,locator, target_parameters):
+    '''
+    This function reads the CEA outputs
+    :param building_name:
+    :param locator:
+    :param target_parameters:
+    :return:
+    '''
+    raw_nn_targets = pd.read_csv(locator.get_demand_results_file(building_name), usecols=target_parameters)
+    raw_nn_targets = np.array(raw_nn_targets)
     return raw_nn_targets
 
 def run_as_script():
@@ -72,23 +104,10 @@ def run_as_script():
     locator = cea.inputlocator.InputLocator(scenario_path=scenario_path)
     building_properties, schedules_dict, date = properties_and_schedule(gv, locator)
     list_building_names = building_properties.list_building_names()
-    building_counter=0
-    for building_name in list_building_names:
-        raw_nn_targets = get_cea_outputs(building_name,locator)
-        raw_nn_inputs = get_cea_inputs(building_name)
-        NN_input_ready, NN_target_ready=prep_NN_delay(raw_nn_inputs,raw_nn_targets,nn_delay)
-        if building_counter < 1:
-            urban_input_matrix=NN_input_ready
-            urban_taget_matrix=NN_target_ready
-        else:
-            urban_input_matrix=np.concatenate((urban_input_matrix,NN_input_ready))
-            urban_taget_matrix=np.concatenate((urban_taget_matrix,NN_target_ready))
+    target_parameters=['Qhsf', 'Qcsf', 'Qwwf','Ef', 'theta_a']
+    input_prepare_main(list_building_names, locator, target_parameters)
 
-        building_counter = building_counter+1
 
-        print (building_counter)
-
-    return urban_input_matrix , urban_taget_matrix
 
 
 if __name__ == '__main__':
