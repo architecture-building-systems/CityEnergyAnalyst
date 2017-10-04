059814c28cffea7a79a0bdfaf6414efd45def9d2
Daren Thomas <daren-thomas@users.noreply.github.com>
2017-10-04 16:02:20 +0200

    next candidate for ceatest.bat

--- bin/ceatest.bat
+++ bin/ceatest.bat
@@ -5,13 +5,13 @@ set PATH=%USERPROFILE%\Miniconda2\Scripts\;%PATH%
 set PATH=%PROGRAMDATA%\Miniconda2\Scripts\;%PATH%
 echo %USERPROFILE%
 where conda
-pause
+set CONDA_ENVS_PATH=%LOCALAPPDATA%\conda\conda\envs
 conda env remove -y -q --name cea
 conda env create -q
-set PATH=C:\Users\darthoma\AppData\Local\conda\conda\envs\cea;%PATH%
-set PATH=C:\Users\darthoma\AppData\Local\conda\conda\envs\cea\Scripts;%PATH%
-where pip
-pip install .[dev]
+set PATH=%LOCALAPPDATA%\conda\conda\envs\cea;%PATH%
+set PATH=%LOCALAPPDATA%\conda\conda\envs\cea\Scripts;%PATH%
+set CONDA_DEFAULT_ENV=cea
+%LOCALAPPDATA%\conda\conda\envs\cea\Scripts\pip.exe install .[dev]
 where cea
 cea test
 
