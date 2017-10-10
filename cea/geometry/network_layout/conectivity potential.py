99d3ccd6227959b3930fd151d8dba65509338495
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-10-10 17:46:48 +0800

    finishing, up and running MST

--- cea/geometry/network_layout/conectivity potential.py
+++ cea/geometry/network_layout/conectivity potential.py
@@ -8,6 +8,14 @@ import cea.globalvar
 import cea.inputlocator
 from cea.interfaces.arcgis.modules import arcpy
 
+__author__ = "Jimeno A. Fonseca"
+__copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Jimeno A. Fonseca"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
 
 def calc_connectivity_network(path_arcgis_db, streets_shp, connection_point_buildings_shp, potential_network):
     """
