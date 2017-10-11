fb9421a878b2d1196c48455ac1387c605c787cd0
Daren Thomas <thomas@arch.ethz.ch>
2017-10-11 10:07:40 +0200

    working on mobility...

--- cea/analysis/lca/mobility.py
+++ cea/analysis/lca/mobility.py
@@ -7,13 +7,10 @@ M. Mosteiro Romero  script development          31.08.16
 from __future__ import division
 
 import os
-
 import pandas as pd
 from geopandas import GeoDataFrame as gpdf
 
 from cea import inputlocator
-
-reload(inputlocator)
 
 __author__ = "Martin Mosteiro Romero"
 __copyright__ = "Copyright 2016, Architecture and Building Systems - ETH Zurich"
@@ -85,17 +82,11 @@ def lca_mobility(locator):
     mobility[fields_to_plot].to_csv(locator.get_lca_mobility(), index=False, float_format='%.2f')
 
 def run_as_script(scenario_path=None):
-    import cea.globalvar
-    gv = cea.globalvar.GlobalVariables()
-    if not scenario_path:
-        scenario_path = gv.scenario_reference
+    assert os.path.exists(scenario_path), 'Scenario not found: %s' % scenario_path
     locator = cea.inputlocator.InputLocator(scenario_path=scenario_path)
     lca_mobility(locator=locator)
 
 if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--scenario', help='Path to the scenario folder')
-    args = parser.parse_args()
-    run_as_script(scenario_path=args.scenario)
+    import cea.config
+    config = cea.config.Configuration()
+    run_as_script(scenario_path=config.scenario)
