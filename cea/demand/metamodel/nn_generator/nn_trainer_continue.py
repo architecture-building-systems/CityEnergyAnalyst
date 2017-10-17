ce68218f6e6db70d97a44ed521557f423f3d86d9
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-10-17 17:38:06 +0800

    nn_trainer continue working

--- cea/demand/metamodel/nn_generator/nn_trainer_continue.py
+++ cea/demand/metamodel/nn_generator/nn_trainer_continue.py
@@ -18,7 +18,7 @@ __status__ = "Production"
 
 import numpy as np
 from cea.demand.metamodel.nn_generator.nn_random_sampler import sampling_main
-from cea.demand.metamodel.nn_generator.nn_settings import nn_passes, random_variables, target_parameters
+from cea.demand.metamodel.nn_generator.nn_settings import nn_passes, random_variables, target_parameters, autoencoder
 from cea.demand.metamodel.nn_generator.nn_trainer import nn_input_collector
 
 import cea
@@ -26,7 +26,8 @@ from cea.demand.demand_main import properties_and_schedule
 from cea.demand.metamodel.nn_generator.nn_trainer_resume import neural_trainer_resume, nn_model_collector
 
 
-def run_nn_continue(locator, random_variables, target_parameters, list_building_names, weather_path, gv):
+def run_nn_continue(locator, random_variables, target_parameters, list_building_names, weather_path, gv, autoencoder,
+                    multiprocessing):
     '''
     this function continues a pipeline of tasks by calling a random sampler and a neural network trainer
     :param locator: points to the variables
@@ -42,23 +43,29 @@ def run_nn_continue(locator, random_variables, target_parameters, list_building_
         #   fix a different seed number (for random generation) in each loop
         np.random.seed(i)
         #   create n random sample of the whole dataset of buildings. n is accessible from 'nn_settings.py'
-        sampling_main(locator, random_variables, target_parameters, list_building_names, weather_path, gv)
+        sampling_main(locator, random_variables, target_parameters, list_building_names, weather_path, gv,
+                      multiprocessing)
         #   reads the n random files from the previous step and creat the input and targets for the neural net
         urban_input_matrix, urban_taget_matrix = nn_input_collector(locator)
         #   reads the saved model and the normalizer
         model, scalerT, scalerX = nn_model_collector(locator)
         #   resume training of the neural net
-        neural_trainer_resume(urban_input_matrix, urban_taget_matrix, model, scalerX, scalerT, locator)
+        neural_trainer_resume(urban_input_matrix, urban_taget_matrix, model, scalerX, scalerT, locator, autoencoder)
         print (i)
 
+
 def run_as_script():
+    import cea.config
+    config = cea.config.Configuration()
     gv = cea.globalvar.GlobalVariables()
-    scenario_path = gv.scenario_reference
-    locator = cea.inputlocator.InputLocator(scenario_path=scenario_path)
-    weather_path = locator.get_default_weather()
+
+    locator = cea.inputlocator.InputLocator(scenario_path=config.scenario)
+
     building_properties, schedules_dict, date = properties_and_schedule(gv, locator)
     list_building_names = building_properties.list_building_names()
-    run_nn_continue(locator, random_variables, target_parameters, list_building_names, weather_path, gv)
+    run_nn_continue(locator, random_variables, target_parameters, list_building_names, config.weather, gv, autoencoder,
+                    multiprocessing=config.multiprocessing)
+
 
 if __name__ == '__main__':
     run_as_script()
