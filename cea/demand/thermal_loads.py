2ca92f9baaefdf5f5dc3f573748e48e724d3b87d
Gabriel Happle <happle@arch.ethz.ch>
2017-10-25 16:37:06 +0800

    bugfix
    - if statement in thermal_loads was looking up from supply systems
    instead of hvac systems
    - if statement hardcoded values ('T2', etc) moved to central location in
    control_heating_cooling_systems
    - documentation

--- cea/demand/thermal_loads.py
+++ cea/demand/thermal_loads.py
@@ -14,6 +14,7 @@ from cea.utilities.dbfreader import dbf_to_dataframe
 from cea.demand import occupancy_model, rc_model_crank_nicholson_procedure, ventilation_air_flows_simple
 from cea.demand import ventilation_air_flows_detailed
 from cea.demand import sensible_loads, electrical_loads, hotwater_loads, refrigeration_loads, datacenter_loads
+from cea.demand import control_heating_cooling_systems
 from cea.technologies import controllers, heatpumps
 from cea.utilities import helpers
 
@@ -206,10 +207,10 @@ def calc_thermal_loads(building_name, bpr, weather_data, usage_schedules, date, 
 
     ## electricity demand due to heatpumps/cooling units in the building
     # TODO: do it for heatpumps tsd['Egenf_cs']
-    if bpr.supply['type_cs'] == 'T2' or bpr.supply['type_cs'] == 'T3':
-        if bpr.supply['type_cs'] == 'T2':
+    if control_heating_cooling_systems.cooling_system_is_ac(bpr):
+        if control_heating_cooling_systems.cooling_system_is_mini_split(bpr):
             tsource = (tsd['T_ext'] + 273)
-        if bpr.supply['type_cs'] == 'T3':
+        if control_heating_cooling_systems.cooling_system_is_central_ac(bpr):
             tsource = (tsd['T_ext_wetbulb'] + 273)
         tsd['Egenf_cs'] = np.vectorize(heatpumps.HP_air_air)(tsd['mcpcsf'], (tsd['Tcsf_sup'] + 273),
                                                              (tsd['Tcsf_re'] + 273), tsource, gv)
