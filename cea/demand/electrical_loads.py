6c1aa2745ddea445c97741dd3f47e8f96e646ce2
Gabriel Happle <happle@arch.ethz.ch>
2017-10-26 10:52:49 +0800

    including centralized supply systems in building HP electricity calculation
    - including additional types (T0, T1, T4, T5) with zero electricity in
    calculation
    - refactoring
    - moving function to electrical loads script
    - documentation

--- cea/demand/electrical_loads.py
+++ cea/demand/electrical_loads.py
@@ -5,9 +5,9 @@ Electrical loads
 from __future__ import division
 import numpy as np
 from cea.utilities import physics
-from cea.demand import occupancy_model
+from cea.technologies import heatpumps
 
-__author__ = "Jimeno A. Fonseca"
+__author__ = "Jimeno A. Fonseca, Gabriel Happle"
 __copyright__ = "Copyright 2016, Architecture and Building Systems - ETH Zurich"
 __credits__ = ["Jimeno A. Fonseca"]
 __license__ = "MIT"
@@ -210,3 +210,41 @@ def calc_Eauxf_fw(freshw, nf, gv):
                 time = t0 + 11 + t
                 Eaux_fw[time] = Energy_hourWh
     return Eaux_fw
+
+
+def calc_heatpump_cooling_electricity(bpr, tsd, gv):
+    """
+    calculates electricity demand due to heatpumps/cooling units in the building for different cooling supply systems.
+
+    :param bpr: Building Properties Row object
+    :type bpr: cea.demand.thermal_loads.BuildingPropertiesRow
+    :param tsd: Time series data of building
+    :type tsd: dict
+    :param gv: global variables
+    :type gv: cea.globalvar.GlobalVariables
+    :return: (updates tsd)
+    """
+    # if cooling supply system is hp air-air (T2) or hp water-water (T3)
+    if bpr.supply['type_cs'] in {'T2', 'T3'}:
+        if bpr.supply['type_cs'] == 'T2':
+            tsource = (tsd['T_ext'] + 273)
+        if bpr.supply['type_cs'] == 'T3':
+            tsource = (tsd['T_ext_wetbulb'] + 273)
+        tsd['Egenf_cs'] = np.vectorize(heatpumps.HP_air_air)(tsd['mcpcsf'], (tsd['Tcsf_sup'] + 273),
+                                                             (tsd['Tcsf_re'] + 273), tsource, gv)
+
+    # if cooling supply from district network (T4, T5) or no supply (T0)
+    elif bpr.supply['type_cs'] in {'T4', 'T5', 'T0'}:
+        tsd['Egenf_cs'] = np.zeros(8760)
+
+    # if cooling supply from ground source heat pump
+    elif bpr.supply['type_cs'] in {'T1'}:
+        tsd['Egenf_cs'] = np.zeros(8760)
+        print('Warning: Soil-water HP currently not available.')
+
+    # if unknown cooling supply
+    else:
+        tsd['Egenf_cs'] = np.zeros(8760)
+        print('Error: Unknown Cooling system')
+
+    return
