a21241edbe439f14c28a0ad83494131e95489053
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-11-10 17:04:49 +0800

    working!

--- cea/resources/radiation_daysim/daysim_main.py
+++ cea/resources/radiation_daysim/daysim_main.py
@@ -106,7 +106,7 @@ def calc_sensors_zone(geometry_3D_zone, locator, sensor_parameters):
     return sensors_coords_zone, sensors_dir_zone, sensors_total_number_list, names_zone, sensors_code_zone
 
 
-def isolation_daysim(chunk_n, rad, geometry_3D_zone, locator, weather_path, elevation, latitude, longitude, timezone, settings):
+def isolation_daysim(chunk_n, rad, geometry_3D_zone, locator, weather_path, settings):
 
     # folder for data work
     daysim_dir = locator.get_temporary_file("temp" + str(chunk_n))
@@ -129,7 +129,6 @@ def isolation_daysim(chunk_n, rad, geometry_3D_zone, locator, weather_path, elev
 
     # add_elevation_weather_file(weather_path)
     rad.execute_epw2wea(weather_path, ground_reflectance = settings.terrain_parameters['albedo'])
-        #execute_epw2wea(rad, weather_path, elevation, latitude, longitude, timezone, settings.terrain_parameters['albedo'])
     rad.execute_radfiles2daysim()
     rad_params = settings.rad_parameters
     rad.write_radiance_parameters(rad_params['RAD_AB'], rad_params['RAD_AD'], rad_params['RAD_AS'],
@@ -157,83 +156,3 @@ def isolation_daysim(chunk_n, rad, geometry_3D_zone, locator, weather_path, elev
             json.dump(items_sensor_name_and_result, outfile)
         index = sensors_number_building
 
-def execute_epw2wea(rad, epwweatherfile, elevation, latitude, longitude, timezone, ground_reflectance):
-    # prepare inputs
-    import ntpath
-    import subprocess
-    import math
-
-    longitude = -longitude  # contrary to common notation, daysim assumes positive to the west
-    meridian = round(math.ceil(longitude),0) #contrary to common notation, daysim assumes positive to the west
-
-    daysimdir_wea = rad.daysimdir_wea
-    if daysimdir_wea == None:
-        raise NameError("run .initialise_daysim function before running execute_epw2wea")
-    head, tail = ntpath.split(epwweatherfile)
-    wfilename_no_extension = tail.replace(".epw", "")
-
-    #create wea file with radiance commands
-    weaweatherfilename = wfilename_no_extension + "_60min.wea"
-    weaweatherfile = os.path.join(daysimdir_wea, weaweatherfilename)
-    command1 = "epw2wea" + " " + epwweatherfile + " " + weaweatherfile
-    f = open(rad.command_file, "a")
-    f.write(command1)
-    f.write("\n")
-    f.close()
-    proc = subprocess.Popen(command1, stdout=subprocess.PIPE, shell=True)
-    site_headers = proc.stdout.read()
-
-    #fix wea file according to shapefile's location
-    line_to_override = ["place" + " " + "somewhere-over-the-rainbow\n",
-                        "latitude" + " " + str(latitude)+"\n",
-                        "longitude" + " " + str(longitude)+"\n",
-                        "time_zone" + " " + str(meridian)+"\n",
-                        "site_elevation" + " " + str(elevation)+"\n",
-                        "weather_data_file_units" + " " + "1\n"]
-
-    with open(weaweatherfile, 'r') as file:
-        # read a list of lines into data
-        data = file.readlines()
-
-    for row, line in enumerate(line_to_override):
-        data[row] = line
-
-    with open(weaweatherfile, 'w') as file:
-        file.writelines(data)
-
-    # write hea file for radiance
-    hea_filepath = rad.hea_file
-    hea_file = open(hea_filepath, "a")
-    hea_file.write("\nplace" + " " + "somewhere-over-the-rainbow")
-    hea_file.write("\nlatitude" + " " + str(latitude))
-    hea_file.write("\nlongitude" + " " + str(longitude))
-    hea_file.write("\ntime_zone" + " " + str(meridian))
-    hea_file.write("\nsite_elevation" + " " + str(elevation))
-    hea_file.write("\nweather_data_file_units" + " " + "1")
-    hea_file.write("\nground_reflectance" + " " + str(ground_reflectance))
-    # get the directory of the long weatherfile
-    hea_file.write("\nwea_data_file" + " " + os.path.join(head, wfilename_no_extension + "_60min.wea"))
-    hea_file.write("\ntime_step" + " " + "60")
-    hea_file.write("\nwea_data_short_file" + " " + os.path.join("wea", wfilename_no_extension + "_60min.wea"))
-    hea_file.write("\nwea_data_short_file_units" + " " + "1")
-    hea_file.write("\nlower_direct_threshold" + " " + "2")
-    hea_file.write("\nlower_diffuse_threshold" + " " + "2")
-    hea_file.close()
-    # check for the sunuphours
-    results = open(weaweatherfile, "r")
-    result_lines = results.readlines()
-    result_lines = result_lines[6:]
-    sunuphrs = 0
-    for result in result_lines:
-        words = result.replace("\n", "")
-        words1 = words.split(" ")
-        direct = float(words1[-1])
-        diffuse = float(words1[-2])
-        total = direct + diffuse
-        if total > 0:
-            sunuphrs = sunuphrs + 1
-
-    results.close()
-    rad.sunuphrs = sunuphrs
-
-    return rad
