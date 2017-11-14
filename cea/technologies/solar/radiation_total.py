c61b4f94e6a9fa3377af5220191dc21a8d41c576
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-11-14 14:22:13 +0800

    totaling the radiation Whperm2

--- cea/technologies/solar/radiation_total.py
+++ cea/technologies/solar/radiation_total.py
@@ -18,7 +18,7 @@ __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
-def calc_spatio_temporal_visuals(locator, period, list_of_buildings, initial_date, config):
+def calc_spatio_temporal_visuals(locator, list_of_buildings, initial_date, config):
 
     # now the dates in which the building demand is calculated is stored in 'date'
     date = pd.date_range(initial_date, periods=8760, freq='H')
@@ -35,30 +35,25 @@ def calc_spatio_temporal_visuals(locator, period, list_of_buildings, initial_dat
     for i, building in enumerate(building_names):
         # importing corresponding variables of each building and then slicing it to take just a single period value
         # i.e a time step
-        yearly_horizontal_rad = weather_data.glohorrad_Whm2.sum()  # [Wh/m2/year]
-
         # read radiation file
         sensors_rad = pd.read_json(locator.get_radiation_building(building_name=building))
         sensors_metadata = pd.read_csv(locator.get_radiation_metadata(building_name= building))
-        sensors_rad.to_csv(locator.radiation_results(building_name=building + 'a'), index=True, float_format='%.2f')
 
-        # join total radiation to sensor_metadata
-        sensors_rad['total_rad_Whm2'] = sensors_rad.sum(1)  # add new row with yearly radiation
-        sensors_rad['date'] = time
-        sensors_metadata.set_index('SURFACE', inplace=True)
-        sensors_metadata = sensors_metadata.merge(sensors_rad, left_index=True, right_index=True)  # [Wh/m2]
+        sensors_metadata_roof = sensors_metadata[sensors_metadata.TYPE == 'roofs']
+        sensors_metadata_walls = sensors_metadata[sensors_metadata.TYPE == 'walls']
+        sensors_metadata_windows = sensors_metadata[sensors_metadata.TYPE == 'windows']
 
-        # remove window surfaces
-        sensors_metadata = sensors_metadata[sensors_metadata.TYPE != 'windows']
+        roof_cols = [c for c in sensors_rad.columns if c in sensors_metadata_roof.SURFACE.tolist()]
+        sensors_rad_roof = sensors_rad[roof_cols]
 
-        # keep sensors if allow pv installation on walls or on roofs
-        if config.solar.panel_on_roof is False:
-            sensors_metadata = sensors_metadata[sensors_metadata.TYPE != 'roofs']
-        if config.solar.panel_on_wall is False:
-            sensors_metadata = sensors_metadata[sensors_metadata.TYPE != 'walls']
+        walls_cols = [c for c in sensors_rad.columns if c in sensors_metadata_walls.SURFACE.tolist()]
+        sensors_rad_walls = sensors_rad[walls_cols]
 
-        sensors_rad_clean = sensors_rad[sensors_metadata.index.tolist()]  # keep sensors above min radiation
-        sensors_rad.to_csv(locator.radiation_results(building_name=building), index=True, float_format='%.2f')  # print PV generation potential
+        sensors_rad_final = pd.DataFrame()
+        sensors_rad_final['total_roof_rad_Whperm2'] = sensors_rad_roof.sum(1)
+        sensors_rad_final['total_wall_rad_Whperm2'] = sensors_rad_walls.sum(1)
+        sensors_rad_final['date'] = time
+        sensors_rad_final.to_csv(locator.radiation_results(building_name=building), index=True, float_format='%.2f')  # print PV generation potential
 
 
 
@@ -67,8 +62,7 @@ def main(config):
     locator = cea.inputlocator.InputLocator(scenario=config.scenario)
     initial_date = '1/1/2015'
     list_of_buildings = ['all']  # 'all' for all buildings or else provide a list of building names
-    period = [1680, 1848] # period in hours of the year to viualize
-    calc_spatio_temporal_visuals(locator, period, list_of_buildings, initial_date, config)
+    calc_spatio_temporal_visuals(locator, list_of_buildings, initial_date, config)
 
 
 if __name__ == '__main__':
