98be4c1e8d15a95948428d75cc5d591129b7cbac
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-11-27 18:10:07 +0800

    and more refactoring

--- cea/demand/metamodel/nn_generator/input_matrix.py
+++ cea/demand/metamodel/nn_generator/input_matrix.py
@@ -18,7 +18,7 @@ from cea.technologies import controllers
 from cea.utilities import epwreader
 from cea.demand.sensible_loads import calc_I_sol
 import numpy as np
-from cea.demand.metamodel.nn_generator.nn_settings import nn_delay, climatic_variables
+from cea.config.neural_network import nn_delay, climatic_variables
 
 __author__ = "Jimeno A. Fonseca","Fazel Khayatian"
 __copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
