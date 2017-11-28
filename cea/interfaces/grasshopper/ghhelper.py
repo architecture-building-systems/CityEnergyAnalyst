e9f7f810782278cc36cadbb6cbe6ecc25cb307fd
Daren Thomas <thomas@arch.ethz.ch>
2017-11-28 11:10:20 +0100

    install-grasshopper

---
+++ cea/interfaces/grasshopper/ghhelper.py
@@ -0,0 +1,7 @@
+"""
+A library of helper-functions for working with the CEA from grasshopper. Install this with ``cea install-grasshopper``.
+
+.. note:: This module is meant to be run from grasshopper, which uses an IronPython interpreter. PyCharm will have a
+    hard time with some of the imports here.
+"""
+
---
+++ cea/interfaces/grasshopper/install_grasshopper.py
@@ -0,0 +1,70 @@
+"""
+Install the grasshopper interface. This assumes that the python path for grasshopper Python scripts is in
+``%APPDATA%\McNeel\Rhinoceros\5.0\scripts``.
+"""
+
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import shutil
+
+__author__ = "Daren Thomas"
+__copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Daren Thomas"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
+
+
+def main(_):
+    """
+    Install a subset of the CEA for use inside grasshopper Python scripts.
+
+    Since we don't know for sure, if ``cea install-toolbox`` (for the ArcGIS interface) was run, we need to re-create
+    some stuff done there too, like storing the path to the python executable to use.
+
+    :param _: ignored.
+    :type _: cea.config.Configuration
+    :return:
+    """
+    with open(os.path.expanduser('~/cea_python.pth'), 'w') as f:
+        f.write(sys.executable)
+
+    scripts_folder = os.path.expandvars(r'%APPDATA%\McNeel\Rhinoceros\5.0\scripts')
+    copy_library(scripts_folder)
+
+    print('Installed grasshopper interface.')
+
+
+def copy_library(scripts_folder):
+    """Copy the library functions"""
+    lib_dst_folder = os.path.join(scripts_folder, 'cea', 'interfaces', 'grasshopper')
+    if not os.path.exists(lib_dst_folder):
+        os.makedirs(lib_dst_folder)
+
+    lib_src_folder = os.path.dirname(__file__)
+    shutil.copy(os.path.join(lib_src_folder, 'ghhelper.py'), lib_dst_folder)
+
+    # we also need access to the cli.config file (ghhelper uses this to figure out the parameters)
+    lib_cli_dst_folder = os.path.join(scripts_folder, 'cea', 'interfaces', 'cli')
+    if not os.path.exists(lib_cli_dst_folder):
+        os.makedirs(lib_cli_dst_folder)
+    shutil.copy(os.path.join(lib_src_folder, '..', 'cli', 'cli.config'), lib_cli_dst_folder)
+
+    # add `__init__.py` files to interfaces and arcgis folders
+    with open(os.path.join(lib_dst_folder, '..', '..', '__init__.py'), 'w') as f:
+        f.write('')
+    with open(os.path.join(lib_dst_folder, '..', '__init__.py'), 'w') as f:
+        f.write('')
+    with open(os.path.join(lib_dst_folder, '__init__.py'), 'w') as f:
+        f.write('')
+    with open(os.path.join(lib_cli_dst_folder, '__init__.py'), 'w') as f:
+        f.write('')
+
+
+if __name__ == '__main__':
+    main(None)
