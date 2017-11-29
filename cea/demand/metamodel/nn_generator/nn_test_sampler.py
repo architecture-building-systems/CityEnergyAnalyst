30ea87cfe602331ed254830ec77acdd8f98d21ee
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-11-29 14:43:00 +0800

    and one more

--- cea/demand/metamodel/nn_generator/nn_test_sampler.py
+++ cea/demand/metamodel/nn_generator/nn_test_sampler.py
@@ -75,7 +75,7 @@ def main(config):
     weather_path = config.weather
     building_properties, schedules_dict, date = properties_and_schedule(gv, locator)
     list_building_names = building_properties.list_building_names()
-    urban_input_matrix, urban_taget_matrix=sampling_single(locator, random_variables, target_parameters, list_building_names, weather_path, gv)
+    urban_input_matrix, urban_taget_matrix = sampling_single(locator, random_variables, target_parameters, list_building_names, weather_path, gv)
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
