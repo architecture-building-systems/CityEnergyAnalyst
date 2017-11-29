977d3400038673628273768328fc05e835504429
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-11-29 14:42:34 +0800

    fixing last details of NN

--- cea/demand/metamodel/nn_generator/nn_test_sampler.py
+++ cea/demand/metamodel/nn_generator/nn_test_sampler.py
@@ -71,13 +71,11 @@ def sampling_single(locator, random_variables, target_parameters, list_building_
 
 def main(config):
     gv = cea.globalvar.GlobalVariables()
-    locator = cea.inputlocator.InputLocator(scenario_path=config.scenario)
+    locator = cea.inputlocator.InputLocator(scenario=config.scenario)
     weather_path = config.weather
     building_properties, schedules_dict, date = properties_and_schedule(gv, locator)
     list_building_names = building_properties.list_building_names()
     urban_input_matrix, urban_taget_matrix=sampling_single(locator, random_variables, target_parameters, list_building_names, weather_path, gv)
-
-
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
