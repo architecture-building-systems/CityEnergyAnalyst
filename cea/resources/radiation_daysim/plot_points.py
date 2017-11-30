8845cd2be781ebc07ad55cffbfc6815192743b50
JIMENOFONSECA <fonseca@arch.ethz.ch>
2017-11-30 10:40:20 +0800

    fixes to account for daysim radiation when no windows

--- cea/resources/radiation_daysim/plot_points.py
+++ cea/resources/radiation_daysim/plot_points.py
@@ -1,16 +1,36 @@
 from __future__ import division
 import pandas as pd
 import matplotlib.pyplot as plt
+from cea import config
+import numpy as np
+from cea import inputlocator
+
+configur = config.Configuration()
+locator = inputlocator.InputLocator(scenario=configur.scenario)
+
+# index = pd.date_range('1/1/2016', periods=8760, freq='H')
+file = pd.read_csv(r'C:\reference-case-open\baseline\outputs\data\demand/total_demand.csv')
+# file.set_index(index, inplace=True)
+print file[['Qhsf_MWhyr', 'Ef_MWhyr','Qcsf_MWhyr']].sum(axis = 0 )
+
+list_buildings = pd.read_csv(locator.get_total_demand())['Name'].values
+I_rad = np.array([])
+I_sol = np.array([])
+for building in list_buildings:
+    data = pd.read_excel(r'C:\reference-case-open\baseline\outputs\data\demand/'+building+'.xls')
+    I_rad = np.append(I_rad, [data['I_rad'].sum()/1000])
+    I_sol = np.append(I_sol, [data['I_sol'].sum()/1000])
+
+I_rad2 = np.nansum(I_rad)
+I_sol2 = np.nansum(I_sol)
+print I_rad2
+print I_sol2
 
 
-index = pd.date_range('1/1/2016', periods=8760, freq='H')
-file = pd.read_json(r'C:\reference-case-ecocampus\baseline\outputs\data\solar-radiation/B001_insolation_Whm2.json')
-file.set_index(index, inplace=True)
-print file.sum(axis = 0 )/1000
 
 #print file[2300:2370]
 file.plot()
 
-plt.show()
+# plt.show()
 
 
