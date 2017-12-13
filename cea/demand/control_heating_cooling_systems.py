97c3bec1a716b809073af41fb10b447592978e16
Gabriel Happle <happle@arch.ethz.ch>
2017-12-13 13:45:22 +0800

    heating and cooling season is now extracted from a location specific database
    - create heating and cooling season local database in archtypes folders
    - add heating and cooling season parameters to building properties
    - refactor and clean up helpers script
    - remove obsolete parameters in config
    - uncomment some obsolete parameters in gv
    - remove test helpers

--- cea/demand/control_heating_cooling_systems.py
+++ cea/demand/control_heating_cooling_systems.py
@@ -2,8 +2,8 @@
 
 
 from __future__ import division
+import numpy as np
 from cea.demand import rc_model_SIA
-from cea.utilities import helpers
 
 __author__ = "Gabriel Happle"
 __copyright__ = "Copyright 2016, Architecture and Building Systems - ETH Zurich"
@@ -79,7 +79,7 @@ def cooling_system_is_ac(bpr):
     if bpr.hvac['type_cs'] in {'T2', 'T3'}:  # mini-split ac and central ac
         return True
     elif bpr.hvac['type_cs'] in {'T0', 'T1'}:
-        return Fal
+        return False
     else:
         print('Error: Unknown cooling system')
         return False
@@ -90,7 +90,7 @@ def is_active_heating_system(bpr, tsd, t):
     # check for heating system in building
     # check for heating season
     # check for heating demand
-    if helpers.is_heatingseason_hoy(t) \
+    if is_heating_season(t, bpr) \
             and has_heating_system(bpr) \
             and rc_model_SIA.has_heating_demand(bpr, tsd, t):
 
@@ -104,7 +104,7 @@ def is_active_cooling_system(bpr, tsd, t):
     # check for cooling system in building
     # check for cooling season
     # check for cooling demand
-    if helpers.is_coolingseason_hoy(t) \
+    if is_cooling_season(t, bpr) \
             and has_cooling_system(bpr) \
             and tsd['T_ext'][t] >= tsd['ta_cs_set'][t] \
             and rc_model_SIA.has_cooling_demand(bpr, tsd, t):
@@ -112,3 +112,108 @@ def is_active_cooling_system(bpr, tsd, t):
         return True
     else:
         return False
+
+
+def convert_date_to_hour(date):
+    """
+    converts date in 'MM-DD' format into hour of the year (first hour of the day)
+    i.e. '01-01' results in 0
+
+    :param date: date in 'MM-DD' format (from .xlsx database input)
+    :type date: str
+    :return: hour of the year (first hour of the day)
+    :rtype: int
+    """
+
+    hours_in_day = 24
+    days_in_month = np.array([0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+
+    # convert unicode string to month and day of month
+    month = int(date[0:2])
+    day_of_month = int(date[3:5])
+
+    # hour of year
+    hour_of_year = (np.cumsum(days_in_month)[month - 1] + day_of_month - 1) * hours_in_day
+
+    if 0 <= hour_of_year <= 8736:
+        return hour_of_year
+    else:
+        raise
+
+
+def is_heating_season(t, bpr):
+    """
+    checks if time step is part of the heating season for the building
+
+    :param t: hour of the year, simulation time step [0...8760]
+    :type t: int
+    :param bpr: BuildingPropertiesRow
+    :param bpr: cea.demand.building_properties.BuildingPropertiesRow
+    :return: True or False
+    :rtype: bool
+    """
+
+    if bpr.hvac['has-heating-season']:
+
+        heating_start = convert_date_to_hour(bpr.hvac['heating-season-start'])
+        heating_end = convert_date_to_hour(bpr.hvac['heating-season-end']) + 24 # end at the last hour of the day
+
+        # check if heating season is at the end of the year (north hemisphere) or in the middle of the year (south)
+        if heating_start < heating_end and \
+            heating_start <= t <= heating_end:
+
+            # heating season time on south hemisphere
+            return True
+
+        elif heating_start > heating_end and \
+                (heating_start <= t <= 8760 or 0 <= t <= heating_end):
+            # heating season over the year end (north hemisphere)
+            return True
+
+        else:
+            # not time of heating season
+            return False
+
+    elif not bpr.hvac['has-heating-season']:
+        # no heating season
+
+        return False
+
+
+def is_cooling_season(t, bpr):
+    """
+    checks if time step is part of the cooling season for the building
+
+    :param t: hour of the year, simulation time step [0...8760]
+    :type t: int
+    :param bpr: BuildingPropertiesRow
+    :param bpr: cea.demand.building_properties.BuildingPropertiesRow
+    :return: True or False
+    :rtype: bool
+        """
+
+    if bpr.hvac['has-cooling-season']:
+
+        cooling_start = convert_date_to_hour(bpr.hvac['cooling-season-start'])
+        cooling_end = convert_date_to_hour(bpr.hvac['cooling-season-end']) + 24  # end at the last hour of the day
+
+        # check if cooling season is at the end of the year (south hemisphere) or in the middle of the year (norht)
+        if cooling_start < cooling_end and \
+                cooling_start <= t <= cooling_end:
+
+            # cooling season time on north hemisphere
+            return True
+
+        elif cooling_start > cooling_end and \
+                (cooling_start <= t <= 8760 or 0 <= t <= cooling_end):
+            # cooling season around the year end (south hemisphere)
+            return True
+
+        else:
+            # not time of cooling season
+            return False
+
+    elif not bpr.hvac['has-cooling-season']:
+        # no cooling season
+        return False
+
