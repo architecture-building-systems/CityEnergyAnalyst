a22dc2d6e4c14d8c8ca03decd6d226be154657bf
Daren Thomas <thomas@arch.ethz.ch>
2017-12-15 11:15:40 +0100

    working on implementing the tab behavior from bootstrap

--- cea/interfaces/config_editor/static/js/config_editor.js
+++ cea/interfaces/config_editor/static/js/config_editor.js
@@ -6,8 +6,10 @@ $(document).ready(function() {
 
 
 function select_section(section_name) {
+/*
     $(".selected").removeClass("selected");
 
     $("#section-editor-" + section_name).addClass("selected");
     $("#section-list-" + section_name).addClass("selected");
+*/
 }
