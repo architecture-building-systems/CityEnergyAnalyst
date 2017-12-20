4cefa38ac6a9eee32238315e7c67dea03e533f66
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-12-20 12:56:58 +0800

    updates to sampling procedure for optimization nn

--- cea/optimization/metamodel/nn_generator/nn_random_sampler.py
+++ cea/optimization/metamodel/nn_generator/nn_random_sampler.py
@@ -14,10 +14,9 @@ import cea.optimization.master.generation as generation
 import cea.optimization.master.evaluation as evaluation
 import cea.optimization.distribution.network_opt_main as network_opt
 from cea.optimization.preprocessing.preprocessing_main import preproccessing
-from cea.demand import occupancy_model
-from cea.demand import thermal_loads
-from cea.demand.thermal_loads import BuildingProperties
-from cea.utilities import epwreader
+from cea.optimization.prices import Prices as Prices
+
+
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
@@ -29,107 +28,50 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 import cea
-from cea.demand import demand_main
-from cea.demand.calibration.bayesian_calibrator.calibration_sampling import apply_sample_parameters
-from cea.demand.calibration.latin_sampler import latin_sampler
-from cea.demand.demand_main import properties_and_schedule
-from cea.demand.metamodel.nn_generator.nn_settings import number_samples, random_variables, \
-    target_parameters, boolean_vars
-from cea.demand.metamodel.nn_generator.input_prepare import input_prepare_main
-
-
-def input_dropout(urban_input_matrix, urban_taget_matrix):
-    rows, cols = urban_input_matrix.shape
-    drop_random_array = np.random.rand(rows)
-    drop_idx_filter = drop_random_array > 0.5
-    drop_idx = np.where(drop_idx_filter)
-    urban_input_matrix = np.delete(urban_input_matrix, drop_idx, 0)
-    urban_taget_matrix = np.delete(urban_taget_matrix, drop_idx, 0)
-
-    return urban_input_matrix, urban_taget_matrix
-
-
-def sampling_main(locator, random_variables, target_parameters, list_building_names, weather_path, gv, multiprocessing):
-    '''
-    this function creates a number of random samples for the entire district (city)
-    :param locator: points to the variables
-    :param random_variables: a list containing the names of variables associated with uncertainty (can be accessed from 'nn_settings.py')
-    :param target_parameters: a list containing the name of desirable outputs (can be accessed from 'nn_settings.py')
-    :param list_building_names: a list containing the name of desired buildings
-    :param weather_path: weather path
-    :param gv: global variables
-    :return: -
-    '''
-
-    #   get number of buildings
-    size_city = np.shape(list_building_names)
-    size_city = size_city[0]
-    #   create random samples of the entire district
-    for i in range(number_samples):  # the parameter "number_samples" is accessible from 'nn_settings.py'
-        bld_counter = 0
-        # create list of samples with a LHC sampler and save to disk
-        samples, pdf_list = latin_sampler(locator, size_city, random_variables)
-        samples = samples[0]  # extract the non-normalized samples
-
-        # create a file of overides with the samples
-        dictionary = dict(zip(random_variables, samples.transpose()))
-        overides_dataframe = pd.DataFrame(dictionary)
-        overides_dataframe['Name'] = list_building_names
-
-        # replace the 1, 0 with True and False
-        for var in boolean_vars:
-            overides_dataframe[var].replace(1, "True", inplace=True)
-            overides_dataframe[var].replace(0, "False", inplace=True)
-            overides_dataframe[var].replace(0.0, "False", inplace=True)
-
-        # save file so the demand calculation can know about it.
-        overides_dataframe.to_csv(locator.get_building_overrides())
-
-        #   run cea demand
-        demand_main.demand_calculation(locator, weather_path, gv, multiprocessing=multiprocessing)
-        #   prepare the inputs for feeding into the neural network
-        urban_input_matrix, urban_taget_matrix = input_prepare_main(list_building_names, locator, target_parameters, gv)
-        #   drop half the inputs and targets to avoid overfitting and save RAM / Disk space
-        urban_input_matrix, urban_taget_matrix = input_dropout(urban_input_matrix, urban_taget_matrix)
-        #   get the pathfor saving the files
-        nn_inout_path = locator.get_nn_inout_folder()
-        #   save inputs with sequential naming
-        file_path_inputs = os.path.join(nn_inout_path, "input%(i)s.csv" % locals())
-        data_file_inputs = pd.DataFrame(urban_input_matrix)
-        data_file_inputs.to_csv(file_path_inputs, header=False, index=False)
-        #   save inputs with sequential naming
-        file_path_targets = os.path.join(nn_inout_path, "target%(i)s.csv" % locals())
-        data_file_targets = pd.DataFrame(urban_taget_matrix)
-        data_file_targets.to_csv(file_path_targets, header=False, index=False)
-
-        # return urban_input_matrix, urban_taget_matrix
 
 def main(config):
     locator = cea.inputlocator.InputLocator(scenario=config.scenario)
     gv = cea.globalvar.GlobalVariables()
     total_demand = pd.read_csv(locator.get_total_demand())
     building_names = total_demand.Name.values
+    gv.num_tot_buildings = total_demand.Name.count()
+    ntwList = ["1"*total_demand.Name.count()]
+
+    prices = Prices(locator, config)
+
     extra_costs, extra_CO2, extra_primary_energy, solarFeat = preproccessing(locator, total_demand, building_names,
-                                                                   config.weather, gv)
+                                                                   config.weather, gv, config, prices)
     network_features = network_opt.network_opt_main()
 
-    input_data = []
-    for i in range(1000):
-        individual = generation.generate_main(len(building_names), gv)
-        print (individual)
-        individual = evaluation.check_invalid(individual, len(building_names), gv)
-        print (individual)
-        (costs, CO2, prim) = evaluation.evaluation_main(individual, building_names, locator, extra_costs, extra_CO2, extra_primary_energy, solarFeat,
-                                                        network_features, gv)
-        input_data.append(individual)
+    number_samples = 1000
 
+    input_data = pd.DataFrame(range(number_samples), columns=['index'])
+    input_data_individual = []
+    output_costs = []
+    output_CO2 = []
+    output_prim = []
     print (input_data)
 
+    for i in range(number_samples):
 
-    building_properties, schedules_dict, date = properties_and_schedule(gv, locator)
-    list_building_names = building_properties.list_building_names()
-    sampling_main(locator, random_variables, target_parameters, list_building_names, config.weather, gv,
-                  multiprocessing=config.multiprocessing)
+        individual = generation.generate_main(len(building_names))
+        individual = evaluation.check_invalid(individual, len(building_names), gv)
+        evaluation.checkNtw(individual, ntwList, locator, gv, config)
+
+        (costs, CO2, prim) = evaluation.evaluation_main(individual, building_names, locator, extra_costs, extra_CO2, extra_primary_energy, solarFeat,
+                                                        network_features, gv, config, prices)
+
+        input_data_individual.append(individual)
+        output_costs.append(costs)
+        output_CO2.append(CO2)
+        output_prim.append(prim)
+
+    input_data['individual'] = input_data_individual
+    input_data['costs'] = output_costs
+    input_data['CO2'] = output_CO2
+    input_data['prim'] = output_prim
+    print (input_data)
+    input_data.to_csv('C:\Users\Bhargava\Desktop/input_data.csv')
 
 
 
