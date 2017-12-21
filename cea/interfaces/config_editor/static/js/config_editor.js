59279bd8cbc638d8b94bfe6d55bca241cb0644dc
Daren Thomas <thomas@arch.ethz.ch>
2017-12-21 10:35:07 +0100

    working on saving configuration

--- cea/interfaces/config_editor/static/js/config_editor.js
+++ cea/interfaces/config_editor/static/js/config_editor.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
     $("#list-general-list").tab("show");
 
 });
+
+/**
+ * Calls back to Backend.save_section after collecting the values from all the controls.
+ * @param section
+ */
+function save_section_js(section) {
+    alert(section);
+}
