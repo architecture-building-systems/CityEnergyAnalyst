ec1c0b6f2342ee552a13d9c161294f29541e8414
Daren Thomas <thomas@arch.ethz.ch>
2018-01-08 14:07:24 +0100

    pass values back to python from javascript

--- cea/interfaces/config_editor/static/js/config_editor.js
+++ cea/interfaces/config_editor/static/js/config_editor.js
@@ -9,5 +9,37 @@ $(document).ready(function() {
  * @param section
  */
 function save_section_js(section) {
-    alert(section);
+    parameters = JSON.parse(backend.get_parameters(section));
+    data = {};
+    for (parameter_name in parameters) {
+        data[parameter_name] = read_value(section, parameter_name, parameters[parameter_name])
+    }
+    backend.save_section(section, JSON.stringify(data));
+}
+
+/**
+ * Read out the value of the parameter as defined by the form input - this depends on the parameter_type.
+ *
+ * @param section
+ * @param parameter_name
+ * @param parameter_type
+ */
+function read_value(section, parameter_name, parameter_type) {
+    value = null;
+    switch (parameter_type) {
+        case "ChoiceParameter":
+            value = $('#' + section + '-' + parameter_name)[0].value;
+        case "WeatherPathParameter":
+            value = $('#' + section + '-' + parameter_name)[0].value;
+        case "BooleanParameter":
+            value = $('#' + section + '-' + parameter_name)[0].checked;
+            break;
+        case "PathParameter":
+            value = $('#' + section + '-' + parameter_name)[0].value;
+            break;
+        default:
+            // handle the default case
+            value = $('#' + section + '-' + parameter_name)[0].value;
+    }
+    return value;
 }
