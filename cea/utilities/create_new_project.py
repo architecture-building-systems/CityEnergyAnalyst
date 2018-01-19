e0c24519015565eb7998140c8b89eea9bc382007
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-01-19 10:07:04 +0800

    creating a file to start

---
+++ cea/utilities/create_new_project.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from __future__ import division
+
+"""
+A tool to create a new project / scenario with the CEA.
+"""
+
+import pysal
+import numpy as np
+import pandas as pd
+import os
+
+__author__ = "Jimeno A. Fonseca"
+__copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Jimeno A. Fonseca"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
+
+
