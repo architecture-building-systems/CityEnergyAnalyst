c075a00df5d22c91e4517b371f6b4e448fe2215d
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-01-25 11:11:36 +0800

    updating laod curve

--- cea/plots/demand/load_curve.py
+++ cea/plots/demand/load_curve.py
@@ -5,13 +5,12 @@ from cea.plots.variable_naming import LOGO
 def load_curve(data_frame, analysis_fields, title, output_path):
 
     traces = []
-    x = data_frame.index.values
     for field in analysis_fields:
         y = data_frame[field].values
         if field in ["T_int_C", "T_out_dry_C"]:
-            trace = go.Scatter(x= x, y= y, name = field.split('t', 1)[0], yaxis='y2', opacity = 0.2)
+            trace = go.Scatter(x= data_frame.index, y= y, name = field.split('t', 1)[0], yaxis='y2', opacity = 0.2)
         else:
-            trace = go.Scatter(x= x, y= y, name = field.split('_', 1)[0])
+            trace = go.Scatter(x= data_frame.index, y= y, name = field.split('_', 1)[0])
         traces.append(trace)
 
     # CREATE FIRST PAGE WITH TIMESERIES
