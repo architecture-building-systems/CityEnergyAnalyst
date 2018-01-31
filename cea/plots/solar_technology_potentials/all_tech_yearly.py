08a82547e61af37e39ee67835bb51ad5f1ed7d9f
shanshanhsieh <hsieh@arch.ethz.ch>
2018-01-31 10:29:26 +0800

    improvements on all tech annual plot
    - hide legends in the beginning (visible = 'legendonly')
    - group PVT el and heat output together

--- cea/plots/solar_technology_potentials/all_tech_yearly.py
+++ cea/plots/solar_technology_potentials/all_tech_yearly.py
@@ -4,11 +4,12 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 from cea.plots.variable_naming import LOGO
 from cea.plots.color_code import ColorCodeCEA
+
 COLOR = ColorCodeCEA()
 
 
-def all_tech_district_yearly(data_frame, pv_analysis_fields, pvt_analysis_fields, sc_analysis_fields, title, output_path):
-
+def all_tech_district_yearly(data_frame, pv_analysis_fields, pvt_analysis_fields, sc_analysis_fields, title,
+                             output_path):
     E_analysis_fields = []
     Q_analysis_fields = []
 
@@ -20,19 +21,20 @@ def all_tech_district_yearly(data_frame, pv_analysis_fields, pvt_analysis_fields
     E_analysis_fields.extend(pvt_E_analysis_fields_used)
     pvt_Q_analysis_fields_used = data_frame.columns[data_frame.columns.isin(pvt_analysis_fields[5:10])].tolist()
     Q_analysis_fields.extend(pvt_Q_analysis_fields_used)
-    data_frame_MWh = data_frame/1000 # to MWh
+    data_frame_MWh = data_frame / 1000  # to MWh
 
-    #CALCULATE GRAPH
+    # CALCULATE GRAPH
     traces_graph, x_axis = calc_graph(E_analysis_fields, Q_analysis_fields, data_frame_MWh)
 
-    #CALCULATE TABLE
+    # CALCULATE TABLE
     traces_table = calc_table(E_analysis_fields, Q_analysis_fields, data_frame_MWh)
 
-    #PLOT GRAPH
+    # PLOT GRAPH
     traces_graph.append(traces_table)
 
     # CREATE BUTTON
-    annotations = list([dict(text='Building:', x=-0.15, y=1.05, xref='paper', yref='paper', align='left', showarrow=False)])
+    annotations = list(
+        [dict(text='Building:', x=-0.15, y=1.05, xref='paper', yref='paper', align='left', showarrow=False)])
     list_buttons = [dict(label='None', method='relayout', args=['xaxis.range[0]', -0.5])]
     labels = x_axis
     for i, label in enumerate(labels):
@@ -42,7 +44,7 @@ def all_tech_district_yearly(data_frame, pv_analysis_fields, pvt_analysis_fields
         list_buttons.append(new_button)
     updatemenus = list([dict(buttons=list_buttons)])
 
-    layout = go.Layout(images=LOGO,title=title, barmode='stack', updatemenus=updatemenus,annotations=annotations,
+    layout = go.Layout(images=LOGO, title=title, barmode='stack', updatemenus=updatemenus, annotations=annotations,
                        yaxis=dict(title='Solar radiation [MWh/yr]', domain=[0.35, 1]),
                        xaxis=dict(title='Building'))
     fig = go.Figure(data=traces_graph, layout=layout)
@@ -55,48 +57,61 @@ def calc_graph(E_analysis_fields, Q_analysis_fields, data_frame):
 
     data_frame['total_E'] = total_E = data_frame[E_analysis_fields].sum(axis=1)
     data_frame['total_Q'] = total_Q = data_frame[Q_analysis_fields].sum(axis=1)
-    #data_frame = data_frame.sort_values(by='total', ascending=False) # this will get the maximum value to the left
+    # data_frame = data_frame.sort_values(by='total', ascending=False) # this will get the maximum value to the left
     for field in E_analysis_fields:
         y = data_frame[field]
-        total_perc = (y/total_E.sum()*100).round(2)
-        total_perc_txt = ["("+str(x)+" %)" for x in total_perc]
-        trace1 = go.Bar(x=data_frame.index, y=y, name=field, text = total_perc_txt,
-                       marker=dict(color=COLOR.get_color_rgb(field.split('_kWh', 1)[0])), width=0.3, offset=-0.35 )
+        total_perc = (y / total_E.sum() * 100).round(2)
+        total_perc_txt = ["(" + str(x) + " %)" for x in total_perc]
+        if field.split('_')[0] == 'PVT':
+            trace1 = go.Bar(x=data_frame.index, y=y, name=field.split('_kWh', 1)[0], text=total_perc_txt,
+                            marker=dict(color=COLOR.get_color_rgb(field.split('_kWh', 1)[0])), visible='legendonly',
+                            width=0.3, offset=-0.35, legendgroup=field.split('_')[1])
+        else:
+            trace1 = go.Bar(x=data_frame.index, y=y, name=field.split('_kWh', 1)[0], text=total_perc_txt,
+                            marker=dict(color=COLOR.get_color_rgb(field.split('_kWh', 1)[0])), visible='legendonly',
+                            width=0.3, offset=-0.35)
         graph.append(trace1)
 
     for field in Q_analysis_fields:
         y = data_frame[field]
-        total_perc = (y/total_Q*100).round(2).values
-        total_perc_txt = ["("+str(x)+" %)" for x in total_perc]
-        trace2 = go.Bar(x=data_frame.index, y=y, name=field, text = total_perc_txt,
-                        marker=dict(color=COLOR.get_color_rgb(field.split('_kWh', 1)[0]), line=dict(
-                            color="rgb(105,105,105)", width=1)), opacity=0.7, base=0, width=0.3, offset=0)
+        total_perc = (y / total_Q * 100).round(2).values
+        total_perc_txt = ["(" + str(x) + " %)" for x in total_perc]
+        if field.split('_')[0] == 'PVT':
+            trace2 = go.Bar(x=data_frame.index, y=y, name=field.split('_kWh', 1)[0], text=total_perc_txt,
+                            marker=dict(color=COLOR.get_color_rgb(field.split('_kWh', 1)[0]), line=dict(
+                                color="rgb(105,105,105)", width=1)), opacity=0.7, visible='legendonly', base=0,
+                            width=0.3, offset=0, legendgroup=field.split('_')[1])
+        else:
+            trace2 = go.Bar(x=data_frame.index, y=y, name=field.split('_kWh', 1)[0], text=total_perc_txt,
+                            marker=dict(color=COLOR.get_color_rgb(field.split('_kWh', 1)[0]), line=dict(
+                                color="rgb(105,105,105)", width=1)), opacity=0.7, visible='legendonly', base=0,
+                            width=0.3, offset=0)
 
         graph.append(trace2)
 
     return graph, data_frame.index,
 
-def calc_table(E_analysis_fields, Q_analysis_fields, data_frame):
 
+def calc_table(E_analysis_fields, Q_analysis_fields, data_frame):
     analysis_fields = []
     total_perc = []
     median = []
 
     E_median = data_frame[E_analysis_fields].median().round(2).tolist()
     E_total = data_frame[E_analysis_fields].sum().round(2).tolist()
-    E_total_perc = [str(x)+" ("+str(round(x/sum(E_total)*100,1))+" %)" for x in E_total]
+    E_total_perc = [str(x) + " (" + str(round(x / sum(E_total) * 100, 1)) + " %)" for x in E_total]
     analysis_fields.extend(E_analysis_fields)
     total_perc.extend(E_total_perc)
     median.extend(E_median)
 
-    Q_median = (data_frame[Q_analysis_fields].median()/1000).round(2).tolist() # to MWh
+    Q_median = (data_frame[Q_analysis_fields].median() / 1000).round(2).tolist()  # to MWh
     Q_total = data_frame[Q_analysis_fields].sum().round(2).tolist()
-    Q_total_perc = [str(x)+" ("+str(round(x/sum(Q_total)*100,1))+" %)" for x in Q_total]
+    Q_total_perc = [str(x) + " (" + str(round(x / sum(Q_total) * 100, 1)) + " %)" for x in Q_total]
     analysis_fields.extend(Q_analysis_fields)
     total_perc.extend(Q_total_perc)
     median.extend(Q_median)
 
-    analysis_fields = filter(None,[x for field in analysis_fields for x in field.split('_kWh',1)])
+    analysis_fields = filter(None, [x for field in analysis_fields for x in field.split('_kWh', 1)])
 
     # calculate graph
     anchors = []
@@ -105,10 +120,11 @@ def calc_table(E_analysis_fields, Q_analysis_fields, data_frame):
     for field in Q_analysis_fields:
         anchors.append(calc_top_three_anchor_loads(data_frame, field))
     table = go.Table(domain=dict(x=[0, 1], y=[0.0, 0.2]),
-                            header=dict(values=['Surface', 'Total [MWh/yr]', 'Median [MWh/yr]', 'Top 3 most irradiated']),
-                            cells=dict(values=[analysis_fields, total_perc, median, anchors ]))
+                     header=dict(values=['Surface', 'Total [MWh/yr]', 'Median [MWh/yr]', 'Top 3 most irradiated']),
+                     cells=dict(values=[analysis_fields, total_perc, median, anchors]))
 
     return table
+
 
 def calc_top_three_anchor_loads(data_frame, field):
     data_frame = data_frame.sort_values(by=field, ascending=False)
