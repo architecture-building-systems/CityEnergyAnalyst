b38bea631e435dee0d75aeb3f90bdeb3b4c717fc
Daren Thomas <thomas@arch.ethz.ch>
2018-02-01 12:00:29 +0100

    implemented running scripts - output still needs some work

--- cea/interfaces/launcher/launcher.py
+++ cea/interfaces/launcher/launcher.py
@@ -5,7 +5,10 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import sys
 import json
+import subprocess
+import tempfile
 import htmlPy
 import cea.config
 import cea.interfaces.cli.cli
@@ -31,21 +34,70 @@ class Backend(htmlPy.Object):
         self.config = config
 
     @htmlPy.Slot(str, str, result=None)
-    def save_section(self, section_name, json_data):
-        print("Saving section: %s" % section_name)
-        print(json_data)
+    def run_script(self, script, json_data):
+        print("Running script: %s" % script)
         values = json.loads(json_data)
-        print(values)
-        for key, value in values.items():
-            print("Setting %s to %s" % (key, value))
-            self.config.sections[section_name].parameters[key].set(value)
-        self.config.save()
+        print('json_data: %s' % values)
+
+        parameters = parameters_for_script(script, self.config)
+        run_cli(script, **{p.name: p.encode(values[p.name]) for p in parameters})
         return
 
     @htmlPy.Slot(str, result=str)
-    def get_parameters(self, section_name):
-        result = json.dumps({p.name: p.typename for p in self.config.sections[section_name].parameters.values()})
+    def get_parameters(self, script):
+        """Returns a dictionary mapping all parameters for a script to the respective parameter type name"""
+        result = json.dumps({p.name: p.typename for p in parameters_for_script(script, self.config)})
         return result
+
+
+def parameters_for_script(script, config):
+    """Return a list consisting of :py:class:`cea.config.Parameter` objects for each parameter of a script"""
+    cli_config = cea.interfaces.cli.cli.get_cli_config()
+    parameters = [p for s, p in config.matching_parameters(cli_config.get('config', script).split())]
+    return parameters
+
+
+def add_message(script, message):
+    """Append a message to the output div of a script"""
+    print("%(script)s: %(message)s" % locals())
+    message = message.replace("'", "\\'")
+    app.evaluate_javascript("add_message_js('%(script)s', '%(message)s');" % locals())
+
+
+def run_cli(script, **parameters):
+    """Run the CLI in a subprocess without showing windows"""
+    app.evaluate_javascript('clear("%s")' % script)
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    command = [sys.executable, '-u', '-m', 'cea.interfaces.cli.cli', script]
+    for parameter_name, parameter_value in parameters.items():
+        parameter_name = parameter_name.replace('_', '-')
+        command.append('--' + parameter_name)
+        command.append(str(parameter_value))
+    add_message(script, 'Executing: ' + ' '.join(command))
+    process = subprocess.Popen(command, startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               env=get_environment(), cwd=tempfile.gettempdir())
+    while True:
+        next_line = process.stdout.readline()
+        if next_line == '' and process.poll() is not None:
+            break
+        add_message(script, next_line.rstrip())
+    stdout, stderr = process.communicate()
+    add_message(script, stdout)
+    add_message(script, stderr)
+    if process.returncode != 0:
+        raise Exception('Tool did not run successfully')
+
+
+def get_environment():
+    """Return the system environment to use for the execution - this is based on the location of the python
+    interpreter in ``get_python_exe``"""
+    root_dir = os.path.dirname(sys.executable)
+    scripts_dir = os.path.join(root_dir, 'Scripts')
+    env = os.environ.copy()
+    env['PATH'] = ';'.join((root_dir, scripts_dir, os.environ['PATH']))
+    return os.environ
 
 
 def main(config):
@@ -56,6 +108,7 @@ def main(config):
     :type config: cea.config.Configuration
     :return:
     """
+    global app
     app = htmlPy.AppGUI(title=u"CEA Launcher", maximized=False, developer_mode=True)
 
     app.template_path = os.path.join(BASE_DIR, 'templates')
@@ -63,7 +116,7 @@ def main(config):
 
     cli_config = cea.interfaces.cli.cli.get_cli_config()
     scripts = sorted(cli_config.options('scripts'))
-    parameters = {script_name: list(config.matching_parameters(cli_config.get('config', script_name).split()))
+    parameters = {script_name: parameters_for_script(script_name, config)
                   for script_name in scripts}
 
     app.template = ("launcher.html", {"config": config, "scripts": scripts, "parameters": parameters})
