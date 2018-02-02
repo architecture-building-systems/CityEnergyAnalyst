06ce86308c60d4a1c784fb3e5e980fc06c761865
Daren Thomas <thomas@arch.ethz.ch>
2018-02-02 15:49:30 +0100

    saving changes before they get lost

--- cea/interfaces/launcher/static/js/launcher.js
+++ cea/interfaces/launcher/static/js/launcher.js
@@ -4,40 +4,56 @@ $(document).ready(function() {
     
 });
 
+
 /**
  * Run the current script with the parameters shown.
  * @param script
  */
 function run_script_js(script) {
-    if ($('#run-' + script).attr('disabled')) {
+    if (backend.is_script_running(script)) {
         // avoid running until previous call is done
+        backend.log('disabled');
         return;
     }
-    setTimeout(function(){
-        clear(script);
-        $('#run-' + script).attr('disabled', true);
-        $('#run-' + script).removeClass('btn-primary');
-        $('#run-' + script).addClass('btn-outline-primary');
-    }, 50);
+    $('#output-' + script).empty();
+    $('#run-' + script).attr('disabled', true);
+    $('#run-' + script).removeClass('btn-primary');
+    $('#run-' + script).addClass('btn-outline-primary');
+
+
+    parameters = JSON.parse(backend.get_parameters(script));
+    data = {};
+    for (parameter_name in parameters) {
+        data[parameter_name] = read_value(script, parameter_name, parameters[parameter_name])
+    }
+    backend.run_script(script, JSON.stringify(data));
 
     setTimeout(function() {
-        parameters = JSON.parse(backend.get_parameters(script));
-        data = {};
-        for (parameter_name in parameters) {
-            data[parameter_name] = read_value(script, parameter_name, parameters[parameter_name])
-        }
-    }, 1000);
+        id = setInterval(function() {
+            backend.log('INTERVAL FIRED')
+            append_script_output(script);
 
-    setTimeout(function() {
-        backend.run_script(script, JSON.stringify(data));
+            if (! backend.is_script_running(script)) {
+                backend.log('IS_SCRIPT_RUNNING = FALSE');
+                $('#run-' + script).addClass('btn-primary');
+                $('#run-' + script).removeClass('btn-outline-primary');
+                $('#run-' + script).attr('disabled', false);
+                clearInterval(id);
+            }
+        }, 1000);
     }, 1000);
+    backend.log('EXIT run_script_js')
+}
 
-
-    setTimeout(function() {
-        $('#run-' + script).addClass('btn-primary');
-        $('#run-' + script).removeClass('btn-outline-primary');
-        $('#run-' + script).attr('disabled', false);
-    }, 1000);
+function append_script_output(script) {
+    backend.log('ENTER append_script_output');
+    while (backend.has_output(script)) {
+        next_line = backend.next_output(script);
+        setTimeout(function (next_line) {
+            $('#output-' + script).append(next_line);
+        }, 50, next_line);
+    }
+    backend.log('EXIT append_script_output');
 }
 
 /**
@@ -73,23 +89,4 @@ function read_value(script, parameter_name, parameter_type) {
             value = $('#' + script + '-' + parameter_name)[0].value;
     }
     return value;
-}
-
-/**
- * Append a message to the output div for the script
- * @param script
- * @param message
- */
-function add_message_js(script, message) {
-    //alert(message);
-    try {
-        $('#output-' + script).append(atob(message));
-    }
-    catch (err) {
-        alert(message);
-    }
-}
-
-function clear(script) {
-    $('#output-' + script).empty();
 }
