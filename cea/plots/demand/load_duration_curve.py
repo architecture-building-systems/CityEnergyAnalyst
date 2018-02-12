ff204047efcf2fb4b66eb9c6c48b9309e0c3dd03
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-02-12 16:15:04 +0800

    moving folders around

--- cea/plots/demand/load_duration_curve.py
+++ cea/plots/demand/load_duration_curve.py
@@ -23,7 +23,7 @@ def load_duration_curve(data_frame, analysis_fields, title, output_path):
     layout = go.Layout(images=LOGO, title=title,xaxis=dict(title='Duration Normalized [%]', domain=[0, 1]),
                        yaxis=dict(title='Load [kW]', domain=[0.0, 0.7]))
     fig = go.Figure(data=traces_graph, layout=layout)
-    plot(fig, auto_open=False, filename=output_path)
+    return plot(fig, auto_open=False, filename=output_path)
 
 
 def calc_table(analysis_fields, data_frame):
