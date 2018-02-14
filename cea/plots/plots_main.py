055c1bb82bfcf8c924bc51413c4bfc7f356c889c
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-02-14 17:51:03 +0800

    fixing solar technologies to generate all outputs

--- cea/plots/plots_main.py
+++ cea/plots/plots_main.py
@@ -41,26 +41,31 @@ def plots_main(config):
         locator = cea.inputlocator.InputLocator(config.scenario)
         dashboard_solar(locator, config)
         print("solar potential plots successfully saved in plots folder of scenario: ", config.scenario)
+
     if "technology_potentials" in categories_to_plot:
-        from cea.plots.solar_technology_potentials.dashboard_district import dashboard as dashboard_technology
+        from cea.plots.solar_technology_potentials.dashboard_district import plot_main as dashboard_technology
         locator = cea.inputlocator.InputLocator(config.scenario)
         dashboard_technology(locator, config)
         print("technology potential plots successfully saved in plots folder of scenario: ", config.scenario)
+
     if "energy_demand" in categories_to_plot:
         from cea.plots.demand.dashboard_district import dashboard as dashboard_demand
         locator = cea.inputlocator.InputLocator(config.scenario)
         dashboard_demand(locator, config)
         print("energy demand plots successfully saved in plots folder of scenario: ", config.scenario)
+
     if "life_cycle" in categories_to_plot:
         from cea.plots.life_cycle.dashboard_district import plots_main as dashboard_lca
         locator = cea.inputlocator.InputLocator(config.scenario)
         dashboard_lca(locator, config)
         print("life cycle plots successfully saved in plots folder of scenario: ", config.scenario)
+
     if "optimization" in categories_to_plot:
         from cea.plots.life_cycle.dashboard_district import plots_main as dashboard_lca
         locator = cea.inputlocator.InputLocator(config.scenario)
         dashboard_lca(locator, config)
         print("optimization plots successfully saved in plots folder of scenario: ", config.scenario)
+
     if "scenarios_comparisons" in categories_to_plot:
         from cea.plots.comparisons.dashboard_district import plots_main as dashboard_comparisons
         dashboard_comparisons(config)
@@ -70,10 +75,12 @@ def plots_main(config):
     time_elapsed = time.clock() - t0
     print('done - time elapsed: %d.2f seconds' % time_elapsed)
 
+
 def main(config):
     # print out all configuration variables used by this script
     print("Running plots for the next categories = %s" % config.dashboard.categories)
     plots_main(config)
+
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
