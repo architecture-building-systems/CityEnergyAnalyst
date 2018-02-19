973904b7c4ff8c6ba6ba79e0e9a6256f581a1524
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-02-19 11:19:45 +0800

    chaning inputs

--- cea/plots/optimization/individual_activation_curve.py
+++ cea/plots/optimization/individual_activation_curve.py
@@ -23,7 +23,7 @@ def individual_activation_curve(data_frame, analysis_fields_loads, analysis_fiel
 
 def calc_graph(analysis_fields, analysis_fields_loads, data_frame):
     # main data about technologies
-    data = (data_frame['activation_units_data'] / 1000).round(2)  # to kW
+    data = (data_frame/1000).round(2)  # to kW
     graph = []
     for field in analysis_fields:
         y = data[field].values
@@ -31,10 +31,10 @@ def calc_graph(analysis_fields, analysis_fields_loads, data_frame):
         graph.append(trace)
 
     # data about demand
-    data_demand = (data_frame['buildings_demand_W'] / 1000).round(2)  # to kW
-    y = data_demand[analysis_fields_loads]
-    trace = go.Scatter(x=data.index, y=y, name=analysis_fields_loads,
-                       line=dict(color=COLOR.get_color_rgb(analysis_fields_loads), width=1))
+    for field in analysis_fields_loads:
+        y = data[field]
+        trace = go.Scatter(x=data.index, y=y, name=field,
+                           line=dict(color=COLOR.get_color_rgb(field), width=1))
 
     graph.append(trace)
 
