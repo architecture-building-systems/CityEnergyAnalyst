290fa894acb95e3dd6094b85348f82d16ce4d952
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2018-02-23 17:47:06 +0800

    changing the cooling operation to be similar to heating

--- cea/optimization/slave/cooling_net.py
+++ cea/optimization/slave/cooling_net.py
@@ -31,19 +31,19 @@ __status__ = "Production"
 
 # technical model
 
-def coolingMain(locator, configKey, ntwFeat, HRdata, gv, prices):
+def coolingMain(locator, configKey, ntwFeat, heat_recovery_data_center, gv, prices):
     """
     Computes the parameters for the cooling of the complete DCN
 
     :param locator: path to res folder
     :param configKey: configuration key for the District Heating Network (DHN)
     :param ntwFeat: network features
-    :param HRdata: Heat recovery data, 0 if no heat recovery data, 1 if so
+    :param heat_recovery_data_center: Heat recovery data, 0 if no heat recovery data, 1 if so
     :param gv: global variables
     :type locator: string
     :type configKey: string
     :type ntwFeat: class
-    :type HRdata: int
+    :type heat_recovery_data_center: int
     :type gv: class
     :return: costs, co2, prim
     :rtype: tuple
@@ -69,12 +69,11 @@ def coolingMain(locator, configKey, ntwFeat, HRdata, gv, prices):
     try:
         dfSlave = pd.read_csv(locator.get_optimization_slave_pp_activation_pattern(configKey), usecols=["Q_coldsource_HPLake_W"])
         Q_lake_Array_W = np.array(dfSlave)
-        Q_lake_W = np.sum(Q_lake_Array_W)
 
     except:
-        Q_lake_W = 0
+        Q_lake_Array_W = [0]
 
-    Q_avail_W = DeltaU + Q_lake_W
+    Q_avail_W = DeltaU + np.sum(Q_lake_Array_W)
 
     ############# Output results
     costs = ntwFeat.pipesCosts_DCN
@@ -83,31 +82,58 @@ def coolingMain(locator, configKey, ntwFeat, HRdata, gv, prices):
 
     nBuild = int(np.shape(arrayData)[0])
     nHour = int(np.shape(coolArray)[0])
-    CT_Load_W = np.zeros(nHour)
     VCC_nom_W = 0
 
-    calFactor = 0
-    TotalCool = 0
+    calfactor = []
+    TotalCool = []
+    Q_cooling_from_Lake_W = []
+    Q_cooling_from_VCC_W = []
+    CT_load_from_VCC_W = np.zeros(8760)
+    opex_var = []
+    co2_list = []
+    prim_list = []
+    calfactor_total = 0
 
-    ############ Function for cooling operation
+    VCC_nom_Ini_W = []
+    Q_from_Lake_cumulative_W = 0
 
 
-    ########## Cooling operation with Circulating pump and VCC
+    for hour in range(8760):
+        opex_output, co2_output, prim_output, calfactor_output, Q_from_Lake_W, Q_from_VCC_W, CT_Load_W = cooling_resource_activator(
+            coolArray, hour, Q_avail_W, gv, Q_from_Lake_cumulative_W, prices, TempSup=T_sup_Cool_K)
 
-    opex_total, co2_total, prim_total, calfactor_total, Q_cooling_total, Q_availCopy_W, VCC_nom_Ini_W = coolOperation(coolArray, nHour, Q_avail_W,
-                                                                                            TempSup=T_sup_Cool_K)
-    costs += opex_total
-    CO2 += co2_total
-    prim += prim_total
-    calFactor += calfactor_total
-    TotalCool += Q_cooling_total
+        Q_from_Lake_cumulative_W = Q_from_Lake_cumulative_W + Q_from_Lake_W
+        opex_var.append(opex_output)
+        co2_list.append(co2_output)
+        prim_list.append(prim_output)
+        calfactor.append(calfactor_output)
+        Q_cooling_from_Lake_W.append(Q_from_Lake_W)
+        Q_cooling_from_VCC_W.append(Q_from_VCC_W)
+        CT_load_from_VCC_W[hour] = CT_Load_W
+
+    # Fix this with VCC max size
+    # if Q_need_W > VCC_nom_Ini_W:
+    #     VCC_nom_Ini_W = Q_need_W * (1 + Qmargin_Disc)
+
+    costs += np.sum(opex_var)
+    CO2 += np.sum(co2_list)
+    prim += np.sum(prim_list)
+    calfactor_total += np.sum(calfactor)
+    TotalCool += np.sum(Q_cooling_from_Lake_W) + np.sum(Q_cooling_from_VCC_W)
+    VCC_nom_Ini_W = np.amax(Q_cooling_from_VCC_W) * (1 + Qmargin_Disc)
     VCC_nom_W = max(VCC_nom_W, VCC_nom_Ini_W)
-    Q_avail_W = Q_availCopy_W
 
     mdot_Max_kgpers = np.amax(coolArray[:, 1])
     Capex_pump, Opex_fixed_pump = PumpModel.calc_Cinv_pump(2 * ntwFeat.DeltaP_DCN, mdot_Max_kgpers, etaPump, gv, locator)
     costs += (Capex_pump + Opex_fixed_pump)
-    if HRdata == 0:
+    if heat_recovery_data_center == 0:
+        opex_var_data_center = []
+        co2_list_data_center = []
+        prim_list_data_center = []
+        calfactor_data_center = []
+        Q_cooling_from_Lake_W_data_center = []
+        Q_cooling_from_VCC_W_data_center = []
+        CT_load_from_VCC_W_data_center = np.zeros(8760)
         for i in range(nBuild):
             if arrayData[i][1] > 0:
                 buildName = arrayData[i][0]
@@ -119,16 +145,33 @@ def coolingMain(locator, configKey, ntwFeat, HRdata, gv, prices):
                 mdot_max_Data_kWperC = abs(np.amax(arrayBuild[:, -1]) / gv.cp * 1E3)
                 Capex_pump, Opex_fixed_pump = PumpModel.calc_Cinv_pump(2 * ntwFeat.DeltaP_DCN, mdot_max_Data_kWperC, etaPump, gv, locator)
                 costs += (Capex_pump + Opex_fixed_pump)
-                opex_total, co2_total, prim_total, calfactor_total, Q_cooling_total, Q_availCopy_W, VCC_nom_Ini_W = coolOperation(arrayBuild,
-                                                                                                        nHour, Q_avail_W)
-                costs += opex_total
-                CO2 += co2_total
-                prim += prim_total
-                calFactor += calfactor_total
-                TotalCool += Q_cooling_total
-                VCC_nom_W = max(VCC_nom_W, VCC_nom_Ini_W)
-                Q_avail_W = Q_availCopy_W
+                for hour in range(8760):
+                    opex_output, co2_output, prim_output, calfactor_output, Q_from_Lake_W, Q_from_VCC_W, CT_Load_W = cooling_resource_activator(
+                        coolArray, hour, Q_avail_W, gv, TempSup=T_sup_Cool_K)
 
+                    opex_var_data_center.append(opex_output)
+                    co2_list_data_center.append(co2_output)
+                    prim_list_data_center.append(prim_output)
+                    calfactor_data_center.append(calfactor_output)
+                    Q_cooling_from_Lake_W_data_center.append(Q_from_Lake_W)
+                    Q_cooling_from_VCC_W_data_center.append(Q_from_VCC_W)
+                    CT_load_from_VCC_W_data_center[hour] = CT_Load_W
+
+                costs += np.sum(opex_var_data_center)
+                CO2 += np.sum(co2_list_data_center)
+                prim += np.sum(prim_list_data_center)
+                calfactor_total += np.sum(calfactor_data_center)
+                TotalCool += np.sum(Q_cooling_from_Lake_W_data_center) + np.sum(Q_cooling_from_VCC_W_data_center)
+                VCC_nom_Ini_W = np.amax(Q_cooling_from_VCC_W_data_center) * (1 + Qmargin_Disc)
+                VCC_nom_W = max(VCC_nom_W, VCC_nom_Ini_W)
+
+    opex_var_ice_ring = []
+    co2_list_ice_ring = []
+    prim_list_ice_ring = []
+    calfactor_ice_ring = []
+    Q_cooling_from_Lake_W_ice_ring = []
+    Q_cooling_from_VCC_W_ice_ring = []
+    CT_load_from_VCC_W_ice_ring = np.zeros(8760)
     for i in range(nBuild):
         if arrayQice[i][1] > 0:
             buildName = arrayQice[i][0]
@@ -139,29 +182,39 @@ def coolingMain(locator, configKey, ntwFeat, HRdata, gv, prices):
             mdot_max_ice_kgpers = abs(np.amax(arrayBuild[:, -1]) / gv.cp * 1E3)
             Capex_pump, Opex_fixed_pump = PumpModel.calc_Cinv_pump(2 * ntwFeat.DeltaP_DCN, mdot_max_ice_kgpers, etaPump, gv, locator)
             costs += (Capex_pump + Opex_fixed_pump)
-            opex_total, co2_total, prim_total, calfactor_total, Q_cooling_total, Q_availCopy_W, VCC_nom_Ini_W = coolOperation(arrayBuild, nHour,
-                                                                                                    Q_avail_W)
-            costs += opex_total
-            CO2 += co2_total
-            prim += prim_total
-            calFactor += calfactor_total
-            TotalCool += Q_cooling_total
+            for hour in range(8760):
+                opex_output, co2_output, prim_output, calfactor_output, Q_from_Lake_W, Q_from_VCC_W, CT_Load_W = cooling_resource_activator(
+                    arrayBuild, hour, Q_avail_W, gv, TempSup=T_sup_Cool_K)
+
+                opex_var_ice_ring.append(opex_output)
+                co2_list_ice_ring.append(co2_output)
+                prim_list_ice_ring.append(prim_output)
+                calfactor_ice_ring.append(calfactor_output)
+                Q_cooling_from_Lake_W_ice_ring.append(Q_from_Lake_W)
+                Q_cooling_from_VCC_W_ice_ring.append(Q_from_VCC_W)
+                CT_load_from_VCC_W_ice_ring[hour] = CT_Load_W
+
+            costs += np.sum(opex_var_ice_ring)
+            CO2 += np.sum(co2_list_ice_ring)
+            prim += np.sum(prim_list_ice_ring)
+            calfactor_total += np.sum(calfactor_ice_ring)
+            TotalCool += np.sum(Q_cooling_from_Lake_W_ice_ring) + np.sum(Q_cooling_from_VCC_W_ice_ring)
+            VCC_nom_Ini_W = np.amax(Q_cooling_from_VCC_W_ice_ring) * (1 + Qmargin_Disc)
             VCC_nom_W = max(VCC_nom_W, VCC_nom_Ini_W)
-            Q_avail_W = Q_availCopy_W
 
 
     ########## Operation of the cooling tower
-    CT_nom_W = np.amax(CT_Load_W)
-    costCopy = costs
+    CT_max_from_VCC = np.amax(CT_load_from_VCC_W)
+    CT_max_from_ice_ring = np.amax(CT_load_from_VCC_W_ice_ring)
+    CT_max_from_data_center = np.amax(CT_load_from_VCC_W_data_center)
+    CT_nom_W = max(CT_max_from_VCC, CT_max_from_data_center, CT_max_from_ice_ring)
     if CT_nom_W > 0:
         for i in range(nHour):
-            wdot = CTModel.calc_CT(CT_Load_W[i], CT_nom_W, gv)
+            wdot = CTModel.calc_CT(CT_load_from_VCC_W[i], CT_nom_W, gv)
 
             costs += wdot * prices.ELEC_PRICE
             CO2 += wdot * EL_TO_CO2 * 3600E-6
             prim += wdot * EL_TO_OIL_EQ * 3600E-6
-
-
 
     ########## Add investment costs
 
@@ -172,7 +225,7 @@ def coolingMain(locator, configKey, ntwFeat, HRdata, gv, prices):
 
 
     ########### Adjust and add the pumps for filtering and pre-treatment of the water
-    calibration = calFactor / 50976000
+    calibration = calfactor_total / 50976000
 
     extraElec = (127865400 + 85243600) * calibration
     costs += extraElec * prices.ELEC_PRICE
--- cea/optimization/slave/cooling_resource_activation.py
+++ cea/optimization/slave/cooling_resource_activation.py
@@ -4,63 +4,54 @@ import numpy as np
 from cea.optimization.constants import *
 from cea.optimization import prices
 import pandas as pd
+import cea.technologies.cooling_tower as CTModel
+import cea.technologies.chillers as VCCModel
+import cea.technologies.pumps as PumpModel
 
-def cooling_resource_activator(dataArray, el, Q_availIni_W, TempSup=0):
+def cooling_resource_activator(cool_array, hour, Q_avail_W, gv, Q_from_Lake_cumulative_W, prices, TempSup):
     """
     :param dataArray:
     :param el:
-    :param Q_availIni_W:
+    :param Q_avail_W:
     :param TempSup:
     :type dataArray: list
     :type el:
-    :type Q_availIni_W: float?
+    :type Q_avail_W: float?
     :type TempSup:
     :return: toCosts, toCO2, toPrim, toCalfactor, toTotalCool, QavailCopy, VCCnomIni
     :rtype: float, float, float, float, float, float, float
     """
-    Q_cooling_total = 0
-    calfactor_total = 0
-    opex_total = 0
-    co2_total = 0
-    prim_total = 0
 
-    Q_availCopy_W = Q_availIni_W
-    VCC_nom_Ini_W = 0
+    # if TempSup > 0:
+    # print (cool_array[hour])
+    T_sup_K = TempSup
+    T_re_K = cool_array[hour][-2]
+    mdot_kgpers = abs(cool_array[hour][-1])
+    # else:
+    #     T_sup_K = cool_array[hour][-3] + 273
+    #     T_re_K = cool_array[hour][-2] + 273
+    #     mdot_kgpers = abs(cool_array[hour][-1] * 1E3 / gv.cp)
 
-    for i in range(8760):
+    Q_need_W = abs(mdot_kgpers * gv.cp * (T_re_K - T_sup_K))
 
-        if TempSup > 0:
-            T_sup_K = TempSup
-            T_re_K = dataArray[i][-2]
-            mdot_kgpers = abs(dataArray[i][-1])
-        else:
-            T_sup_K = dataArray[i][-3] + 273
-            T_re_K = dataArray[i][-2] + 273
-            mdot_kgpers = abs(dataArray[i][-1] * 1E3 / gv.cp)
+    if Q_from_Lake_cumulative_W + Q_need_W <= Q_avail_W:  # Free cooling possible from the lake
+        Q_from_Lake_W = Q_need_W
+        Q_from_VCC_W = 0
+        CT_Load_W = 0
+        # Delta P from linearization after distribution optimization
+        deltaP = 2 * (DeltaP_Coeff * mdot_kgpers + DeltaP_Origin)
+        calfactor_output = deltaP * mdot_kgpers / 1000 / etaPump
+        opex_output = deltaP * mdot_kgpers / 1000 * prices.ELEC_PRICE / etaPump
+        co2_output = deltaP * mdot_kgpers / 1000 * EL_TO_CO2 / etaPump * 0.0036
+        prim_output = deltaP * mdot_kgpers / 1000 * EL_TO_OIL_EQ / etaPump * 0.0036
+    else:
+        Q_from_Lake_W = 0
+        Q_from_VCC_W = Q_need_W
+        wdot_W, qhotdot_W = VCCModel.calc_VCC(mdot_kgpers, T_sup_K, T_re_K, gv)
+        calfactor_output = 0
+        opex_output = wdot_W * prices.ELEC_PRICE
+        co2_output = wdot_W * EL_TO_CO2 * 3600E-6
+        prim_output = wdot_W * EL_TO_OIL_EQ * 3600E-6
+        CT_Load_W = qhotdot_W
 
-        Q_need_W = abs(mdot_kgpers * gv.cp * (T_re_K - T_sup_K))
-        Q_cooling_total += Q_need_W
-
-        if Q_availCopy_W - Q_need_W >= 0:  # Free cooling possible from the lake
-            Q_availCopy_W -= Q_need_W
-
-            # Delta P from linearization after distribution optimization
-            deltaP = 2 * (DeltaP_Coeff * mdot_kgpers + DeltaP_Origin)
-
-            calfactor_total += deltaP * mdot_kgpers / 1000 / etaPump
-            opex_total += deltaP * mdot_kgpers / 1000 * prices.ELEC_PRICE / etaPump
-            co2_total += deltaP * mdot_kgpers / 1000 * EL_TO_CO2 / etaPump * 0.0036
-            prim_total += deltaP * mdot_kgpers / 1000 * EL_TO_OIL_EQ / etaPump * 0.0036
-
-        else:
-            wdot_W, qhotdot_W = VCCModel.calc_VCC(mdot_kgpers, T_sup_K, T_re_K, gv)
-            if Q_need_W > VCC_nom_Ini_W:
-                VCC_nom_Ini_W = Q_need_W * (1 + Qmargin_Disc)
-
-            opex_total += wdot_W * prices.ELEC_PRICE
-            co2_total += wdot_W * EL_TO_CO2 * 3600E-6
-            prim_total += wdot_W * EL_TO_OIL_EQ * 3600E-6
-
-            CT_Load_W[i] += qhotdot_W
-
-    return opex_total, co2_total, prim_total, calfactor_total, Q_cooling_total, Q_availCopy_W, VCC_nom_Ini_W
+    return opex_output, co2_output, prim_output, calfactor_output, Q_from_Lake_W, Q_from_VCC_W, CT_Load_W
