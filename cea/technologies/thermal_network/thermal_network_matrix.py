f7cbd5f50b77dbae3c81109d7c4da0106550e991
Daren Thomas <thomas@arch.ethz.ch>
2018-03-14 12:40:01 +0100

    t_target_supply_df --> thermal_network

--- cea/technologies/thermal_network/thermal_network_matrix.py
+++ cea/technologies/thermal_network/thermal_network_matrix.py
@@ -1498,7 +1498,6 @@ def solve_network_temperatures(locator, gv, thermal_network, network_parameters,
                                                                        thermal_network.edge_node_df)
 
         # initialize target temperatures in Kelvin as initial value for K_value calculation
-        initial_guess_temp = np.asarray(network_parameters['t_target_supply_df'].loc[t] + 273.15, order='C')
         t_edge__k = calc_edge_temperatures(initial_guess_temp, thermal_network.edge_node_df)
         # initialization of K_value
         k = calc_aggregated_heat_conduction_coefficient(network_parameters['edge_mass_flow'].ix[t].values,
@@ -1512,8 +1511,7 @@ def solve_network_temperatures(locator, gv, thermal_network, network_parameters,
                                                                                   thermal_network.edge_node_df,
                                                                                   network_parameters[
                                                                                       'edge_mass_flow'].ix[t].values, k,
-                                                                                  network_parameters[
-                                                                                      't_target_supply_df'].loc[t],
+                                                                                  thermal_network.t_target_supply_df.loc[t],
                                                                                   thermal_network.network_type,
                                                                                   thermal_network.all_nodes_df)
 
