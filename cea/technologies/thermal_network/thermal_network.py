4b11afb618f15f24d042fabf7f29e22e364b084d
Lenny Rogenhofer <lennartr@ethz.ch>
2018-03-22 11:40:03 +0800

    Last file, somehow this got left behind

--- cea/technologies/thermal_network/thermal_network.py
+++ cea/technologies/thermal_network/thermal_network.py
@@ -4,8 +4,9 @@ hydraulic network
 
 
 from __future__ import division
+import pandas as pd
 
-__author__ = "Thuy-An Nguyen"
+__author__ = "Thuy-An Nguyen, Lennart Rogenhofer"
 __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
 __credits__ = ["Thuy-An Nguyen", "Tim Vollrath", "Jimeno A. Fonseca"]
 __license__ = "MIT"
@@ -37,3 +38,48 @@ def calc_Cinv_network_linear(LengthNetwork, gV):
 
     return InvCa
 
+# Pumping Cost
+
+def calc_Closs_pressure(locator, network_name, network_type):
+    """
+    This function calculates the cost caused by the pump making up for the pressure losses in the system
+    :param locator:
+    :param network_name:
+    :param network_type:
+
+    :return hourly_cost:
+    :return total_cost:
+    """
+    #read in electric requirement of pump per timestep
+    pressure_loss_kw = pd.read_csv(locator.get_ploss(network_name, network_type))
+    # read in electricity price
+    electricity_price = 1 # todo: find timeseries of data and read in
+    #multiply with electricity cost
+    hourly_cost = pressure_loss_kw * electricity_price
+    #sum over all hours
+    total_cost = sum(hourly_cost)
+    return hourly_cost, total_cost
+
+
+# Thermal losses Cost
+def calc_Closs_heat(locator, network_name, network_type):
+    """
+    This function calculates the cost of additional heat which has to be produced to make up for heat
+    losses in the system
+
+    :param locator:
+    :param network_name:
+    :param network_type:
+
+    :return hourly_cost:
+    :return total_cost:
+    """
+    # read in electric requirement of pump per timestep
+    heat_loss_kw = pd.read_csv(locator.get_qloss(network_name, network_type))
+    # read in electricity price
+    heat_price = 1  # todo: find timeseries of data of cost of producing 1 kWh at plant in that timestep and read in
+    # multiply with electricity cost
+    hourly_cost = heat_loss_kw * heat_price
+    # sum over all hours
+    total_cost = sum(hourly_cost)
+    return hourly_cost, total_cost
