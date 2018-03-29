d4cfad00b624cbd73010bb68ac5412d41c388c37
Daren Thomas <thomas@arch.ethz.ch>
2018-03-29 15:57:59 +0200

    first stap at creating a trace of inputlocator usage

---
+++ cea/tests/trace_inputlocator.py
@@ -0,0 +1,67 @@
+"""
+Trace the InputLocator calls in a vareity of scripts.
+"""
+import sys
+import os
+
+import cea.config
+import cea.api
+import pprint
+from collections import defaultdict
+from datetime import datetime
+from jinja2 import Template
+
+
+def create_trace_function(results_set):
+    """results_set is a set of tuples (locator, filename)"""
+    def trace_function(frame, event, arg):
+        """Trace any calls to the InputLocator"""
+        co = frame.f_code
+        func_name = co.co_name
+        if func_name == 'write':
+            # Ignore write() calls from print statements
+            return
+        line_no = frame.f_lineno
+        filename = co.co_filename
+        if event == 'call':
+            # decend into the stack...
+            return trace_function
+        elif event == 'return':
+            if 'inputlocator' in filename.lower() and not func_name.startswith('_'):
+                results_set.add((func_name, arg))
+                print('%s => %s' % (func_name, arg))
+        return
+    return trace_function
+
+
+def main(config):
+    # force single-threaded execution, see settrace docs for why
+    config.multiprocessing = False
+    # scripts = ['data-helper', 'demand']
+    scripts = ['data-helper']
+
+    trace_data = set()  # {(direction, script, locator, file)}
+    orig_trace = sys.gettrace()
+    for script_name in scripts:
+        script_start = datetime.now()
+        script_func = getattr(cea.api, script_name.replace('-', '_'))
+        results_set = set()  # {(locator, filename)}
+
+        sys.settrace(create_trace_function(results_set))
+        script_func()
+        sys.settrace(orig_trace)
+
+        for locator, filename in results_set:
+            print locator, filename
+            mtime = datetime.fromtimestamp(os.path.getmtime(filename))
+            if script_start < mtime:
+                trace_data.add(('output', script_name, locator, filename))
+            else:
+                trace_data.add(('input', script_name, locator, filename))
+
+    template_path = os.path.join(os.path.dirname(__file__), 'trace_inputlocator.gv')
+    template = Template(open(template_path, 'r'))
+    print(template.render(trace_data=trace_data))
+
+if __name__ == '__main__':
+    main(cea.config.Configuration())
