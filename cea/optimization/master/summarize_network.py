def5d69de5d76fed7c60be64a77036c4ff31ed1a
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2018-04-16 17:13:17 +0800

    fixing network calculations as the mdot is taken as min instead of all

--- cea/optimization/master/summarize_network.py
+++ cea/optimization/master/summarize_network.py
@@ -192,23 +192,23 @@ def network_main(locator, total_demand, building_names, config, gv, key):
                                                      HEAT_CAPACITY_OF_WATER_JPERKGK, "positive")
 
     T_DCN_space_cooling_and_refrigeration_re_K = np.vectorize(calc_temp_withlosses)(T_DCN_space_cooling_and_refrigeration_withoutlosses_re_K,
-                                                                                    Q_DC_space_cooling_and_refrigeration_losses_sup_W,
-                                                                                    mdot_cool_space_cooling_and_refrigeration_netw_min_kgpers,
+                                                                                    Q_DC_space_cooling_and_refrigeration_losses_re_W,
+                                                                                    mdot_cool_space_cooling_and_refrigeration_netw_all_kgpers,
                                                                                     HEAT_CAPACITY_OF_WATER_JPERKGK, "positive")
 
     T_DCN_space_cooling_data_center_and_refrigeration_re_K = np.vectorize(calc_temp_withlosses)(T_DCN_space_cooling_data_center_and_refrigeration_withoutlosses_re_K,
-                                                                                    Q_DC_space_cooling_data_center_and_refrigeration_losses_sup_W,
-                                                                                    mdot_cool_space_cooling_data_center_and_refrigeration_netw_min_kgpers,
+                                                                                    Q_DC_space_cooling_data_center_and_refrigeration_losses_re_W,
+                                                                                    mdot_cool_space_cooling_data_center_and_refrigeration_netw_all_kgpers,
                                                                                     HEAT_CAPACITY_OF_WATER_JPERKGK, "positive")
 
     T_DCN_space_cooling_and_refrigeration_sup_K = np.vectorize(calc_temp_withlosses)(T_DCN_space_cooling_and_refrigeration_withoutlosses_sup_K,
                                                                                      Q_DC_space_cooling_and_refrigeration_losses_sup_W,
-                                                                                     mdot_cool_space_cooling_and_refrigeration_netw_min_kgpers,
+                                                                                     mdot_cool_space_cooling_and_refrigeration_netw_all_kgpers,
                                                                                      HEAT_CAPACITY_OF_WATER_JPERKGK, "negative")
 
     T_DCN_space_cooling_data_center_and_refrigeration_sup_K = np.vectorize(calc_temp_withlosses)(T_DCN_space_cooling_data_center_and_refrigeration_withoutlosses_sup_K,
                                                                                      Q_DC_space_cooling_data_center_and_refrigeration_losses_sup_W,
-                                                                                     mdot_cool_space_cooling_data_center_and_refrigeration_netw_min_kgpers,
+                                                                                     mdot_cool_space_cooling_data_center_and_refrigeration_netw_all_kgpers,
                                                                                      HEAT_CAPACITY_OF_WATER_JPERKGK, "negative")
 
     day_of_max_heatmassflow_fin = np.zeros(8760)
