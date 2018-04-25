9b63f2fb22d2624f542f29973f1322fc0d37f8fc
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-04-25 11:21:57 +0800

    fixing error

--- cea/technologies/heatpumps.py
+++ cea/technologies/heatpumps.py
@@ -7,7 +7,7 @@ from __future__ import division
 from math import floor, log
 import pandas as pd
 from cea.optimization.constants import HP_DELTA_T_COND, HP_DELTA_T_EVAP, HP_ETA_EX, HP_AUXRATIO, GHP_AUXRATIO, \
-    HP_MAX_T_COND, T_GROUND, GHP_ETA_EX, GHP_CMAX_SIZE_TH, HP_MAX_SIZE
+    HP_MAX_T_COND, GHP_ETA_EX, GHP_CMAX_SIZE_TH, HP_MAX_SIZE
 from cea.constants import HEAT_CAPACITY_OF_WATER_JPERKGK
 
 __author__ = "Thuy-An Nguyen"
@@ -66,7 +66,7 @@ def HP_air_air(mdot_cp_WC, t_sup_K, t_re_K, tsource_K):
     return E_req_W
 
 
-def calc_Cop_GHP(mdot_kgpers, T_DH_sup_K, T_re_K):
+def calc_Cop_GHP(ground_temp, mdot_kgpers, T_DH_sup_K, T_re_K):
     """
     For the operation of a Geothermal heat pump (GSHP) supplying DHN.
 
@@ -104,7 +104,7 @@ def calc_Cop_GHP(mdot_kgpers, T_DH_sup_K, T_re_K):
         tsup2_K = tcond_K - HP_DELTA_T_COND  # lower the supply temp if necessary, tsup2 < tsup if max load is not enough
 
     # calculate evaporator temperature
-    tevap_K = T_GROUND - HP_DELTA_T_EVAP
+    tevap_K = ground_temp - HP_DELTA_T_EVAP
     COP = GHP_ETA_EX / (1 - tevap_K / tcond_K)     # [O. Ozgener et al., 2005]_
 
     qhotdot_W = mdot_kgpers * HEAT_CAPACITY_OF_WATER_JPERKGK * (tsup2_K - T_re_K)
