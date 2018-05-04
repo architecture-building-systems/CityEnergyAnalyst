5b0ad559e120cb3dd92e65f502fb76b1874bacca
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2018-05-04 14:36:06 +0800

    fixing example

--- cea/demand/CONCEPT/example_1_pyomo.py
+++ cea/demand/CONCEPT/example_1_pyomo.py
@@ -45,9 +45,13 @@ B = 2
 C = 1
 D = 0
 
-model.x = Var([nx, predictionHorizon + 1])
-model.u = Var([nu, predictionHorizon])
-model.y = Var([ny, predictionHorizon + 1])
+# model.x = Var([nx, predictionHorizon + 1])
+# model.u = Var([nu, predictionHorizon])
+# model.y = Var([ny, predictionHorizon + 1])
+
+model.x = Var(range(predictionHorizon+1 + 1))
+model.u = Var(range(predictionHorizon + 1))
+model.y = Var(range(predictionHorizon + 1 + 1))
 
 # for i in range(predictionHorizon):
 #     Constraint =
@@ -59,13 +63,29 @@ model.Constraint4 = Constraint(expr=-1 <= model.y[1] <= 1)
 
 model.Constraint5 = Constraint(expr=model.x[3] == A*model.x[2] + B*model.u[2])
 model.Constraint6 = Constraint(expr=model.y[2] == C*model.x[3] + D*model.u[2])
-model.Constraint7 = Constraint(expr=model.x[2] == x0)
-model.Constraint8 = Constraint(expr=-1 <= model.y[2] <= 1)
 
-model.Constraint9 = Constraint(expr=model.x[4] == A*model.x[3] + B*model.u[3])
-model.Constraint10 = Constraint(expr=model.y[3] == C*model.x[4] + D*model.u[3])
-model.Constraint11 = Constraint(expr=model.x[3] == x0)
-model.Constraint12 = Constraint(expr=-1 <= model.y[3] <= 1)
+model.Constraint7 = Constraint(expr=model.x[4] == A*model.x[3] + B*model.u[3])
+model.Constraint8 = Constraint(expr=model.y[3] == C*model.x[4] + D*model.u[3])
+
+
+model.Constraint9 = Constraint(expr=model.x[5] == A*model.x[4] + B*model.u[4])
+model.Constraint10 = Constraint(expr=model.y[4] == C*model.x[5] + D*model.u[4])
+
+model.Constraint11 = Constraint(expr=model.x[6] == A*model.x[5] + B*model.u[5])
+model.Constraint12 = Constraint(expr=model.y[5] == C*model.x[6] + D*model.u[5])
+
+model.Constraint11 = Constraint(expr=model.x[7] == A*model.x[6] + B*model.u[6])
+model.Constraint12 = Constraint(expr=model.y[6] == C*model.x[7] + D*model.u[6])
+
+model.Constraint13 = Constraint(expr=model.x[8] == A*model.x[7] + B*model.u[7])
+model.Constraint14 = Constraint(expr=model.y[7] == C*model.x[8] + D*model.u[7])
+
+model.Constraint15 = Constraint(expr=model.x[9] == A*model.x[8] + B*model.u[8])
+model.Constraint16 = Constraint(expr=model.y[8] == C*model.x[9] + D*model.u[8])
+
+model.Constraint17 = Constraint(expr=model.x[10] == A*model.x[9] + B*model.u[9])
+model.Constraint18 = Constraint(expr=model.y[9] == C*model.x[10] + D*model.u[9])
+
 
 model.OBJ = Objective(expr=c*model.u[1])
 
@@ -79,3 +99,5 @@ print(x_value1)
 
 x_value2 = np.array(model.x[1].value)
 print(x_value2)
+
+print (model.y[9].value)
