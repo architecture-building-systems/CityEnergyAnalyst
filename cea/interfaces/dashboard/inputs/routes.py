71abe101b34e4c31d03bb86210bd0522cc692e4b
Daren Thomas <thomas@arch.ethz.ch>
2018-05-04 11:55:57 +0200

    test.html - first steps towards editable tables...

--- cea/interfaces/dashboard/inputs/routes.py
+++ cea/interfaces/dashboard/inputs/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, current_app, request, abort, make_response
+from flask import Blueprint, render_template, redirect, url_for, current_app, request, abort, make_response, jsonify
 
 import cea.inputlocator
 import geopandas
+import numpy as np
 
 blueprint = Blueprint(
     'inputs_blueprint',
@@ -11,6 +12,33 @@ blueprint = Blueprint(
     static_folder='static',
 )
 
+
+@blueprint.route('/test')
+def route_test():
+    return render_template('test.html')
+
+
+@blueprint.route('/json/<db>',  methods=['GET'])
+def route_get_json(db):
+    """Return the records of an input database in JSON format suitable for the bootstrap-table-editable"""
+    locator = cea.inputlocator.InputLocator(current_app.cea_config.scenario)
+    table_df = geopandas.GeoDataFrame.from_file(locator.get_zone_geometry()).drop('geometry', 1)
+    result = [{column: fix_types(getattr(row, column)) for column in table_df.columns} for row in table_df.itertuples()]
+    return jsonify(result)
+
+
+@blueprint.route('/json/<db>', methods=['POST'])
+def route_post_json(db):
+    """Save a row to the database"""
+    print('got here!')
+    return jsonify(True)
+
+
+
+def fix_types(object):
+    if type(object) == np.int64:
+        return int(object)
+    return object
 
 @blueprint.route('/zone')
 def zone():
