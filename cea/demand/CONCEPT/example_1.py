7c76a6528a23fa477b237f4b6694d6d822a3b258
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2018-05-07 15:15:37 +0800

    fixing the example

--- cea/demand/CONCEPT/example_1.py
+++ cea/demand/CONCEPT/example_1.py
@@ -1,41 +1,25 @@
-from __future__ import division
+# iterative1.py
+
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
-import numpy as np
 
-model = ConcreteModel()
-
-c = 1
-
-x0 = 1
-
-A = 3
-B = 2
-C = 1
-D = 0
-
-model.x = Var([1,2,3])
-model.y = Var([1,2])
-model.u = Var([1,2])
-
-model.Constraint1 = Constraint(expr = model.x[2] == A*model.x[1] + B*model.u[1])
-model.Constraint2 = Constraint(expr = model.y[1] == C*model.x[2] + D*model.u[1])
-model.Constraint3 = Constraint(expr = model.x[1] == x0)
-model.Constraint4 = Constraint(expr = -1 <= model.y[1] <= 1)
-
-model.Constraint5 = Constraint(expr = model.x[3] == A*model.x[2] + B*model.u[2])
-model.Constraint6 = Constraint(expr = model.y[2] == C*model.x[3] + D*model.u[2])
-
-
-model.OBJ = Objective(expr=c*model.u[1])
-
+# Create a solver
 opt = SolverFactory('cplex')
 
-results = opt.solve(model)
-model.display()
+#
+# A simple model with binary variables and
+# an empty constraint list.
+#
+model = AbstractModel()
+model.n = Param(default=4)
+model.x = Var(RangeSet(model.n), within=Binary)
+def o_rule(model):
+    return summation(model.x)
+model.o = Objective(rule=o_rule)
+model.c = ConstraintList()
 
-x_value1 = np.array(model.x)
-print(x_value1)
+# Create a model instance and optimize
+instance = model.create_instance()
+results = opt.solve(instance)
+instance.display()
 
-x_value2 = np.array(model.x[1].value)
-print(x_value2)
--- cea/demand/CONCEPT/example_1_Trial.py
+++ cea/demand/CONCEPT/example_1_Trial.py
@@ -1,11 +1,11 @@
-from __future__ import division
+# iterative1.py
+
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 import numpy as np
 
-model = ConcreteModel()
-
-# Settings
+# Create a solver
+opt = SolverFactory('cplex')
 # Define prediction horizon
 predictionHorizon = 10
 
@@ -14,28 +14,6 @@ nx = 10
 ny = 5
 nu = 15
 nv = 20
-
-c = np.ones((1, predictionHorizon))
-
-# MPC Setup: Get Model
-# Generate model matrices
-A = np.random.rand(nx, nx) - 0.5
-Bu = np.random.rand(nx, nu) - 0.5
-Bv = np.random.rand(nx, nv) - 0.5
-C = np.eye(ny, nx)
-Du = np.zeros((ny, nu))
-Dv = np.zeros((ny, nv))
-
-# Generate initial state and disturbances
-x0 = np.random.rand(nx, 1)
-v = np.random.rand(nv, predictionHorizon)  # e.g.solar irradiation
-
-# Define constraints
-umax = 100 * np.ones((nu, predictionHorizon))
-umin = -100 * np.ones((nu, predictionHorizon))
-ymax = 2 * np.ones((ny, predictionHorizon + 1))
-ymin = -2 * np.ones((ny, predictionHorizon + 1))
-
 c = 1
 
 x0 = 1
@@ -45,22 +23,23 @@ B = 2
 C = 1
 D = 0
 
-# model.x = Var([nx, predictionHorizon + 1])
-# model.u = Var([nu, predictionHorizon])
-# model.y = Var([ny, predictionHorizon + 1])
+# Generate initial state and disturbances
+v = np.random.rand(nv, predictionHorizon)  # e.g.solar irradiation
 
-model.x = Var(range(predictionHorizon+1 + 1))
-model.u = Var(range(predictionHorizon + 1))
-model.y = Var(range(predictionHorizon + 1 + 1))
+model = ConcreteModel()
+model.n = Param(default=nx)
+model.x = Var(RangeSet(model.n), within=Reals)
+model.y = Var(RangeSet(model.n), within=Reals)
+model.u = Var(RangeSet(model.n - 1), within=Reals)
+def o_rule(model):
+    return summation(model.u)
 
-# for i in range(predictionHorizon):
-#     Constraint =
+model.o = Objective(rule=o_rule)
 
 model.Constraint1 = Constraint(expr=model.x[2] == A*model.x[1] + B*model.u[1])
 model.Constraint2 = Constraint(expr=model.y[1] == C*model.x[2] + D*model.u[1])
 model.Constraint3 = Constraint(expr=model.x[1] == x0)
 model.Constraint4 = Constraint(expr=-1 <= model.y[1] <= 1)
-
 model.Constraint5 = Constraint(expr=model.x[3] == A*model.x[2] + B*model.u[2])
 model.Constraint6 = Constraint(expr=model.y[2] == C*model.x[3] + D*model.u[2])
 
@@ -95,24 +74,20 @@ model.Constraint24 = Constraint(expr=-2 <= model.y[7] <= 2)
 model.Constraint25 = Constraint(expr=-2 <= model.y[8] <= 2)
 model.Constraint26 = Constraint(expr=-2 <= model.y[9] <= 2)
 
+model.Constraint27 = Constraint(expr=-100 <= model.u[1] <= 100)
+model.Constraint28 = Constraint(expr=-100 <= model.u[2] <= 100)
+model.Constraint29 = Constraint(expr=-100 <= model.u[3] <= 100)
+model.Constraint30 = Constraint(expr=-100 <= model.u[4] <= 100)
+model.Constraint31 = Constraint(expr=-100 <= model.u[5] <= 100)
+model.Constraint32 = Constraint(expr=-100 <= model.u[6] <= 100)
+model.Constraint33 = Constraint(expr=-100 <= model.u[7] <= 100)
+model.Constraint34 = Constraint(expr=-100 <= model.u[8] <= 100)
+model.Constraint35 = Constraint(expr=-100 <= model.u[9] <= 100)
 
 
+# Create a model instance and optimize
+instance = model.create_instance()
+results = opt.solve(instance)
+instance.solutions.store_to(results)
+print (results)
 
-model.OBJ = Objective(
-    expr=c * model.u[1] + c * model.u[2] + c * model.u[3] + c * model.u[4] + c * model.u[5] + c * model.u[6] + c *
-         model.u[7] + c * model.u[8] + c * model.u[9])
-
-opt = SolverFactory('cplex')
-
-results = opt.solve(model)
-model.display()
-
-x_value1 = np.array(model.x)
-print(x_value1)
-
-x_value2 = np.array(model.x[1].value)
-print(x_value2)
-
-print (np.array(model.y))
-print (model.OBJ.value)
-print (model.u[5].value)
