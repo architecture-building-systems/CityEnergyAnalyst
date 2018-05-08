17a347fa5148060600661b44fcc22394251bc674
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-05-08 11:40:00 +0800

    fixes to logo CEA

--- cea/plots/variable_naming.py
+++ cea/plots/variable_naming.py
@@ -1,16 +1,16 @@
 import os
 import pandas as pd
-import logo
+import cea.plots
 
 naming_file = pd.read_csv(os.path.join(os.path.dirname(cea.plots.__file__), "naming.csv"))
 keys = naming_file["VARIABLE"]
 values = naming_file["SHORT_DESCRIPTION"]
 NAMING = dict(zip(keys, values))
 
-source = os.path.join(os.path.dirname(logo.__file__), "CEA.png")
+source = "https://raw.githubusercontent.com/architecture-building-systems/CityEnergyAnalyst/master/logo/CEA.png"
 LOGO =  [dict(
         source= source,
         xref="paper", yref="paper",
         x=0.1, y=1.05,
-        sizex=0.35, sizey=0.35,
+        sizex=0.4, sizey=0.4,
         xanchor="center", yanchor="bottom")]
