f94b9b2c5fd139df3b85bbd3831b5fbbb79a2b1e
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-05-08 11:28:56 +0800

    moving logo back

--- cea/plots/variable_naming.py
+++ cea/plots/variable_naming.py
@@ -1,13 +1,13 @@
 import os
 import pandas as pd
-import cea.logo
+import logo
 
 naming_file = pd.read_csv(os.path.join(os.path.dirname(cea.plots.__file__), "naming.csv"))
 keys = naming_file["VARIABLE"]
 values = naming_file["SHORT_DESCRIPTION"]
 NAMING = dict(zip(keys, values))
 
-source = os.path.join(os.path.dirname(cea.logo.__file__), "CEA.png")
+source = os.path.join(os.path.dirname(logo.__file__), "CEA.png")
 LOGO =  [dict(
         source= source,
         xref="paper", yref="paper",
