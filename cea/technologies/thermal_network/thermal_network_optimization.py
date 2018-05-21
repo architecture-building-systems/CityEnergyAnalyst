f4ac15b2d5d61cff4e0e75fc51d7f3e27bb845f7
Lennart Rogenhofer <lennartr@ethz.ch>
2018-05-21 16:56:16 +0800

    Large changes, refactoring

--- cea/technologies/thermal_network/thermal_network_optimization.py
+++ cea/technologies/thermal_network/thermal_network_optimization.py
@@ -37,34 +37,59 @@ def calc_Ctot_pump_netw(ntwFeat, gv, locator, prices, config):
     :returns pumpCosts: pumping cost
     """
     pumpCosts = 0
-
     network_type = config.thermal_network.network_type
-    network_names = config.thermal_network.network_names
-    if len(network_names) == 0:
-        network_names = ['']
-    pumpCosts = 0
 
-    for network_name in network_names:
-        # read in node mass flows
-        df = pd.read_csv(locator.get_node_mass_flow_csv_file(network_type, network_name), index_col=0)
-        mdotA_kgpers = np.array(df)
-        mdotnMax_kgpers = np.amax(mdotA_kgpers) #find highest mass flow of all nodes at all timesteps (should be at plant)
+    # read in node mass flows
+    df = pd.read_csv(locator.get_node_mass_flow_csv_file(network_type, ''), index_col=0)
+    mdotA_kgpers = np.array(df)
+    mdotnMax_kgpers = np.amax(mdotA_kgpers) #find highest mass flow of all nodes at all timesteps (should be at plant)
 
-        # read in total pressure loss in kW
-        deltaP_kW = pd.read_csv(locator.get_ploss(network_name, network_type))
-        deltaP_kW = deltaP_kW['pressure_loss_total_kW'].sum()
-        pumpCosts = deltaP_kW * prices.ELEC_PRICE
+    # read in total pressure loss in kW
+    deltaP_kW = pd.read_csv(locator.get_ploss('', network_type))
+    deltaP_kW = deltaP_kW['pressure_loss_total_kW'].sum()
+    pumpCosts = deltaP_kW * prices.ELEC_PRICE
 
-        if config.thermal_network.network_type == 'DH':
-            deltaPmax = np.max(ntwFeat.DeltaP_DHN)
-        else:
-            deltaPmax = np.max(ntwFeat.DeltaP_DCN)
+    if config.thermal_network.network_type == 'DH':
+        deltaPmax = np.max(ntwFeat.DeltaP_DHN)
+    else:
+        deltaPmax = np.max(ntwFeat.DeltaP_DCN)
 
-        Capex_a, Opex_fixed = pumps.calc_Cinv_pump(2*deltaPmax, mdotnMax_kgpers, PUMP_ETA, gv, locator, 'PU1')  # investment of Machinery
-        pumpCosts += Opex_fixed
+    Capex_a, Opex_fixed = pumps.calc_Cinv_pump(2*deltaPmax, mdotnMax_kgpers, PUMP_ETA, gv, locator, 'PU1')  # investment of Machinery
+    pumpCosts += Opex_fixed
 
     return Capex_a, pumpCosts
 
+
+def plant_location_cost_calculation(config, locator, building_index, Cost_storage, network_type):
+    building_name = Cost_storage.columns(building_index)
+    network_layout(config, locator, building_name, optimization_flag=True)
+    thermal_network_matrix.main(config)
+
+    ## Cost calculations
+    prices = Prices(locator, config)
+    network_features = network_opt.network_opt_main(config, locator)
+    # calculate Network costs
+    # OPEX neglected, see Documentation Master Thesis Lennart Rogenhofer
+    if network_type == 'DH':
+        Capex_a_netw = network_features.pipesCosts_DHN
+    else:
+        Capex_a_netw = network_features.pipesCosts_DCN
+    # calculate Pressure loss and Pump costs
+    Capex_a_pump, Opex_fixed_pump = calc_Ctot_pump_netw(network_features, gv, locator, prices, config)
+    # calculate Heat loss costs
+    if network_type == 'DH':
+        # Assume a COP of 1 e.g. in CHP plant
+        Opex_heat = network_features.thermallosses_DHN * prices.ELEC_PRICE
+    else:
+        # Assume a COp of 4 e.g.
+        Opex_heat = network_features.thermallosses_DCN / 4 * prices.ELEC_PRICE
+
+    Cost_storage[building_name]['Capex_total'] = Capex_a_netw + Capex_a_pump
+    Cost_storage[building_name]['Opex_total'] = Opex_fixed_pump + Opex_heat
+    Cost_storage[building_name]['Cost_total'] = Cost_storage['Capex_total'][building_name] + \
+                                                Cost_storage['Opex_total'][building_name]
+
+    return Cost_storage
 
 # ============================
 # test
@@ -90,33 +115,13 @@ def main(config):
     Cost_storage.index = ['Capex_total', 'Opex_total', 'Cost_Total']
 
 
-    for building_name in building_names:
-        network_layout(config, locator, building_name, optimization_flag=True)
-        thermal_network_matrix.main(config)
+    for building_index in range(len(building_names)):
+        Cost_storage = plant_location_cost_calculation(config, locator, building_index, Cost_storage, network_type)
 
-        ## Cost calculations
-        prices = Prices(locator, config)
-        network_features = network_opt.network_opt_main(config, locator)
-        # calculate Network costs
-        # OPEX neglected, see Documentation Master Thesis Lennart Rogenhofer
-        if network_type == 'DH':
-            Capex_a_netw = network_features.pipesCosts_DHN
-        else:
-            Capex_a_netw = network_features.pipesCosts_DCN
-        # calculate Pressure loss and Pump costs
-        Capex_a_pump, Opex_fixed_pump = calc_Ctot_pump_netw(network_features, gv, locator, prices, config)
-        # calculate Heat loss costs
-        if network_type == 'DH':
-            # Assume a COP of 1 e.g. in CHP plant
-            Opex_heat = network_features.thermallosses_DHN * prices.ELEC_PRICE
-        else:
-            # Assume a COp of 4 e.g.
-            Opex_heat = network_features.thermallosses_DCN / 4 * prices.ELEC_PRICE
-
-        Cost_storage['Capex_total'][building_name] = Capex_a_netw + Capex_a_pump
-        Cost_storage['Opex_total'][building_name] = Opex_fixed_pump + Opex_heat
-        Cost_storage['Cost_total'][building_name] = Cost_storage['Capex_total'][building_name] + \
-                                                    Cost_storage['Opex_total'][building_name]
+    # output results file to csv
+    # Sort values for easier evaluation
+    Cost_storage = Cost_storage.reindex_axis(sorted(Cost_storage.columns, key=lambda x: float(x[1:])), axis=1)
+    Cost_storage.to_csv(locator.get_optimization_network_plant_location_results_file(network_type))
 
     print('thermal_network_optimization_main() succeeded')
     print('total time: ', time.time() - start)
