c31c518607562e63b296c48f1f5f1501f384be7b
shanshanhsieh <hsieh@arch.ethz.ch>
2018-05-22 20:47:55 +0800

    solar technology hourly curve

---
+++ cea/plots/solar_technology_potentials/all_tech_hourly.py
@@ -0,0 +1,41 @@
+from __future__ import division
+
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+from cea.plots.variable_naming import LOGO, COLOR, NAMING
+
+
+def solar_tech_hourly_curve(data_frame, analysis_fields, title, output_path):
+    # CALCULATE GRAPH
+    traces_graph = calc_graph(analysis_fields, data_frame)
+
+    layout = dict(images=LOGO, title=title,
+                  yaxis=dict(domain=dict(x=[0, 1], y=[0.0, 0.7]), title='Solar Radiation [kW]'),
+                  yaxis2=dict(title='Temperature [C]', overlaying='y',
+                              side='right'), xaxis=dict(rangeselector=dict(buttons=list([
+            dict(count=1, label='1d', step='day', stepmode='backward'),
+            dict(count=1, label='1w', step='week', stepmode='backward'),
+            dict(count=1, label='1m', step='month', stepmode='backward'),
+            dict(count=6, label='6m', step='month', stepmode='backward'),
+            dict(step='all')])), rangeslider=dict(), type='date'))
+
+    fig = dict(data=traces_graph, layout=layout)
+    plot(fig, auto_open=False, filename=output_path)
+
+    return {'data': traces_graph, 'layout': layout}
+
+
+def calc_graph(analysis_fields, data_frame):
+    graph = []
+    x = data_frame.DATE
+    for field in analysis_fields:
+        y = data_frame[field].values
+        name = NAMING[field]
+        if field == "T_ext_C":
+            trace = go.Scatter(x=x, y=y, name=name, yaxis='y2', opacity=0.2)
+        else:
+            trace = go.Scatter(x=x, y=y, name=name,
+                               marker=dict(color=COLOR[field]))
+        graph.append(trace)
+    return graph
