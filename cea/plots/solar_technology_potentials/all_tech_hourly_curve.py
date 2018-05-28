b34edde90550f9b0b1e678722016def6f7387e03
shanshanhsieh <hsieh@arch.ethz.ch>
2018-05-28 16:29:29 +0800

    modify hourly curve

--- cea/plots/solar_technology_potentials/all_tech_hourly_curve.py
+++ cea/plots/solar_technology_potentials/all_tech_hourly_curve.py
@@ -5,12 +5,12 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 from cea.plots.variable_naming import LOGO, COLOR, NAMING
 
-def all_tech_district_hourly(data_frame, analysis_fields, title, output_path):
+def all_tech_district_hourly(data_frame, all_tech_analysis_fields, title, output_path):
 
     traces = []
-    for tech in analysis_fields:
-        tech_fields = analysis_fields[tech]
-        for field in tech_fields:
+    for tech in all_tech_analysis_fields:
+        analysis_fields = all_tech_analysis_fields[tech]
+        for field in analysis_fields:
             y = data_frame[field].values
             name = field.split('_kWh', 1)[0]
             if tech == 'PV':
@@ -20,8 +20,8 @@ def all_tech_district_hourly(data_frame, analysis_fields, title, output_path):
             traces.append(trace)
 
     # CREATE FIRST PAGE WITH TIMESERIES
-    layout = dict(images=LOGO, title=title, yaxis=dict(title='Load [kW]'), yaxis2=dict(title='Temperature [C]', overlaying='y',
-                   side='right'),xaxis=dict(rangeselector=dict(buttons=list([
+    layout = dict(images=LOGO, title=title, yaxis=dict(title='Hourly production [kWh]'),
+                  xaxis=dict(rangeselector=dict(buttons=list([
                     dict(count=1,label='1d',step='day',stepmode='backward'),
                     dict(count=1,label='1w',step='week',stepmode='backward'),
                     dict(count=1,label='1m',step='month',stepmode='backward'),
