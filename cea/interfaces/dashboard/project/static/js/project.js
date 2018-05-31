fbc9d924e604d34b097af6e2ee3c612d10a38647
Daren Thomas <thomas@arch.ethz.ch>
2018-05-31 10:13:17 +0200

    removing the dashboard to it's own repository (cea-dashboard)

--- cea/interfaces/dashboard/project/static/js/project.js
+++
@@ -1,6 +0,0 @@
-function weather_name_clicked(weather_name) {
-    $.get('weather/' + weather_name, {}, function(data){
-        console.log(data);
-        $('#weather-path').val(data);
-    });
-}
