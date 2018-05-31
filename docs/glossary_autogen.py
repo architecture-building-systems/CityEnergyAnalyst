def7df907845c8f9b30386c2563730079ae2e99c
Jack-Hawthorne <jack.hawthorne187@gmail.com>
2018-05-31 16:27:30 +0200

    glossary_autogen.py now functional

--- docs/glossary_autogen.py
+++ docs/glossary_autogen.py
@@ -1,13 +1,10 @@
-import sys
-import os
+import string
 import csv
 
 with open('metadatabase.csv','rb') as databases:
     reader = csv.reader(databases,delimiter=';')
 
-sys.path.insert(0, os.path.abspath('../docs'))
-
-## creates list of mini- variable tables (stored as a list) corresponding to each database
+    ## creates list of mini-tables corresponding to each database
 
     start = [] # stores the first row adjacent to database name
     var_table = [] # stores the values following the first database name until there is an input in the database name
@@ -20,7 +17,6 @@ sys.path.insert(0, os.path.abspath('../docs'))
     tag = []
     info=[]
     details = []
-
 
     for row, col in enumerate(reader):
         if row == 0:
@@ -42,10 +38,76 @@ sys.path.insert(0, os.path.abspath('../docs'))
 del body[0]
 body.append([])
 
-
 table_contents = map(lambda x, y: x+y, start,body)
 table_contents = map(lambda x, y: x+y, listheaders,table_contents)
 
+with open('glossary.rst', 'w') as file:
+    file.write('Glossary'+'\n'+'========'+'\n')
+    for x in range(0, len(table_contents)):
+        data = table_contents[x]
+        info = details[x]
 
-file = open('glossary.rst', 'w')
-file.write('Glossary'+'\n'+'========'+'\n')
+        file.write(str(database_name[x]) + '\n' + len(database_name[x]) * '-' + '\n')
+        for y in range(len(info)):
+            file.write('**' + tag[y] + "**" + ": " + info[y] + '\n\n')
+
+        def as_rest_table(data, full=False):
+            data = data if data else [['']]
+
+            table = []
+            # max size of each column
+            sizes = map(max, zip(*[[len(str(elt)) for elt in member]
+                                   for member in data]))
+            num_elts = len(sizes)
+
+            if full:
+                start_of_line = '| '
+                vertical_separator = ' | '
+                end_of_line = ' |'
+                line_marker = '-'
+            else:
+                start_of_line = ''
+                vertical_separator = '  '
+                end_of_line = ''
+                line_marker = '='
+
+            meta_template = vertical_separator.join(['{{{{{0}:{{{0}}}}}}}'.format(i)
+                                                     for i in range(num_elts)])
+            template = '{0}{1}{2}'.format(start_of_line,
+                                          meta_template.format(*sizes),
+                                          end_of_line)
+            # determine top/bottom borders
+            if full:
+                to_separator = string.maketrans('| ', '+-')
+            else:
+                to_separator = string.maketrans('|', '+')
+            start_of_line = start_of_line.translate(to_separator)
+            vertical_separator = vertical_separator.translate(to_separator)
+            end_of_line = end_of_line.translate(to_separator)
+            separator = '{0}{1}{2}'.format(start_of_line,
+                                           vertical_separator.join([x * line_marker for x in sizes]),
+                                           end_of_line)
+            # determine header separator
+            th_separator_tr = string.maketrans('-', '=')
+            start_of_line = start_of_line.translate(th_separator_tr)
+            line_marker = line_marker.translate(th_separator_tr)
+            vertical_separator = vertical_separator.translate(th_separator_tr)
+            end_of_line = end_of_line.translate(th_separator_tr)
+            th_separator = '{0}{1}{2}'.format(start_of_line,
+                                              vertical_separator.join([x * line_marker for x in sizes]),
+                                              end_of_line)
+            # prepare result
+            table.append(separator)
+            # set table header
+            titles = data[0]
+            table.append(template.format(*titles))
+            table.append(th_separator)
+
+            for d in data[1:-1]:
+                table.append(template.format(*d))
+                if full:
+                    table.append(separator)
+            table.append(template.format(*data[-1]))
+            table.append(separator)
+            return '\n'.join(table)
+        file.write(as_rest_table(data, True) + '\n\n')
