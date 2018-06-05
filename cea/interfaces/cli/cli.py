9b34e57737aa2a3cfdca584060a6346ac2721174
Daren Thomas <thomas@arch.ethz.ch>
2018-06-05 09:47:40 +0200

    small change to trigger the jenkins

--- cea/interfaces/cli/cli.py
+++ cea/interfaces/cli/cli.py
@@ -11,6 +11,14 @@ import importlib
 import ConfigParser
 import cea.config
 
+__author__ = "Daren Thomas"
+__copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Daren Thomas"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Daren Thomas"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
 
 
 def main(config=None):
