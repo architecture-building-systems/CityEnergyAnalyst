4e30526e885c32a7a503a4e582f663e0d375949b
Daren Thomas <thomas@arch.ethz.ch>
2018-06-14 09:45:44 +0200

    some small changes for the trace-inputlocator

--- bin/trace-inputlocator.bat
+++ bin/trace-inputlocator.bat
@@ -4,7 +4,9 @@ rem first delete the config file (force default config)
 del %userprofile%\cea.config
 
 rem new reference-case in C:\reference-case-open\baseline
-cea extract-reference-case
+rem cea extract-reference-case
+
+cea-config copy-default-databases --scenario c:\reference-case-WTP-reduced\WTP_MIX_m\
 
 cea trace-inputlocator --scripts copy-default-databases, data-helper
 cea trace-inputlocator --scripts radiation-daysim, demand
@@ -16,7 +18,10 @@ cea trace-inputlocator --scripts solar-collector
 cea trace-inputlocator --scripts photovoltaic, photovoltaic-thermal
 cea trace-inputlocator --scripts sewage-potential, lake-potential
 
-cea trace-inputlocator --scripts thermal-network-matrix, decentralized
+cea trace-inputlocator --scripts network-layout
+cea trace-inputlocator --scripts thermal-network-matrix
+
+cea trace-inputlocator --scripts decentralized
 
 cea-config optimization --initialind 2 --ngen 2
 cea trace-inputlocator --scripts optimization
