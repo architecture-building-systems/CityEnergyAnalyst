df982c1e32aaf35792659480d6644d1eaab1211d
shanshanhsieh <hsieh@arch.ethz.ch>
2018-06-19 12:16:05 +0800

    change unit of annual radiation threshold input in config

--- cea/technologies/solar/photovoltaic_thermal.py
+++ cea/technologies/solar/photovoltaic_thermal.py
@@ -603,7 +603,7 @@ def main(config):
     locator = cea.inputlocator.InputLocator(scenario=config.scenario)
 
     print('Running photovoltaic-thermal with scenario = %s' % config.scenario)
-    print('Running photovoltaic-thermal with annual-radiation-threshold = %s' % config.solar.annual_radiation_threshold)
+    print('Running photovoltaic-thermal with annual-radiation-threshold-kWh/m2 = %s' % config.solar.annual_radiation_threshold_kWh_per_m2)
     print('Running photovoltaic-thermal with panel-on-roof = %s' % config.solar.panel_on_roof)
     print('Running photovoltaic-thermal with panel-on-wall = %s' % config.solar.panel_on_wall)
     print('Running photovoltaic-thermal with solar-window-solstice = %s' % config.solar.solar_window_solstice)
