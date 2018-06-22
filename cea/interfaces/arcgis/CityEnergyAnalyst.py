f32e6b430ae74fece62bdaa48ce4b6864b8e9d93
Daren Thomas <thomas@arch.ethz.ch>
2018-06-22 15:35:16 +0200

    adding plots-scenario-comparisons to CityEnergyAnalysts.pyt

--- cea/interfaces/arcgis/CityEnergyAnalyst.py
+++ cea/interfaces/arcgis/CityEnergyAnalyst.py
@@ -253,6 +253,7 @@ class PlotsSupplySystemTool(CeaTool):
         self.canRunInBackground = False
         self.category = 'Visualization'
 
+
 class PlotsScenarioComparisonsTool(CeaTool):
     def __init__(self):
         self.cea_tool = 'plots-scenario-comparisons'
@@ -260,19 +261,6 @@ class PlotsScenarioComparisonsTool(CeaTool):
         self.description = 'Plots comparing urban scenarios and supply system configurations'
         self.canRunInBackground = False
         self.category = 'Visualization'
-
-    # find subfolders if scenario changes
-    def updateParameters(self, parameters):
-        super(PlotsScenarioComparisonsTool, self).updateParameters(parameters)
-        parameters = dict_parameters(parameters)
-        config = cea.config.Configuration()
-        config.scenario = parameters['plots-scenario-comparisons:base-scenario'].valueAsText
-        subfolders = config.sections['plots-scenario-comparisons'].parameters['scenarios'].get_folders()
-
-        if set(subfolders) != set(parameters['plots-scenario-comparisons:scenarios'].filter.list):
-            parameters['plots-scenario-comparisons:scenarios'].filter.list = subfolders
-            parameters['plots-scenario-comparisons:scenarios'].value = []
-
 
 
 class HeatmapsTool(CeaTool):
