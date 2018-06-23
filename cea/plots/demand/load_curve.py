ce18a7eca424b69a00c5b4516873ac0d0ec01832
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-06-23 14:36:38 +0800

    tool working :-) first steps map chart

--- cea/plots/demand/load_curve.py
+++ cea/plots/demand/load_curve.py
@@ -27,7 +27,9 @@ def load_curve(data_frame, analysis_fields, title, output_path):
                     dict(count=1,label='1m',step='month',stepmode='backward'),
                     dict(count=6,label='6m',step='month', stepmode='backward'),
                     dict(count=1,label='1y',step='year',stepmode='backward'),
-                    dict(step='all') ])),rangeslider=dict(),type='date'))
+                    dict(step='all') ])),rangeslider=dict(),type='date',range= [data_frame.index[0],
+                                                                                 data_frame.index[168]],
+                                                                          fixedrange=False))
 
     fig = dict(data=traces, layout=layout)
     plot(fig,  auto_open=False, filename=output_path)
