42d5500a326156f36f5c9eef5a7e74dae8305b1f
JIMENOFONSECA <fonseca@arch.ethz.ch>
2018-06-28 13:37:51 +0800

    chanign to barchart

--- cea/plots/supply_system/bar_chart_costs.py
+++ cea/plots/supply_system/bar_chart_costs.py
@@ -12,26 +12,20 @@ def bar_chart_costs(data_frame, analysis_fields, title, output_path):
     traces_graph = calc_graph(analysis_fields, data_frame)
 
     # PLOT GRAPH
-    layout = go.Layout(images=LOGO, title=title,showlegend=True)
+    layout = go.Layout(images=LOGO, title=title, showlegend=True, barmode='relative',
+                       yaxis=dict(title='Cost [USD$(2015)/year]', domain=[0.0, 1.0]))
     fig = go.Figure(data=traces_graph, layout=layout)
     plot(fig, auto_open=False, filename=output_path)
 
     return {'data': traces_graph, 'layout': layout}
 
 def calc_graph(analysis_fields, data_frame):
-    # calculate graph
+    # main data about technologies
+    data = (data_frame.copy())
     graph = []
-    labels = []
-    values = []
-    colors = []
-    text = []
-    for field in analysis_fields:
-        text.append(str(round(data_frame[field], 2)) +" [USD$(2015) Mio/yr]")
-        values.append(data_frame[field])
-        labels.append(NAMING[field])
-        colors.append(COLOR[field])
-
-    trace = go.Pie(labels=labels, values=values, text = text, hoverinfo='label+percent+text', marker=dict(colors=colors))
-    graph.append(trace)
+    for i, field in enumerate(analysis_fields):
+        y = data[field].values
+        trace = go.Bar(x="district", y=y, name=NAMING[field], marker=dict(color=COLOR[field]))
+        graph.append(trace)
 
     return graph
