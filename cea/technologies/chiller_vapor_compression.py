995cd827a3114989b2cdf0aa17d93408a93e3a9a
Lennart Rogenhofer <lennartr@ethz.ch>
2018-07-14 14:22:41 +0200

    Big update to COP calc of chiller

--- cea/technologies/chiller_vapor_compression.py
+++ cea/technologies/chiller_vapor_compression.py
@@ -6,8 +6,12 @@ import pandas as pd
 from math import log, ceil
 import numpy as np
 import cea.config
+from cea.utilities import epwreader
 from cea.optimization.constants import VCC_T_COOL_IN
 from cea.constants import HEAT_CAPACITY_OF_WATER_JPERKGK
+from cea.technologies.constants import G_VALUE_CENTRALIZED, G_VALUE_DECENTRALIZED, CHILLER_DELTA_T_HEX_CT, \
+    CHILLER_DELTA_T_APPROACH, T_EVAP_AHU, T_EVAP_ARU, T_EVAP_SCU, DT_NETWORK_CENTRALIZED, CENTRALIZED_AUX_PERCENTAGE, \
+    DECENTRALIZED_AUX_PERCENTAGE
 
 __author__ = "Thuy-An Nguyen"
 __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
@@ -49,7 +53,8 @@ def calc_VCC(mdot_kgpers, T_sup_K, T_re_K, q_nom_chw_W, number_of_VCC_chillers):
         q_cw_W = 0
 
     else:
-        q_chw_W = mdot_kgpers * HEAT_CAPACITY_OF_WATER_JPERKGK * (T_re_K - T_sup_K)  # required cooling at the chiller evaporator
+        q_chw_W = mdot_kgpers * HEAT_CAPACITY_OF_WATER_JPERKGK * (
+                    T_re_K - T_sup_K)  # required cooling at the chiller evaporator
         T_cw_in_K = VCC_T_COOL_IN  # condenser water inlet temperature in [K]
 
         if q_chw_W <= q_nom_chw_W:  # the maximum capacity is assumed to be 3.5 MW, other wise the COP becomes negative
@@ -84,7 +89,6 @@ def calc_VCC(mdot_kgpers, T_sup_K, T_re_K, q_nom_chw_W, number_of_VCC_chillers):
 
             wdot_W = (q_nom_chw_W / COP) * number_of_VCC_chillers
             q_cw_W = wdot_W + q_chw_W  # heat rejected to the cold water (cw) loop
-
 
     chiller_operation = {'wdot_W': wdot_W, 'q_cw_W': q_cw_W}
 
@@ -143,21 +147,56 @@ def calc_Cinv_VCC(qcold_W, locator, config, technology_type):
                 Inv_IR = (VCC_cost_data.iloc[0]['IR_%']) / 100
                 Inv_LT = VCC_cost_data.iloc[0]['LT_yr']
                 Inv_OM = VCC_cost_data.iloc[0]['O&M_%'] / 100
-                InvC = Inv_a + Inv_b * (Q_nom_each_chiller) ** Inv_c + (Inv_d + Inv_e * Q_nom_each_chiller) * log(Q_nom_each_chiller)
+                InvC = Inv_a + Inv_b * (Q_nom_each_chiller) ** Inv_c + (Inv_d + Inv_e * Q_nom_each_chiller) * log(
+                    Q_nom_each_chiller)
                 Capex_a1 = InvC * (Inv_IR) * (1 + Inv_IR) ** Inv_LT / ((1 + Inv_IR) ** Inv_LT - 1)
                 Capex_a = Capex_a + Capex_a1
                 Opex_fixed = Opex_fixed + Capex_a1 * Inv_OM
     return Capex_a, Opex_fixed
 
 
+def calc_VCC_COP(config, loads, centralized=True):
+    """
+    Calculates the VCC COP based on evaporator and compressor temperatures, VCC g-value, and an assumption of
+    auxiliary power demand for centralized and decentralized systems.
+    :param loads:
+    :param centralized:
+    :return:
+    """
+    if centralized == True:
+        g_value = G_VALUE_CENTRALIZED
+    else:
+        g_value = G_VALUE_DECENTRALIZED
+    T_evap = 10000000 # some high enough value
+    for load in loads: # find minimum evap temperature
+        if load == 'ahu':
+            T_evap = min(T_evap, T_EVAP_AHU)
+        elif load == 'aru':
+            T_evap = min(T_evap, T_EVAP_ARU)
+        elif load == 'scu':
+            T_evap = min(T_evap, T_EVAP_SCU)
+        else:
+            print 'Undefined cooling load for chiller COP calculation.'
+    if centralized == True: #Todo: improve this to a better approximation than a static value DT_Network
+        T_evap = T_evap - DT_NETWORK_CENTRALIZED
+    weather_data = epwreader.epw_reader(config.weather)[['year', 'drybulb_C', 'wetbulb_C']]
+    T_cond = np.mean(weather_data['wetbulb_C']) + CHILLER_DELTA_T_APPROACH + CHILLER_DELTA_T_HEX_CT + 273.15
+    cop_chiller = g_value * T_evap / (T_cond - T_evap)
+    if centralized == True:
+        cop_system = 1/(1/cop_chiller * (1 + CENTRALIZED_AUX_PERCENTAGE/100))
+    else:
+        cop_system = 1/(1 / cop_chiller * (1 + DECENTRALIZED_AUX_PERCENTAGE/100))
+    print 'System COP = ', cop_system
+    return cop_system
+
+
 def main():
     Qc_W = 3.5
     T_chw_sup_K = 273.15 + 6
     T_chw_re_K = 273.15 + 11
-    mdot_chw_kgpers = Qc_W/(HEAT_CAPACITY_OF_WATER_JPERKGK*(T_chw_re_K-T_chw_sup_K))
+    mdot_chw_kgpers = Qc_W / (HEAT_CAPACITY_OF_WATER_JPERKGK * (T_chw_re_K - T_chw_sup_K))
     chiller_operation = calc_VCC(mdot_chw_kgpers, T_chw_sup_K, T_chw_re_K)
     print chiller_operation
-
 
 
 if __name__ == '__main__':
