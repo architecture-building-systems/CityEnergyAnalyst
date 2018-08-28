137c1b91b355751740dd31c93b1efd93b03bb66c
Bhargava Krishna Sreepathi <sreepathi@arch.ethz.ch>
2018-08-28 15:00:42 +0800

    removing passing of many variables

--- cea/optimization/master/test/master_main.py
+++ cea/optimization/master/test/master_main.py
@@ -100,8 +100,13 @@ def evolutionary_algo_main(locator, building_names, extra_costs, extra_CO2, extr
         :return: returns costs, CO2, primary energy and the master_to_slave_vars
         """
         print ( 'cea optimization progress: individual ' + str(individual_number) + ' and generation '+ str(generation) + '/' + str(config.optimization.ngen))
-        costs, CO2, prim, master_to_slave_vars, valid_individual = evaluation.evaluation_main(individual, building_names, locator, extra_costs, extra_CO2, extra_primary_energy, solar_features,
-                                                                                              network_features, gv, config, prices, lca, individual_number, generation)
+        costs, CO2, prim, master_to_slave_vars, valid_individual = evaluation.evaluation_main(individual,
+                                                                                              building_names, locator,
+                                                                                              solar_features,
+                                                                                              network_features, gv,
+                                                                                              config, prices, lca,
+                                                                                              individual_number,
+                                                                                              generation)
         return costs, CO2, prim, master_to_slave_vars, valid_individual
 
     # SET-UP EVOLUTIONARY ALGORITHM
