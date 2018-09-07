46fd6be8f8a6990c68b7258f41d33d06e4a79c23
Daren Thomas <thomas@arch.ethz.ch>
2018-09-07 13:02:35 +0200

    some debugging for radiation...

--- cea/interfaces/arcgis/CityEnergyAnalyst.py
+++ cea/interfaces/arcgis/CityEnergyAnalyst.py
@@ -39,8 +39,8 @@ class Toolbox(object):
         self.tools = [tool for tool in globals().values()
                       if inspect.isclass(tool) and issubclass(tool, CeaTool) and not tool is CeaTool]
 
-# here some magic: create the list of script classes based on the ``scripts.yml`` file. any tools that need more
-# configuration can just be overwritten below.
+# here some magic: create the list of script classes based on the ``scripts.yml`` file.
+# any tools that need more configuration can just be overwritten below.
 import cea.scripts
 for cea_script in cea.scripts.for_interface('arcgis'):
     tool = create_cea_tool(cea_script)
