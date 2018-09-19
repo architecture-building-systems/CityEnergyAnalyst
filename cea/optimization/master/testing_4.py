0cbecdeb353b303553364167351b556d7762bb1d
Bhargava Krishna Sreepathi <sreepathi@arch.ethz.ch>
2018-09-19 12:28:37 +0800

    replacing the old master_main

--- cea/optimization/master/testing_4.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-from scoop import futures
-
-def helloWorld(value):
-    return "Hello World from Future #{0}".format(value)
-
-if __name__ == "__main__":
-    returnValues = list(futures.map(helloWorld, range(16)))
-    print("\n".join(returnValues))
