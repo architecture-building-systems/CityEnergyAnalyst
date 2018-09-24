696f6867b9ffcf9a4cbead88de84673f8ba7b301
shanshanhsieh <hsieh@arch.ethz.ch>
2018-09-24 15:13:13 +0200

    directly read from osmose

--- cea/osmose/extract_demand_outputs.py
+++ cea/osmose/extract_demand_outputs.py
@@ -11,14 +11,14 @@ BUILDINGS_DEMANDS_COLUMNS = ['Name', 'Qww_sys_kWh', 'Qcdata_sys_kWh',
                              'Qcs_sys_ahu_kWh', 'Qcs_sys_aru_kWh', 'Qcs_sys_scu_kWh',
                              'people', 'x_int', 'T_int_C', 'T_ext_C']
 
-TSD_COLUMNS = ['T_ext', 'rh_ext', 'I_sol_and_I_rad','Q_gain_lat_peop', 'Q_gain_sen_peop', 'Q_gain_sen_vent',
+TSD_COLUMNS = ['T_ext', 'rh_ext', 'w_int', 'I_sol_and_I_rad', 'Q_gain_lat_peop', 'Q_gain_sen_peop', 'Q_gain_sen_vent',
                'g_dhu_ld', 'm_ve_inf', 'm_ve_mech', 'm_ve_rec', 'm_ve_required', 'm_ve_window']
 
 Q_GAIN_ENV = ['Q_gain_sen_base', 'Q_gain_sen_roof', 'Q_gain_sen_wall', 'Q_gain_sen_wind']
 
 # 'Q_loss_sen_base','Q_loss_sen_roof', 'Q_loss_sen_wall', 'Q_loss_sen_wind']
 
-Q_GAIN_INT = ['Q_gain_sen_app', 'Q_gain_sen_data', 'Q_gain_sen_light', 'Q_gain_sen_pro','Q_loss_sen_ref']
+Q_GAIN_INT = ['Q_gain_sen_app', 'Q_gain_sen_data', 'Q_gain_sen_light', 'Q_gain_sen_pro', 'Q_loss_sen_ref']
 
 TSD_COLUMNS.extend(Q_GAIN_ENV)
 TSD_COLUMNS.extend(Q_GAIN_INT)
@@ -27,8 +27,8 @@ H_WE_Jperkg = 2466e3  # (J/kg) Latent heat of vaporization of water [section 6.3
 floor_height_m = 2.5  # FIXME: read from CEA
 v_CO2_Lpers = 0.0048  # [L/s/person]
 rho_CO2_kgperm3 = 1.98  # [kg/m3]
-CO2_env_ppm = 400/1e6
-CO2_int_max_ppm = 800/1e6
+CO2_env_ppm = 400 / 1e6  # [m3 CO2/m3]
+CO2_int_max_ppm = 800 / 1e6  # [m3 CO2/m3]
 
 
 def main():
@@ -44,62 +44,79 @@ def main():
 
         # reduce to 24 hours
         start_t = 3217
-        end_t = (start_t + 24)
+        timesteps = 24
+        end_t = (start_t + timesteps)
         reduced_demand_df = demand_df[start_t:end_t]
         reduced_tsd_df = tsd_df[start_t:end_t]
         output_df = pd.concat([demand_df[start_t:end_t], tsd_df[start_t:end_t]], axis=1)
+
+        # change units
+        output_df['rh_ext'] = reduced_tsd_df['rh_ext'] / 100
+        output_df['w_ext'] = np.vectorize(calc_w_from_rh)(output_df['rh_ext'], reduced_tsd_df['T_ext'])
 
         ## building size
         output_df.loc[:, 'Af_m2'] = Af_m2[name]
         output_df.loc[:, 'Vf_m3'] = floor_height_m * Af_m2[name]
 
         ## humidity gain
-        output_df['w_gain_kgpers'] = (reduced_demand_df[
-                                          'Qcs_lat_sys_kWh'] / H_WE_Jperkg) / 3.6  # FIXME: check unit with Gabriel
+        output_df['w_gain_occ_kgpers'] = reduced_tsd_df['w_int']
+        output_df['w_gain_infil_kgpers'] = reduced_tsd_df['m_ve_inf'] * output_df['w_ext'] / 1000
+
         ## heat gain
-        output_df['Q_gain_kWh'] = abs(reduced_demand_df['Qcs_sen_sys_kWh'])
-        # radiation
-        output_df['Q_gain_rad_kWh'] = reduced_tsd_df['I_sol_and_I_rad']/1000
-        # environment
-        output_df['Q_gain_env_kWh'] = reduced_tsd_df.loc[:, Q_GAIN_ENV].sum(axis=1)/1000
-        # internal (appliances, lighting...)
-        output_df['Q_gain_int_kWh'] = reduced_tsd_df.loc[:, Q_GAIN_INT].sum(axis=1)/1000
-        # occupant
-        output_df['Q_gain_occ_kWh'] = reduced_tsd_df['Q_gain_sen_peop']/1000
+        calc_sensible_gains(output_df, reduced_tsd_df)
+        output_df['Q_gain_kWh'] = abs(reduced_demand_df['Qcs_sen_sys_kWh'])  # redundant
 
         ## CO2 gain
-        output_df['CO2_ext_ppm'] = CO2_env_ppm  # TODO: get acutal profile?
-        # from occupants
-        output_df['v_CO2_occupant_m3perhr'] = calc_co2_from_occupants(demand_df['people'])*3600  #to hr
-        # from inlet air
-        reduced_tsd_df['rho_air'] = np.vectorize(calc_rho_air)(reduced_tsd_df['T_ext'])
-        reduced_tsd_df['v_in_infil_window'] = (reduced_tsd_df['m_ve_inf'] + reduced_tsd_df['m_ve_window'])/reduced_tsd_df['rho_air']
-        output_df['v_CO2_infil_window_m3perhr'] = reduced_tsd_df['v_in_infil_window']*3600*output_df['CO2_ext_ppm']
-        output_df['v_CO2_in_infil_occupant_m3perhr'] = output_df['v_CO2_occupant_m3perhr'] + output_df['v_CO2_infil_window_m3perhr'] #to hr
-
-        max_ppl = demand_df['people'].max()
-        output_df['profile'] = reduced_demand_df['people'] / max_ppl
-        # reduced_demand_df['m_exhaust_min_kgpers'] = reduced_demand_df['people'] * Ve_lps[name] * calc_rho_air(
-        #     24) / 1000
-        # reduced_demand_df['m_exhaust_min_kgpers'] = reduced_demand_df['profile'] * Ve_lps[name] * calc_rho_air(
-        #     24) / 1000
+        calc_CO2_gains(demand_df, output_df, reduced_tsd_df)
 
         # exhaust rate from SS553
-        output_df['m_exhaust_min_kgpers'] = output_df['profile'] * Af_m2[
-            name] * SS553_lps_m2 * calc_rho_air(24) / 1000
-
-        # change units
-        output_df['rh_ext'] = reduced_tsd_df['rh_ext']/100
-        output_df['w_ext'] = np.vectorize(calc_w_from_rh)(output_df['rh_ext'],reduced_tsd_df['T_ext'])
+        output_df['m_exhaust_from_SS553'] = Af_m2[name] * (SS553_lps_m2 / 1000) * calc_rho_air(24)  # TODO: CHECK CALC
 
         # add hour of the day
-        output_df['hour'] = range(1, 25)
-        #output_df.reset_index(inplace=True)
-        #output_df = output_df.drop(range(10))  # FIXME: simulate from 11 AM, osmose wouldnt solve when there's zero demand
-        output_df = output_df.round(4)
+        output_df['hour'] = range(1, 1 + timesteps)
+        # TODO: delete the first few hours without demand (howwwwww?)
+        output_df = output_df.round(4)  # osmose does not read more decimals (observation)
+        output_df = output_df.reset_index()
+        output_df = output_df.drop(['index'], axis=1)
+        output_df = output_df.drop(output_df.index[range(8)])
         output_df.T.ix[1:].to_csv(path_to_osmose_project(name), header=False)
-
     return
+
+
+def calc_CO2_gains(demand_df, output_df, reduced_tsd_df):
+    output_df['CO2_ext_ppm'] = CO2_env_ppm  # TODO: get acutal profile?
+    output_df['CO2_max_ppm'] = CO2_int_max_ppm
+    # from occupants
+    output_df['v_CO2_occupant_m3pers'] = calc_co2_from_occupants(demand_df['people'])
+    # output_df['v_CO2_occupant_m3perhr'] = calc_co2_from_occupants(demand_df['people'])*3600  #to hr
+    # from inlet air
+    reduced_tsd_df['rho_air'] = np.vectorize(calc_rho_air)(reduced_tsd_df['T_ext'])
+    reduced_tsd_df['v_in_infil_window'] = (reduced_tsd_df['m_ve_inf'] + reduced_tsd_df['m_ve_window']) / \
+                                          reduced_tsd_df['rho_air']
+    output_df['v_CO2_infil_window_m3pers'] = reduced_tsd_df['v_in_infil_window'] * output_df['CO2_ext_ppm']
+    output_df['v_CO2_in_infil_occupant_m3pers'] = output_df['v_CO2_occupant_m3pers'] + output_df[
+        'v_CO2_infil_window_m3pers']
+
+    # output_df['v_CO2_infil_window_m3perhr'] = reduced_tsd_df['v_in_infil_window']*3600*output_df['CO2_ext_ppm']
+    # output_df['v_CO2_in_infil_occupant_m3perhr'] = output_df['v_CO2_occupant_m3perhr'] + output_df['v_CO2_infil_window_m3perhr'] #to hr
+    output_df['m_exhaust_from_CO2'] = np.vectorize(calc_m_exhaust_from_CO2)(output_df['CO2_max_ppm'],
+                                                                            output_df['CO2_ext_ppm'],
+                                                                            output_df['v_CO2_occupant_m3pers'],
+                                                                            reduced_tsd_df['rho_air'])
+
+
+def calc_sensible_gains(output_df, reduced_tsd_df):
+    # radiation
+    output_df['Q_gain_rad_kWh'] = reduced_tsd_df['I_sol_and_I_rad'] / 1000
+    # environment
+    output_df['Q_gain_env_kWh'] = reduced_tsd_df.loc[:, Q_GAIN_ENV].sum(axis=1) / 1000
+    # internal (appliances, lighting...)
+    output_df['Q_gain_int_kWh'] = reduced_tsd_df.loc[:, Q_GAIN_INT].sum(axis=1) / 1000
+    # occupant
+    output_df['Q_gain_occ_kWh'] = reduced_tsd_df['Q_gain_sen_peop'] / 1000
+    # total
+    output_df['Q_gain_total_kWh'] = output_df['Q_gain_rad_kWh'] + output_df['Q_gain_env_kWh'] + output_df[
+        'Q_gain_int_kWh'] + output_df['Q_gain_occ_kWh']
 
 
 def calc_co2_from_occupants(occupants):
@@ -109,12 +126,18 @@ def calc_co2_from_occupants(occupants):
     return v_CO2_m3pers
 
 
-def calc_w_from_rh(rh,t):
+def calc_w_from_rh(rh, t):
     pws = p_ws_from_t(t)
-    pw = p_w_from_rh_p_and_ws(rh,pws)
-    p = 101325 # [Pa]
-    w = hum_ratio_from_p_w_and_p(pw,p)
-    return w*1000 # g/kg d.a.
+    pw = p_w_from_rh_p_and_ws(rh, pws)
+    p = 101325  # [Pa]
+    w = hum_ratio_from_p_w_and_p(pw, p)
+    return w * 1000  # g/kg d.a.
+
+
+def calc_m_exhaust_from_CO2(CO2_room, CO2_ext, CO2_gain_m3pers, rho_air):
+    # assuming m_exhaust = m_ve_mech
+    m_exhaust_kgpers = CO2_gain_m3pers * rho_air / (CO2_room - CO2_ext)
+    return m_exhaust_kgpers
 
 
 def path_to_demand_output(building_name):
