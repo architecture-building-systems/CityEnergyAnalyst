5a3016c804d69a4787379e3f6ce5e0d8533661c4
martin-mosteiro <mosteiro@arch.ethz.ch>
2018-10-18 19:00:36 +0200

    Adding documentation to my code
    Noticed a lot of things that are wrong and not fully developed... will
    work on this all day tomorrow.

--- cea/demand/calibration/calibration_optimization.py
+++ cea/demand/calibration/calibration_optimization.py
@@ -39,16 +39,37 @@ toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 def create_and_run_one_individual(individual, individual_name, locator_temp):
+    '''
+    For a given individual, this function should first copy the reference case into a temp directory, then edit the
+    corresponding input files with the given individual's input parameters, then run the demand.
+
+    :param individual:
+    :param individual_name:
+    :param locator_temp:
+    :return:
+    '''
+
+    # TODO: define individual
+    # At the moment, this function doesn't do anything because the individual is not yet defined (I'm not sure  what
+    # type of data, etc. it will be in). Once this is clear to me, I can add the part where the input dbf files are
+    # edited.
+
+    # get temp scenario building properties path (this step is actually not necessary)
     temp_properties_path = os.path.join(locator_temp.get_temporary_folder, 'calibration_scenario', 'inputs', 'building-properties')
-    shutil.copytree(locator_temp.get_building_properties_folder, temp_properties_path)
+    # get dbf files that need to be edited
     architecture_df = dbf_to_dataframe(os.path.join(temp_properties_path, 'architecture.dbf')).set_index('Name')
     internal_loads_df = dbf_to_dataframe(os.path.join(temp_properties_path, 'internal_loads.dbf')).set_index('Name')
     indoor_comfort_df = dbf_to_dataframe(os.path.join(temp_properties_path, 'indoor_comfort.dbf')).set_index('Name')
     # TODO: edit the dbf's with the individual's variables
+    # export edited dbf files
     dataframe_to_dbf(architecture_df.reset_index(), os.path.join(temp_properties_path, 'architecture.dbf'))
     dataframe_to_dbf(internal_loads_df.reset_index(), os.path.join(temp_properties_path, 'internal_loads.dbf'))
     dataframe_to_dbf(indoor_comfort_df.reset_index(), os.path.join(temp_properties_path, 'indoor_comfort.dbf'))
+    # calculate demand
+    # TODO: remove unnecessary demand_calculation_calibration function (see TODO below) and use normal demand function
     demand_calculation_calibration(locator_temp)
+    # copy results from the given individual to the actual scenario
+    # TODO: this function uses an inputlocator instance that hasn't been defined - I will fix this tomorrow
     shutil.copy(locator_temp.get_total_demand, os.path.join(locator.get_calibration_folder, individual_name+'.csv'))
 
 def demand_calculation_calibration(locator, gv, config):
@@ -136,25 +157,40 @@ def demand_calculation_calibration(locator, gv, config):
 
     time_elapsed = time.clock() - t0
     print('done - time elapsed: %d.2f seconds' % time_elapsed)
-
+    # TODO: this function is not necessary
+    # actually I could just create a new config object that points to the corresponding temp case study and calls the
+    # normal demand function from demand_main.py. I will do this tomorrow.
 
 def main(config):
     locator = cea.inputlocator.InputLocator(scenario=config.scenario)
+    # for each individual, the input files should be edited with the corresponding values and then passed to the
+    # demand calculation. but we don't want to edit the actual case study, so instead we create a temp folder for the
+    # temporary scenarios that need to be created.
+    # TODO: create one temp folder per processor for multiprocessing?
     temp_scenario_path = os.path.join(locator.get_temporary_folder(), 'calibration_scenario')
     shutil.copytree(locator.get_project_path, temp_scenario_path)
     locator_temp = cea.inputlocator.InputLocator(scenario=temp_scenario_path)
+    # get names of buildings to be calibrated
     building_names = config.calibration_optimization.buildings
+    # get variables based on which the parameters should be calibrated (e.g. Ef_Wm2)
     building_loads = config.calibration_optimization.loads
+    # get materials that need to be assigned (e.g. walls)
     materials = config.calibration_optimization.materials
+    # get internal loads that need to be calibrated (e.g. Ea_Wm2)
     internal_loads = config.calibration_optimization.internal_loads
+    # get indoor comfort properties that need to be calibrated (e.g. ve_lps)
     indoor_comfort = config.calibration_optimization.indoor_comfort
+    # get measured data
     building_metering = os.path.join(locator.get_demand_measured_folder, 'Yearly_demand_metering.csv')
+    # get possible values the parameters to be calibrated may take, namely:
+    ## type of material (e.g. type_win = 'T1')
     material_choices = {'type_cons': list(pd.read_excel(locator.get_envelope_systems, 'CONSTRUCTION')['code']),
                         'type_leak': list(pd.read_excel(locator.get_envelope_systems, 'LEAKAGE')['code']),
                         'type_win': list(pd.read_excel(locator.get_envelope_systems, 'WINDOW')['code']),
                         'type_roof': list(pd.read_excel(locator.get_envelope_systems, 'ROOF')['code']),
                         'type_wall': list(pd.read_excel(locator.get_envelope_systems, 'WALL')['code']),
                         'type_shade': list(pd.read_excel(locator.get_envelope_systems, 'SHADING')['code'])}
+    ## indoor comfort and internal load variable distributions
     # TODO: I am just choosing these three values as the only options for now, but ideally these should depend on the distribution of the uncertainties
     uncertainty_distributions = locator.get_uncertainty_db(config.region)
     internal_loads_choices = pd.read_excel(uncertainty_distributions, 'INTERNAL_LOADS')[['name', 'min', 'mu', 'max']].set_index('name')
