c81bbad9eac3167deb670e11449f6246e084f6bd
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2018-11-09 11:16:09 +0800

    adding few more values to the testing loop

--- bin/test_substation_calc_hex_cooling.py
+++ bin/test_substation_calc_hex_cooling.py
@@ -7,12 +7,12 @@ from cea.technologies.substation import *
 
 def main():
     # the first set of values in the lists is taken from a dry-run
-    Q_cooling_W = [0.0, 1000.0, 1234.0]
+    Q_cooling_W = [0.0, 1000.0, 1234.0, 246.0, 1049.0, 1019.0]
     UA = [72493.5189434, ]
-    thi_K = [271.0, 251.0, 291.0, 255.0, 295.0]
-    tho_K = [273.0, 253.0, 293.0]
-    tci_K = [239.0, 249.0, 259.0]
-    ch_kWperK = [0.0, 1.0, 2.0, 100.0]
+    ch_kWperK = [0.0, 1.0, 2.0, 100.0, 28.0, 118.0, 113.0]
+    thi_K = [271.0, 251.0, 291.0, 255.0, 295.0, 289.3, 289.4, 289.5]
+    tho_K = [273.0, 253.0, 293.0, 280.5, 280.5, 280.5]
+    tci_K = [239.0, 249.0, 259.0, 278.5, 278.5, 278.5]
 
     for q in Q_cooling_W:
         for ua in UA:
