a7929d0c950495a3ccd6708be4927c07250f19a7
martin-mosteiro <mosteiro@arch.ethz.ch>
2018-12-06 17:06:41 +0100

    continue merge

--- cea/datamanagement/create_new_project.py
+++ cea/datamanagement/create_new_project.py
@@ -6,15 +6,17 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+from shutil import copyfile
 
 from geopandas import GeoDataFrame as Gdf
 from osgeo import gdal
 
 import cea.config
 import cea.inputlocator
+from cea.datamanagement.databases_verification import verify_input_geometry_zone, verify_input_geometry_district, \
+    verify_input_occupancy, verify_input_age, verify_input_terrain, COLUMNS_ZONE_OCCUPANCY, COLUMNS_ZONE_AGE
 from cea.utilities.dbf import dataframe_to_dbf, dbf_to_dataframe
-from cea.utilities.standarize_coordinates import shapefile_to_WSG_and_UTM, raster_to_WSG_and_UTM
-from shutil import copyfile
+from cea.utilities.standardize_coordinates import shapefile_to_WSG_and_UTM, raster_to_WSG_and_UTM
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
@@ -25,12 +27,6 @@ __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
-COLUMNS_ZONE_GEOMETRY = ['Name', 'floors_bg', 'floors_ag', 'height_bg', 'height_ag']
-COLUMNS_DISTRICT_GEOMETRY = ['Name', 'height_ag']
-COLUMNS_ZONE_AGE = ['built', 'roof', 'windows', 'partitions', 'basement', 'HVAC', 'envelope']
-COLUMNS_ZONE_OCCUPANCY = ['MULTI_RES', 'OFFICE', 'RETAIL', 'SCHOOL', 'HOTEL', 'GYM', 'HOSPITAL', 'INDUSTRIAL',
-                          'RESTAURANT','SINGLE_RES', 'SERVERROOM', 'SWIMMING', 'FOODSTORE', 'LIBRARY', 'COOLROOM', 'PARKING']
-
 
 def create_new_project(locator, config):
     # Local variables
@@ -39,35 +35,32 @@ def create_new_project(locator, config):
     street_geometry_path = config.create_new_project.streets
     terrain_path = config.create_new_project.terrain
     occupancy_path = config.create_new_project.occupancy
-    age_path  = config.create_new_project.age
+    age_path = config.create_new_project.age
 
-    # verify files (if they have the columns cea needs) and then save to new project location
+    # import file
     zone, lat, lon = shapefile_to_WSG_and_UTM(zone_geometry_path)
-    try:
-        zone_test = zone[COLUMNS_ZONE_GEOMETRY]
-    except ValueError:
-        print("one or more columns in the input file is not compatible with cea, please ensure the column" +
-              " names comply with:", COLUMNS_ZONE_GEOMETRY)
-    else:
-        # apply coordinate system of terrain into zone and save zone to disk.
-        terrain = raster_to_WSG_and_UTM(terrain_path, lat, lon)
-        zone.to_file(locator.get_zone_geometry())
-        driver = gdal.GetDriverByName('GTiff')
-        driver.CreateCopy(locator.get_terrain(), terrain)
+    # verify if input file is correct for CEA, if not an exception will be released
+    verify_input_geometry_zone(zone)
+    zone.to_file(locator.get_zone_geometry())
+
+
+    # apply coordinate system of terrain into zone and save zone to disk.
+    terrain = raster_to_WSG_and_UTM(terrain_path, lat, lon)
+    driver = gdal.GetDriverByName('GTiff')
+    verify_input_terrain(driver, locator.get_terrain(), terrain)
+    driver.CreateCopy(locator.get_terrain(), terrain)
 
     # now create the district file if it does not exist
     if district_geometry_path == '':
         print("there is no district file, we proceed to create it based on the geometry of your zone")
         zone.to_file(locator.get_district_geometry())
     else:
+        # import file
         district, _, _ = shapefile_to_WSG_and_UTM(district_geometry_path)
-        try:
-            district_test = district[COLUMNS_DISTRICT_GEOMETRY]
-        except ValueError:
-            print("one or more columns in the input file is not compatible with cea, please ensure the column" +
-                  " names comply with:", COLUMNS_DISTRICT_GEOMETRY)
-        else:
-            district.to_file(locator.get_district_geometry())
+        # verify if input file is correct for CEA, if not an exception will be released
+        verify_input_geometry_district(zone)
+        # create new file
+        district.to_file(locator.get_district_geometry())
 
     # now transfer the streets
     if street_geometry_path == '':
@@ -85,36 +78,36 @@ def create_new_project(locator, config):
         zone[COLUMNS_ZONE_OCCUPANCY[:2]] = 0.5  # adding 0.5 area use to the first two uses
         dataframe_to_dbf(zone[['Name'] + COLUMNS_ZONE_OCCUPANCY], locator.get_building_occupancy())
     else:
-        try:
-            occupancy_file = dbf_to_dataframe(occupancy_path)
-            occupancy_file_test = occupancy_file[['Name']+COLUMNS_ZONE_OCCUPANCY]
-            copyfile(occupancy_path, locator.get_building_occupancy())
-        except ValueError:
-            print("one or more columns in the input file is not compatible with cea, please ensure the column" +
-                  " names comply with:", COLUMNS_ZONE_OCCUPANCY)
+        # import file
+        occupancy_file = dbf_to_dataframe(occupancy_path)
+        occupancy_file_test = occupancy_file[['Name'] + COLUMNS_ZONE_OCCUPANCY]
+        # verify if input file is correct for CEA, if not an exception will be released
+        verify_input_occupancy(occupancy_file_test)
+        # create new file
+        copyfile(occupancy_path, locator.get_building_occupancy())
 
     ## create age file
     if age_path == '':
-        print("there is no file with the age of the buildings, we proceed to create it based on the geometry of your zone")
+        print(
+            "there is no file with the age of the buildings, we proceed to create it based on the geometry of your zone")
         zone = Gdf.from_file(zone_geometry_path).drop('geometry', axis=1)
         for field in COLUMNS_ZONE_AGE:
             zone[field] = 0.0
         zone['built'] = 2017  # adding year of construction
         dataframe_to_dbf(zone[['Name'] + COLUMNS_ZONE_AGE], locator.get_building_age())
     else:
-        try:
-            age_file = dbf_to_dataframe(age_path)
-            age_file_test = age_file[['Name']+COLUMNS_ZONE_AGE]
-            copyfile(age_path, locator.get_building_age())
-        except ValueError:
-            print("one or more columns in the input file is not compatible with cea, please ensure the column" +
-                  " names comply with:", COLUMNS_ZONE_AGE)
-
+        # import file
+        age_file = dbf_to_dataframe(age_path)
+        age_file_test = age_file[['Name'] + COLUMNS_ZONE_AGE]
+        # verify if input file is correct for CEA, if not an exception will be released
+        verify_input_age(age_file_test)
+        # create new file
+        copyfile(age_path, locator.get_building_age())
 
     # add other folders by calling the locator
     locator.get_measurements()
-    locator.get_input_network_folder("DH","")
-    locator.get_input_network_folder("DC","")
+    locator.get_input_network_folder("DH", "")
+    locator.get_input_network_folder("DC", "")
     locator.get_weather_folder()
 
 
