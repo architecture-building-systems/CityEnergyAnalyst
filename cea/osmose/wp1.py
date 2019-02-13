037baaa2a03270b5c1784245c0f4143c40a17c3d
shanshanhsieh <hsieh@arch.ethz.ch>
2019-02-13 20:54:42 +0800

    Update wp1.py

--- cea/osmose/wp1.py
+++ cea/osmose/wp1.py
@@ -2,54 +2,89 @@ from __future__ import division
 import numpy as np
 import pandas as pd
 import os
-import time
 import subprocess
+import csv
 
 import cea.osmose.extract_demand_outputs as extract_demand_outputs
-import cea.osmose.run_osmose as run_osmose
 import cea.osmose.plot_osmose_result as plot_results
 
-TECHS = ['HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_LD', 'HCS_IEHX']
-# TECHS = ['HCS_coil']
+# import from config # TODO: add to config
+# TECHS = ['HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_LD', 'HCS_IEHX']
+TECHS = ['HCS_coil']
+case = 'WTP_CBD_m_WP1_HOT'  # FIXME: config
+timesteps = 24  # 168 (week)
+if timesteps == 168:
+    start_t = 5040  # 5/16: 3240, Average Annual 7/30-8/5: 5040-5207
+elif timesteps == 24:
+    start_t = 3240
+specified_buildings = ["B001"]  # FIXME: config
+osmose_project_path = "C:\\OSMOSE_projects\\hcs_windows\\Projects"
+ampl_lic_path = "C:\\Users\\Shanshan\\Desktop\\ampl"
+
 
 def main():
     # extract demand outputs
-    case = 'WTP_CBD_m_WP1_HOT'
-    start_t = 5040  # 5/16: 3240, Average Annual 7/30-8/5: 5040-5207
-    timesteps = 168  # 168 (week)
-    _ = extract_demand_outputs.extract_cea_outputs_to_osmose_main(case, start_t, timesteps)
-    building_names = ['B007'] # FIXME: temporary
+    building_names = extract_demand_outputs.extract_cea_outputs_to_osmose_main(case, start_t, timesteps,
+                                                                               specified_buildings)
+
+    # start ampl license
+    start_ampl_license(ampl_lic_path, "start")
 
     # run osmose
-    p_ampllic = subprocess.Popen("C:\\Users\\Shanshan\\Desktop\\ampl\\ampl_lic.exe start",
-                                 cwd="C:\\Users\\Shanshan\\Desktop\\ampl")
+    write_value_to_csv(timesteps, osmose_project_path, "timesteps.csv")  # osmose input
+    for building in building_names:
+        write_value_to_csv(building, osmose_project_path, "building_name.csv")  # osmose input
+        for tech in TECHS:
+            exec_osmose(tech, osmose_project_path)
+
+    start_ampl_license(ampl_lic_path, "stop")
+    # plot_results.main(building, TECHS) #TODO: change paths in osmose
+
+    return np.nan
+
+
+def write_value_to_csv(timesteps, osmose_project_path, filename):
+    csvData = [[timesteps, "N/A"], []]
+    file_path = os.path.join(osmose_project_path, filename)
+    with open(file_path, 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(csvData)
+    csvFile.close()
+    return np.nan
+
+
+def start_ampl_license(ampl_lic_path, action):
+    command = os.path.join(ampl_lic_path, "ampl_lic.exe " + action)
+    p_ampllic = subprocess.Popen(command, cwd=ampl_lic_path)
     ampllic_out, ampllic_err = p_ampllic.communicate()
     if ampllic_out is not None:
         print ampllic_out.decode('utf-8')
     if ampllic_err is not None:
         ampllic_err.decode('utf-8')
-
-    for building in building_names:
-        # TODO: create folder to store results, or copy results
-        # pause(building)
-        for tech in TECHS:
-            # timeout_sec = calc_timeout_sec(tech)
-            # run_osmose.exec_osmose(tech, timeout_sec)
-            run_osmose.exec_osmose(tech)
-
-        # plot_results.main(building, TECHS) #TODO: change paths in osmose
-
     return np.nan
 
 
-def calc_timeout_sec(tech):
-    time_dict = {"HCS_coil" : 600, "HCS_ER0": 11000, "HCS_3for2": 1200, "HCS_LD": 120, "HCS_IEHX": 3000}
-    return float(time_dict[tech])
+def exec_osmose(tech, osmose_project_path):
+    frontend_file = tech + "_frontend.lua"
+    frontend_path = osmose_project_path + "\\" + frontend_file
+    project_path = "C:\\OSMOSE_projects\\hcs_windows"
 
+    p = subprocess.Popen(["lua", (frontend_path)], cwd=project_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    print "running Lua: ", frontend_file
+    output, err = p.communicate()
 
-def pause(building):
-    print "Change building name in frontend to ", building
-    return raw_input("press ENTER to continue")
+    if err.decode('utf-8') is not '':
+        print(err.decode('utf-8'))
+        if err.decode('utf-8').startswith('WARNING:'):
+            print 'warning', err.decode('utf-8')
+        elif err.decode('utf-8').startswith('pandoc: Could not find image'):
+            print 'warning', err.decode('utf-8')
+        else:
+            print "ERROR : ", err.decode('utf-8')
+
+    print(output.decode('utf-8'))
+    return 'ok', output.decode('utf-8')
 
 
 if __name__ == '__main__':
