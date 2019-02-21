d3d9ed417224a991e7ef8d7105bb65178fa1de46
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2019-02-21 17:29:30 +0800

    modifications on the predefined file to include error documentation

--- cea/demand/set_point_from_predefined_file.py
+++ cea/demand/set_point_from_predefined_file.py
@@ -4,6 +4,7 @@
 from __future__ import division
 import numpy as np
 import pandas as pd
+import os
 from cea.demand.control_heating_cooling_systems import get_heating_system_set_point
 from cea.demand.control_heating_cooling_systems import get_cooling_system_set_point
 
@@ -30,15 +31,18 @@ def calc_set_point_from_predefined_file(tsd, bpr, weekday, building_name, config
     :return: tsd with updated columns
     :rtype: dict
     """
+    if os.path.isfile(locator.get_predefined_hourly_setpoints(building_name, type_of_district_network='space heating')):
+        predefined_set_temperatures_space_heating = pd.read_excel(locator.get_predefined_hourly_setpoints(building_name, type_of_district_network='space heating'))
+        tsd['ta_hs_set'] = predefined_set_temperatures_space_heating['temperature'].values
+    else:
+        print ('predefined set points file for space heating is not provided. It is running with the default archetype set points')
+        tsd['ta_hs_set'] = np.vectorize(get_heating_system_set_point)(tsd['people'], range(8760), bpr, weekday)
 
-    predefined_set_temperatures_space_heating = locator.get_predefined_hourly_setpoints(building_name, type_of_district_network='space heating')
-    tsd['ta_hs_set'] = predefined_set_temperatures_space_heating['temperature'].values
-
-    predefined_set_temperatures_space_cooling = locator.get_predefined_hourly_setpoints(building_name, type_of_district_network='space cooling')
-    tsd['ta_cs_set'] = predefined_set_temperatures_space_cooling['temperature'].values
-
-
-    tsd['ta_hs_set'] = np.vectorize(get_heating_system_set_point)(tsd['people'], range(8760), bpr, weekday)
-    tsd['ta_cs_set'] = np.vectorize(get_cooling_system_set_point)(tsd['people'], range(8760), bpr, weekday)
+    if os.path.isfile(locator.get_predefined_hourly_setpoints(building_name, type_of_district_network='space cooling')):
+        predefined_set_temperatures_space_cooling = pd.read_excel(locator.get_predefined_hourly_setpoints(building_name, type_of_district_network='space cooling'))
+        tsd['ta_cs_set'] = predefined_set_temperatures_space_cooling['temperature'].values
+    else:
+        print ('predefined set points file for space cooling is not provided. It is running with the default archetype set points')
+        tsd['ta_cs_set'] = np.vectorize(get_cooling_system_set_point)(tsd['people'], range(8760), bpr, weekday)
 
     return tsd
