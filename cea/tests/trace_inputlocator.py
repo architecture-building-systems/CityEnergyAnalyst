6905f97a622f2f392a8554288a3598e35ee97c45
Jack-Hawthorne <36536537+Jack-Hawthorne@users.noreply.github.com>
2019-03-26 13:28:32 +0100

    false date catigorization fix

--- cea/tests/trace_inputlocator.py
+++ cea/tests/trace_inputlocator.py
@@ -172,7 +172,7 @@ def create_graphviz_output(trace_data, graphviz_output_file):
 def is_date(data):
     # TODO replace hardcoded with a reference
     codes = ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12', 'T13', 'T14', 'T15'
-             'T16', 'T17', 'T18', 'T19', 'T20', 'T21', 'T22', 'T23', 'T24', 'T25']
+             'T16', 'T17', 'T18', 'T19', 'T20', 'T21', 'T22', 'T23', 'T24', 'T25','m','m2','m3']
     if type(data) == unicode and data not in codes:
         try:
             parse(data)
