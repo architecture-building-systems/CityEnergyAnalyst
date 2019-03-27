0d7e3d8db9e5ce20eb5b6652d62d8a92594ff358
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-03-27 15:49:56 +0800

    move objective function

--- cea/optimization/flexibility_model/electric_and_thermal_grid_planning/electrical_grid_calculations.py
+++ cea/optimization/flexibility_model/electric_and_thermal_grid_planning/electrical_grid_calculations.py
@@ -1,19 +1,37 @@
-import pyomo_multi_linetype
-from pyomo.environ import *
 import time
-from concept_parameters import *
-import cea.inputlocator
-import cea.config
-import cea.optimization.flexibility_model.electric_and_thermal_grid_planning.plot_functions as pf
 
-__author__ =  "Sreepathi Bhargava Krishna"
+from pyomo.environ import *
+
+import cea.config
+import cea.inputlocator
+import cea.optimization.flexibility_model.electric_and_thermal_grid_planning.plot_functions as pf
+import pyomo_multi_linetype
+from concept_parameters import *
+
+__author__ = "Sreepathi Bhargava Krishna"
 __copyright__ = "Copyright 2018, Architecture and Building Systems - ETH Zurich"
-__credits__ = [ "Sreepathi Bhargava Krishna", "Thanh"]
+__credits__ = ["Sreepathi Bhargava Krishna", "Thanh"]
 __license__ = "MIT"
 __version__ = "0.1"
 __maintainer__ = "Daren Thomas"
 __email__ = "thomas@arch.ethz.ch"
 __status__ = "Production"
+
+
+def electric_network_optimization(locator, building_names, config, generation, individual, network_number):
+    # ============================
+    # Solve the electrical grid problem, and decide on the best electrical line types and lengths. It is an optimization
+    # problem for a fixed demand
+    # ============================
+    dict_connected = {}
+    for i in range(len(building_names)):
+        dict_connected[i] = individual[i]
+
+    # the reason why we send disconnected buiuldings is to calculate the demand of the grid. THis will influence the size of the electrical lines
+    # This algortihm will always connect all buildings to the electrical grid.
+    m = electrical_grid_calculations(dict_connected, config, locator, network_number, generation)
+    return m, dict_connected
+
 
 def electrical_grid_calculations(dict_connected, config, locator, network_number, generation):
     # ============================
@@ -52,7 +70,6 @@ def electrical_grid_calculations(dict_connected, config, locator, network_number
 
 
 if __name__ == '__main__':
-
     dict_connected = {0: 1, 1: 1, 2: 0,
                       3: 1, 4: 0, 5: 1,
                       6: 0, 7: 1, 8: 1,
@@ -65,12 +82,12 @@ if __name__ == '__main__':
                       15: 1, 16: 1, 17: 1,
                       18: 1, 19: 1, 20: 1,
                       21: 1, 22: 1, 23: 1}
-#                        , 10: 1, 11: 1,
-#                   12: 1, 13: 1, 14: 1,
-#                   15: 1, 16: 1, 17: 1,
-#                   18: 1, 19: 1, 20: 1,
-#                   21: 1, 22: 1, 23: 1,
-#                   }
+    #                        , 10: 1, 11: 1,
+    #                   12: 1, 13: 1, 14: 1,
+    #                   15: 1, 16: 1, 17: 1,
+    #                   18: 1, 19: 1, 20: 1,
+    #                   21: 1, 22: 1, 23: 1,
+    #                   }
 
     t0 = time.clock()
     config = cea.config.Configuration()
