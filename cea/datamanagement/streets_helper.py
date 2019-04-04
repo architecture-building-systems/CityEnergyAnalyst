e7bcedd5f35e6c91f8066443c6c0e6a63f666392
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-04-04 15:11:38 +0800

    fixing config and defaults, and small fixes

--- cea/datamanagement/streets_helper.py
+++ cea/datamanagement/streets_helper.py
@@ -23,11 +23,14 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
-def calc_bounding_box(shapefile):
-    data = Gdf.from_file(shapefile)
+def calc_bounding_box(shapefile_district, shapefile_zone):
+    #connect both files and avoid repetition
+    data_zone = Gdf.from_file(shapefile_zone)
+    data_dis = Gdf.from_file(shapefile_district)
+    data_dis = data_dis.loc[~data_dis["Name"].isin(data_zone["Name"])]
+    data = data_dis.append(data_zone, ignore_index = True, sort=True)
     data = data.to_crs(get_geographic_coordinate_system())
     result = data.total_bounds  # in float
-    result = [str(x) for x in result]  # in string
     return result
 
 
@@ -41,6 +44,7 @@ def geometry_extractor_osm(locator, config):
     list_of_bounding_box = config.streets_helper.bbox
     type_of_streets = config.streets_helper.streets
     shapefile_out_path = locator.get_street_network()
+    extra_border = 0.0010 # adding extra 150m (in degrees equivalent) to avoid errors of no data
 
     #get the bounding box coordinates
     if list_of_bounding_box == []:
@@ -48,24 +52,24 @@ def geometry_extractor_osm(locator, config):
         assert os.path.exists(
             locator.get_district_geometry()), 'Get district geometry file first or the coordinates of the area where to extract the streets from in the next format: lon_min, lat_min, lon_max, lat_max: %s'
         print("generating streets from District Geometry")
-        bounding_box_district_file = calc_bounding_box(locator.get_district_geometry())
-        lon_min = bounding_box_district_file[0]
-        lat_min = bounding_box_district_file[1]
-        lon_max = bounding_box_district_file[2]
-        lat_max = bounding_box_district_file[3]
+        bounding_box_district_file = calc_bounding_box(locator.get_district_geometry(), locator.get_zone_geometry())
+        lon_min = bounding_box_district_file[0]-extra_border
+        lat_min = bounding_box_district_file[1]-extra_border
+        lon_max = bounding_box_district_file[2]+extra_border
+        lat_max = bounding_box_district_file[3]+extra_border
     elif len(list_of_bounding_box) == 4:
         print("generating streets from your bounding box")
         # the list is not empty, the user has indicated a specific set of coordinates
-        lon_min = list_of_bounding_box[0]
-        lat_min = list_of_bounding_box[1]
-        lon_max = list_of_bounding_box[2]
-        lat_max = list_of_bounding_box[3]
+        lon_min = list_of_bounding_box[0]-extra_border
+        lat_min = list_of_bounding_box[1]-extra_border
+        lon_max = list_of_bounding_box[2]+extra_border
+        lat_max = list_of_bounding_box[3]+extra_border
     elif len(list_of_bounding_box) != 4:
         raise ValueError(
             "Please indicate the coordinates of the area where to extract the streets from in the next format: lon_min, lat_min, lon_max, lat_max")
 
     #get and clean the streets
-    G = ox.graph_from_bbox(north=float(lat_max), south=float(lat_min), east=float(lon_max), west=float(lon_min),
+    G = ox.graph_from_bbox(north=lat_max, south=lat_min, east=lon_max, west=lon_min,
                            network_type=type_of_streets)
     data = ox.save_load.graph_to_gdfs(G, nodes=False, edges=True, node_geometry=False, fill_edge_geometry=True)
 
@@ -77,27 +81,6 @@ def geometry_extractor_osm(locator, config):
     data.loc[:, "name"] = [x[0] if type(x) == list else x for x in data["name"].values]
     data.fillna(value="Unknown", inplace=True)
     data[['geometry', "name", "highway"]].to_file(shapefile_out_path)
-
-    ##THIS IS ONE METHOD KEEP IT JUST IN CASE. IT IS FUNCTIONAL, BUT LESS EFECTIVE THAN THE ONE ABOVE
-    # bounding_box = 'way(' + lat_min + ',' + lon_min + ',' + lat_max + ',' + lon_max + ')'
-    # query = bounding_box + '["highway"];(._;>;);out body;'
-    # api = overpy.Overpass()
-    # # fetch all ways and nodes
-    # result = api.query(query)
-    # schema = {'geometry': 'LineString', 'properties': {'Name': 'str:80'}}
-    # with fiona.open(shapefile_out_path, 'w', crs=crs,
-    #                 driver='ESRI Shapefile', schema=schema) as output:
-    #     for way in result.ways:
-    #         # the shapefile geometry use (lon,lat)
-    #         line = {'type': 'LineString', 'coordinates': [(node.lon, node.lat) for node in way.nodes]}
-    #         prop = {'Name': way.tags.get("name", "n/a")}
-    #         output.write({'geometry': line, 'properties': prop})
-    # #project coordinates
-    # data = Gdf.from_file(shapefile_out_path)
-    # data = data.to_crs(get_projected_coordinate_system(float(lat_min), float(lon_min)))
-    # data.to_file(shapefile_out_path)
-
-    # get_projected_coordinate_system(float(lat_min), float(lon_min))
 
 
 def main(config):
