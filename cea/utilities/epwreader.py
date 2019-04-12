e7150cff49380fc1f366c677db0e21687554755f
Daren Thomas <daren-thomas@users.noreply.github.com>
2019-04-12 20:03:08 +0200

    Restrict length of year to 8760 hours

--- cea/utilities/epwreader.py
+++ cea/utilities/epwreader.py
@@ -27,6 +27,7 @@ def epw_reader(weather_path):
                   'snowdepth_cm', 'days_last_snow', 'Albedo', 'liq_precip_depth_mm', 'liq_precip_rate_Hour']
 
     result = pd.read_csv(weather_path, skiprows=8, header=None, names=epw_labels).drop('datasource', axis=1)
+    result = result.loc[0:8759]
     result['date'] = pd.Series(pd.date_range(str(result["year"][0])+"/1/1", periods=8760, freq='H'))
     result['dayofyear'] = pd.date_range(str(result["year"][0])+"/1/1", periods=8760, freq='H').dayofyear
     result['ratio_diffhout'] = result['difhorrad_Whm2'] / result['glohorrad_Whm2']
