d19c344cda21389eb3d9f4a4ecb34c21f346a163
martin-mosteiro <mosteiro@arch.ethz.ch>
2019-05-05 12:08:47 +0200

    Add function to inputlocator

--- cea/inputlocator.py
+++ cea/inputlocator.py
@@ -554,6 +554,9 @@ class InputLocator(object):
     def get_terrain_folder(self):
         return self._ensure_folder(self.scenario, 'inputs', 'topography')
 
+    def get_microclimate_folder(self):
+        return self._ensure_folder(self.scenario, 'inputs', 'microclimate-data')
+
     def get_zone_geometry(self):
         """scenario/inputs/building-geometry/zone.shp"""
         shapefile_path = os.path.join(self.get_building_geometry_folder(), 'zone.shp')
