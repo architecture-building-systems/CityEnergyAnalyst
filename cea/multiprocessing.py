b31a0807e60d32fdf4a2a4e94b2406a6847c341f
Daren Thomas <thomas@arch.ethz.ch>
2019-05-13 13:18:52 +0200

    adding chunks function

--- cea/multiprocessing.py
+++ cea/multiprocessing.py
@@ -3,5 +3,13 @@ This file implements ``multiprocessing.pool.map``, but captures stdout and stder
 """
 import sys
 
+# copied from here: https://stackoverflow.com/a/54802737/2260
+def chunks(l, n):
+    """Yield n number of sequential chunks from l."""
+    d, r = divmod(len(l), n)
+    for i in range(n):
+        si = (d+1)*(i if i < r else r) + d*(0 if i < r else i - r)
+        yield l[si:si+(d+1 if i < r else d)]
+
 def map(number_of_processes, func, iterable, stdout=sys.stdout, stderr=sys.stderr):
     pool =
