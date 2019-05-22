e70e49f887533f3caf422334e4eaf8489886fd2a
Reynold Mok <reyold@hotmail.com>
2019-05-22 13:44:23 +0800

    Enable toggle for 3d

--- cea/interfaces/dashboard/inputs/static/js/inputs.js
+++ cea/interfaces/dashboard/inputs/static/js/inputs.js
@@ -1,107 +1,5 @@
-var map;
-var zoneJson;
-var districtJson;
-var zoneLayer;
-var districtLayer;
-/**
- * Zoom in on the map for the input...
- */
 
 $(document).ready(function() {
-    // $.getJSON('http://localhost:5050/inputs/geojson/zone', function(geojson){
-    //     console.log(geojson.bbox);
-    //     bbox = L.latLng([
-    //         (geojson.bbox[1] + geojson.bbox[3]) / 2,
-    //         (geojson.bbox[0] + geojson.bbox[2]) / 2]);
-    //     map = L.map('mapid').setView(bbox, 16);
-    //     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-    //         {attribution: "Data copyright OpenStreetMap contributors"}).addTo(map);
-    //     L.geoJSON(geojson, {
-    //         onEachFeature: function onEachFeature(feature, layer) {
-    //             layer.on('click', function (e) {
-    //                 console.log(e);
-    //                 console.log(e.target.feature.properties.Name);
-    //                 var pk_field = $('#cea-table').bootstrapTable('getOptions').uniqueId;
-    //                 var pk = e.target.feature.properties[pk_field];
-    //                 var row = $('#cea-table').bootstrapTable('getRowByUniqueId', pk);
-    //                 edit_row(row);
-    //             });
-    //         }
-    //     }).addTo(map)
-    var getZone = $.getJSON('http://localhost:5050/inputs/geojson/zone', function(json) {
-        console.log(json);
-        zoneJson = json;
-    }).fail(function() {
-        console.log("Get zone failed.")
-    });
-
-    var getDistrict = $.getJSON('http://localhost:5050/inputs/geojson/district', function(json) {
-        console.log(json);
-        districtJson = json;
-    }).fail(function() {
-        console.log("Get district failed.")
-    }).done();
-
-    $.when(getZone, getDistrict).always(function () {
-        var layers = [];
-
-        if(zoneJson !== null) {
-        zoneLayer = new deck.GeoJsonLayer({
-            id: 'zone',
-            data: zoneJson,
-            opacity: 0.5,
-            stroked: false,
-            filled: true,
-            extruded: true,
-            wireframe: true,
-
-            getElevation: f => f.properties['height_ag'],
-            getFillColor: f => [0, 0, 255],
-
-            pickable: true,
-            autoHighlight: true,
-
-            onHover: updateTooltip,
-            onClick: editProperties
-        });
-
-        layers.push(zoneLayer)
-        };
-
-        if(districtJson !== null) {
-        districtLayer = new deck.GeoJsonLayer({
-            id: 'district',
-            data: districtJson,
-            opacity: 0.5,
-            stroked: false,
-            filled: true,
-            extruded: true,
-            wireframe: true,
-
-            getElevation: f => f.properties['height_ag'],
-            getFillColor: f => [255, 0, 0],
-
-            pickable: true,
-            autoHighlight: true,
-
-            onHover: updateTooltip,
-            onClick: editProperties
-        });
-
-        layers.push(districtLayer)
-        };
-
-        new deck.DeckGL({
-            container: 'mapid',
-            mapboxApiAccessToken: 'pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pudzRtaWloMDAzcTN2bzN1aXdxZHB5bSJ9.2bkj3IiRC8wj3jLThvDGdA',
-            mapStyle: 'mapbox://styles/mapbox/streets-v11',
-            latitude: (zoneJson.bbox[1] + zoneJson.bbox[3]) / 2,
-            longitude: (zoneJson.bbox[0] + zoneJson.bbox[2]) / 2,
-            zoom: 16,
-            pitch: 45,
-            layers: layers
-        });
-    });
 
     $('#cea-table').on('click-row.bs.table', function(e, row, $element, field) {
         edit_row(row);
@@ -112,7 +10,6 @@ $(document).ready(function() {
         $('#cea-table-data').val(JSON.stringify($('#cea-table').bootstrapTable('getData', false)));
         return true;
     });
-
 });
 
 
