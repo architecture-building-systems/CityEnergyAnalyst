aab5c6df6ec2115a336eca238c57c4f288f86377
shanshanhsieh <hsieh@arch.ethz.ch>
2019-05-28 14:09:41 +0800

    Update compare_el_usages.py
    chw scatter with Qc and rank by el_total

--- cea/osmose/compare_el_usages.py
+++ cea/osmose/compare_el_usages.py
@@ -17,7 +17,8 @@ CONFIG_TABLE = {'coil': 'Config|1', 'ER0': 'Config|2', '3for2': 'Config|3', 'LD'
 def main(building, building_result_path, case):
     # get paths to chiller_T_tech_df and el csv
     paths = path_to_elec_csv_files(building_result_path)
-    chiller_paths = path_to_chiller_csv_files(building_result_path)
+    chiller_paths = path_to_chiller_csv_files(building_result_path, 'chiller')
+    chiller_Qc_paths = path_to_chiller_csv_files(building_result_path, 'chiller_Qc')
 
     el_dfs = {}
     cop_mean = {}
@@ -39,11 +40,15 @@ def main(building, building_result_path, case):
         hourly_cop_shr_dict[tech] = cop_shr_df
     compare_df.to_csv(path_to_save_compare_df(building, building_result_path))
 
-    # get chiller_T_tech_df df from all tech
-    chiller_T_all_tech_df = get_chiller_T_from_all_techs(chiller_paths)
-    plot_chiller_temperatures_bar(chiller_T_all_tech_df, building, building_result_path)
-    plot_chiller_temperatures_scatter(chiller_T_all_tech_df, building, building_result_path, case)
+    # get chiller_T_tech_df from all tech
+    # chiller_T_all_tech_df = get_chiller_T_from_all_techs(chiller_paths)
+    # plot_chiller_temperatures_bar(chiller_T_all_tech_df, building, building_result_path)
+    # plot_chiller_temperatures_scatter(chiller_T_all_tech_df, building, building_result_path, case)
 
+    # get chiller_T_Qc_all_tech_df from all tech
+    chiller_T_Qc_all_tech_df = get_chiller_T_from_all_techs(chiller_Qc_paths)
+    tech_rank = get_tech_rank_by_el_total(compare_df)
+    plot_chiller_T_Qc_scatter(chiller_T_Qc_all_tech_df, tech_rank, building, building_result_path, case)
 
     # # plot T_SA, w_SA
     plot_el_compare(building, building_result_path, compare_df)
@@ -52,6 +57,13 @@ def main(building, building_result_path, case):
 
     return
 
+
+def get_tech_rank_by_el_total(compare_df):
+    el_total_all_tech_df = compare_df.T.filter(like='el_total')
+    if 'status' in el_total_all_tech_df.index: el_total_all_tech_df.drop(index='status', inplace=True)
+    if 'LD' in el_total_all_tech_df.index: el_total_all_tech_df.drop(index='LD', inplace=True)
+    tech_rank = el_total_all_tech_df.sort_values(by='el_total').index
+    return tech_rank
 
 def plot_T_w_scatter(ax, compare_df):
     # fig = plt.figure()
@@ -66,11 +78,12 @@ def plot_T_w_scatter(ax, compare_df):
             label = CONFIG_TABLE[tech]
             color = color_codes[tech]
             ax.scatter(w_SA, T_SA, s=70, c=color, label=label)
-    ax.legend(loc="lower left",ncol=3,bbox_to_anchor=(-0.05,0.7),fontsize='small',columnspacing=0.05)
+    ax.legend(loc="lower left", ncol=3, bbox_to_anchor=(-0.05, 0.7), fontsize='small', columnspacing=0.05)
     ax.set(xlabel='Humidity Ratio [g/kg air]', ylabel='Temperature [C]', ylim=(14, 20))
     ax.set_title('OAU Supply Air Conditions ')
     # plt.show()
     return np.nan
+
 
 def plot_COP_SHR_scatter(hourly_cop_shr_dict, building, case, building_result_path):
     fig = plt.figure()
@@ -81,12 +94,13 @@ def plot_COP_SHR_scatter(hourly_cop_shr_dict, building, case, building_result_pa
             COP = hourly_cop_shr_dict[tech]['COP']
             SHR = hourly_cop_shr_dict[tech]['SHR']
             color = color_codes[tech]
-            ax.scatter(SHR,COP,c=color)
-    ax.set(xlabel='SHR', ylabel='COP',ylim=(3,9),xlim=(0,1))
+            ax.scatter(SHR, COP, c=color)
+    ax.set(xlabel='SHR', ylabel='COP', ylim=(3, 9), xlim=(0, 1))
     ax.set_title(building + '_' + case.split('_')[4] + '_' + case.split('_')[0], fontsize=16)
     fig.savefig(path_to_save_cop_shr_scatter(building, building_result_path))
-    #plt.show()
+    # plt.show()
     return
+
 
 def get_chiller_T_from_all_techs(chiller_paths):
     i = 0
@@ -147,7 +161,7 @@ def plot_chiller_temperatures_scatter(chiller_df, building, building_result_path
         x_labels_shown.append(label_dict[i])
     x_values = list(range(len(x_labels)))
     # lookup table mapping category
-    lookup_table = dict((v, k) for k, v in enumerate(x_labels))
+    lookup_table = dict((v, k) for k, v in enumerate(x_labels))  # determine the x-axis order
     # build a list of points (x,y,annotation)
     points = [(lookup_table[action], key, anno)
               for action, values in chiller_dict.items()
@@ -161,21 +175,71 @@ def plot_chiller_temperatures_scatter(chiller_df, building, building_result_path
     # marker size
     area = tuple(map(lambda x: 10 * x, anno))
 
+    # figure name
+    figure_name = 'chw_freq'
+
+    format_chw_scatter_plot(area, building, building_result_path, case, figure_name, x, x_labels_shown, x_values,
+                            y_float, y_labels, y_values)
+    return np.nan
+
+
+def plot_chiller_T_Qc_scatter(chiller_df, tech_rank, building, building_result_path, case):
+    chiller_dict = chiller_df.T.to_dict()
+
+    # x axis (keys of the first level dict)
+    chiller_dict_keys = list(chiller_dict.keys())
+    x_labels = []
+    # for i in ['HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_LD', 'HCS_IEHX']:
+    #     if i in chiller_dict_keys:
+    #         x_labels.append(i)
+    for i in tech_rank:
+        x_labels.append('HCS_' + i)
+    x_labels_shown = []
+    label_dict = {'HCS_coil': 'Config|1', 'HCS_ER0': 'Config|2', 'HCS_3for2': 'Config|3', 'HCS_LD': 'Config|4',
+                  'HCS_IEHX': 'Config|5'}
+    for i in x_labels:
+        x_labels_shown.append(label_dict[i])
+    x_values = list(range(len(x_labels)))
+
+    # lookup table mapping category
+    lookup_table = dict((v, k) for k, v in enumerate(x_labels))  # determine the x-axis order
+
+    # build a list of points (x,y,annotation)
+    points = [(lookup_table[action], key, anno)
+              for action, values in chiller_dict.items()
+              for key, anno in (values if values else {}).items()]
+    x, y, anno = zip(*points)
+    y_float = tuple(map(lambda i: float(i), y))
+    # y axis (keys of the second level dict)
+    y_values = [8.1, 8.75, 9.4, 10.05, 10.7, 11.35, 12., 12.65, 13.3, 13.95]
+    # y_values = [float(i) for i in chiller_df.columns]
+    y_labels = [str(v) for v in y_values]
+    # marker size
+    area = tuple(map(lambda x: (x / 1000) ** 2, anno))  # area = Qc
+
+    # figure name
+    figure_name = 'chw_Qc'
+
+    format_chw_scatter_plot(area, building, building_result_path, case, figure_name, x, x_labels_shown, x_values,
+                            y_float, y_labels, y_values)
+    return np.nan
+
+
+def format_chw_scatter_plot(area, building, building_result_path, case, figure_name, x, x_labels_shown, x_values,
+                            y_float, y_labels, y_values):
     # format the plt
     plt.figure()
     case_name = case.split('_')[4]
     plt.title(case.split('_')[0] + ' ' + CASE_TABLE[case_name] + ' ' + building, fontsize=16)
     # plt.xlabel('x')
-
     plt.xticks(x_values, x_labels_shown, fontsize=16)
     plt.yticks(y_values, y_labels, fontsize=16)
     plt.axis([min(x_values) - 0.5, max(x_values) + 0.5,
               min(y_values) - 0.5, max(y_values) + 0.5])
-    plt.scatter(x, y_float, s=area, c='#454545') # navy: #14453
+    plt.scatter(x, y_float, s=area, c='#454545')  # navy: #14453
     plt.ylabel('Chilled water temperature [C]', fontsize=16)
-    #plt.show()
-    plt.savefig(path_to_save_chw_scatter(building, building_result_path))
-    return np.nan
+    # plt.show()
+    plt.savefig(path_to_save_chw_scatter(building, building_result_path, figure_name))
 
 
 def plot_el_compare(building, building_result_path, compare_df):
@@ -200,7 +264,7 @@ def plot_el_compare(building, building_result_path, compare_df):
         el_per_unit_df = pd.read_csv(path_to_elec_unit_csv(building, building_result_path, tech))
         el_per_unit_df = el_per_unit_df.iloc[72:96]  # WED
         ax3 = plot_electricity_usages_units(ax3, el_per_unit_df, config)
-        #ax3.legend(loc=(0.2,-0.35), ncol=3, fontsize='small', columnspacing=0.1)
+        # ax3.legend(loc=(0.2,-0.35), ncol=3, fontsize='small', columnspacing=0.1)
     # ax4
     config = 'Config|3 \n Desiccant Wheels'
     tech = 'HCS_3for2'
@@ -221,7 +285,7 @@ def plot_el_compare(building, building_result_path, compare_df):
         ax5 = plot_electricity_usages_units(ax5, el_per_unit_df, config)
         ax5.set(xlabel='Time [hr]')
         ax5.xaxis.label.set_size(10)
-        ax5.legend(loc="upper center",ncol=3,fontsize='small',columnspacing=0.15)
+        ax5.legend(loc="upper center", ncol=3, fontsize='small', columnspacing=0.15)
     # ax6
     config = 'Config|5 \n Indirect Evaporative Cooling'
     tech = 'HCS_IEHX'
@@ -234,9 +298,9 @@ def plot_el_compare(building, building_result_path, compare_df):
 
     # plot layout
     filename = 'el_usages_compare_' + 'WED'
-    #plt.legend()
+    # plt.legend()
     plt.tight_layout()
-    #plt.show()
+    # plt.show()
     fig.savefig(path_to_save_fig(building, building_result_path, tech, filename))
     return np.nan
 
@@ -247,8 +311,8 @@ def plot_electricity_usages_units(ax, el_per_unit_df, config):
     # plotting
     bar_width = 0.5
     opacity = 1
-    #color_table = {'OAU': '#14453D', 'LCU': '#4D9169', 'SCU': '#BACCCC'}
-    #color_table = {'OAU': '#171717', 'RAU': '#454545', 'SCU': '#737373'}
+    # color_table = {'OAU': '#14453D', 'LCU': '#4D9169', 'SCU': '#BACCCC'}
+    # color_table = {'OAU': '#171717', 'RAU': '#454545', 'SCU': '#737373'}
     color_table = {'OAU': '#080808', 'RAU': '#707070', 'SCU': '#C8C8C8'}  # Gray scale
     # initialize the vertical-offset for the stacked bar chart
     y_offset = np.zeros(el_per_unit_df.shape[0])
@@ -259,8 +323,8 @@ def plot_electricity_usages_units(ax, el_per_unit_df, config):
         ax.bar(x_ticks, el_per_unit_df[column], bar_width, bottom=y_offset, alpha=opacity, color=color_table[unit],
                label=unit)
         y_offset = y_offset + el_per_unit_df[column]
-    ax.set(xlim=(1,24),ylim=(0, 25))
-    ax.set_xticks(range(1,25,4))
+    ax.set(xlim=(1, 24), ylim=(0, 25))
+    ax.set_xticks(range(1, 25, 4))
     # ax.legend(loc='upper left')
     ax.set_title(config)
     return ax
@@ -294,7 +358,7 @@ def path_to_elec_csv_files(building_result_path):
     return path_to_files
 
 
-def path_to_chiller_csv_files(building_result_path):
+def path_to_chiller_csv_files(building_result_path, name):
     """
     find files with _el.csv and return a list of paths
     :param building:
@@ -303,7 +367,7 @@ def path_to_chiller_csv_files(building_result_path):
     # path_to_folder = PATH_TO_FOLDER + building
     all_files_in_path = os.listdir(building_result_path)
     path_to_files = []
-    file_name = 'chiller.csv'
+    file_name = name + '.csv'
     for file in all_files_in_path:
         if file_name in file:
             path_to_file = os.path.join(building_result_path, file)
@@ -323,10 +387,11 @@ def path_to_save_chiller_t(building, building_result_path):
     return path_to_file
 
 
-def path_to_save_chw_scatter(building, building_result_path):
-    filename = building + '_chw_scatter.png'
+def path_to_save_chw_scatter(building, building_result_path, name):
+    filename = building + '_' + name + '.png'
     path_to_file = os.path.join(building_result_path, filename)
     return path_to_file
+
 
 def path_to_save_cop_shr_scatter(building, building_result_path):
     filename = building + '_shr_cop_scatter.png'
@@ -335,20 +400,20 @@ def path_to_save_cop_shr_scatter(building, building_result_path):
 
 
 if __name__ == '__main__':
-    buildings = ["B001", "B002", "B003", "B004", "B005", "B006", "B007", "B008", "B009", "B010"]
-    #buildings = ["B003"]
+    # buildings = ["B001", "B002", "B003", "B004", "B005", "B006", "B007", "B008", "B009", "B010"]
+    buildings = ["B003"]
     timestep = "168"
-    # cases = ['WTP_CBD_m_WP1_HOT', 'WTP_CBD_m_WP1_OFF', 'WTP_CBD_m_WP1_RET']
-    cases = ["HKG_CBD_m_WP1_RET", "HKG_CBD_m_WP1_OFF", "HKG_CBD_m_WP1_HOT",
-             "ABU_CBD_m_WP1_RET", "ABU_CBD_m_WP1_OFF", "ABU_CBD_m_WP1_HOT",
-             "MDL_CBD_m_WP1_RET", "MDL_CBD_m_WP1_OFF", "MDL_CBD_m_WP1_HOT",
-             "WTP_CBD_m_WP1_RET", "WTP_CBD_m_WP1_OFF", "WTP_CBD_m_WP1_HOT"]
-    #cases = ["HKG_CBD_m_WP1_RET", "HKG_CBD_m_WP1_OFF", "HKG_CBD_m_WP1_HOT"]
-    #cases = ["ABU_CBD_m_WP1_RET", "ABU_CBD_m_WP1_OFF", "ABU_CBD_m_WP1_HOT"]
-    #cases = ["MDL_CBD_m_WP1_RET", "MDL_CBD_m_WP1_OFF", "MDL_CBD_m_WP1_HOT"]
-    #cases = ['WTP_CBD_m_WP1_RET']
+    cases = ['WTP_CBD_m_WP1_HOT', 'WTP_CBD_m_WP1_OFF', 'WTP_CBD_m_WP1_RET']
+    # cases = ["HKG_CBD_m_WP1_RET", "HKG_CBD_m_WP1_OFF", "HKG_CBD_m_WP1_HOT",
+    #          "ABU_CBD_m_WP1_RET", "ABU_CBD_m_WP1_OFF", "ABU_CBD_m_WP1_HOT",
+    #          "MDL_CBD_m_WP1_RET", "MDL_CBD_m_WP1_OFF", "MDL_CBD_m_WP1_HOT",
+    #          "WTP_CBD_m_WP1_RET", "WTP_CBD_m_WP1_OFF", "WTP_CBD_m_WP1_HOT"]
+    # cases = ["HKG_CBD_m_WP1_RET", "HKG_CBD_m_WP1_OFF", "HKG_CBD_m_WP1_HOT"]
+    # cases = ["ABU_CBD_m_WP1_RET", "ABU_CBD_m_WP1_OFF", "ABU_CBD_m_WP1_HOT"]
+    # cases = ["MDL_CBD_m_WP1_RET", "MDL_CBD_m_WP1_OFF", "MDL_CBD_m_WP1_HOT"]
+    # cases = ['WTP_CBD_m_WP1_RET']
     result_folder = 'C:\\Users\\Shanshan\\Documents\\WP1_0515'
-    #result_folder = "C:\\Users\\Shanshan\\Documents\\WP1_results"
+    # result_folder = "C:\\Users\\Shanshan\\Documents\\WP1_results"
     for case in cases:
         print case
         case_folder = os.path.join(result_folder, case)
