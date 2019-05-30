e2d83d5b5082f6f14d9acbfc973becfeabcb058c
Reynold Mok <reyold@hotmail.com>
2019-05-30 11:44:28 +0800

    Move tooltip for move visibility

--- cea/interfaces/dashboard/inputs/static/js/map.js
+++ cea/interfaces/dashboard/inputs/static/js/map.js
@@ -232,8 +232,6 @@ function updateTooltip({x, y, object, layer}) {
     const tooltip = document.getElementById('tooltip');
 
     if (object) {
-        tooltip.style.top = `${y}px`;
-        tooltip.style.left = `${x}px`;
         var innerHTML = '';
         for (let prop in object.properties) {
             innerHTML += `<div><b>${prop}</b>: ${object.properties[prop]}</div>`;
@@ -242,6 +240,7 @@ function updateTooltip({x, y, object, layer}) {
             var area = turf.area(object);
             innerHTML += `<br><div><b>area</b>: ${Math.round(area * 1000) / 1000}m<sup>2</sup></div>
             <div><b>volume</b>: ${Math.round(area * object.properties['height_ag'] * 1000) / 1000}m<sup>3</sup></div>`;
+            x += 25
         }
         if (layer.id == 'dc_networks' || layer.id == 'dh_networks') {
             if (!object.properties.hasOwnProperty("Building")) {
@@ -250,6 +249,8 @@ function updateTooltip({x, y, object, layer}) {
             }
         }
         tooltip.innerHTML = innerHTML;
+        tooltip.style.top = `${y}px`;
+        tooltip.style.left = `${x}px`;
     } else {
         tooltip.innerHTML = '';
     }
