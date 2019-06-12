24daf2582895cacdb2a5945f40d202352cea3f1d
Daren Thomas <thomas@arch.ethz.ch>
2019-06-12 16:27:39 +0200

    first work on SupplyReturnAmbientCurvePlot

--- cea/plots/thermal_networks/Supply_Return_Outdoor.py
+++ cea/plots/thermal_networks/Supply_Return_Outdoor.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import numpy as np
 import plotly.graph_objs as go
 from plotly.offline import plot
-
+import cea.plots.thermal_networks
 from cea.plots.variable_naming import LOGO, NAMING, COLOR
 
 __author__ = "Lennart Rogenhofer"
@@ -15,6 +15,41 @@ __version__ = "0.1"
 __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
+
+
+class SupplyReturnAmbientCurvePlot(cea.plots.thermal_networks.ThermalNetworksPlotBase):
+    """Implement the heat and pressure losses plot"""
+    name = "Supply and Return vs. Ambient Temp"
+
+    expected_parameters = {
+        'scenario-name': 'general:scenario-name',
+        'network-type': 'thermal-network:network-type',
+        'network-names': 'thermal-network:network-names',
+    }
+
+    def __init__(self, project, parameters, cache):
+        super(SupplyReturnAmbientCurvePlot, self).__init__(project, parameters, cache)
+        self.network_args = [self.network_type, self.network_name]
+        self.input_files = [(self.locator.get_thermal_demand_csv_file, self.network_args),
+                            (self.locator.get_thermal_network_layout_pressure_drop_kw_file, self.network_args),
+                            (self.locator.get_thermal_network_qloss_system_file, self.network_args)]
+
+    @property
+    def layout(self):
+        return dict(title=self.title, yaxis=dict(title=self.yaxis_title),
+                    yaxis2=dict(title='Demand [kWh]', overlaying='y',
+                                side='right'), xaxis=dict(rangeselector=dict(buttons=list([
+                dict(count=1, label='1d', step='day', stepmode='backward'),
+                dict(count=1, label='1w', step='week', stepmode='backward'),
+                dict(count=1, label='1m', step='month', stepmode='backward'),
+                dict(count=6, label='6m', step='month', stepmode='backward'),
+                dict(count=1, label='1y', step='year', stepmode='backward'),
+                dict(step='all')])), rangeslider=dict(), type='date', range=[self.date[0],
+                                                                             self.date[168]],
+                fixedrange=False))
+
+    def calc_graph(self):
+        data_frame = self.calc_data_frame()
 
 
 def supply_return_ambient_temp_plot(data_frame, data_frame_2, analysis_fields, title, output_path):
@@ -40,3 +75,25 @@ def supply_return_ambient_temp_plot(data_frame, data_frame_2, analysis_fields, t
     plot(fig, auto_open=False, filename=output_path)
 
     return {'data': traces, 'layout': layout}
+
+
+def main():
+    """Test this plot"""
+    import cea.config
+    import cea.plots.cache
+    config = cea.config.Configuration()
+    cache = cea.plots.cache.PlotCache(config.project)
+    cache = cea.plots.cache.NullPlotCache()
+    LossCurvePlot(config.project, {'network-type': config.thermal_network.network_type,
+                                   'scenario-name': config.scenario_name,
+                                   'network-names': config.thermal_network.network_names},
+                  cache).plot(auto_open=True)
+
+    LossCurveRelativePlot(config.project, {'network-type': config.thermal_network.network_type,
+                                           'scenario-name': config.scenario_name,
+                                           'network-names': config.thermal_network.network_names},
+                          cache).plot(auto_open=True)
+
+
+if __name__ == '__main__':
+    main()
