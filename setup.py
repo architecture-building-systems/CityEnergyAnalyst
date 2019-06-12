a30bfcf093ce7e49db7284613432dae4c864e183
Daren Thomas <thomas@arch.ethz.ch>
2019-06-12 16:46:06 +0200

    fixes a bug with pip installing pymc3 on Euler

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ INSTALL_REQUIRES = ['SALib==1.2',  # last version to work with python2
                     'numba',
                     'plotly',
                     'py4design==0.27',
-                    'pymc3',
+                    'pymc3==3.6',  # last version known to work with python2
                     'pysal',
                     'pyyaml',
                     'requests',
