37a2c38a27cd9b81dbcf4d1487df6ddb290ecfff
Jack-Hawthorne <36536537+Jack-Hawthorne@users.noreply.github.com>
2019-06-18 18:08:56 +0200

    cleaned up the cea-doc html
    looking nice now, preview is working well enough.

--- cea/utilities/doc_html.py
+++ cea/utilities/doc_html.py
@@ -14,7 +14,7 @@ import subprocess
 
 __author__ = "Jack Hawthorne"
 __copyright__ = "Copyright 2018, Architecture and Building Systems - ETH Zurich"
-__credits__ = ["Jack Hawthorne","Daren Thomas"]
+__credits__ = ["Jack Hawthorne", "Daren Thomas"]
 __license__ = "MIT"
 __version__ = "2.14"
 __maintainer__ = "Daren Thomas"
@@ -24,58 +24,72 @@ __status__ = "Production"
 locator = cea.inputlocator.InputLocator(cea.config.Configuration().scenario)
 
 
-def preview_set():
+def preview_files(documentation_dir):
     """
     This method performs a Gitdiff, storing the documentation relevant change files as a set.
     :return: preview_docs: set of documentation relevant files produced by Gitdiff
     """
     cea_path = os.path.dirname(cea.__file__)
 
-    gitdiff = subprocess.check_output('git diff --name-only', shell=True, cwd=cea_path).split('\n')
+
+    gitdiff = subprocess.check_output('git diff --name-only', shell=True, cwd=cea_path, stderr=open(os.devnull, 'wb')).split('\n')
+
     preview_docs = set()
+
+    # the directories and file types docs is concerned with
+    dir_list = ['cea', 'docs']
+    file_types = ['py', 'rst']
+
     for altered_file in gitdiff:
-        if altered_file.split('/')[0] == 'cea' or altered_file.split('/')[0] == 'docs':
-            if altered_file.split('.')[-1] == 'py' or altered_file.split('.')[-1] == 'rst':
-                preview_docs.add(altered_file)
-
-    return preview_docs
-
-
-def rebuild_altered_module_documentation(preview_docs, documentation_dir):
-    """
-
-    sphinx-apidoc skips any pre-existing module api documentation, even if changes have occurred.
-    This method deletes any old api documentation, rebuilding relevant cea.modules.rst files.
-
-    :param preview_docs: set of documentation relevant files produced by Gitdiff
-    :return: none
-    """
+        dir_name = os.path.dirname(os.path.split(altered_file)[0])
+        file_type = altered_file.split('.')[-1]
+        if dir_name in dir_list and file_type in file_types:
+            preview_docs.add(altered_file)
 
     for doc in preview_docs:
-        if doc.split('.')[-1] == 'py' and doc.split('/')[0] == 'cea':
-            print 'Rebuilding %s api documentation' % doc
+        if os.path.basename(os.path.dirname(doc)) != 'modules' and doc.rsplit('.')[-1] == 'py':
+            preview_html = os.path.dirname(doc).replace('/', '.')+'.html'
+            os.system(os.path.join(os.path.abspath(documentation_dir), '_build', 'html', 'modules', preview_html))
+        elif os.path.dirname(doc) == 'docs':
+            preview_html = os.path.basename(doc).rsplit('.', 1)[0] + '.html'
+            os.system(os.path.join(documentation_dir, '_build', 'html', preview_html))
 
-            rst_name = doc.rsplit('/', 1)[0].replace('/', '.')+'.rst'
-            rst_path = os.path.join(documentation_dir, 'modules', rst_name)
-            doc_path = os.path.join(documentation_dir, '..', doc)
 
-            if os.path.isfile(rst_path):
-                os.remove(rst_path)
-                if not os.path.isfile(doc_path):
-                    os.remove(rst_path)
-    subprocess.check_call(os.path.join(documentation_dir, 'make-api-doc.bat'))
+def get_all_module_rsts(cea_path):
+    module_rst_set = set()
+    for dirpath, dirnames, filenames in os.walk(cea_path):
+        rel_path = os.path.relpath(dirpath, os.path.join(cea_path, '..'))
+        for module in filenames:
+            if module.rsplit('.')[-1] == 'py':
+                module_rst_set.add(rel_path.replace(os.sep, '.') + '.rst')
+    return module_rst_set
+
+
+def rebuild_altered_module_documentation(documentation_dir):
+    """
+    This method deletes any old api documentation; rebuilding relevant cea.modules.rst files.
+    """
+
+    module_rst_set = get_all_module_rsts(os.path.abspath(os.path.join(documentation_dir, '..', 'cea')))
+
+    # if the rst exists but the module does not, delete the documentation for it
+    for document in os.listdir(os.path.join(documentation_dir, 'modules')):
+        if document not in module_rst_set and document != 'modules.rst':
+            print 'Removing %s documentation' % document
+            os.remove(os.path.join(documentation_dir, 'modules', document))
+    subprocess.check_call(os.path.join(documentation_dir, 'make-api-doc.bat'), cwd=documentation_dir)
 
 
 def main(_):
-    documentation_dir = os.path.join(os.path.dirname(cea.config.__file__), '..', 'docs')
-    # get all relevant change files
-    preview_files = preview_set()
-    # change the dir to docs
+    documentation_dir = os.path.join(os.path.dirname(os.path.dirname(cea.config.__file__)), 'docs')
+
     # compare python modules to pre-existing documentation and rebuild
-    rebuild_altered_module_documentation(preview_files, documentation_dir)
+    rebuild_altered_module_documentation(documentation_dir)
+
     # run the make.bat from docs
     subprocess.check_call([os.path.join(documentation_dir, 'make.bat'), 'html'])
 
-    # next step ----- make the changed files automatically open for sphinx build checking
-    # for doc in Preview:
-        # os.system('./_build/html/%s') etc....
+    # preview uncommitted module and documentation htmls
+    preview_files(documentation_dir)
+
+
---
+++ docs/modules/cea.rst
@@ -0,0 +1,78 @@
+cea package
+===========
+
+.. automodule:: cea
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Subpackages
+-----------
+
+.. toctree::
+
+    cea.analysis
+    cea.datamanagement
+    cea.demand
+    cea.examples
+    cea.interfaces
+    cea.optimization
+    cea.plots
+    cea.resources
+    cea.supply
+    cea.technologies
+    cea.tests
+    cea.utilities
+
+Submodules
+----------
+
+cea.api module
+--------------
+
+.. automodule:: cea.api
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+cea.config module
+-----------------
+
+.. automodule:: cea.config
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+cea.constants module
+--------------------
+
+.. automodule:: cea.constants
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+cea.globalvar module
+--------------------
+
+.. automodule:: cea.globalvar
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+cea.inputlocator module
+-----------------------
+
+.. automodule:: cea.inputlocator
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+cea.scripts module
+------------------
+
+.. automodule:: cea.scripts
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
