9d0d3797c38efa022a88ff5e3f2b0eee47128658
Daren Thomas <thomas@arch.ethz.ch>
2019-06-19 16:27:03 +0200

    Update trace-inputlocator.bat

--- bin/trace-inputlocator.bat
+++ bin/trace-inputlocator.bat
@@ -1,18 +1,26 @@
 rem run the trace-inputlocator for as many tools as possible, collecting the data
 
-cea-config copy-default-databases --scenario c:\reference-case-WTP-reduced\WTP_MIX_m\
-if %errorlevel% neq 0 exit /b %errorlevel%
+rem set up the case study
+cea extract-reference-case --destination %temp% --case cooling
+cea-config demand --scenario %temp%\reference-case-cooling\baseline --weather Singapore
 
-cea trace-inputlocator --scripts copy-default-databases, data-helper
+rem data-helper
+cea-config data-helper --region SG
+cea trace-inputlocator --scripts data-helper
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cea trace-inputlocator --scripts radiation-daysim, demand
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-rem make sure to run the quicker version (type-scpanel=FP)
-cea-config solar-collector --type-scpanel FP
+cea trace-inputlocator --scripts emissions, operation-costs
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+cea-config network-layout --network-type DC
+cea trace-inputlocator --scripts network-layout
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+rem make sure to run the quicker version (type-scpanel=FP)
+cea-config solar-collector --type-scpanel FP
 cea trace-inputlocator --scripts solar-collector
 if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -28,18 +36,16 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cea trace-inputlocator --scripts network-layout
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cea trace-inputlocator --scripts thermal-network-matrix
+cea trace-inputlocator --scripts thermal-network
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cea trace-inputlocator --scripts decentralized
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cea-config optimization --initialind 2 --ngen 2
-if %errorlevel% neq 0 exit /b %errorlevel%
-
+cea-config optimization --initialind 2 --ngen 2 --halloffame 2 --random-seed 1234
 cea trace-inputlocator --scripts optimization
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cea trace-inputlocator --scripts plots
+cea trace-inputlocator --scripts multi-criteria-analysis
 if %errorlevel% neq 0 exit /b %errorlevel%
 
