0370ca5c31c98e7ec794b48dc216da8062e3b744
Reynold Mok <reyold@hotmail.com>
2019-06-27 19:57:27 +0800

    Implement tracking of changes

--- cea/interfaces/dashboard/inputs/static/js/store.js
+++ cea/interfaces/dashboard/inputs/static/js/store.js
@@ -4,10 +4,14 @@ class InputStore {
         this.geojsons = store['geojsons'];
         this.columns = store['columns'];
         this.column_types = store['column_types'];
-        this.changes = {};
+        this.changes = {update:{},delete:{}};
 
         this.data = {};
+        this.geojsondata = {};
         this.selected = [];
+
+        this.generateGeojsonData();
+        this.generateData();
     }
 
     getColumns(table) {
@@ -19,17 +23,12 @@ class InputStore {
     }
 
     getData(table) {
-        var _this = this;
-        if (!Object.keys(_this.data).length) {
-            $.each(_this.tables, function (property, table) {
-                var out = [];
-                $.each(table, function (building, columns) {
-                    out.push({'Name': building, ...columns});
-                });
-                _this.data[property] = [...out];
-            });
-        }
         return this.data[table]
+    }
+
+    getDataID(name, building) {
+        var table = this.data[name];
+        return table.findIndex(x => x['Name'] === building);
     }
 
     getSelected() {
@@ -40,12 +39,54 @@ class InputStore {
         this.selected = array;
     }
 
-    getGeojson(type) {
-        return this.geojsons[type]
+    getGeojson(layer) {
+        return this.geojsondata[layer]
+    }
+
+    createNewGeojson(layer) {
+        this.geojsondata[layer] = JSON.parse(JSON.stringify(this.geojsondata[layer]));
+    }
+
+    getGeojsonID(layer, building) {
+        var features = this.geojsondata[layer]['features'];
+        return features.findIndex(x => x['properties']['Name'] === building);
     }
 
     // TODO: Process change here, remove change if same as default
-    addChange(change) {
+    addChange(method, table, building, column, value) {
+        var change = {[column]:value};
+        if (method === 'update') {
+            //Check if update is the same as default
+            if (this.tables[table][building][column] === value) {
+                delete this.changes[method][table][building][column];
+            } else {
+                this.changes[method][table] = this.changes[method][table] || {};
+                this.changes[method][table][building] = this.changes[method][table][building] || {};
+                Object.assign(this.changes[method][table][building], change);
+            }
+            console.log(this.changes);
+        }
 
+    }
+
+    generateGeojsonData() {
+        this.geojsondata = JSON.parse(JSON.stringify(this.geojsons));
+    }
+
+    generateData() {
+        var _this = this;
+        $.each(_this.tables, function (property, table) {
+            var out = [];
+            $.each(table, function (building, columns) {
+                out.push({'Name': building, ...columns});
+            });
+            _this.data[property] = [...out];
+        });
+    }
+
+    resetChanges() {
+        this.changes = {update:{},delete:{}};
+        this.generateData();
+        this.generateGeojsonData();
     }
 }
