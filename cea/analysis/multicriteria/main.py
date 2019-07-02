821a078a186da6602b7e50f535758325cf177e11
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-07-02 11:24:59 +0800

    refactorede preprocessing DH to a more sane structure

--- cea/analysis/multicriteria/main.py
+++ cea/analysis/multicriteria/main.py
@@ -13,7 +13,7 @@ import cea.config
 import cea.inputlocator
 from cea.optimization.lca_calculations import LcaCalculations
 from cea.technologies.heat_exchangers import calc_Cinv_HEX
-import cea.optimization.distribution.network_opt_main as network_opt
+import cea.optimization.distribution.network_optimization_features as network_opt
 from cea.analysis.multicriteria.optimization_post_processing.locating_individuals_in_generation_script import \
     locating_individuals_in_generation_script, create_data_address_file
 from math import ceil, log
@@ -41,7 +41,9 @@ def multi_criteria_main(locator, config):
         data_address = pd.read_csv(locator.get_address_of_individuals_of_a_generation(generation))
 
     # import data storing information about the generation
-    data_processed, district_heating_network, district_cooling_network = preprocessing_generations_data(locator, generation)
+    data_processed = preprocessing_generations_data(locator, generation)
+    district_heating_network = data_processed['district_heating_network']
+    district_cooling_network = data_processed['district_cooling_network']
 
     # compile cost data
     if district_cooling_network:
@@ -164,6 +166,7 @@ def preprocessing_generations_data(locator, generation_number):
 
     district_heating_network = data['district_heating_network']
     district_cooling_network = data['district_cooling_network']
+    detailed_electricity_pricing = data['detailed_electricity_pricing']
 
     # change units of population (e.g., form kW to MW)
     df_population, individual_barcode, individual_names = Change_units_population(data)
@@ -181,7 +184,7 @@ def preprocessing_generations_data(locator, generation_number):
     # compile network barcodes
     df_network = df_network_barcodes(data, district_heating_network, district_cooling_network, individual_names)
 
-    #get metadata of the genearation
+    # get metadata of the genearation
     data_generation = pd.read_csv(locator.get_optimization_individuals_in_generation(generation_number))
 
     data_processed = {'population': df_population,
@@ -190,12 +193,15 @@ def preprocessing_generations_data(locator, generation_number):
                       'spread': data['spread'],
                       'euclidean_distance': data['euclidean_distance'],
                       'individual_barcode': df_individual_barcode,
-                      'individual_list':individual_list,
-                      'data_generation':data_generation}
+                      'individual_list': individual_list,
+                      'data_generation': data_generation,
+                      'district_heating_network': district_heating_network,
+                      'district_cooling_network': district_cooling_network,
+                      'detailed_electricity_pricing': detailed_electricity_pricing}
 
     print('compiled data of all individuals in this generation: ', generation_number)
 
-    return data_processed, district_heating_network, district_cooling_network
+    return data_processed
 
 
 def df_network_barcodes(data, district_heating_network, district_cooling_network, individual_names):
@@ -245,12 +251,14 @@ def change_units_hall_of_fame(data):
 
 
 def preprocessing_cost_data_DC(individual, df_current_individual, locator, data_raw, data_address, config):
-
     # local variables
     total_demand = pd.read_csv(locator.get_total_demand())
     building_names = total_demand.Name.values
-    string_network = data_raw['network'].loc[individual].values[0]
+    district_network_barcode = data_raw['network'].loc[individual].values[0]
     individual_barcode_list = data_raw['individual_barcode'].loc[individual].values[0]
+    district_heating_network = data_raw['district_heating_network']
+    district_cooling_network = data_raw['district_cooling_network']
+    detailed_electricity_pricing = data_raw['detailed_electricity_pricing']
 
     # get data about individual (pointer)
     data_address = data_address[data_address['individual_list'] == individual]
@@ -313,9 +321,8 @@ def preprocessing_cost_data_DC(individual, df_current_individual, locator, data_
     Capex_total_disconnected_USD = 0
     Opex_total_disconnected_USD = 0
     Capex_a_total_disconnected_USD = 0
-    DCN_barcode = string_network  # TODO: switch between DCN and DHN
 
-    for (index, building_name) in zip(DCN_barcode, building_names):
+    for (index, building_name) in zip(district_network_barcode, building_names):
         if index is '0':
             df = pd.read_csv(locator.get_optimization_decentralized_folder_building_result_cooling(building_name,
                                                                                                    configuration='AHU_ARU_SCU'))
@@ -355,19 +362,13 @@ def preprocessing_cost_data_DC(individual, df_current_individual, locator, data_
     data_costs['emissions_kiloton'] = data_raw['population']['emissions_kiloton'][individual]
     data_costs['prim_energy_TJ'] = data_raw['population']['prim_energy_TJ'][individual]
 
-    # Network costs
-    network_features = network_opt.network_opt_main(config,locator)  # TODO: check if it is using the latest run of result, if yes, it is wrong
-    network_costs_a_USD = network_features.pipesCosts_DCN_USD * DCN_barcode.count("1") / len(
-        DCN_barcode)  # FIXME: what does this mean?
-    data_costs['Network_costs_USD'] = network_costs_a_USD
-    Inv_IR = 0.05
-    Inv_LT = 20
-    network_costs_total_USD = (network_costs_a_USD * ((1 + Inv_IR) ** Inv_LT - 1) / (Inv_IR) * (1 + Inv_IR) ** Inv_LT)
-    data_costs['Network_costs_Total_USD'] = network_costs_total_USD
+    # NETWORK COSTS TODO:this should be part of the optimization
+    data_costs = calc_network_costs(data_costs, config, district_cooling_network, district_heating_network,
+                                    district_network_barcode, locator)
     # Substation costs
     substation_costs_a_USD = 0
     substation_costs_total_USD = 0
-    for (index, building_name) in zip(DCN_barcode, building_names):
+    for (index, building_name) in zip(district_network_barcode, building_names):
         if index == "1":
             if df_current_individual['Data Centre'][0] == 1:
                 df = pd.read_csv(locator.get_optimization_substations_results_file(building_name),
@@ -468,33 +469,78 @@ def preprocessing_cost_data_DC(individual, df_current_individual, locator, data_
 
 
 def preprocessing_cost_data_DH(individual, df_current_individual, locator, data_raw, data_address, config):
-
     # local variables
-    string_network = data_raw['network'].loc[individual].values[0]
+    district_network_barcode = data_raw['network'].loc[individual].values[0]
     total_demand = pd.read_csv(locator.get_total_demand())
     building_names = total_demand.Name.values
+    district_heating_network = data_raw['district_heating_network']
+    district_cooling_network = data_raw['district_cooling_network']
+    detailed_electricity_pricing = data_raw['detailed_electricity_pricing']
 
     # get data about individual (pointer)
     data_address = data_address[data_address['individual_list'] == individual]
     generation_number = data_address['generation_number_address'].values[0]
     individual_number = data_address['individual_number_address'].values[0]
 
-    # get data about the activation patterns of these buildings (main units)
-    data_costs = pd.read_csv(
-        os.path.join(locator.get_optimization_slave_investment_cost_detailed(individual_number, generation_number)))
-    data_activation_pattern = pd.read_csv(
-        os.path.join(locator.get_optimization_slave_heating_activation_pattern(individual_number, generation_number)))
-    data_electricity = pd.read_csv(os.path.join(
-        locator.get_optimization_slave_electricity_activation_pattern_heating(individual_number, generation_number)))
-    data_emissions = pd.read_csv(
-        os.path.join(locator.get_optimization_slave_investment_cost_detailed(individual_number, generation_number)))
+    # GET COSTS FOR GENERATION UNITS
+    data_costs = pd.read_csv(locator.get_optimization_slave_investment_cost_detailed(individual_number,
+                                                                                     generation_number))
+    data_costs['Capex_a_connected_Mio'] = data_costs.filter(like='Capex_a').sum(axis=1)[0] / 1E6
+    data_costs['Capex_total_connected_Mio'] = data_costs.filter(like='Capex').sum(axis=1)[0] / 1E6
+    data_costs['Opex_total_connected_Mio'] = data_costs.filter(like='Opex').sum(axis=1)[0] / 1E6
 
-    # Disconnected Buildings
+    # GET OBJECTIVES
+    data_costs['costs_Mio'] = data_raw['population']['costs_Mio'][individual]
+    data_costs['emissions_kiloton'] = data_raw['population']['emissions_kiloton'][individual]
+    data_costs['prim_energy_TJ'] = data_raw['population']['prim_energy_TJ'][individual]
+
+    # COSTS OF DISCONNECTED BUILDINGS TODO:this should be part of the optimization
+    data_costs = calc_disconnected_buildings_costs(building_names, data_costs, district_network_barcode, locator)
+
+    # NETWORK COSTS TODO:this should be part of the optimization
+    data_costs = calc_network_costs(data_costs, config, district_cooling_network, district_heating_network,
+                                    district_network_barcode, locator)
+
+    # SUBSTATIONS COSTS TODO:this should be part of the optimization
+    data_costs = calc_substations_costs(data_costs, building_names, config, district_network_barcode, locator)
+
+    # EXTRA - ELECTRICITY COSTS (AN UGLY FIX) - TODO:this should be part of the optimization
+    data_electricity = pd.read_csv(
+        locator.get_optimization_slave_electricity_activation_pattern_heating(individual_number,
+                                                                              generation_number))
+    lca = LcaCalculations(locator, detailed_electricity_pricing)
+    data_costs = calc_electricity_costs(data_costs, data_electricity, lca)
+
+    # SUMMARY OF ALL TOTAL COSTS (CAPEX AND OPEX)
+    data_costs['Capex_a_total_Mio'] = data_costs['Capex_a_connected_Mio'] + \
+                                      data_costs['Capex_a_disconnected_Mio'] + \
+                                      data_costs['Substation_costs_USD'] / 1E6 + \
+                                      data_costs['Network_costs_USD'] / 1E6
+    data_costs['Capex_total_Mio'] = data_costs['Capex_total_connected_Mio'] + \
+                                    data_costs['Capex_total_disconnected_Mio'] + \
+                                    data_costs['Substation_costs_Total_USD'] / 1E6 + \
+                                    data_costs['Network_costs_Total_USD'] / 1E6
+
+    data_costs['Opex_total_Mio'] = (data_costs['Opex_total_connected_Mio'] +
+                                    data_costs['Total_electricity_demand_GW'] * 1E9 * lca.ELEC_PRICE.mean()) / 1E6
+
+    data_costs['TAC_Mio'] = data_costs['Capex_a_total_Mio'] + data_costs['Opex_total_Mio']
+
+    # SUMMARY OF ALL EMISSIONS (AN UGLY FIX) - TODO:there was a message saying that it should be fixed in 20018, did that happen?
+    data_emissions = pd.read_csv(locator.get_optimization_slave_investment_cost_detailed(individual_number,
+                                                                                         generation_number))
+    data_costs['total_emissions_kiloton'] = data_costs['emissions_kiloton'] - abs(
+        2 * data_emissions['CO2_PV_disconnected'] / 1E6)
+    data_costs['total_prim_energy_TJ'] = data_costs['prim_energy_TJ'] - abs(
+        2 * data_emissions['Eprim_PV_disconnected'] / 1E6)
+
+    return data_costs
+
+
+def calc_disconnected_buildings_costs(building_names, data_costs, district_network_barcode, locator):
     Capex_total_disconnected_USD = 0
     Opex_total_disconnected_USD = 0
     Capex_a_total_disconnected_USD = 0
-    district_network_barcode = string_network
-
     for (index, building_name) in zip(district_network_barcode, building_names):
         if index is '0':
             df = pd.read_csv(locator.get_optimization_decentralized_folder_building_result_heating(building_name))
@@ -502,29 +548,56 @@ def preprocessing_cost_data_DH(individual, df_current_individual, locator, data_
             Opex_total_disconnected_USD += dfBest["Operation Costs [CHF]"].iloc[0]
             Capex_a_total_disconnected_USD += dfBest["Annualized Investment Costs [CHF]"].iloc[0]
             Capex_total_disconnected_USD += dfBest["Total Costs [CHF]"].iloc[0]
-
     data_costs['Capex_total_disconnected_Mio'] = Capex_total_disconnected_USD / 1E6
     data_costs['Opex_total_disconnected_Mio'] = Opex_total_disconnected_USD / 1E6
     data_costs['Capex_a_disconnected_Mio'] = Capex_a_total_disconnected_USD / 1E6
-
     data_costs['Capex_a_disconnected_USD'] = Capex_a_total_disconnected_USD
     data_costs['Opex_total_disconnected_USD'] = Opex_total_disconnected_USD
+    return data_costs
 
-    # get objectives
-    data_costs['costs_Mio'] = data_raw['population']['costs_Mio'][individual]
-    data_costs['emissions_kiloton'] = data_raw['population']['emissions_kiloton'][individual]
-    data_costs['prim_energy_TJ'] = data_raw['population']['prim_energy_TJ'][individual]
 
-    # get Network costs
-    network_features = network_opt.network_opt_main(config, locator)
-    network_costs_a_USD = network_features.pipesCosts_DCN_USD * district_network_barcode.count("1") / len(
-        district_network_barcode)
-    data_costs['Network_costs_USD'] = network_costs_a_USD
+def calc_electricity_costs(data_costs, data_electricity, lca):
+    data_costs['Total_electricity_demand_GW'] = (data_electricity['E_total_req_W'].sum()) / 1E9  # GW
+    data_costs['Electricity_for_hotwater_GW'] = (data_electricity['E_hotwater_total_W'].sum()) / 1E9  # GW
+    data_costs['Electricity_for_appliances_GW'] = (data_electricity['E_appliances_total_W'].sum()) / 1E9  # GW
+    renewable_share_electricity = (data_electricity['E_PV_directload_W'].sum() +
+                                   data_electricity['E_PV_grid_W'].sum() +
+                                   data_electricity['E_PVT_directload_W'].sum() +
+                                   data_electricity['E_PVT_grid_W'].sum()) * 100 / \
+                                  (data_costs['Total_electricity_demand_GW'] * 1E9)  # [%]
+    data_costs['renewable_share_electricity'] = renewable_share_electricity
+    data_costs['Electricity_Costs_Mio'] = ((data_electricity['E_GRID_directload_W'].sum() +
+                                            data_electricity['E_total_to_grid_W_negative'].sum()) *
+                                           lca.ELEC_PRICE.mean()) / 1E6
+    return data_costs
+
+
+def calc_network_costs(data_costs, config, district_cooling_network, district_heating_network, district_network_barcode,
+                       locator):
+    # Intitialize class
+    network_features = network_opt.NetworkOptimizationFeatures(config, locator)
+    network_features.district_heating_network = district_heating_network
+    network_features.district_cooling_network = district_cooling_network
+
+    data_costs['Network_costs_USD'] = 0.0
+    if district_heating_network:
+        # get costs
+        data_costs['Network_costs_USD'] += network_features.pipesCosts_DHN_USD * district_network_barcode.count(
+            "1") / len(
+            district_network_barcode)
+    if district_cooling_network:
+        data_costs['Network_costs_USD'] += network_features.pipesCosts_DCN_USD * district_network_barcode.count(
+            "1") / len(
+            district_network_barcode)
+
     Inv_IR = 0.05
     Inv_LT = 20
-    network_costs_total_USD = (network_costs_a_USD * ((1 + Inv_IR) ** Inv_LT - 1) / (Inv_IR) * (1 + Inv_IR) ** Inv_LT)
-    data_costs['Network_costs_Total_USD'] = network_costs_total_USD
-    # Substation costs
+    data_costs['Network_costs_Total_USD'] = (
+            data_costs['Network_costs_USD'] * ((1 + Inv_IR) ** Inv_LT - 1) / (Inv_IR) * (1 + Inv_IR) ** Inv_LT)
+    return data_costs
+
+
+def calc_substations_costs(data_costs, building_names, config, district_network_barcode, locator):
     substation_costs_a_USD = 0
     substation_costs_total_USD = 0
     for (index, building_name) in zip(district_network_barcode, building_names):
@@ -532,7 +605,6 @@ def preprocessing_cost_data_DH(individual, df_current_individual, locator, data_
             df = pd.read_csv(locator.get_optimization_substations_results_file(building_name),
                              usecols=["Q_dhw_W", "Q_heating_W"])
             subsArray = np.array(df)
-
             Q_max_W = np.amax(subsArray[:, 0] + subsArray[:, 1])
             Capex_a_HEX_building_USD, Opex_fixed_HEX_building_USD, Capex_HEX_building_USD = calc_Cinv_HEX(Q_max_W,
                                                                                                           locator,
@@ -540,47 +612,8 @@ def preprocessing_cost_data_DH(individual, df_current_individual, locator, data_
                                                                                                           'HEX1')
             substation_costs_total_USD += Capex_HEX_building_USD
             substation_costs_a_USD += Capex_a_HEX_building_USD + Opex_fixed_HEX_building_USD
-
     data_costs['Substation_costs_USD'] = substation_costs_a_USD
     data_costs['Substation_costs_Total_USD'] = substation_costs_total_USD
-
-    # Electricity Details/Renewable Share
-    lca = LcaCalculations(locator, config.detailed_electricity_pricing)
-
-    data_costs['Total_electricity_demand_GW'] = (data_electricity['E_total_req_W'].sum()) / 1E9  # GW
-    data_costs['Electricity_for_hotwater_GW'] = (data_electricity['E_hotwater_total_W'].sum()) / 1E9  # GW
-    data_costs['Electricity_for_appliances_GW'] = (data_electricity['E_appliances_total_W'].sum()) / 1E9  # GW
-
-    renewable_share_electricity = (data_electricity['E_PV_directload_W'].sum() +
-                                   data_electricity['E_PV_grid_W'].sum() + data_electricity[
-                                       'E_PVT_directload_W'].sum() +
-                                   data_electricity['E_PVT_grid_W'].sum()) * 100 / \
-                                  (data_costs['Total_electricity_demand_GW'] * 1E9)  # [%]
-    data_costs['renewable_share_electricity'] = renewable_share_electricity
-
-    data_costs['Electricity_Costs_Mio'] = ((data_electricity['E_GRID_directload_W'].sum() +
-                                            data_electricity[
-                                                'E_total_to_grid_W_negative'].sum()) * lca.ELEC_PRICE.mean()) / 1E6
-
-    # analyzed criteria
-    capex_a_sum = data_costs.filter(like='Capex_a').sum(axis=1)[0]
-    data_costs['Capex_a_total_Mio'] = (
-                                              capex_a_sum + Capex_a_total_disconnected_USD + substation_costs_a_USD + network_costs_a_USD) / 1E6
-    capex_sum = data_costs.filter(like='Capex').sum(axis=1)[0] - capex_a_sum
-    data_costs['Capex_total_Mio'] = (
-                                            capex_sum + Capex_total_disconnected_USD + substation_costs_total_USD + network_costs_total_USD) / 1E6
-    opex_sum = data_costs.filter(like='Opex').sum(axis=1)[0]
-    data_costs['Opex_total_Mio'] = (opex_sum + data_costs[
-        'Total_electricity_demand_GW'] * 1E9 * lca.ELEC_PRICE.mean()) / 1E6
-
-    data_costs['TAC_Mio'] = data_costs['Capex_a_total_Mio'] + data_costs['Opex_total_Mio']
-
-    # temporary fix for bug in emissions calculation, change it after executive course
-    data_costs['total_emissions_kiloton'] = data_costs['emissions_kiloton'] - abs(
-        2 * data_emissions['CO2_PV_disconnected'] / 1E6)
-    data_costs['total_prim_energy_TJ'] = data_costs['prim_energy_TJ'] - abs(
-        2 * data_emissions['Eprim_PV_disconnected'] / 1E6)
-
     return data_costs
 
 
