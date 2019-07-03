5d1c870fd27efd15d136562234a2c2f3b19acdbf
Reynold Mok <reyold@hotmail.com>
2019-07-03 15:51:21 +0800

    Create global 500 status page

--- cea/interfaces/dashboard/base/routes.py
+++ cea/interfaces/dashboard/base/routes.py
@@ -60,6 +60,8 @@ def not_found_error(error):
     return render_template('errors/page_404.html'), 404
 
 
-@blueprint.errorhandler(500)
+@blueprint.app_errorhandler(500)
 def internal_error(error):
-    return render_template('errors/page_500.html'), 500
+    import traceback
+    error_trace = traceback.format_exc()
+    return render_template('errors/page_500.html', error=error_trace), 500
