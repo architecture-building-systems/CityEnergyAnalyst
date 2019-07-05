a10c8fa462cbaa87b93a1dfa5c50c46f3dbb894a
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-07-05 14:06:11 +0800

    fixing units of prices

--- cea/inputlocator.py
+++ cea/inputlocator.py
@@ -141,6 +141,12 @@ class InputLocator(object):
         return os.path.join(self.get_optimization_slave_results_folder(gen_num),
                             'ind_%(ind_num)s_Cooling_Activation_Pattern.csv' % locals())
 
+    def get_optimization_slave_cooling_opex_var(self, ind_num, gen_num):
+
+        """scenario/outputs/data/calibration/clustering/checkpoints/..."""
+        return os.path.join(self.get_optimization_slave_results_folder(gen_num),
+                            'ind_%(ind_num)s_Cooling_Opex_var.csv' % locals())
+
     def get_optimization_slave_electricity_activation_pattern_heating(self, ind_num, gen_num):
         """scenario/outputs/data/calibration/clustering/checkpoints/..."""
         return os.path.join(self.get_optimization_slave_results_folder(gen_num),
@@ -210,10 +216,10 @@ class InputLocator(object):
         return os.path.join(self.get_optimization_slave_results_folder(gen_num),
                             'ind_%(ind_num)s_detailed_capacity.csv' % locals())
 
-    def get_optimization_slave_investment_cost_detailed_cooling(self, ind_num, gen_num):
+    def get_optimization_slave_cooling_ecological_costs(self, ind_num, gen_num):
         """scenario/outputs/data/calibration/clustering/checkpoints/..."""
         return os.path.join(self.get_optimization_slave_results_folder(gen_num),
-                            'ind_%(ind_num)s_cooling_InvestmentCostDetailed.csv' % locals())
+                            'ind_%(ind_num)s_cooling_ecological_costs.csv' % locals())
 
     def get_preprocessing_costs(self):
         """scenario/outputs/data/calibration/clustering/checkpoints/..."""
