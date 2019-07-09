fixing error about computation of heat pumps

cea/optimization/preprocessing/preprocessing_main.py
@@ -117,20 +117,22 @@ class SolarFeatures(object):
         A_SC_FP_m2 = np.zeros(HOURS_IN_YEAR)
         A_SC_ET_m2 = np.zeros(HOURS_IN_YEAR)
 
+        #import and sum all the area available
         for name in building_names:
             building_PV = pd.read_csv(os.path.join(locator.get_potentials_solar_folder(), name + '_PV.csv'))
             building_PVT = pd.read_csv(os.path.join(locator.get_potentials_solar_folder(), name + '_PVT.csv'))
             building_SC_FP = pd.read_csv(os.path.join(locator.get_potentials_solar_folder(), name + '_SC_FP.csv'))
             building_SC_ET = pd.read_csv(os.path.join(locator.get_potentials_solar_folder(), name + '_SC_ET.csv'))
-            E_PV_gen_kWh = E_PV_gen_kWh + building_PV['E_PV_gen_kWh']
-            E_PVT_gen_kWh = E_PVT_gen_kWh + building_PVT['E_PVT_gen_kWh']
-            Q_PVT_gen_kWh = Q_PVT_gen_kWh + building_PVT['Q_PVT_gen_kWh']
-            Q_SC_FP_gen_kWh = Q_SC_FP_gen_kWh + building_SC_FP['Q_SC_gen_kWh']
-            Q_SC_ET_gen_kWh = Q_SC_ET_gen_kWh + building_SC_ET['Q_SC_gen_kWh']
-            A_PV_m2 = building_PV['Area_PV_m2']
-            A_PVT_m2 = building_PVT['Area_PVT_m2']
-            A_SC_FP_m2 = building_SC_FP['Area_SC_m2']
-            A_SC_ET_m2 = building_SC_ET['Area_SC_m2']
+
+            E_PV_gen_kWh += E_PV_gen_kWh + building_PV['E_PV_gen_kWh']
+            E_PVT_gen_kWh += E_PVT_gen_kWh + building_PVT['E_PVT_gen_kWh']
+            Q_PVT_gen_kWh += Q_PVT_gen_kWh + building_PVT['Q_PVT_gen_kWh']
+            Q_SC_FP_gen_kWh += Q_SC_FP_gen_kWh + building_SC_FP['Q_SC_gen_kWh']
+            Q_SC_ET_gen_kWh += Q_SC_ET_gen_kWh + building_SC_ET['Q_SC_gen_kWh']
+            A_PV_m2 += building_PV['Area_PV_m2']
+            A_PVT_m2 += building_PVT['Area_PVT_m2']
+            A_SC_FP_m2 += building_SC_FP['Area_SC_m2']
+            A_SC_ET_m2 += building_SC_ET['Area_SC_m2']
 
         self.Peak_PV_Wh = E_PV_gen_kWh.values.max() * 1E3
         self.A_PV_m2 = A_PV_m2.values.max()