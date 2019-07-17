84b73193d8fed5d8b4ebe8219e420e12a488caa2
shanshanhsieh <hsieh@arch.ethz.ch>
2019-07-17 16:28:45 +0800

    refactor function and calculate detail OAU Ex

--- cea/osmose/post_compute_osmose_results.py
+++ cea/osmose/post_compute_osmose_results.py
@@ -24,9 +24,6 @@ def set_up_ex_df(results, operation_df, electricity_df, air_flow_df):
     Ex_min_Qc_kWh, Ex_min_air_kWh, Ex_min_w_kWh, rh_ref = calc_theoretical_Ex_requirement(T_ref_C, ex_air_ext,
                                                                                           ex_air_int, results,
                                                                                           w_RA_gperkg)
-    # total
-    Ex_min_total_kWh = Ex_min_Qc_kWh + Ex_min_air_kWh + Ex_min_w_kWh
-
     ## exergy destruction at building interface
     oau_Ex_air_kWh, oau_Ex_w_kWh, rau_EX_air_kWh, rau_Ex_w_kWh, scu_Ex_kWh = calc_process_Ex_requirement(T_ref_C,
                                                                                                          air_flow_df,
@@ -37,17 +34,14 @@ def set_up_ex_df(results, operation_df, electricity_df, air_flow_df):
                                                                                                          rh_ref,
                                                                                                          w_RA_gperkg,
                                                                                                          w_ref_sat_gperkg)
-    # total
-    Ex_process_total = (oau_Ex_air_kWh + oau_Ex_w_kWh) + (rau_EX_air_kWh + rau_Ex_w_kWh) + scu_Ex_kWh
-
     ## Exergy of cooling/heating utilities
     if results.filter(like='q_oau_chi').size == 0:
         oau_Ex_c_kWh = np.nan_to_num(np.vectorize(calc_Ex.calc_Ex_Qc)(results['Qc_de'], results['T_s_i_de']-273.15, T_ref_C))
     else:
         oau_Ex_c_kWh = np.nan_to_num(np.vectorize(calc_Ex.calc_Ex_Qc)(results.filter(like='q_oau_chi').sum(axis=1), results['T_evap'], T_ref_C))
     oau_Ex_h_kWh = results['q_reheat'].values
-    rau_Ex_c_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_chi_lt'], 7, T_ref_C)
-    scu_Ex_c_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_chi_ht'], 16, T_ref_C)
+    rau_Ex_c_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_coi_scu'], 7, T_ref_C)
+    scu_Ex_c_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_coi_lcu'], 16, T_ref_C)
 
     # ex_df
     ex_df = pd.DataFrame()
@@ -71,15 +65,41 @@ def set_up_ex_df(results, operation_df, electricity_df, air_flow_df):
     return ex_df
 
 
+# def calc_process_Ex_requirement(T_ref_C, air_flow_df, ex_air_ext, ex_air_int, operation_df, results, rh_ref,
+#                                 w_RA_gperkg, w_ref_sat_gperkg):
+#     # OAU
+#     oau_ex_air_in = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(operation_df['T_SA'], operation_df['w_SA'], T_ref_C,
+#                                                                   w_ref_sat_gperkg)
+#     oau_Ex_air_kWh = np.nan_to_num((air_flow_df['OAU_in'] * (oau_ex_air_in - ex_air_ext)).values)
+#     oau_ex_w_cond_kJperkg = np.nan_to_num(np.vectorize(calc_Ex.calc_exergy_liquid_water)(results['T_chw'], T_ref_C, rh_ref))
+#     oau_Ex_w_kWh = np.nan_to_num(results['m_w_coil_cond'].values * oau_ex_w_cond_kJperkg)
+#     # RAU
+#     rau_T_SA = 10.27
+#     rau_w_SA = aux.calc_w_ss_from_T(rau_T_SA)
+#     rau_ex_air_in = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(rau_T_SA, rau_w_SA, T_ref_C, w_ref_sat_gperkg)
+#     m_RAU_kgpers = results['w_lcu'] * 1000 / (w_RA_gperkg - rau_w_SA)
+#     rau_EX_air_kWh = np.nan_to_num((m_RAU_kgpers * (rau_ex_air_in - ex_air_int)).values)
+#     rau_ex_w_cond_kJperkg = np.nan_to_num(np.vectorize(calc_Ex.calc_exergy_liquid_water)(rau_T_SA, T_ref_C, rh_ref))
+#     rau_Ex_w_kWh = (results['w_lcu'] * rau_ex_w_cond_kJperkg).values
+#     # SCU
+#     scu_T_SA = 20
+#     scu_Ex_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_scu_sen'], scu_T_SA, T_ref_C)
+#     return oau_Ex_air_kWh, oau_Ex_w_kWh, rau_EX_air_kWh, rau_Ex_w_kWh, scu_Ex_kWh
+
 def calc_process_Ex_requirement(T_ref_C, air_flow_df, ex_air_ext, ex_air_int, operation_df, results, rh_ref,
                                 w_RA_gperkg, w_ref_sat_gperkg):
     # OAU
-    oau_ex_air_in = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(operation_df['T_SA'], operation_df['w_SA'], T_ref_C,
-                                                                  w_ref_sat_gperkg)
-    oau_Ex_air_kWh = np.nan_to_num((air_flow_df['OAU_in'] * (oau_ex_air_in - ex_air_ext)).values)
-    oau_ex_w_cond_kJperkg = np.nan_to_num(np.vectorize(calc_Ex.calc_exergy_liquid_water)(results['T_chw'], T_ref_C, rh_ref))
-    oau_Ex_w_kWh = np.nan_to_num(results['m_w_coil_cond'].values * oau_ex_w_cond_kJperkg)
+    oau_Ex_air_kWh, oau_Ex_w_kWh, oau_Ex_total_kWh = calc_OAU_process_Ex(T_ref_C, air_flow_df, ex_air_ext, operation_df,
+                                                                         results, rh_ref, w_ref_sat_gperkg)
     # RAU
+    rau_EX_air_kWh, rau_Ex_w_kWh = calc_RAU_process_Ex(T_ref_C, ex_air_int, results, rh_ref, w_RA_gperkg, w_ref_sat_gperkg)
+    # SCU
+    scu_T_SA = 20
+    scu_Ex_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_scu_sen'], scu_T_SA, T_ref_C)
+    return oau_Ex_air_kWh, oau_Ex_w_kWh, rau_EX_air_kWh, rau_Ex_w_kWh, scu_Ex_kWh
+
+
+def calc_RAU_process_Ex(T_ref_C, ex_air_int, results, rh_ref, w_RA_gperkg, w_ref_sat_gperkg):
     rau_T_SA = 10.27
     rau_w_SA = aux.calc_w_ss_from_T(rau_T_SA)
     rau_ex_air_in = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(rau_T_SA, rau_w_SA, T_ref_C, w_ref_sat_gperkg)
@@ -87,10 +107,36 @@ def calc_process_Ex_requirement(T_ref_C, air_flow_df, ex_air_ext, ex_air_int, op
     rau_EX_air_kWh = np.nan_to_num((m_RAU_kgpers * (rau_ex_air_in - ex_air_int)).values)
     rau_ex_w_cond_kJperkg = np.nan_to_num(np.vectorize(calc_Ex.calc_exergy_liquid_water)(rau_T_SA, T_ref_C, rh_ref))
     rau_Ex_w_kWh = (results['w_lcu'] * rau_ex_w_cond_kJperkg).values
-    # SCU
-    scu_T_SA = 20
-    scu_Ex_kWh = np.vectorize(calc_Ex.calc_Ex_Qc)(results['q_scu_sen'], scu_T_SA, T_ref_C)
-    return oau_Ex_air_kWh, oau_Ex_w_kWh, rau_EX_air_kWh, rau_Ex_w_kWh, scu_Ex_kWh
+    return rau_EX_air_kWh, rau_Ex_w_kWh
+
+
+def calc_OAU_process_Ex(T_ref_C, air_flow_df, ex_air_ext, operation_df, results, rh_ref, w_ref_sat_gperkg):
+    # ex of air
+    oau_ex_air_in = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(operation_df['T_SA'], operation_df['w_SA'], T_ref_C,
+                                                                  w_ref_sat_gperkg)
+    oau_ex_air_RA = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(results['T_RA'], 10.29, T_ref_C, w_ref_sat_gperkg)
+    oau_ex_air_EA = np.vectorize(calc_Ex.calc_exergy_moist_air_2)(results['OAU_T_EA'], results['OAU_w_EA'], T_ref_C,
+                                                                  w_ref_sat_gperkg)
+    # Ex of air
+    oau_Ex_air_kWh = np.nan_to_num((air_flow_df['OAU_in'] * (oau_ex_air_in - ex_air_ext)).values)
+    oau_Ex_air_exhaust_kWh = np.nan_to_num((results['m_oau_out'] * (oau_ex_air_EA - oau_ex_air_RA)).values)
+    # ex of water
+    oau_ex_w_cond_kJperkg = np.nan_to_num(
+        np.vectorize(calc_Ex.calc_exergy_liquid_water)(results['T_chw'], T_ref_C, rh_ref))
+    oau_ex_w_RA_kJperkg = np.nan_to_num(
+        np.vectorize(calc_Ex.calc_exergy_liquid_water)(results['T_RA'], T_ref_C, rh_ref))
+    oau_Ex_w_removed_kWh_dict = {}
+    for i in range(results.filter(like='T_w_removed').columns.size):
+        oau_ex_w_removed = np.nan_to_num(np.vectorize(calc_Ex.calc_exergy_liquid_water)(results['T_w_removed_'+str(i+1)], T_ref_C, rh_ref))
+        oau_Ex_w_removed_kWh_dict['Ex_w_removed_'+str(i+1)] = np.nan_to_num(results['m_w_removed_'+str(i+1)].values * oau_ex_w_removed)
+    # Ex of water
+    oau_Ex_w_kWh = np.nan_to_num(results['m_w_coil_cond'].values * oau_ex_w_cond_kJperkg)
+    oau_Ex_w_removed_kWh = sum(oau_Ex_w_removed_kWh_dict.values())
+    oau_Ex_w_add_kWh = np.nan_to_num(results['m_w_add'].values * oau_ex_w_RA_kJperkg)
+    # total balance
+    oau_Ex_total_kWh = oau_Ex_air_kWh + oau_Ex_air_exhaust_kWh + oau_Ex_w_kWh + oau_Ex_w_removed_kWh - oau_Ex_w_add_kWh
+    oau_Ex_total_in_kWh = oau_Ex_air_kWh + oau_Ex_w_kWh
+    return oau_Ex_air_kWh, oau_Ex_w_kWh, oau_Ex_total_kWh
 
 
 def calc_theoretical_Ex_requirement(T_ref_C, ex_air_ext, ex_air_int, results, w_RA_gperkg):
