e96e3f8347c8c6ed6e3018185639119265ad3a92
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-08-02 18:29:01 +0800

    fixing problems of double counting emissions and pen in heating demand

--- cea/optimization/master/generation.py
+++ cea/optimization/master/generation.py
@@ -9,13 +9,12 @@ import random
 from cea.optimization.constants import DH_CONVERSION_TECHNOLOGIES_CAPACITY, \
     DH_CONVERSION_TECHNOLOGIES_SHARE, \
     DC_CONVERSION_TECHNOLOGIES_CAPACITIES, \
-    DC_CONVERSION_TECHNOLOGIES_SHARE, \
-    DH_CONVERSION_TECHNOLOGIES_WITH_SPACE_RESTRICTIONS, DC_TECHNOLOGIES_SHARING_SPACE
+    DC_CONVERSION_TECHNOLOGIES_SHARE
 from cea.optimization.master.validation import validation_main
 
-__author__ = "Thuy-An Nguyen"
+__author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2015, Architecture and Building Systems - ETH Zurich"
-__credits__ = ["Thuy-An Nguyen", "Tim Vollrath", "Jimeno A. Fonseca"]
+__credits__ = [ "Jimeno A. Fonseca"]
 __license__ = "MIT"
 __version__ = "0.1"
 __maintainer__ = "Daren Thomas"
@@ -79,15 +78,15 @@ def generate_main(individual_with_names_dict,
                                                                   column_names_buildings_cooling)
 
     populated_individual_with_name_dict = validation_main(populated_individual_with_name_dict,
-                                              heating_unit_names,
-                                              cooling_unit_names,
-                                              heating_unit_names_share,
-                                              cooling_unit_names_share,
-                                              column_names_buildings_heating,
-                                              column_names_buildings_cooling,
-                                              district_heating_network,
-                                              district_cooling_network
-                                              )
+                                                          heating_unit_names,
+                                                          cooling_unit_names,
+                                                          heating_unit_names_share,
+                                                          cooling_unit_names_share,
+                                                          column_names_buildings_heating,
+                                                          column_names_buildings_cooling,
+                                                          district_heating_network,
+                                                          district_cooling_network
+                                                          )
 
     # CONVERT BACK INTO AN INDIVIDUAL STRING IMPORTANT TO USE column_names to keep the order
     individual = []
@@ -101,7 +100,6 @@ def populate_individual(empty_individual_with_names_dict,
                         name_conversion_technologies,
                         name_share_conversion_technologies,
                         columns_buildings_name):
-
     # do it for units that are activated
     for column, limits in name_conversion_technologies:
         lim_inf = limits[0]
@@ -119,6 +117,7 @@ def populate_individual(empty_individual_with_names_dict,
         empty_individual_with_names_dict[column] = random.randint(0, 1)
 
     return empty_individual_with_names_dict
+
 
 def individual_to_barcode(individual, column_names, column_names_buildings_heating,
                           column_names_buildings_cooling):
@@ -143,4 +142,3 @@ def individual_to_barcode(individual, column_names, column_names_buildings_heati
             DCN_barcode += str(int(individual_with_name_dict[name]))
 
     return DHN_barcode, DCN_barcode, individual_with_name_dict
-
