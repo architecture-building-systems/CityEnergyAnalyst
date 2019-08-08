846fde07cf09348dd0155b03bf43e5f0ee3e95a7
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-08-08 14:53:43 +0800

    supply_system_map data ready

--- cea/plots/supply_system/requirements_curve_electricity.py
+++ cea/plots/supply_system/requirements_curve_electricity.py
@@ -67,6 +67,7 @@ class RequirementsCurveDistrictElectricityPlot(cea.plots.supply_system.SupplySys
 
         ]
         self.analysis_field_demand = ['E_electricalnetwork_sys_req_W']
+        self.timeframe = self.parameters['timeframe']
         self.input_files = [(self.locator.get_optimization_slave_electricity_requirements_data,
                              [self.individual, self.generation])]
 
