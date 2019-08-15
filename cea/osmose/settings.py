9a2908a04fcdd0ced7af9f404ed526319c55c073
shanshanhsieh <hsieh@arch.ethz.ch>
2019-08-16 02:28:16 +0800

    Update settings.py

--- cea/osmose/settings.py
+++ cea/osmose/settings.py
@@ -1,25 +1,28 @@
 # import from config # TODO: add to config
 
 ## GENERAL ##
-# TECHS = ['HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_LD', 'HCS_IEHX']
+T_b_CDD = 25.0
+# TECHS = ['HCS_LD', 'HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_IEHX']
 TECHS = ['HCS_coil']
-timesteps = 168  # 168 (week)
+# timesteps = 168  # 168 (week)
+timesteps = "typical days"  # 168 (week)
+typical_days_path = "E:\\WP2"
 PLOTS = ['electricity_usages','air_flow','OAU_T_w_supply','exergy_usages', 'humidity_balance', 'humidity_storage', 'heat_balance']
 new_calculation = False
 
 # GENERAL INPUTS
-season = 'Autumn'
-specified_buildings = ["B001"]
+season = 'Summer'
+specified_buildings = ["B010"]
 #specified_buildings = ["B001","B002","B003","B004","B005","B006","B007","B008","B009","B010"]
-cases = ['ABU_CBD_m_WP1_HOT']
+cases = ['WTP_CBD_m_WP1_RET','WTP_CBD_m_WP1_HOT','WTP_CBD_m_WP1_OFF']
 
 ## LAPTOP ##
-# ampl_lic_path = "C:\\Users\\Shanshan\\Desktop\\ampl"
-# osmose_project_path = "D:\\SH\\HCS\\Projects"
-# result_destination = "D:\\test_0805"
+ampl_lic_path = "C:\\Users\\Shanshan\\Desktop\\ampl"
+osmose_project_path = "C:\\OSMOSE_projects\\HCS\\Projects"
+result_destination = "E:\\test_0805"
 
 ## WORK STATION ##
-ampl_lic_path = "C:\\Users\\Zhongming\\Desktop\\SH\\ampl"
+# ampl_lic_path = "C:\\Users\\Zhongming\\Desktop\\SH\\ampl"
 # osmose_project_path = "C:\\Users\\Zhongming\\Documents\\HCS\\Projects"
 # result_destination = "D:\\SH\\test_0805"
 
@@ -65,13 +68,13 @@ ampl_lic_path = "C:\\Users\\Zhongming\\Desktop\\SH\\ampl"
 # ==============================================================================================
 
 # == ABU Autumn ==
-season = 'Autumn'
-cases = ['ABU_CBD_m_WP1_HOT']
-specified_buildings = ["B006","B002"]
-# cases = ['ABU_CBD_m_WP1_OFF','ABU_CBD_m_WP1_HOT','ABU_CBD_m_WP1_RET']
-# specified_buildings = ["B001","B002","B003","B004","B005","B006","B007","B008","B009","B010"]
-osmose_project_path = "C:\\Users\\Zhongming\\Documents\\HCS\\Projects"
-result_destination = "D:\\SH\\WP2\\ABU_Autumn"
+# season = 'Autumn'
+# cases = ['ABU_CBD_m_WP1_HOT']
+# specified_buildings = ["B006","B002"]
+# # cases = ['ABU_CBD_m_WP1_OFF','ABU_CBD_m_WP1_HOT','ABU_CBD_m_WP1_RET']
+# # specified_buildings = ["B001","B002","B003","B004","B005","B006","B007","B008","B009","B010"]
+# osmose_project_path = "C:\\Users\\Zhongming\\Documents\\HCS\\Projects"
+# result_destination = "D:\\SH\\WP2\\ABU_Autumn"
 # ==============================================================================================
 
 
