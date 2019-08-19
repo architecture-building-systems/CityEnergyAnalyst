43764682caefc37eead9b69ad0830f7c8b18e0e2
Daren Thomas <thomas@arch.ethz.ch>
2019-08-19 16:11:51 +0200

    listing jobs

--- cea/interfaces/dashboard/server/jobs.py
+++ cea/interfaces/dashboard/server/jobs.py
@@ -1,3 +1,22 @@
 from flask_restplus import Namespace, Resource, fields
 
 api = Namespace('Jobs', description='A job server for cea-worker processes')
+
+
+# FIXME: replace with database or similar solution
+class Job(object):
+    """Store all the information required to run a job"""
+    def __init__(self, id, script, project, scenario, parameters):
+        self.id = id
+        self.script = script
+        self.project = project
+        self.scenario = scenario
+        self.parameters = parameters
+
+
+jobs = [Job(id=1, script="demand", project="reference-case-cooling", scenario="baseline", parameters={})]
+
+@api.route('/list')
+class ListJobs(Resource):
+    def get(self):
+        return [j.id for j in jobs]
