8d810226184e308a48222f1294486c4006b1a19b
Daren Thomas <thomas@arch.ethz.ch>
2019-08-20 14:06:15 +0200

    simple proof of concept

--- cea/interfaces/dashboard/server/jobs.py
+++ cea/interfaces/dashboard/server/jobs.py
@@ -2,21 +2,47 @@ from flask_restplus import Namespace, Resource, fields
 
 api = Namespace('Jobs', description='A job server for cea-worker processes')
 
+# Job states
+JOB_STATE_PENDING = 0
+JOB_STATE_STARTED = 1
+JOB_STATE_SUCCESS = 2
+JOB_STATE_ERROR = 3
+
+job_info_model = api.model('JobInfo', {
+    'id': fields.Integer,
+    'script': fields.String,
+    'state': fields.Integer,
+    'parameters': fields.Raw,
+})
+
 
 # FIXME: replace with database or similar solution
-class Job(object):
+class JobInfo(object):
     """Store all the information required to run a job"""
-    def __init__(self, id, script, project, scenario, parameters):
+    def __init__(self, id, script, parameters):
         self.id = id
         self.script = script
-        self.project = project
-        self.scenario = scenario
         self.parameters = parameters
+        self.state = JOB_STATE_PENDING
 
 
-jobs = [Job(id=1, script="demand", project="reference-case-cooling", scenario="baseline", parameters={})]
+jobs = {
+    1: JobInfo(id=1, script="demand", parameters={
+        "scenario": r"c:\Users\darthoma\Documents\CityEnergyAnalyst\projects\reference-case-open\baseline",
+        "multiprocessing": False,
+    }),
+}
 
-@api.route('/list')
+
+@api.route("/<int:jobid>")
+class Job(Resource):
+    @api.marshal_with(job_info_model)
+    def get(self, jobid):
+        return jobs[jobid]
+
+
+@api.route("/list")
 class ListJobs(Resource):
+    @api.marshal_with(job_info_model, as_list=True)
     def get(self):
-        return [j.id for j in jobs]
+        return jobs.values()
