48fa524c0548c72c8e28eb5539c6eda220242690
Daren Thomas <thomas@arch.ethz.ch>
2019-08-21 15:36:29 +0200

    emit cea-worker stream messages to socket.io

--- cea/interfaces/dashboard/server/streams.py
+++ cea/interfaces/dashboard/server/streams.py
@@ -6,8 +6,10 @@ FIXME: when does this data get cleared?
 from __future__ import division
 from __future__ import print_function
 
-from flask import request
+from flask import request, current_app
 from flask_restplus import Namespace, Resource, fields
+from cea.interfaces.dashboard.dashboard import socketio
+
 
 __author__ = "Daren Thomas"
 __copyright__ = "Copyright 2019, Architecture and Building Systems - ETH Zurich"
@@ -20,6 +22,7 @@ __status__ = "Production"
 
 api = Namespace('Streams', description='A collection of output from cea-worker processes')
 
+# map jobid to a list of messages
 streams = {}
 
 
@@ -27,4 +30,9 @@ streams = {}
 class WriteStream(Resource):
     def put(self, jobid):
         # FIXME: this output needs to bubble up to the user interface...
-        print(request.data, end='')
+        msg = request.data
+        streams.setdefault(jobid, []).append(msg)
+
+        # emit the message using socket.io
+        socketio.emit('cea-worker-message', {"message": msg, "jobid": jobid})
+        print(msg, end='')
