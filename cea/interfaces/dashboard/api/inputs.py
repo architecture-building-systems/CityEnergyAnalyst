7f8d7d6a643dae3039b0cc8bb46735fcd1af02e7
Reynold Mok <reyold@hotmail.com>
2019-08-23 19:22:39 +0800

    Update input api

--- cea/interfaces/dashboard/api/inputs.py
+++ cea/interfaces/dashboard/api/inputs.py
@@ -3,6 +3,8 @@ from flask_restplus import Namespace, Resource, fields, abort
 import cea.config
 import cea.inputlocator
 import cea.utilities.dbf
+from cea.utilities.standardize_coordinates import get_geographic_coordinate_system
+
 import pandas
 import geopandas
 import os
@@ -34,6 +36,8 @@ def read_inputs_field_types():
 
 
 INPUTS = read_inputs_field_types()
+INPUT_KEYS = INPUTS.keys()
+GEOJSON_KEYS = ['zone', 'district', 'streets', 'dc', 'dh']
 
 # INPUT_MODEL = api.model('Input', {
 #     'fields': fields.List(fields.String, description='Column names')
@@ -52,133 +56,151 @@ INPUTS = read_inputs_field_types()
 @api.route('/')
 class InputList(Resource):
     def get(self):
-        return {'db': INPUTS.keys(), 'others': ['streets', 'networks']}
+        return {'buildingProperties': INPUT_KEYS, 'others': ['streets', 'dc', 'dh']}
 
 
-@api.route('/<string:db>')
+@api.route('/building-properties/<string:db>')
 class Input(Resource):
     def get(self, db):
-        if not db in INPUTS:
-            abort(400, 'Input file not found: %s' % db, choices=INPUTS.keys())
+        if db not in INPUTS:
+            abort(400, 'Input file not found: %s' % db, choices=INPUT_KEYS)
         db_info = INPUTS[db]
         columns = OrderedDict()
         for column in db_info['fieldnames']:
             columns[column] = db_info['fieldtypes'][column].__name__
-
         return columns
 
 
-@api.route('/<string:db>/geojson')
-class InputGeojson(Resource):
+@api.route('/building-properties/<string:db>/geojson')
+class InputDatabases(Resource):
     def get(self, db):
-        config = cea.config.Configuration()
-        if not db in INPUTS:
-            abort(400, 'Input file not found: %s' % db)
+        if not (db in INPUT_KEYS and db in GEOJSON_KEYS):
+            abort(400, 'Input file not found: %s' % db, choices=list(set(INPUT_KEYS) & set(GEOJSON_KEYS)))
         db_info = INPUTS[db]
-        locator = cea.inputlocator.InputLocator(
-            config.scenario)
+        config = cea.config.Configuration()
+        locator = cea.inputlocator.InputLocator(config.scenario)
         location = getattr(locator, db_info['location'])()
         if db_info['type'] != 'shp':
             abort(400, 'Invalid database for geojson: %s' % location)
-        table_df = geopandas.GeoDataFrame.from_file(location)
-        # make sure that the geojson is coded in latitude / longitude
-        table_df = table_df.to_crs(epsg=4326)
-        return json.loads(table_df.to_json(show_bbox=True))
+        return df_to_json(location, bbox=True)[0]
 
 
-@api.route('/others/networks/<string:kind>/geojson')
-class InputNetwork(Resource):
+@api.route('/others/<string:kind>/geojson')
+class InputOthers(Resource):
     def get(self, kind):
         config = cea.config.Configuration()
         locator = cea.inputlocator.InputLocator(
             config.scenario)
-        # TODO: Get a list of names and send all in the json
-        name = ''
-        edges = locator.get_network_layout_edges_shapefile(kind, name)
-        nodes = locator.get_network_layout_nodes_shapefile(kind, name)
-        network_json = df_to_json(edges)
-        network_json['features'].extend(df_to_json(nodes)['features'])
-        return network_json
-
-
-@api.route('/others/streets/geojson')
-class InputStreets(Resource):
-    def get(self):
-        config = cea.config.Configuration()
-        locator = cea.inputlocator.InputLocator(config.scenario)
-        location = locator.get_street_network()
-        return df_to_json(location)
+        if kind == 'streets':
+            return df_to_json(locator.get_street_network())[0]
+        if kind in ['dc', 'dh']:
+            return get_network(locator, kind)[0]
+        abort(400, 'Input file not found: %s' % kind, choices=['streets', 'dh', 'dc'])
 
 
 @api.route('/building-properties')
 class BuildingProperties(Resource):
     def get(self):
-        import cea.glossary
+        return get_building_properties()
+
+
+@api.route('/all-inputs')
+class AllInputs(Resource):
+    def get(self):
         config = cea.config.Configuration()
-
-        # FIXME: Find a better way to ensure order of tabs
-        tabs = ['zone', 'age', 'occupancy', 'architecture', 'internal-loads',
-                'supply-systems', 'indoor-comfort', 'district', 'restrictions']
-
         locator = cea.inputlocator.InputLocator(config.scenario)
-        store = {'tables': {}, 'geojsons': {}, 'columns': {}, 'crs': {}}
-        glossary = cea.glossary.read_glossary_df()
-        filenames = glossary['FILE_NAME'].str.split(pat='/').str[-1]
-        for db in INPUTS:
-            db_info = INPUTS[db]
-            location = getattr(locator, db_info['location'])()
-            try:
-                if db_info['type'] == 'shp':
-                    table_df = geopandas.GeoDataFrame.from_file(location)
-                    # Store crs of original dataframe
-                    store['crs'][db] = table_df.crs
-                    from cea.utilities.standardize_coordinates import get_geographic_coordinate_system
-                    store['geojsons'][db] = json.loads(table_df.to_crs(
-                        get_geographic_coordinate_system()).to_json(show_bbox=True))
 
-                    table_df = pandas.DataFrame(
-                        table_df.drop(columns='geometry'))
-                    if 'REFERENCE' in db_info['fieldnames'] and 'REFERENCE' not in table_df.columns:
-                        table_df['REFERENCE'] = None
-                    store['tables'][db] = json.loads(
-                        table_df.set_index('Name').to_json(orient='index'))
-                else:
-                    assert db_info['type'] == 'dbf', 'Unexpected database type: %s' % db_info['type']
-                    table_df = cea.utilities.dbf.dbf_to_dataframe(location)
-                    if 'REFERENCE' in db_info['fieldnames'] and 'REFERENCE' not in table_df.columns:
-                        table_df['REFERENCE'] = None
-                    store['tables'][db] = json.loads(
-                        table_df.set_index('Name').to_json(orient='index'))
-
-                columns = OrderedDict()
-                db_glossary = json.loads(glossary[filenames == '%s.%s' % (db.replace('-', '_'), db_info['type'])]
-                                         [['VARIABLE', 'UNIT', 'DESCRIPTION']].set_index('VARIABLE').to_json(orient='index'))
-                for column in db_info['fieldnames']:
-                    if column == 'REFERENCE':
-                        continue
-                    columns[column] = {}
-                    columns[column]['type'] = db_info['fieldtypes'][column].__name__
-                    columns[column]['description'] = db_glossary[column]['DESCRIPTION']
-                    columns[column]['unit'] = db_glossary[column]['UNIT']
-                store['columns'][db] = columns
-
-            except IOError as e:
-                print(e)
-                store['tables'][db] = {}
+        # FIXME: Find a better way, current used to test for Input Editor
+        store = get_building_properties()
+        store['geojsons'] = {}
+        store['crs'] = {}
+        store['geojsons']['zone'], store['crs']['zone'] = df_to_json(locator.get_zone_geometry(), bbox=True, trigger_abort=False)
+        store['geojsons']['district'], store['crs']['district'] = df_to_json(locator.get_district_geometry(), bbox=True, trigger_abort=False)
+        store['geojsons']['streets'], store['crs']['streets'] = df_to_json(locator.get_street_network(), trigger_abort=False)
+        store['geojsons']['dc'], store['crs']['dc'] = get_network(locator, 'dc', trigger_abort=False)
+        store['geojsons']['dh'], store['crs']['dh'] = get_network(locator, 'dh', trigger_abort=False)
 
         return store
 
 
-def df_to_json(file_location):
+def get_building_properties():
+    import cea.glossary
+    # FIXME: Find a better way to ensure order of tabs
+    tabs = ['zone', 'age', 'occupancy', 'architecture', 'internal-loads',
+            'supply-systems', 'indoor-comfort', 'district', 'restrictions']
+
+    config = cea.config.Configuration()
+    locator = cea.inputlocator.InputLocator(config.scenario)
+    store = {'tables': {}, 'columns': {}, 'order': tabs}
+    glossary = cea.glossary.read_glossary_df()
+    filenames = glossary['FILE_NAME'].str.split(pat='/').str[-1]
+    for db in INPUTS:
+        db_info = INPUTS[db]
+        location = getattr(locator, db_info['location'])()
+        try:
+            if db_info['type'] == 'shp':
+                table_df = geopandas.GeoDataFrame.from_file(location)
+                table_df = pandas.DataFrame(
+                    table_df.drop(columns='geometry'))
+                if 'REFERENCE' in db_info['fieldnames'] and 'REFERENCE' not in table_df.columns:
+                    table_df['REFERENCE'] = None
+                store['tables'][db] = json.loads(
+                    table_df.set_index('Name').to_json(orient='index'))
+            else:
+                assert db_info['type'] == 'dbf', 'Unexpected database type: %s' % db_info['type']
+                table_df = cea.utilities.dbf.dbf_to_dataframe(location)
+                if 'REFERENCE' in db_info['fieldnames'] and 'REFERENCE' not in table_df.columns:
+                    table_df['REFERENCE'] = None
+                store['tables'][db] = json.loads(
+                    table_df.set_index('Name').to_json(orient='index'))
+
+            columns = OrderedDict()
+            db_glossary = json.loads(glossary[filenames == '%s.%s' % (db.replace('-', '_'), db_info['type'])]
+            [['VARIABLE', 'UNIT', 'DESCRIPTION']].set_index('VARIABLE').to_json(orient='index'))
+
+            for column in db_info['fieldnames']:
+                if column == 'REFERENCE':
+                    continue
+                columns[column] = {}
+                columns[column]['type'] = db_info['fieldtypes'][column].__name__
+                columns[column]['description'] = db_glossary[column]['DESCRIPTION']
+                columns[column]['unit'] = db_glossary[column]['UNIT']
+            store['columns'][db] = columns
+
+        except IOError as e:
+            print(e)
+            store['tables'][db] = {}
+
+    return store
+
+
+def get_network(locator, kind, trigger_abort=True):
+    # TODO: Get a list of names and send all in the json
+    name = ''
+    edges = locator.get_network_layout_edges_shapefile(kind, name)
+    nodes = locator.get_network_layout_nodes_shapefile(kind, name)
+    network_json, crs = df_to_json(edges, trigger_abort=trigger_abort)
+    nodes_json, _ = df_to_json(nodes, trigger_abort=trigger_abort)
+    network_json['features'].extend(nodes_json['features'])
+    return network_json, crs
+
+
+def df_to_json(file_location, bbox=False, trigger_abort=True):
+    from cea.utilities.standardize_coordinates import get_lat_lon_projected_shapefile, get_projected_coordinate_system
     try:
         table_df = geopandas.GeoDataFrame.from_file(file_location)
-        from cea.utilities.standardize_coordinates import get_geographic_coordinate_system
+        # Save coordinate system
+        lat, lon = get_lat_lon_projected_shapefile(table_df)
+        crs = get_projected_coordinate_system(lat, lon)
         # make sure that the geojson is coded in latitude / longitude
-        table_df = table_df.to_crs(get_geographic_coordinate_system())
-        return json.loads(table_df.to_json())
+        out = table_df.to_crs(get_geographic_coordinate_system())
+        out = json.loads(out.to_json(show_bbox=bbox))
+        return out, crs
     except IOError as e:
         print(e)
-        abort(400, 'Input file not found: %s' % file_location)
+        if trigger_abort:
+            abort(400, 'Input file not found: %s' % file_location)
     except RuntimeError as e:
         print(e)
-        abort(400, e.message)
+        if trigger_abort:
+            abort(400, e.message)
