0e0a586e861311ca41e98aa604999ef51fd0a386
Daren Thomas <thomas@arch.ethz.ch>
2019-08-23 10:16:29 +0200

    linting...

--- cea/interfaces/dashboard/base/static/cea-worker-ticker.js
+++ cea/interfaces/dashboard/base/static/cea-worker-ticker.js
@@ -1,22 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // start up the SocketIO connection to the server
     let socket = io.connect(`http://${document.domain}:${location.port}`);
     let $ticker = $("#cea-worker-ticker");
     // this is a callback that triggers when the "my response" event is emitted by the server.
-    socket.on('cea-worker-message', function(data) {
+    socket.on("cea-worker-message", function (data) {
         let lines = data.message.split(/\r?\n/).map(x => x.trim()).filter(x => x.length > 0);
         let last_line = lines[lines.length - 1];
 
         $ticker.text(`${data.jobid}: ${last_line.substr(0, 80)}`);
     });
-    
-    socket.on('cea-worker-success', function(data) {
+
+    socket.on("cea-worker-success", function (data) {
         $ticker.text(`${data.jobid}: completed`);
     });
 
-    socket.on('cea-worker-error', function(job_info) {
+    socket.on("cea-worker-error", function (job_info) {
         console.log("cea-worker-error: job_info:", job_info);
-        alert(`ERROR running ${job_info.script} (${job_info.id})\n\n${job_info.error}`);
+        alert(`ERROR running ${job_info.script} (${job_info.id})
+
+${job_info.error}`);
         $ticker.text(`${job_info.id}: error`);
     });
 });
