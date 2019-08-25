3a0d89a5b87458a589fdaf853a7b7c888f3292e6
Reynold Mok <reyold@hotmail.com>
2019-08-25 09:34:49 +0800

    Add dashboard endpoint to flaskrestplus

--- cea/interfaces/dashboard/api/__init__.py
+++ cea/interfaces/dashboard/api/__init__.py
@@ -3,6 +3,7 @@ from flask_restplus import Api
 from .tools import api as tools
 from .project import api as project
 from .inputs import api as inputs
+from .dashboard import api as dashboard
 
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -11,6 +12,7 @@ api = Api(blueprint)
 api.add_namespace(tools, path='/tools')
 api.add_namespace(project, path='/project')
 api.add_namespace(inputs, path='/inputs')
+api.add_namespace(dashboard, path='/dashboard')
 
 
 # @api.errorhandler(InvalidPathException)
