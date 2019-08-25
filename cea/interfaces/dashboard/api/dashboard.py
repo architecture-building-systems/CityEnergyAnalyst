e082077e42e8edec2ddb292165a6fefe868e3a75
Reynold Mok <reyold@hotmail.com>
2019-08-25 09:34:49 +0800

    Add dashboard endpoint to flaskrestplus

---
+++ cea/interfaces/dashboard/api/dashboard.py
@@ -0,0 +1,25 @@
+from flask_restplus import Namespace, Resource, fields, abort
+
+import cea.config
+import cea.plots.cache
+
+api = Namespace('Dashboard', description='Dashboard plots')
+
+
+LAYOUTS = ['row', 'grid', 'map']
+CATEGORIES = {c.name: {'label': c.label, 'plots': [{'id': p.id(), 'name': p.name} for p in c.plots]}
+              for c in cea.plots.categories.list_categories()}
+
+
+@api.route('/')
+class Dashboard(Resource):
+    def get(self):
+        """
+        Get Dashboards from yaml file
+        """
+        config = cea.config.Configuration()
+        plot_cache = cea.plots.cache.PlotCache(config)
+        dashboards = cea.plots.read_dashboards(config, plot_cache)
+
+        return [{'name': d.name, 'description': d.description, 'layout':  d.layout if d.layout in LAYOUTS else 'row',
+                 'plots': [{'title': plot.title, 'scenario': plot.parameters['scenario-name']} for plot in d.plots]} for d in dashboards]
