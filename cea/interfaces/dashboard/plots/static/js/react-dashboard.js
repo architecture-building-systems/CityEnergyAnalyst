3544b5dab75d43accee677bbad7ca83e6cce0816
Reynold Mok <reyold@hotmail.com>
2019-08-25 09:36:54 +0800

    Add row and grid layout

--- cea/interfaces/dashboard/plots/static/js/react-dashboard.js
+++ cea/interfaces/dashboard/plots/static/js/react-dashboard.js
@@ -1,106 +1,242 @@
-const { Spin, Icon, Button, Row, Col } = antd;
+const {
+  Spin,
+  Icon,
+  Button,
+  Row,
+  Col,
+  Select,
+  Card,
+  Affix,
+  Modal,
+  Result
+} = antd;
 const { useState, useEffect } = React;
 
-function Dashboard() {
-  const [dashIndex, setDashIndex] = useState(0);
-  const [plots, setPlots] = useState([]);
+const INITIAL_DASHBOARD = 0;
 
-  const handleChange = event => {
-    setDashIndex(event.target.value);
-  };
-
-  const renderDash = () => {
-    let _plots = [];
-    let numOfplots = 3;
-    for (var index = 0; index < numOfplots; index++) {
-      _plots.push(index);
-    }
-    setPlots(_plots);
-  };
-
-  useEffect(() => {
-    renderDash();
-  }, [dashIndex]);
-
-  useEffect(() => {
-    console.log(plots);
-  }, [plots]);
-
-  return (
-    <div>
-      <input type="text" value={dashIndex} onChange={handleChange} />
-      {plots.map(index => (
-        <Plot key={`${dashIndex}-${index}`} id={index} dashIndex={dashIndex} />
-      ))}
-    </div>
-  );
-}
-
-function Plot({ id, dashIndex }) {
-  const [plotWidth, plotHeight] = [500, 500];
-  const [div, setDiv] = useState(null);
-  const [script, setScript] = useState(null);
-  const [error, setError] = useState(null);
-
-  // TODO: Get the hash
-  const hash = `abced-${id}`;
+const Dashboard = () => {
+  const [dashboards, setDashboards] = useState([]);
+  const [dashIndex, setDashIndex] = useState(INITIAL_DASHBOARD);
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5050/plots/div/${dashIndex}/${id}`)
-      .then(response => {
-        setDiv(
-          HTMLReactParser(response.data, {
-            replace: function(domNode) {
-              if (domNode.type === "script") {
-                setScript(domNode.children[0].data);
-              }
-            }
-          })
-        );
-      })
-      .catch(error => {
-        setError(error);
-      });
+      .get("http://localhost:5050/api/dashboard/")
+      .then(response => setDashboards(response.data));
   }, []);
 
+  if (!dashboards.length) return null;
+
+  const { layout, plots } = dashboards[dashIndex];
+
+  return (
+    <div id="cea-dashboard-content" style={{ minHeight: "100%" }}>
+      <div id="cea-dashboard-content-title" style={{ margin: 10 }}>
+        <DashSelect setDashIndex={setDashIndex} dashboards={dashboards} />
+      </div>
+      <div id="cea-dashboard-layout">
+        {layout === "row" ? (
+          <RowLayout dashIndex={dashIndex} plots={plots} />
+        ) : (
+          <GridLayout dashIndex={dashIndex} plots={plots} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const DashSelect = ({ setDashIndex, dashboards }) => {
+  return (
+    <Affix offsetTop={30}>
+      <Select
+        defaultValue={INITIAL_DASHBOARD}
+        style={{ width: 200 }}
+        onChange={value => setDashIndex(value)}
+      >
+        {dashboards.map((dashboard, index) => (
+          <option key={index} value={index}>
+            {dashboard.name}
+          </option>
+        ))}
+      </Select>
+    </Affix>
+  );
+};
+
+const ModalAddPlot = ({ visible, setVisible }) => {
+  const handleOk = e => {
+    setVisible(false);
+  };
+
+  const handleCancel = e => {
+    setVisible(false);
+  };
+
+  return (
+    <Modal
+      title="Add plot"
+      visible={visible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <p>Some contents...</p>
+    </Modal>
+  );
+};
+
+const RowLayout = ({ dashIndex, plots }) => {
+  if (!plots.length) return <h1>No plots found</h1>;
+  const [modalAdd, setModelAdd] = useState(false);
+
+  return (
+    <React.Fragment>
+      <ModalAddPlot visible={modalAdd} setVisible={setModelAdd} />
+      {plots.map((data, index) => (
+        <Row>
+          <Col>
+            <Plot
+              key={`${dashIndex}-${index}-${data.scenario}`}
+              index={index}
+              dashIndex={dashIndex}
+              data={data}
+            />
+          </Col>
+        </Row>
+      ))}
+      <Affix offsetBottom={100}>
+        <Button type="primary" style={{ float: "right" }} onClick={setModelAdd}>
+          + Add plot
+        </Button>
+      </Affix>
+    </React.Fragment>
+  );
+};
+
+const GridLayout = ({ dashIndex, plots }) => {
+  if (!plots.length) return <h1>No plots found</h1>;
+
+  return (
+    <React.Fragment>
+      <div class="row display-flex">
+        {plots.map((data, index) => (
+          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 plot-widget">
+            <Plot
+              key={`${dashIndex}-${index}-${data.scenario}`}
+              index={index}
+              dashIndex={dashIndex}
+              data={data}
+            />
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Plot = ({ index, dashIndex, data, style }) => {
+  const [div, setDiv] = useState(null);
+  const [error, setError] = useState(null);
+
+  const defaultStyle = { height: 500, margin: 10 };
+  const plotStyle = { ...defaultStyle, ...style };
+
+  // TODO: Maybe find a better solution
+  const hash = `cea-react-${dashIndex}-${index}`;
+
+  // Get plot div
   useEffect(() => {
-    if (script) {
+    axios
+      .get(`http://localhost:5050/plots/div/${dashIndex}/${index}`)
+      .then(response => {
+        setDiv(() => {
+          let script = null;
+          let content = HTMLReactParser(response.data, {
+            replace: function(domNode) {
+              if (domNode.type === "script") {
+                script = domNode.children[0].data;
+              }
+            }
+          });
+          return { content, script };
+        });
+      })
+      .catch(_error => {
+        setError(_error.response);
+      });
+
+    // Clean up script nodes on unmount
+    return () => {
+      let script = document.querySelector(`script[data-id=script-${hash}]`);
+      if (script) script.remove();
+    };
+  }, []);
+
+  // Mount script node when div is mounted
+  useEffect(() => {
+    if (div) {
       var _script = document.createElement("script");
-      _script.dataset.id = hash;
+      _script.dataset.id = `script-${hash}`;
       document.body.appendChild(_script);
-      _script.append(script);
-    }
-    if (!script) {
-      var _script = document.querySelector(`script[data-id=${hash}]`);
-      if (_script) _script.remove();
+      _script.append(div.script);
     }
   }, [div]);
 
   return (
-    <div>
-      <Button
-        onClick={() => {
-          setError(null);
-          setScript(null);
-          setDiv(null);
-        }}
+    <Card
+      title={
+        <React.Fragment>
+          <h3 style={{ float: "left" }}>{data.title}</h3>
+          <small>{data.scenario}</small>
+        </React.Fragment>
+      }
+      extra="Test"
+      style={{ ...plotStyle, height: "" }}
+      bodyStyle={{ height: plotStyle.height }}
+      size="small"
+    >
+      <Spin
+        spinning={!div && !error}
+        indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}
+        tip="Loading Plot..."
       >
-        Remove Plot
-      </Button>
-      <div style={{ height: plotHeight, width: plotWidth }}>
-        <Spin
-          spinning={!div && !error}
-          indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}
-        >
-          {div || <div style={{ height: plotHeight, width: plotWidth }} />}
-        </Spin>
-      </div>
-    </div>
+        {div ? (
+          div.content
+        ) : error ? (
+          <Result
+            status="error"
+            icon="Something went wrong!"
+            title={error.status}
+            subTitle={error.statusText}
+          >
+            <pre style={{ height: 200, overflow: "auto" }}>{error.data}</pre>
+          </Result>
+        ) : (
+          <div style={{ height: plotStyle.height }} />
+        )}
+      </Spin>
+    </Card>
   );
-}
+};
+
+const EmptyPlot = () => {
+  return <div>EmptyPlot</div>;
+};
 
 ReactDOM.render(
   React.createElement(Dashboard),
   document.querySelector("#cea-dashboard")
 );
+
+// To enable resize of Plotly plots
+window.addEventListener("resize", function() {
+  console.log("resizing");
+  $.each($(".plotly-graph-div.js-plotly-plot"), function() {
+    Plotly.Plots.resize($(this).attr("id"));
+  });
+});
+
+document.getElementById("menu_toggle").addEventListener("click", function() {
+  console.log("resizing");
+  $.each($(".plotly-graph-div.js-plotly-plot"), function() {
+    Plotly.Plots.resize($(this).attr("id"));
+  });
+});
