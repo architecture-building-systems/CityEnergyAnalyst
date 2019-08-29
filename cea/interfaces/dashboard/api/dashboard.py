5bc7c24109e933ec1c3691f834d415258cc2b576
Reynold Mok <reyold@hotmail.com>
2019-08-29 19:12:54 +0800

    Add edit plot parameters function

--- cea/interfaces/dashboard/api/dashboard.py
+++ cea/interfaces/dashboard/api/dashboard.py
@@ -1,4 +1,5 @@
 from flask_restplus import Namespace, Resource, fields, abort
+from utils import deconstruct_parameters
 
 import hashlib
 import cea.config
@@ -31,3 +32,41 @@ class Dashboard(Resource):
             out.append(dashboard)
 
         return out
+
+
+@api.route('/plot-parameters/<int:dashboard_index>/<int:plot_index>')
+class DashboardParameters(Resource):
+    def get(self, dashboard_index, plot_index):
+        config = cea.config.Configuration()
+        plot_cache = cea.plots.cache.PlotCache(config)
+        dashboards = cea.plots.read_dashboards(config, plot_cache)
+
+        dashboard = dashboards[dashboard_index]
+        plot = dashboard.plots[plot_index]
+        parameters = []
+        for pname, fqname in plot.expected_parameters.items():
+            parameter = config.get_parameter(fqname)
+            if pname in plot.parameters:
+                parameter.set(plot.parameters[pname])
+            parameters.append(deconstruct_parameters(parameter))
+        return parameters
+
+    def post(self, dashboard_index, plot_index):
+        form = api.payload
+        config = cea.config.Configuration()
+        plot_cache = cea.plots.cache.PlotCache(config)
+        dashboards = cea.plots.read_dashboards(config, plot_cache)
+
+        dashboard = dashboards[dashboard_index]
+        plot = dashboard.plots[plot_index]
+        print('route_post_plot_parameters: expected_parameters: {}'.format(plot.expected_parameters.items()))
+        for pname, fqname in plot.expected_parameters.items():
+            parameter = config.get_parameter(fqname)
+            print('route_post_plot_parameters: fqname={fqname}, pname={pname}'.format(fqname=fqname, pname=pname))
+            if isinstance(parameter, cea.config.MultiChoiceParameter):
+                plot.parameters[pname] = parameter.decode(','.join(form[pname]))
+            else:
+                plot.parameters[pname] = parameter.decode(form[pname])
+        cea.plots.write_dashboards(config, dashboards)
+
+        return plot.parameters
---
+++ cea/interfaces/dashboard/plots/static/js/parameters.js
@@ -0,0 +1,168 @@
+const { Form, Input, Icon, Switch, Select, Divider } = antd;
+
+const ceaParameter = (param, getFieldDecorator) => {
+  const { name, type, value, help } = param;
+
+  const openDialog = () => {
+    console.log('open dialog')
+  };
+
+  let input = [];
+
+  if (["IntegerParameter", "RealParameter"].includes(type)) {
+    const stringValue = value ? value.toString() : "";
+    const regex =
+      type === "IntegerParameter"
+        ? /^(?:[1-9][0-9]*|0)$/
+        : /^(?:[1-9][0-9]*|0)(\.\d+)?$/;
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: stringValue,
+          rules: [
+            {
+              type: "number",
+              message: `Please enter an ${
+                type === "IntegerParameter" ? "integer" : "float"
+              }`,
+              transform: num => {
+                if (num === "") return 0;
+                return regex.test(num) ? Number(num) : NaN;
+              }
+            }
+          ]
+        })(<Input />)}
+      </React.Fragment>
+    );
+  } else if (["PathParameter", "FileParameter"].includes(type)) {
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: value
+        })(
+          <Input
+            addonAfter={
+              <button
+                className={type}
+                type="button"
+                style={{ height: "30px", width: "50px" }}
+                onClick={openDialog}
+              >
+                <Icon type="ellipsis" />
+              </button>
+            }
+          />
+        )}
+      </React.Fragment>
+    );
+  } else if (
+    [
+      "ChoiceParameter",
+      "PlantNodeParameter",
+      "ScenarioNameParameter",
+      "SingleBuildingParameter"
+    ].includes(type)
+  ) {
+    const { choices } = param;
+    const { Option } = Select;
+    const Options = choices.map(choice => (
+      <Option key={choice} value={choice}>
+        {choice}
+      </Option>
+    ));
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: value
+        })(<Select>{Options}</Select>)}
+      </React.Fragment>
+    );
+  } else if (["MultiChoiceParameter", "BuildingsParameter"].includes(type)) {
+    const { choices } = param;
+    const { Option } = Select;
+    const Options = choices.map(choice => (
+      <Option key={choice} value={choice}>
+        {choice}
+      </Option>
+    ));
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: value
+        })(
+          <Select
+            mode="multiple"
+            style={{ width: "100%" }}
+            placeholder="Nothing Selected"
+            showArrow
+          >
+            {Options}
+          </Select>
+        )}
+      </React.Fragment>
+    );
+  } else if (type === "WeatherPathParameter") {
+    const { choices } = param;
+    const { Option } = Select;
+    const Options = Object.keys(choices).map(choice => (
+      <Option key={choice} value={choices[choice]}>
+        {choice}
+      </Option>
+    ));
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: value
+        })(
+          <Select
+            dropdownRender={menu => (
+              <div>
+                {menu}
+                <Divider style={{ margin: "4px 0" }} />
+                <div
+                  style={{ padding: "8px", cursor: "pointer" }}
+                  onMouseDown={openDialog}
+                  role="button"
+                  tabIndex={0}
+                >
+                  <Icon type="plus" /> Browse for weather file
+                </div>
+              </div>
+            )}
+          >
+            {Options}
+          </Select>
+        )}
+      </React.Fragment>
+    );
+  } else if (type === "BooleanParameter") {
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: value
+        })(<Switch defaultChecked={value} />)}
+      </React.Fragment>
+    );
+  } else {
+    input = (
+      <React.Fragment>
+        {getFieldDecorator(name, {
+          initialValue: value
+        })(<Input />)}
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <Form.Item
+      label={name}
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 11, offset: 1 }}
+      key={name}
+    >
+      {input}
+      <br />
+      <small style={{ display: "block", lineHeight: "normal" }}>{help}</small>
+    </Form.Item>
+  );
+};
