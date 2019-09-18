c123818e4dc2f0e558a99afe413228cfd4e0bd46
Reynold Mok <reyold@hotmail.com>
2019-09-18 10:17:07 +0800

    Add scenario image endpoint

--- cea/interfaces/dashboard/api/project.py
+++ cea/interfaces/dashboard/api/project.py
@@ -1,8 +1,12 @@
+import os
+
+import geopandas
 from flask_restplus import Namespace, Resource, fields, abort
+from staticmap import StaticMap, Polygon
 
 import cea.config
-import os
-import re
+import cea.inputlocator
+from cea.utilities.standardize_coordinates import get_geographic_coordinate_system
 
 api = Namespace('Project', description='Current project for CEA')
 
@@ -27,7 +31,8 @@ class Project(Resource):
     def get(self):
         config = cea.config.Configuration()
         name = os.path.basename(config.project)
-        return {'name': name, 'path': config.project, 'scenario': config.scenario_name, 'scenarios': config.get_parameter('general:scenario-name')._choices}
+        return {'name': name, 'path': config.project, 'scenario': config.scenario_name,
+                'scenarios': config.get_parameter('general:scenario-name')._choices}
 
     @api.doc(body=PROJECT_PATH_MODEL, responses={200: 'Success', 400: 'Invalid Path given'})
     def post(self):
@@ -47,12 +52,61 @@ class Project(Resource):
 
         if 'scenario' in payload:
             scenario = payload['scenario']
-            choices = config.get_parameter(
-                'general:scenario-name')._choices
+            choices = config.get_parameter('general:scenario-name')._choices
             if scenario in choices:
                 config.scenario_name = scenario
                 config.save()
                 return {'message': 'Scenario changed'}
             else:
-                abort(
-                    400, 'Scenario does not exist', choices=choices)
+                abort(400, 'Scenario does not exist', choices=choices)
+
+
+@api.route('/<string:scenario>/image')
+class ScenarioImage(Resource):
+    def get(self, scenario):
+        config = cea.config.Configuration()
+        choices = config.get_parameter('general:scenario-name')._choices
+        if scenario in choices:
+            locator = cea.inputlocator.InputLocator(os.path.join(config.project, scenario))
+            zone_path = locator.get_zone_geometry()
+            if os.path.isfile(zone_path):
+                cache_path = os.path.join(config.project, '.cache')
+                image_path = os.path.join(cache_path, scenario + '.png')
+
+                zone_modified = os.path.getmtime(zone_path)
+                if not os.path.isfile(image_path):
+                    image_modified = 0
+                else:
+                    image_modified = os.path.getmtime(image_path)
+
+                if zone_modified > image_modified:
+                    # Make sure .cache folder exists
+                    if not os.path.exists(cache_path):
+                        os.makedirs(cache_path)
+
+                    try:
+                        zone_df = geopandas.read_file(zone_path)
+                        zone_df = zone_df.to_crs(get_geographic_coordinate_system())
+                        polygons = zone_df['geometry']
+
+                        polygons = [list(polygons.geometry.exterior[row_id].coords) for row_id in range(polygons.shape[0])]
+
+                        m = StaticMap(256, 160)
+                        for polygon in polygons:
+                            out = Polygon(polygon, 'blue', 'black', False)
+                            m.add_polygon(out)
+
+                        image = m.render()
+                        image.save(image_path)
+                    except Exception as e:
+                        abort(400, str(e))
+
+                import base64
+                with open(image_path, 'rb') as imgFile:
+                    image = base64.b64encode(imgFile.read())
+
+                return {'image': image}
+            abort(400, 'Zone file not found')
+        else:
+            abort(400, 'Scenario does not exist', choices=choices)
+
