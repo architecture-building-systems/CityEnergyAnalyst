f1b8ce63e8d4df5a23b6cb1fceaccc98c87e9d58
shanshanhsieh <hsieh@arch.ethz.ch>
2019-10-08 08:50:47 +0200

    Update plot_curves_from_osmose_outputs.py

--- cea/osmose/plots/plot_curves_from_osmose_outputs.py
+++ cea/osmose/plots/plot_curves_from_osmose_outputs.py
@@ -14,77 +14,89 @@ rcParams['mathtext.default'] = 'regular'
 # del matplotlib.font_manager.weight_dict['roman']
 # matplotlib.font_manager._rebuild()
 
+COLOR_TABLE = {'base': sns.xkcd_rgb["pale red"], 'separated': sns.xkcd_rgb["sea blue"]}
+COLOR_LIST = ['#C96A50', '#3E9AA3', '#3E9BA3']
+PLOT_SPECS = {'icc':{'ylabel':'Temperature [C]', 'xlabel':'Heat Load [kW]'},
+              'carnot':{'ylabel':'Carnot factor [-]', 'xlabel':'Heat Load [kW]'}}
 
-def plot_icc_base_and_separated(path, t, plot_type, model_name):
-    os.chdir(path)
+def plot_base_and_separated(path, t, plot_type, model_name):
     # figure size
     plt.figure(figsize=(8, 7))
     ax1 = plt.subplot()
-    # load data
-    if model_name == '':
-        file_name_base = plot_type + '_base_m_loc_loc1_t' + str(t) + '_c1_DefaultHeatCascade.txt'
-    else:
-        file_name_base = plot_type + '_base_m_' + model_name + '_loc_loc1_t' + str(t) + '_c1_DefaultHeatCascade.txt'
+    # plot the lines
+    line_types = ['base', 'separated']
+    for line_type in line_types:
+        x, y = load_data_from_txt(path, plot_type, line_type, model_name, t)
+        ax1.plot(x, y, '-', color=COLOR_TABLE[line_type], label=line_type)
 
-    data = np.genfromtxt(file_name_base, delimiter=' ')
-    # x and y axes
-    x = data[:, 0]
-    y = data[:, 1]
-    # plot the figure
-    ax1.plot(x, y, '-', color=sns.xkcd_rgb["pale red"])
-    # load data
-    if model_name == '':
-        file_name_separated = plot_type + '_separated_m_loc_loc1_t' + str(t) + '_c1_DefaultHeatCascade.txt'
-    else:
-        file_name_separated = plot_type + '_separated_m_'+ model_name +'_loc_loc1_t' + str(t) + '_c1_DefaultHeatCascade.txt'
-    data = np.genfromtxt(file_name_separated, delimiter=' ')
-    # x and y axes
-    x = data[:, 0]
-    y = data[:, 1]
-    # plot the figure
-    ax1.plot(x, y, '-', color=sns.xkcd_rgb["sea blue"])
+    # save the figure
+    set_plot_parameters(ax1, PLOT_SPECS[plot_type])
+    fig1 = plt.gcf()
+    fig1.savefig(plot_type + '_' + model_name + '_t' + str(t) + '_DefaultHeatCascade.png', transparent=True)
+    return
+
+##===================
+## general functions
+##===================
+
+def set_plot_parameters(ax1, plot_specs):
+    fontname = 'Times New Roman'
+    fontsize = 18
     # set the legend
-    ax1.legend(["base", "separated", ],
-               loc='upper right', shadow=False, fancybox=True,
-               fontsize=18, prop={'family': 'Times New Roman', 'size': '18'})
+    ax1.legend(loc='upper right', shadow=False, fancybox=True,
+               fontsize=fontsize, prop={'family': 'Times New Roman', 'size': str(fontsize)})
     # set x and y range
     # plt.set_xlim([-766.00311044128,8090.8964687342])
-    # plt.set_ylim([-70.151355125,914.40992288125])
+    ax1.set(ylim=plot_specs['ylim'])
     # format ticks
-    plt.xticks(fontname='Times New Roman', fontsize=18)
-    plt.yticks(fontname='Times New Roman', fontsize=18)
+    plt.xticks(fontname=fontname, fontsize=fontsize)
+    plt.yticks(fontname=fontname, fontsize=fontsize)
     for label in ax1.xaxis.get_majorticklabels():
-        label.set_fontsize(18)
-        label.set_fontname('Times New Roman')
+        label.set_fontsize(fontsize)
+        label.set_fontname(fontname)
     for label in ax1.yaxis.get_majorticklabels():
-        label.set_fontsize(18)
-        label.set_fontname('Times New Roman')
+        label.set_fontsize(fontsize)
+        label.set_fontname(fontname)
     # set the axis labels
-    ax1.set_ylabel('Temperature [C]', fontsize=20, color='black', fontname='Times New Roman', fontweight='normal')
-    ax1.set_xlabel('Heat Load [kW]', fontsize=20, color='black', fontname='Times New Roman', fontweight='normal')
+    ax1.set_ylabel(plot_specs['ylabel'], fontsize=fontsize, color='black', fontname=fontname, fontweight='normal')
+    ax1.set_xlabel(plot_specs['xlabel'], fontsize=fontsize, color='black', fontname=fontname, fontweight='normal')
     # tight layout prevents axis title overlapping
     plt.tight_layout(pad=1, w_pad=3, h_pad=1.0)
-    # show the figure and hold it for saving
-    # plt.show(block=False)
-    # save the figure
-    fig1 = plt.gcf()
-    plt.draw()
-    fig1.savefig(plot_type + '_' + model_name + '_t' + str(t) + '_Clu_DefaultHeatCascade.png', transparent=True)
+    return
 
 
+def load_data_from_txt(path, plot_type, line_type, model_name, t):
+    os.chdir(path)
+    if model_name == '':
+        file_name = plot_type + '_' + line_type + '_m_loc_loc1_t' + str(t) + '_c1_DefaultHeatCascade.txt'
+    else:
+        file_name = plot_type + '_' + line_type + '_m_' + model_name + '_loc_loc1_t' + str(t) + '_c1_DefaultHeatCascade.txt'
+    data = np.genfromtxt(file_name, delimiter=' ')
+    # x and y axes
+    x = data[:, 0]
+    y = data[:, 1]
+    return x, y
 
 
 def main():
     plot_type = 'carnot'
-    model_name = ''
-    folder = 'air_systems'
-    # change directory
-    path_to_scenario = 'E:\\ipese_new\\osmose_mk\\results\\HCS_base\\run_001\\s_001'
-    path_to_models = path_to_scenario + '\\plots\\' + plot_type + '\\' + folder
+    model_name = 'chillers'
+
+    # path
+    # folder_layers = ['E:\\HCS_results_1003',
+    #                  'WTP_CBD_m_WP1_OFF', 'B005_1_24', 'base_3for2',
+    #                  'run', 's_001', 'plots',
+    #                  'carnot', 'models']
+    folder_layers = ['E:\\ipese_new\\osmose_mk\\results\\HCS_base','run_027',
+                     's_001\\plots\\' + plot_type, 'models']
+    path_to_folder = os.path.join('', *folder_layers)
+
+    # plot specifications
 
 
+    # plotting
     for t in np.arange(1,25,1):
-        plot_icc_base_and_separated(path_to_models, t, plot_type, model_name)
+        plot_base_and_separated(path_to_folder, t, plot_type, model_name)
 
 
 
