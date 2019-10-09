5ee222bc93f4958d026d6338013f42097b13e723
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-10-09 18:06:28 +0800

    Update schedule_reader.py

--- cea/utilities/schedule_reader.py
+++ cea/utilities/schedule_reader.py
@@ -4,7 +4,8 @@ from __future__ import print_function
 import csv
 
 import pandas as pd
-
+import cea.config
+import cea.inputlocator
 import cea
 
 __author__ = "Jimeno Fonseca"
@@ -29,7 +30,7 @@ DAY = ['WEEKDAY'] * 24 + ['SATURDAY'] * 24 + ['SUNDAY'] * 24
 HOUR = range(1, 25) + range(1, 25) + range(1, 25)
 
 
-def read_cea_schedule(path_to_building_schedule):
+def read_cea_schedule(path_to_cea_schedule):
     '''
     reader of schedule file
     .ceaschedule
@@ -37,7 +38,7 @@ def read_cea_schedule(path_to_building_schedule):
     :return:
     '''
 
-    with open(path_to_building_schedule) as f:
+    with open(path_to_cea_schedule) as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
             if i == 0:
@@ -45,48 +46,10 @@ def read_cea_schedule(path_to_building_schedule):
             elif i == 1:
                 monthly_multiplier = [round(float(x), 2) for x in row[1:]]
 
-    schedule = pd.read_csv(path_to_building_schedule, skiprows=2)
-    occupancy_weekday = schedule.loc[schedule['DAY'] == 'WEEKDAY']['OCCUPANCY'].values
-    occupancy_saturday = schedule.loc[schedule['DAY'] == 'SATURDAY']['OCCUPANCY'].values
-    occupancy_sunday = schedule.loc[schedule['DAY'] == 'SUNDAY']['OCCUPANCY'].values
-    appliances_weekday = schedule.loc[schedule['DAY'] == 'WEEKDAY']['ELECTRICITY'].values
-    appliances_saturday = schedule.loc[schedule['DAY'] == 'SATURDAY']['ELECTRICITY'].values
-    appliances_sunday = schedule.loc[schedule['DAY'] == 'SUNDAY']['ELECTRICITY'].values
-    domestic_hot_water_weekday = schedule.loc[schedule['DAY'] == 'WEEKDAY']['WATER'].values
-    domestic_hot_water_saturday = schedule.loc[schedule['DAY'] == 'SATURDAY']['WATER'].values
-    domestic_hot_water_sunday = schedule.loc[schedule['DAY'] == 'SUNDAY']['WATER'].values
-    setpoint_heating_weekday = schedule.loc[schedule['DAY'] == 'WEEKDAY']['HEATING'].values
-    setpoint_heating_saturday = schedule.loc[schedule['DAY'] == 'SATURDAY']['HEATING'].values
-    setpoint_heating_sunday = schedule.loc[schedule['DAY'] == 'SUNDAY']['HEATING'].values
-    setpoint_cooling_weekday = schedule.loc[schedule['DAY'] == 'WEEKDAY']['COOLING'].values
-    setpoint_cooling_saturday = schedule.loc[schedule['DAY'] == 'SATURDAY']['COOLING'].values
-    setpoint_cooling_sunday = schedule.loc[schedule['DAY'] == 'SUNDAY']['COOLING'].values
-    processes_weekday = schedule.loc[schedule['DAY'] == 'WEEKDAY']['PROCESSES'].values
-    processes_saturday = schedule.loc[schedule['DAY'] == 'SATURDAY']['PROCESSES'].values
-    processes_sunday = schedule.loc[schedule['DAY'] == 'SUNDAY']['PROCESSES'].values
-
-    schedule_data = {
-        'metadata': metadata,
-        'monthly_multiplier': monthly_multiplier,
-        'occupancy_weekday': occupancy_weekday,
-        'occupancy_saturday': occupancy_saturday,
-        'occupancy_sunday': occupancy_sunday,
-        'appliances_weekday': appliances_weekday,
-        'appliances_saturday': appliances_saturday,
-        'appliances_sunday': appliances_sunday,
-        'domestic_hot_water_weekday': domestic_hot_water_weekday,
-        'domestic_hot_water_saturday': domestic_hot_water_saturday,
-        'domestic_hot_water_sunday': domestic_hot_water_sunday,
-        'setpoint_heating_weekday': setpoint_heating_weekday,
-        'setpoint_heating_saturday': setpoint_heating_saturday,
-        'setpoint_heating_sunday': setpoint_heating_sunday,
-        'setpoint_cooling_weekday': setpoint_cooling_weekday,
-        'setpoint_cooling_saturday': setpoint_cooling_saturday,
-        'setpoint_cooling_sunday': setpoint_cooling_sunday,
-        'processes_weekday': processes_weekday,
-        'processes_saturday': processes_saturday,
-        'processes_sunday': processes_sunday
-    }
+    schedule_data = pd.read_csv(path_to_cea_schedule, skiprows=2).T
+    schedule_data = dict(zip(schedule_data.index, schedule_data.values))
+    schedule_complementray_data = {'METADATA': metadata, 'MONTHLY_MULTIPLIER': monthly_multiplier}
+    schedule_data.update(schedule_complementray_data)
     return schedule_data
 
 
@@ -163,8 +126,8 @@ def save_cea_schedule(schedule_data, path_to_building_schedule):
 
 def main(config):
     locator = cea.inputlocator.InputLocator(scenario=config.scenario)
-    name = locator.get_zone_building_names()[0]
-    path_to_building_schedule = locator.get_building_schedules(name)
+    path_database = locator.get_database_standard_schedules('CH-SIA-2014')
+    path_to_building_schedule = locator.get_database_standard_schedules_use(path_database, 'MULTI_RES')
     read_cea_schedule(path_to_building_schedule)
 
 if __name__ == '__main__':
