ab4245c2e9094d311972e342e68d9f3548c70fff
JIMENOFONSECA <fonseca@arch.ethz.ch>
2019-10-10 10:01:27 +0800

    Update schedule_helper.py

--- cea/datamanagement/schedule_helper.py
+++ cea/datamanagement/schedule_helper.py
@@ -64,9 +64,7 @@ def model_with_standard_database(locator, buildings, path_to_standard_schedule_d
                              'Vww_lpd': 'WATER',
                              'Vw_lpd': 'WATER',
                              'Ths_set_C': 'HEATING',
-                             'Ths_setb_C': 'HEATING',
                              'Tcs_set_C': 'COOLING',
-                             'Tcs_setb_C': 'COOLING',
                              'Qcre_Wm2': 'PROCESSES',
                              'Qhpro_Wm2': 'PROCESSES',
                              'Qcpro_Wm2': 'PROCESSES',
@@ -78,66 +76,54 @@ def model_with_standard_database(locator, buildings, path_to_standard_schedule_d
     building_occupancy_df = dbf_to_dataframe(locator.get_building_occupancy()).set_index('Name')
     building_occupancy_df = building_occupancy_df.ix[buildings]
 
-    internal_DB = pd.read_excel(locator.get_archetypes_properties(), 'INTERNAL_LOADS').set_index('Code')
-
     # validate list of uses in case study
     list_uses = get_list_of_uses_in_case_study(building_occupancy_df)
+
+    from cea.datamanagement.data_helper import calc_mainuse
+    building_occupancy_df['mainuse'] = calc_mainuse(building_occupancy_df, list_uses)
+    internal_DB = pd.read_excel(locator.get_archetypes_properties(), 'INTERNAL_LOADS')
+    comfort_DB = pd.read_excel(locator.get_archetypes_properties(), 'INDOOR_COMFORT')
+    internal_DB = internal_DB.merge(comfort_DB, left_on='Code', right_on='Code')
+    internal_DB = internal_DB.set_index('Code')
+
+
     for building in buildings:
-        schedule_mixed = {}
+        schedule_new_data = {}
+        main_use_this_building = building_occupancy_df['mainuse'][building]
+        monthly_multiplier = schedule_data_all_uses.schedule_complementray_data[main_use_this_building]['MONTHLY_MULTIPLIER']
         for variable, schedule_type in variable_schedule_map.items():
-            current_schedule = np.zeros(len(schedule_data_all_uses.schedule['HOTEL'][schedule_type]))
+            current_schedule = np.zeros(len(schedule_data_all_uses.schedule_data['HOTEL'][schedule_type]))
             normalizing_value = 0.0
             if variable in ['Ths_set_C', 'Tcs_set_C']:
-                main_use_this_building #TODO: CONTINUE HERE WITH THE MAIN USE AND SELECT SCHEDULE
-                schedule_mixed[variable] = schedule_data_all_uses.schedule[main_use][schedule_type]
+                schedule_new_data[variable] = schedule_data_all_uses.schedule_data[main_use_this_building][schedule_type]
             else:
                 for use in list_uses:
                     if building_occupancy_df[use][building] > 0.0:
                         current_share_of_use = building_occupancy_df[use][building]
-                        if variable in ['Ve_lps', 'Qs_Wp', 'X_ghp', 'Vww_lpd', 'Vw_lpd', 'Occ_m2pax']:
+                        if variable in ['Occ_m2pax', 'Ve_lps', 'Qs_Wp', 'X_ghp', 'Vww_lpd', 'Vw_lpd']:
                             # for variables that depend on the number of people, the schedule needs to be calculated by number
                             # of people for each use at each time step, not the share of the occupancy for each
                             share_time_occupancy_density = internal_DB.ix[use, variable] * current_share_of_use * \
                                                            (1/internal_DB.ix[use, 'Occ_m2pax'])
 
-                        elif variable in ['Qcre_Wm2', 'Qhpro_Wm2', 'Qcpro_Wm2', 'Epro_Wm2']:
+                        elif variable in ['Ea_Wm2','El_Wm2', 'Ed_Wm2', 'Qcre_Wm2', 'Qhpro_Wm2', 'Qcpro_Wm2', 'Epro_Wm2']:
                             share_time_occupancy_density = internal_DB.ix[use, variable] * current_share_of_use
 
                         normalizing_value += share_time_occupancy_density
+                        print(variable, building)
                         current_schedule = np.vectorize(calc_average)(current_schedule,
-                                                                      schedule_data_all_uses.schedule[use][schedule_type],
+                                                                      schedule_data_all_uses.schedule_data[use][schedule_type],
                                                                       share_time_occupancy_density)
 
                 if normalizing_value == 0:
-                    schedule_mixed[variable] = current_schedule * 0
+                    schedule_new_data[variable] = current_schedule * 0
                 else:
-                    schedule_mixed[variable] = current_schedule / normalizing_value
+                    schedule_new_data[variable] = current_schedule / normalizing_value
 
-        schedule_data = {
-            'metadata': metadata,
-            'occupancy_density_m2p': occupancy_density_m2p,
-            'monthly_multiplier': monthly_multiplier,
-            'occupancy_weekday': occupancy_weekday,
-            'occupancy_saturday': occupancy_saturday,
-            'occupancy_sunday': occupancy_sunday,
-            'appliances_weekday': appliances_weekday,
-            'appliances_saturday': appliances_saturday,
-            'appliances_sunday': appliances_sunday,
-            'domestic_hot_water_weekday': domestic_hot_water_weekday,
-            'domestic_hot_water_saturday': domestic_hot_water_saturday,
-            'domestic_hot_water_sunday': domestic_hot_water_sunday,
-            'setpoint_heating_weekday': setpoint_heating_weekday,
-            'setpoint_heating_saturday': setpoint_heating_saturday,
-            'setpoint_heating_sunday': setpoint_heating_sunday,
-            'setpoint_cooling_weekday': setpoint_cooling_weekday,
-            'setpoint_cooling_saturday': setpoint_cooling_saturday,
-            'setpoint_cooling_sunday': setpoint_cooling_sunday,
-            'processes_weekday': processes_weekday,
-            'processes_saturday': processes_saturday,
-            'processes_sunday': processes_sunday
-        }
-        path_to_building_schedule = locator.get_building_schedules(name)
-        save_cea_schedule(schedule_data, path_to_building_schedule)
+        # save schedule per buidling
+        schedule_complementray_data = {'METADATA': metadata, 'MONTHLY_MULTIPLIER': monthly_multiplier}
+        path_to_building_schedule = locator.get_building_schedules_predefined(building)
+        save_cea_schedule(schedule_new_data, schedule_complementray_data, path_to_building_schedule)
 
 
 def calc_average(last, current, share_of_use):
@@ -151,7 +137,8 @@ class ScheduleData(object):
     def __init__(self, locator, path_to_standard_schedule_database):
         self.locator = locator
         self.path_database = path_to_standard_schedule_database
-        self.schedule = self.fill_in_data()
+        self.schedule_data, self.schedule_complementray_data = self.fill_in_data()
+
 
     def fill_in_data(self):
         get_list_uses_in_database = []
@@ -160,11 +147,13 @@ class ScheduleData(object):
                 get_list_uses_in_database.append(file.split('.')[0])
 
         data_schedules = []
+        data_schedules_complimentary = []
         for use in get_list_uses_in_database:
             path_to_schedule = self.locator.get_database_standard_schedules_use(self.path_database, use)
-            data_schedule = read_cea_schedule(path_to_schedule)
+            data_schedule, data_metadata = read_cea_schedule(path_to_schedule)
             data_schedules.append(data_schedule)
-        return dict(zip(get_list_uses_in_database, data_schedules))
+            data_schedules_complimentary.append(data_metadata)
+        return dict(zip(get_list_uses_in_database, data_schedules)), dict(zip(get_list_uses_in_database, data_schedules_complimentary))
 
 
 def main(config):
