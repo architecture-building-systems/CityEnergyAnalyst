6905958f29602969a8532d80d7f32edf771bca20
Gabriel Happle <happle@arch.ethz.ch>
2019-10-29 10:52:21 +0800

    creating `Hs_ag` and `Hs_bg` for internal gains proportions in RC-model

--- cea/datamanagement/data_helper.py
+++ cea/datamanagement/data_helper.py
@@ -115,7 +115,8 @@ def data_helper(locator, region, overwrite_technology_folder,
         prop_architecture_df_merged = names_df.merge(prop_architecture_df, on="Name")
 
         fields = ['Name',
-                  'Hs',
+                  'Hs_ag',
+                  'Hs_bg',
                   'Ns',
                   'Es',
                   'void_deck',
@@ -367,13 +368,15 @@ def correct_archetype_areas(prop_architecture_df, architecture_DB, list_uses):
     def calc_average(last, current, share_of_use):
         return last + current * share_of_use
 
-    Hs_list = []
+    Hs_ag_list = []
+    Hs_bg_list = []
     Ns_list = []
     Es_list = []
     for building in prop_architecture_df.index:
-        Hs = 0
-        Ns = 0
-        Es = 0
+        Hs_ag = 0.0
+        Hs_bg = 0.0
+        Ns = 0.0
+        Es = 0.0
         for use in list_uses:
             # if the use is present in the building, find the building archetype properties for that use
             if prop_architecture_df[use][building] > 0.0:
@@ -383,14 +386,16 @@ def correct_archetype_areas(prop_architecture_df, architecture_DB, list_uses):
                                    str(prop_architecture_df['standard'][building])
                 # recalculate heated floor area as an average of the archetype value for each occupancy type in the
                 # building
-                Hs = calc_average(Hs, indexed_DB['Hs'][current_use_code], prop_architecture_df[use][building])
+                Hs_ag = calc_average(Hs_ag, indexed_DB['Hs_ag'][current_use_code], prop_architecture_df[use][building])
+                Hs_bg = calc_average(Hs_ag, indexed_DB['Hs_bg'][current_use_code], prop_architecture_df[use][building])
                 Ns = calc_average(Ns, indexed_DB['Ns'][current_use_code], prop_architecture_df[use][building])
                 Es = calc_average(Es, indexed_DB['Es'][current_use_code], prop_architecture_df[use][building])
-        Hs_list.append(Hs)
+        Hs_ag_list.append(Hs_ag)
+        Hs_bg_list.append(Hs_bg)
         Ns_list.append(Ns)
         Es_list.append(Es)
 
-    return Hs_list, Ns_list, Es_list
+    return Hs_ag_list, Hs_bg_list, Ns_list, Es_list
 
 
 def get_prop_architecture(categories_df, architecture_DB, list_uses):
@@ -426,8 +431,8 @@ def get_prop_architecture(categories_df, architecture_DB, list_uses):
                                                                                                               axis=1)
 
     # adjust share of floor space that is heated ('Hs') for multiuse buildings
-    prop_architecture_df['Hs'], prop_architecture_df['Ns'], prop_architecture_df['Es'] = correct_archetype_areas(
-        prop_architecture_df, architecture_DB, list_uses)
+    prop_architecture_df['Hs_ag'], prop_architecture_df['Hs_bg'], prop_architecture_df['Ns'],\
+        prop_architecture_df['Es'] = correct_archetype_areas(prop_architecture_df, architecture_DB, list_uses)
 
     return prop_architecture_df
 
