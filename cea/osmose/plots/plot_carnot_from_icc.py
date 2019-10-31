ad4b8ccf623d7821dc24009b0bfc5b0907e2dbd7
shanshanhsieh <hsieh@arch.ethz.ch>
2019-10-31 09:25:30 +0100

    Update plot_carnot_from_icc.py

--- cea/osmose/plots/plot_carnot_from_icc.py
+++ cea/osmose/plots/plot_carnot_from_icc.py
@@ -6,6 +6,7 @@ import numpy as np
 import seaborn as sns
 from matplotlib import rcParams
 from cea.osmose.plots.plot_curves_from_osmose_outputs import load_data_from_txt, set_plot_parameters
+from cea.osmose.post_process_osmose_results import read_file_as_df
 
 rcParams['mathtext.default'] = 'regular'
 
@@ -25,11 +26,14 @@ COLOR_CODES = {'HCS_base_3for2': '#C96A50', 'HCS_base_coil': '#3E9AA3', 'HCS_bas
                'HCS_base': '#707070'}
 KEY_TABLE = {'HCS_base_ER0': 'HCS_ER0', 'HCS_base_coil': 'HCS_coil', 'HCS_base_3for2': 'HCS_3for2',
              'HCS_base_IEHX': 'HCS_IEHX', 'HCS_base_LD': 'HCS_LD', 'HCS_base': 'HCS_base'}
-Y_CARNOT_RANGE = [-0.1, 0.0]
+Y_CARNOT_RANGE = [-0.1, 0.02]
 PLOT_SPECS = {'icc':{'ylabel':'Temperature [C]', 'xlabel':'Heat Load [kW]', 'ylim':(273,350)},
-              'carnot':{'ylabel':'Carnot factor [-]', 'xlabel':'Heat Load [kW]', 'ylim':(min(Y_CARNOT_RANGE),max(Y_CARNOT_RANGE))}}
+              'carnot':{'ylabel':'Carnot factor [-]', 'xlabel':'Heat Load [kW]', 'ylim':(min(Y_CARNOT_RANGE),max(Y_CARNOT_RANGE))},
+              'carnot_fraction':{'ylabel':'Carnot factor [-]', 'xlabel':'Qc_load/Qsc [-]',
+                                 'xlim':(0,1), 'ylim':(min(Y_CARNOT_RANGE),max(Y_CARNOT_RANGE))}
+              }
 
-def plot_carnot_from_icc_txt(path, t_list, T_max_list, T_ref_list, line_types, plot_type, model_name):
+def plot_carnot_from_icc_txt(path, t_list, T_ref_list, line_types, plot_type, model_name, total_df, exergy_df):
 
     # figure size
     plt.figure(figsize=(8, 7))
@@ -37,23 +41,30 @@ def plot_carnot_from_icc_txt(path, t_list, T_max_list, T_ref_list, line_types, p
     # plot the lines
     if isinstance(t_list, int):
         label = 'line_type'
-        get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path, plot_type, t_list, label)
+        t = t_list
+        exergy_recovered = exergy_df.loc[t, 'exergy_recovered_total']
+        exergy_usage = exergy_df.loc[t, 'exergy_use_total']
+        exergy_percent_recovered = exergy_recovered / exergy_usage
+        Qsc_t = total_df.loc[str(t), 'Qsc_theoretical']
+        Qsc_real_t = total_df.loc[str(t), 'Qsc_theoretical']
+        extra_values = {'ex_recovered': exergy_percent_recovered, 'Qsc_t': Qsc_t}
+        get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path, plot_type, t_list, label, extra_values)
         fig_name = 'carnot' + '_' + model_name + '_t' + str(t_list) + '_DefaultHeatCascade.png'
     else:
         for t in t_list:
             label = 't'
-            get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path, plot_type, t, label)
+            get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path, plot_type, t, label, '')
         fig_name = 'carnot' + '_' + model_name + '_' + str(t_list.min()) + '_' + str(t_list.max()) + '_DefaultHeatCascade.png'
 
     # save the figure
-    set_plot_parameters(ax1, PLOT_SPECS['carnot'])
+    set_plot_parameters(ax1, PLOT_SPECS['carnot_fraction'])
     fig1 = plt.gcf()
     os.chdir("..\\")
     print('saving fig to...', os.path.abspath(os.curdir))
     fig1.savefig(fig_name, transparent=True)
     return
 
-def plot_carnot_from_icc_txt_techs(paths, t, T_max_dict, T_ref_dict, line_types, plot_type, model_name):
+def plot_carnot_from_icc_txt_techs(paths, t, T_max_dict, T_ref_dict, line_types, plot_type, txt_name):
     case = ''
     # figure size
     plt.figure(figsize=(8, 7))
@@ -67,7 +78,7 @@ def plot_carnot_from_icc_txt_techs(paths, t, T_max_dict, T_ref_dict, line_types,
             print(path)
             if case == '':
                 case = 'B' + path.split('run_')[1].split('_B')[0]
-            x, y = load_data_from_txt(path, plot_type, line_type, model_name, t)
+            x, y = load_data_from_txt(path, plot_type, line_type, txt_name, t)
             y_carnot = np.vectorize(calc_carnot_factor)(T_ref, y + 273.15)
             ax1.plot(x, y_carnot, '-', color=COLOR_CODES[tech], label=KEY_TABLE[tech])
             y_origin = np.zeros(len(y))
@@ -88,12 +99,13 @@ def plot_carnot_from_icc_txt_techs(paths, t, T_max_dict, T_ref_dict, line_types,
     return
 
 
-def get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path, plot_type, t, label):
+def get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path, plot_type, t, label, extra_values):
     T_ref = T_ref_list[t - 1]
     c = 0
     for line_type in line_types:
         x, y = load_data_from_txt(path, plot_type, line_type, model_name, t)
         y_carnot = np.vectorize(calc_carnot_factor)(T_ref, y + 273.15)
+        # format figure
         if label == 't':
             label_name = t
             color = COLOR_LIST[c]
@@ -101,7 +113,19 @@ def get_data_and_plot_curves_of_t(T_ref_list, ax1, line_types, model_name, path,
         else:
             label_name = line_type
             color = COLOR_TABLE[line_type]
-        ax1.plot(x, y_carnot, '-', color=color, label=label_name)
+        # plotting
+        if extra_values != '':
+            x_new = x/extra_values['Qsc_t']
+            exergy_recovered = str(round(extra_values['ex_recovered'] * 100, 1))
+            Qc_load = 'Qc_load : ' + str(round(max(x),1))
+            Qsc = 'Qsc : ' + str(round(extra_values['Qsc_t'],1))
+            ax1.annotate('exergy recovery: ' + exergy_recovered + '% \n' + Qc_load + '\n' + Qsc,
+                         xy=(0.57,0.15), xycoords='figure fraction', fontsize=18)
+                         # bbox=dict(boxstyle="round", fc="none", ec="gray"))
+            ax1.plot(x_new, y_carnot, '-', color=color, label=label_name)
+        else:
+            ax1.plot(x, y_carnot, '-', color=color, label=label_name)
+        ax1.set_title('t = ' + str(t), fontdict={'fontsize': 16, 'fontweight': 'medium'})
 
 
 ##===================
@@ -145,9 +169,9 @@ def calc_T_max(path_to_folder, run_folder):
         T_max_list.append(T_max)
     return T_max_list
 
-def calc_T_ref(path_to_folder, run_folder):
+def calc_T_ref(path_to_run_folder):
     # get output_df
-    path_to_run_folder = os.path.join(path_to_folder, run_folder)
+    # path_to_run_folder = os.path.join(path_to_folder, run_folder)
     files_in_path = os.listdir(path_to_run_folder)
     file_name = [file for file in files_in_path if 'output' in file][0]
     path_to_file = os.path.join(path_to_run_folder, file_name)
@@ -159,41 +183,63 @@ def calc_T_ref(path_to_folder, run_folder):
 
 def main():
     tech = 'HCS_base'
-    path_to_base_folder = 'E:\\HCS_results_1015\\base\\'
+    path_to_base_folder = 'E:\\HCS_results_1022\\HCS_base_m_out_dP\\'
     # path_to_base_folder = 'E:\\OSMOSE_projects\\HCS_mk\\results\\'
-    run_folder = 'run_011_HOT_B001_1_168'
+    run_folder = 'run_003_OFF_B005_1_24_no_r'
 
+    # plot one tech
+    run_folders = os.listdir(os.path.join(path_to_base_folder, tech))
+    for run_folder in ['run_011_RET_B005_1_24_no_r']:
+        if 'run' in run_folder:
+            plot_iccc_for_one_tech(path_to_base_folder, run_folder, tech)
+
+
+    ## plot multiple techs in one figure
+    # paths_to_folder, T_max_dict, T_ref_dict = {}, {}, {}
+    # # for tech in ['HCS_base', 'HCS_base_coil', 'HCS_base_3for2', 'HCS_base_ER0', 'HCS_base_IEHX', 'HCS_base_LD']:
+    # for tech in ['HCS_base']:
+    #     model_folder = [path_to_base_folder, tech, run_folder, 's_001\\plots\\icc\\models']
+    #     paths_to_folder[tech] = os.path.join('', *model_folder)
+    #     T_max_dict[tech] = calc_T_max(path_to_base_folder + tech, run_folder)
+    #     path_to_folder = os.path.join('', *[path_to_base_folder, tech, run_folder])
+    #     T_ref_dict[tech] = calc_T_ref(path_to_folder)
+    #
+    # for t in np.arange(1,25,1):
+    # # for t in np.arange(73,73+24,2):
+    # # for t in [10]:
+    #     line_types = ['separated'] # 'base'
+    #     plot_carnot_from_icc_txt_techs(paths_to_folder, t, T_max_dict, T_ref_dict, line_types, 'icc', 'all_chillers')
+
+
+def plot_iccc_for_one_tech(path_to_base_folder, run_folder, tech):
     ## plot one tech
-    # model_folder = [path_to_base_folder, tech,  run_folder, 's_001\\plots\\icc\\models']
-    # path_to_folder = os.path.join('', *model_folder)
+    plot_type = 'models'   #'locations' #'models'
+    model_folder = [path_to_base_folder, tech, run_folder, 's_001\\plots\\icc\\', plot_type]
+    path_to_models_folder = os.path.join('', *model_folder)
     # T_max_list = calc_T_max(path_to_base_folder + tech, run_folder)
-    # T_ref_list = calc_T_ref(path_to_base_folder + tech, run_folder)
-    # line_types = ['base'] #'separated',
+    path_to_folder = os.path.join('', *[path_to_base_folder, tech, run_folder])
+    T_ref_list = calc_T_ref(path_to_folder)
+    line_types = ['base', 'separated']  # 'separated',
+
+    # get total_df
+    files_in_path = os.listdir(path_to_folder)
+    file_name = [file for file in files_in_path if 'total' in file][0]
+    total_df = pd.read_csv(os.path.join(path_to_folder, file_name), index_col=0)
+    # get exergy_df
+    file_name = [file for file in files_in_path if 'exergy_from_plot' in file][0]
+    exergy_df = pd.read_csv(os.path.join(path_to_folder, file_name), index_col=0)
+
     #
     # ## plotting one t at a time
-    # for t in np.arange(1,25,1):
-    #     plot_carnot_from_icc_txt(path_to_folder, t, T_max_list, T_ref_list, line_types, 'icc', 'all_chillers')
+    for t in np.arange(1, 25, 2):
+    # for t in [12]:
+        txt_name = 'all_chillers' #'all_chillers', 'loc4'
+        plot_carnot_from_icc_txt(path_to_models_folder, t, T_ref_list, line_types, 'icc', txt_name, total_df, exergy_df)
     ## plot multiple t in one figure
     # t_list = np.arange(96+3,96+24,6)
     # model_folder = ['E:\\HCS_results_1015\\base\\', 'HCS_base', 'run_005_RET_B005_1_168', 's_001\\plots\\icc\\models']
     # path_to_folder = os.path.join('', *model_folder)
     # plot_carnot_from_icc_txt(path_to_folder, t_list, T_max_list, T_ref_list, line_types, 'icc', 'all_chillers')
-
-
-    ## plot multiple techs in one figure
-    paths_to_folder, T_max_dict, T_ref_dict = {}, {}, {}
-    # for tech in ['HCS_base', 'HCS_base_coil', 'HCS_base_3for2', 'HCS_base_ER0', 'HCS_base_IEHX', 'HCS_base_LD']:
-    for tech in ['HCS_base','HCS_base_3for2']:
-        model_folder = [path_to_base_folder, tech, run_folder, 's_001\\plots\\icc\\models']
-        paths_to_folder[tech] = os.path.join('', *model_folder)
-        T_max_dict[tech] = calc_T_max(path_to_base_folder + tech, run_folder)
-        T_ref_dict[tech] = calc_T_ref(path_to_base_folder + tech, run_folder)
-
-    # for t in np.arange(1,25,1):
-    # for t in np.arange(73,73+24,1):
-    for t in [100, 112]:
-        line_types = ['separated'] # 'base'
-        plot_carnot_from_icc_txt_techs(paths_to_folder, t, T_max_dict, T_ref_dict, line_types, 'icc', 'all_chillers')
 
 
 if __name__ == '__main__':
