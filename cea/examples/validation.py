8da080cd3b03477cfce3e28aab7b94b40b15b7f5
Luis <luis.santos@tum-create.edu.sg>
2019-11-19 14:59:59 +0800

    Update validation.py

--- cea/examples/validation.py
+++ cea/examples/validation.py
@@ -47,47 +47,49 @@ A    new output csv is generated providing for each building (if measured data i
 
         #mege the datasets
         merged_annual = annual_modelled_data.merge(annual_measured_data, how='inner', on=['Name']) # extract the modelled buildings that have a corresponding Name (Building ID) simulated by CEA
-        model = merged_annual.GRID_MWhyr
-        real = merged_annual.Ec_measured
+        annual_modelled_demand = merged_annual.GRID_MWhyr
+        annual_measured_demand = merged_annual.Ec_measured
 
         # calculate errors
-        MBE_annual = ((model - real)/model)*100
-        print(MBE_annual)
+        mbe_annual = ((annual_modelled_demand - annual_measured_demand) / annual_modelled_demand) * 100 #mean bias error (%)
+        print(mbe_annual)
 
     ## monthly validation
     if monthly == True:
         print("monthly validation")
 
-        # extract measured data (format: BuildingID (corresponding to CEA model) | ZipCode (optional) | monthly energy consumed (kWh) (Jan-Dec)
         measured_data_path = locator.get_measurements()
-        monthly_measured_data= pd.read_csv(measured_data_path + '/monthly_measurements.csv')
+        monthly_measured_data = pd.read_csv(measured_data_path + '/monthly_measurements.csv')
 
-        for i in range (0, len(monthly_measured_data.Name)): #number of buildings that have real data available
+        for i in range(0, len(monthly_measured_data.Name)):  # number of buildings that have real data available
+
+            # extract measured data (format: BuildingID (corresponding to CEA model) | ZipCode (optional) | monthly energy consumed (kWh) (Jan-Dec)
             measured_names = monthly_measured_data.Name.values[i]
+            print(measured_names)
+            monthly_measured_demand = monthly_measured_data.loc[:, 'Ec_m1':'Ec_m12']
+            monthly_measured_demand = monthly_measured_demand.values[i]
 
             # extract model output
             demand_path = locator.get_demand_results_folder()
-            monthly_model_all = str(demand_path) + "\\" + str(measured_names) + '.csv'
-            model = pd.read_csv(monthly_model_all)
-            idx = pd.to_datetime(model.DATE)
-            model['datetime'] = idx
-            model_monthly = model.resample('M', on = 'datetime').sum()
-            monthly_model_data = [model_monthly.GRID_kWh]
-            monthly_model_data = pd.DataFrame(monthly_model_data)
+            building_demand = str(demand_path) + "\\" + str(measured_names) + '.csv'
+            hourly_modelled_data = pd.read_csv(building_demand)
+            date_idx = pd.to_datetime(hourly_modelled_data.DATE)    #this step is required to have allow the conversion from hourly to monthly data
+            hourly_modelled_data['datetime'] = date_idx
+            monthly_modelled_data = hourly_modelled_data.resample('M', on ='datetime').sum()
+            monthly_modelled_demand = [monthly_modelled_data.GRID_kWh]
+            monthly_modelled_demand = pd.DataFrame(monthly_modelled_demand)
+            monthly_modelled_demand = monthly_modelled_demand.values[0]
 
             # calculate errors
-            real_data = monthly_measured_data.loc[:, 'Ec_m1':'Ec_m12']
-            real_data = real_data.values[i]
-            model_data = monthly_model_data.values[0]
-            be = real_data - model_data
-            nmbe =(be.sum()/12)/real_data.mean()
+            be = monthly_measured_demand - monthly_modelled_demand #bias error
+            nmbe = (be.sum()/12) / monthly_measured_demand.mean()
             print(nmbe)
-            mse = mean_squared_error(real_data,model_data)
+            mse = mean_squared_error(monthly_measured_demand, monthly_modelled_demand)
             rmse = sqrt(mse)
-            cvrmse = rmse*100/real_data.mean()
+            cvrmse = rmse * 100 / monthly_measured_demand.mean()
             print(cvrmse)
 
-            print(model_data)
+
         # print (math.sqrt(9))
 
 
@@ -110,7 +112,7 @@ def main(config):
     assert os.path.exists(config.scenario), 'Scenario not found: %s' % config.scenario
     locator = cea.inputlocator.InputLocator(config.scenario)
 
-    template(locator, config.scenario)
+    validation(locator, config.scenario)
 
 
 if __name__ == '__main__':
