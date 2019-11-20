befdac4c064d2ff5f5dc09cc78bd728c68704355
Luis <luis.santos@tum-create.edu.sg>
2019-11-20 10:38:35 +0800

    validation code (updated after Jimeno's review)

--- cea/constants.py
+++ cea/constants.py
@@ -5,6 +5,7 @@ it is highly recommended to keep those constants in a separate file in the subfo
 declare the constants closest to the point of usage.
 """
 from __future__ import absolute_import
+
 __author__ = "Sreepathi Bhargava Krishna"
 __copyright__ = "Copyright 2017, Architecture and Building Systems - ETH Zurich"
 __credits__ = ["Sreepathi Bhargava Krishna"]
@@ -28,6 +29,9 @@ DAYS_IN_YEAR = 365
 HOURS_IN_DAY = 24
 HOURS_IN_YEAR = 8760
 MONTHS_IN_YEAR = 12
+MONTHS_IN_YEAR_NAMES = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL',
+                        'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER',
+                        'OCTOBER', 'NOVEMBER', 'DECEMBER']
 
 # Specific heat
 HEAT_CAPACITY_OF_WATER_JPERKGK = 4185  # [J/kg K]
@@ -52,12 +56,11 @@ MIN_FLOW_LPERS = 9  # in lps got from Rabtherm technology
 T_MIN = 8  # tmin of extraction got from Rabtherm technology
 H0_KWPERM2K = 1.5  # kW/m2K # heat trasnfer coefficient/ got from Rabtherm technology
 
-#ground temperature values
-SOIL_Cp_JkgK = 2000 # _[A. Kecebas et al., 2011]
+# ground temperature values
+SOIL_Cp_JkgK = 2000  # _[A. Kecebas et al., 2011]
 SOIL_lambda_WmK = 1.6
 SOIL_rho_kgm3 = 1600
 
-#insulation of pipes
+# insulation of pipes
 PUR_lambda_WmK = 0.023
 STEEL_lambda_WmK = 76
-
--- cea/examples/validation.py
+++ cea/examples/validation.py
@@ -10,6 +10,7 @@ import cea.inputlocator
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+from cea.constants import MONTHS_IN_YEAR, MONTHS_IN_YEAR_NAMES
 
 __author__ = "Luis Santos"
 __copyright__ = "Copyright 2018, Architecture and Building Systems - ETH Zurich"
@@ -21,72 +22,76 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
-def validation(locator, archetypes): #confirm what goes in parenthesis here
+def validation(locator, archetypes):  # confirm what goes in parenthesis here
     """
     This tool compares observed (real life measured data) and predicted (output of the model data) values.
     Annual data is compared in terms of MBE and monthly data in terms of NMBE and CvRMSE (follwing ASHRAE Guideline 14-2002).
     A new input folder with measurements has to be created, with a csv each for monthly and annual data provided as input for this tool.
     A new output csv is generated providing for each building (if measured data is available): building ID | ZIP Code | Measured data (observed) | Modelled data (predicted) | Model errors
     """
-    
-    #type of validation to run (select only the ones that have a corresponding csv in the inputs folder)
-    annual = False
-    monthly = True
+
+    # type of validation to run (select only the ones that have a corresponding csv in the inputs folder)
+    annual = True
+    monthly = False
+    load = 'GRID'
 
     ## annual validation
-    if annual == True:
+    if annual:
         print("annual validation")
-        
+
         # extract measured data (format: BuildingID (corresponding to CEA model) | ZipCode (optional) | Annual energy consumed (kWh)
-        measured_data_path = locator.get_measurements()
-        annual_measured_data= pd.read_csv(measured_data_path + '/annual_measurements.csv')
+        annual_measured_data = pd.read_csv(locator.get_annual_measurements())
 
-        #extract model output
-        demand_path = locator.get_demand_results_folder()
-        annual_modelled_data = pd.read_csv(demand_path + '/Total_demand.csv')
+        # extract model output
+        annual_modelled_data = pd.read_csv(locator.get_demand_results_file())
 
-        #mege the datasets
-        merged_annual = annual_modelled_data.merge(annual_measured_data, how='inner', on=['Name']) # extract the modelled buildings that have a corresponding Name (Building ID) simulated by CEA
-        annual_modelled_demand = merged_annual.GRID_MWhyr
-        annual_measured_demand = merged_annual.Ec_measured
+        # mege the datasets
+        merged_annual = annual_modelled_data.merge(annual_measured_data, how='inner',
+                                                   on='Name', suffixes=('_model','_measured'))  # extract the modelled buildings that have a corresponding Name (Building ID) simulated by CEA
+        annual_modelled_demand = merged_annual[load+'_MWhyr'+'_model']
+        annual_measured_demand = merged_annual[load+'_Mwhyr'+'_measured']
 
         # calculate errors
-        be_annual = ((annual_modelled_demand - annual_measured_demand) / annual_modelled_demand) * 100 #bias error (%)
-        print(be_annual)
+        biased_error_annual_data = ((annual_modelled_demand - annual_measured_demand) / annual_measured_demand) * 100
+        print(biased_error_annual_data)
 
     ## monthly validation
-    if monthly == True:
+    if monthly:
         print("monthly validation")
-
-        measured_data_path = locator.get_measurements()
-        monthly_measured_data = pd.read_csv(measured_data_path + '/monthly_measurements.csv')
-
-        for i in range(0, len(monthly_measured_data.Name)):  # number of buildings that have real data available
+        monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+        measured_building_names = monthly_measured_data.Name.values
+        for building_name in measured_building_names:  # number of buildings that have real data available
 
             # extract measured data (format: BuildingID (corresponding to CEA model) | ZipCode (optional) | monthly energy consumed (kWh) (Jan-Dec)
-            measured_names = monthly_measured_data.Name.values[i]
-            print(measured_names)
-            monthly_measured_demand = monthly_measured_data.loc[:, 'Ec_m1':'Ec_m12']
-            monthly_measured_demand = monthly_measured_demand.values[i]
+            print(building_name)
+            fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
+            monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
+            monthly_measured_demand = monthly_measured_demand.loc[building_name]
+            monthly_measured_demand = pd.DataFrame({'Month':monthly_measured_demand.index.values,
+                                                    'measurements':monthly_measured_demand.values})
 
             # extract model output
-            demand_path = locator.get_demand_results_folder()
-            building_demand = str(demand_path) + "\\" + str(measured_names) + '.csv'
-            hourly_modelled_data = pd.read_csv(building_demand)
-            date_idx = pd.to_datetime(hourly_modelled_data.DATE)    #this step is required to have allow the conversion from hourly to monthly data
-            hourly_modelled_data['datetime'] = date_idx
-            monthly_modelled_data = hourly_modelled_data.resample('M', on ='datetime').sum()
-            monthly_modelled_demand = [monthly_modelled_data.GRID_kWh]
-            monthly_modelled_demand = pd.DataFrame(monthly_modelled_demand)
-            monthly_modelled_demand = monthly_modelled_demand.values[0]
+            hourly_modelled_data = pd.read_csv(locator.get_demand_results_file(building_name), usecols=['DATE',load+'_kWh'])
+            hourly_modelled_data['DATE'] = pd.to_datetime(hourly_modelled_data['DATE'])
+
+            look_up = {1: 'JANUARY', 2: 'FEBRUARY', 3: 'MARCH', 4: 'APRIL', 5: 'MAY',
+                       6: 'JUNE', 7: 'JULY', 8: 'AUGUST', 9: 'SEPTEMBER', 10: 'OCTOBER', 11: 'NOVEMBER',
+                       12: 'DECEMBER'}
+
+            # this step is required to have allow the conversion from hourly to monthly data
+            monthly_modelled_data = hourly_modelled_data.resample('M', on='DATE').sum()# because data is in kWh
+            monthly_modelled_data['Month'] = monthly_modelled_data.index.month
+            monthly_modelled_data['Month'] = monthly_modelled_data.apply(lambda x: look_up[x['Month']], axis=1)
+            monthly_data = monthly_modelled_data.merge(monthly_measured_demand, on='Month')
 
             # calculate errors
-            be = monthly_measured_demand - monthly_modelled_demand #bias error
-            nmbe = ((be.sum()/12) / monthly_measured_demand.mean())*100 #normalized mean bias error (%)
+            #monthly_data.rename({'GRID_kWh': 'GRID_MWh'}, inplace=True)
+            be = monthly_data['measurements'] - monthly_data[load+'_kWh']  # bias error
+            nmbe = ((be.sum() / 12) / monthly_data['measurements'].mean()) * 100  # normalized mean bias error (%)
             print(nmbe)
-            mse = mean_squared_error(monthly_measured_demand, monthly_modelled_demand) #mean squared error
-            rmse = sqrt(mse) #root mean squared error
-            cvrmse = rmse * 100 / monthly_measured_demand.mean() #coefficient of variation of the root mean squared error (%)
+            mse = mean_squared_error(monthly_data['measurements'], monthly_data[load+'_kWh'])  # mean squared error
+            rmse = sqrt(mse)  # root mean squared error
+            cvrmse = rmse * 100 / monthly_data['measurements'].mean()  # coefficient of variation of the root mean squared error (%)
             print(cvrmse)
 
     pass
