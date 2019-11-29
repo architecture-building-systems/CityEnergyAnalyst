245981639e957ccfa54937951fe76cc71f44336b
shanshanhsieh <hsieh@arch.ethz.ch>
2019-11-29 16:14:46 +0800

    Update settings.py

--- cea/osmose/settings.py
+++ cea/osmose/settings.py
@@ -5,7 +5,7 @@
 T_b_CDD = 25.0
 # TECHS = ['HCS_LD', 'HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_IEHX']
 # TECHS = ['HCS_base_LD', 'HCS_base_coil', 'HCS_base_3for2', 'HCS_base_ER0', 'HCS_base_IEHX',  'HCS_base']
-TECHS = ['HCS_base']
+TECHS = ['HCS_base_LD', 'HCS_base_IEHX', 'HCS_base_ER0']
 # timesteps = [5136, 5144, 5145, 5147, 5148]  # 168 (week) [5389]
 # timesteps = [5145]  # 168 (week) [5389]
 timesteps = 24  # 168 (week) [5389]
@@ -13,8 +13,9 @@ timesteps = 24  # 168 (week) [5389]
 # timesteps = 'dtw hours'
 typical_days_path = "E:\\WP2"
 PLOTS = ['electricity_usages','air_flow','OAU_T_w_supply','exergy_usages', 'humidity_balance', 'humidity_storage', 'heat_balance']
-post_process_json = True
-remove_json = True
+post_process_json = True    # True to extract information from out.json
+remove_json = True          # True to remove out.json after extracting information
+remove_jpg = False          # True to remove the plot files and only keep txt
 post_process_osmose = True
 new_calculation = False
 
@@ -24,29 +25,30 @@ specified_buildings = ["B005"]
 # specified_buildings = ["B001","B002","B005","B006","B009"]
 # specified_buildings = ["B003","B008"]
 # specified_buildings = ["B001","B002","B003","B004","B005","B006","B007","B008","B009","B010"]
-cases = ['WTP_CBD_m_WP1_OFF','WTP_CBD_m_WP1_RET','WTP_CBD_m_WP1_HOT']
+cases = ['WTP_CBD_m_WP1_OFF']
 # cases = ['WTP_CBD_m_WP1_HOT']
 
 ## LAPTOP ##
-# # ampl_lic_path = "C:\\Users\\Shanshan\\Desktop\\ampl"
-#
-# # Branch mk
-# osmose_project_path = "E:\\OSMOSE_projects\\HCS_mk\\Projects"
-# # osmose_project_path = "E:\\ipese_new\\osmose_mk\\Projects"
+ampl_lic_path = "C:\\Users\\Shanshan\\Desktop\\ampl"
+# Branch mk
+osmose_project_path = "E:\\OSMOSE_projects\\HCS_mk\\Projects"
+# osmose_project_path = "E:\\ipese_new\\osmose_mk\\Projects"
 osmose_outMsg_path = "\\s_001\\opt\\hc_outmsg.txt"
-#
-# # Branch master
-# # osmose_project_path = "E:\\OSMOSE_projects\\HCS\\Projects"
-# # osmose_outMsg_path = "\\scenario_1\\tmp\\OutMsg.txt"
-#
-# osmose_project_data_path = osmose_project_path + '\\data'
-# result_destination = "E:\\HCS_results_1015"
+
+# Branch master
+# osmose_project_path = "E:\\OSMOSE_projects\\HCS\\Projects"
+# osmose_outMsg_path = "\\scenario_1\\tmp\\OutMsg.txt"
+
+osmose_project_data_path = osmose_project_path + '\\data'
+result_destination = "E:\\HCS_results_1015"
+## end ##
 
 ## WORK STATION ##
-ampl_lic_path = "C:\\Users\\Zhongming\\Desktop\\SH\\ampl"
-osmose_project_path = "C:\\Users\\Zhongming\\Documents\\HCS_mk\\Projects"
-osmose_project_data_path = osmose_project_path + '\\data'
-result_destination = "D:\\SH\\test_0805"
+# ampl_lic_path = "C:\\Users\\Zhongming\\Desktop\\SH\\ampl"
+# osmose_project_path = "C:\\Users\\Zhongming\\Documents\\HCS_mk\\Projects"
+# osmose_project_data_path = osmose_project_path + '\\data'
+# result_destination = "D:\\SH\\test_0805"
+## end ##
 
 # == HKG Summer ==
 # season = 'Summer'
