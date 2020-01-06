f9f837aaa1597b6628a9ed46224a101a3f67c856
Luis <luis.santos@tum-create.edu.sg>
2020-01-06 18:02:46 +0800

    Update calibration.py

--- cea/examples/calibration.py
+++ cea/examples/calibration.py
@@ -10,6 +10,9 @@ import cea.inputlocator
 from cea.utilities.dbf import dbf_to_dataframe, dataframe_to_dbf
 from cea.demand import demand_main
 from cea.demand.schedule_maker import schedule_maker
+from cea.examples import validation
+import numpy as np
+from scipy.optimize import minimize
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 from math import sqrt
@@ -65,73 +68,23 @@ def calibration(locator, config):  # confirm what goes in parenthesis here
     df_comfort = dataframe_to_dbf(df_comfort, locator.get_building_comfort())
 
     ## run building schedules and energy demand (first run)
-    schedule_maker.schedule_maker_main(locator, config, building=None)
-    demand_main.demand_calculation(locator, config)
+    # schedule_maker.schedule_maker_main(locator, config, building=None)
+    # demand_main.demand_calculation(locator, config)
+    validation.validation(locator)
 
-    # annual = True
-    # monthly = False
-    # load = 'GRID'
+    ## define function to be minimized
+    
+    def rosen(x):
+        return sum(100.0 * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0)
 
-    # ## annual validation
-    # if annual:
-    #     print("annual validation")
-    #
-    #     # extract measured data (format: BuildingID (corresponding to CEA model) | ZipCode (optional) | Annual energy consumed (kWh)
-    #     annual_measured_data = pd.read_csv(locator.get_annual_measurements())
-    #
-    #     # extract model output
-    #     annual_modelled_data = pd.read_csv(locator.get_total_demand())
-    #
-    #     # mege the datasets
-    #     merged_annual = annual_modelled_data.merge(annual_measured_data, how='inner',
-    #                                                on='Name', suffixes=('_model','_measured'))  # extract the modelled buildings that have a corresponding Name (Building ID) simulated by CEA
-    #     annual_modelled_demand = merged_annual[load+'_MWhyr'+'_model']
-    #     annual_measured_demand = merged_annual[load+'_MWhyr'+'_measured']
-    #
-    #     # calculate errors
-    #     biased_error_annual_data = ((annual_modelled_demand - annual_measured_demand) / annual_measured_demand) * 100
-    #     print(biased_error_annual_data)
-    #
-    # ## monthly validation
-    # if monthly:
-    #     print("monthly validation")
-    #     monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
-    #     measured_building_names = monthly_measured_data.Name.values
-    #     for building_name in measured_building_names:  # number of buildings that have real data available
-    #
-    #         # extract measured data (format: BuildingID (corresponding to CEA model) | ZipCode (optional) | monthly energy consumed (kWh) (Jan-Dec)
-    #         print(building_name)
-    #         fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
-    #         monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
-    #         monthly_measured_demand = monthly_measured_demand.loc[building_name]
-    #         monthly_measured_demand = pd.DataFrame({'Month':monthly_measured_demand.index.values,
-    #                                                 'measurements':monthly_measured_demand.values})
-    #
-    #         # extract model output
-    #         hourly_modelled_data = pd.read_csv(locator.get_demand_results_file(building_name), usecols=['DATE',load+'_kWh'])
-    #         hourly_modelled_data['DATE'] = pd.to_datetime(hourly_modelled_data['DATE'])
-    #
-    #         look_up = {1: 'JANUARY', 2: 'FEBRUARY', 3: 'MARCH', 4: 'APRIL', 5: 'MAY',
-    #                    6: 'JUNE', 7: 'JULY', 8: 'AUGUST', 9: 'SEPTEMBER', 10: 'OCTOBER', 11: 'NOVEMBER',
-    #                    12: 'DECEMBER'}
-    #
-    #         # this step is required to have allow the conversion from hourly to monthly data
-    #         monthly_modelled_data = hourly_modelled_data.resample('M', on='DATE').sum()# because data is in kWh
-    #         monthly_modelled_data['Month'] = monthly_modelled_data.index.month
-    #         monthly_modelled_data['Month'] = monthly_modelled_data.apply(lambda x: look_up[x['Month']], axis=1)
-    #         monthly_data = monthly_modelled_data.merge(monthly_measured_demand, on='Month')
-    #
-    #         # calculate errors
-    #         #monthly_data.rename({'GRID_kWh': 'GRID_MWh'}, inplace=True)
-    #         be = monthly_data['measurements'] - monthly_data[load+'_kWh']  # bias error
-    #         nmbe = ((be.sum() / 12) / monthly_data['measurements'].mean()) * 100  # normalized mean bias error (%)
-    #         print(nmbe)
-    #         mse = mean_squared_error(monthly_data['measurements'], monthly_data[load+'_kWh'])  # mean squared error
-    #         rmse = sqrt(mse)  # root mean squared error
-    #         cvrmse = rmse * 100 / monthly_data['measurements'].mean()  # coefficient of variation of the root mean squared error (%)
-    #         print(cvrmse)
-    #
-    # pass
+    ## run optimization algorithm
+    x0 = np.array([Es, Ns, Occ_m2pax, Vww_lpdpax, Ea_Wm2, El_Wm2])
+    print(x0)
+    res = minimize(rosen, x0, method='nelder-mead',
+                   options={'xatol': 1e-8, 'disp': True})
+    print(res.x)
+
+    pass
 
 
 def main(config):
