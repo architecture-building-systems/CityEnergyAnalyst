4d00d8d163547a0130dc836a451c7a0195f390c9
Luis <luis.santos@tum-create.edu.sg>
2020-01-16 17:54:23 +0800

    update

--- cea/examples/calibration2.py
+++ cea/examples/calibration2.py
@@ -50,6 +50,8 @@ def calc_score(static_params, params):
     locator = static_params['locator']
     config = static_params['config']
 
+    schedule_maker.schedule_maker_main(locator, config, building=None)
+
     ## define set of CEA inputs to be calibrated and initial guess values
     Es = params['Es']
     Ns = params['Ns']
@@ -82,16 +84,49 @@ def calc_score(static_params, params):
     df_comfort.Tcs_setb_C = Tcs_setb_C
     df_comfort = dataframe_to_dbf(df_comfort, locator.get_building_comfort())
 
+    # Define monthly multiplier occupancy values in the schedules based on the monthly measured electricity consumption ***
+    monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+    measured_building_names = monthly_measured_data.Name.values
+    measured_building_names = list(measured_building_names)
+    for building_name in measured_building_names:
+        monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+        fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
+        monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
+        monthly_measured_demand = monthly_measured_demand.loc[building_name]
+        monthly_measured_demand = pd.DataFrame({'Month': monthly_measured_demand.index.values,
+                                                'measurements': monthly_measured_demand.values})
+        monthly_measured_load = monthly_measured_demand.measurements/max(monthly_measured_demand.measurements)
+
+        monthly_occupancy = pd.read_csv(locator.get_building_weekly_schedules(building_name))
+        # monthly_occupancy
+        # for row in monthly_occupancy:
+        #     if monthly_occupancy.line_num == 2:
+
+
+
     ## run building schedules and energy demand
 
     # define which buildings will be simulated ***
     # monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
     # measured_building_names = monthly_measured_data.Name.values
-    # for building_name in measured_building_names:
+    # measured_building_names = list(measured_building_names)
+    building_names = measured_building_names
+    # return building_names
 
     schedule_maker.schedule_maker_main(locator, config, building=None)
 
-    # Define monthly multiplier occupancy values in the schedules based on the monthly measured electricity consumption ***
+    # Define monthly multiplier occupancy values in the schedules based on the monthly measured electricity consumption *** moved this section
+    # for building_name in measured_building_names:
+    #     monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+    #     fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
+    #     monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
+    #     monthly_measured_demand = monthly_measured_demand.loc[building_name]
+    #     monthly_measured_demand = pd.DataFrame({'Month': monthly_measured_demand.index.values,
+    #                                             'measurements': monthly_measured_demand.values})
+    #     monthly_measured_load = monthly_measured_demand.measurements/max(monthly_measured_demand.measurements)
+    #
+    #     monthly_occupancy = pd.read_csv(locator.get_building_weekly_schedules(building_name))
+
 
     demand_main.demand_calculation(locator, config)
 
@@ -102,9 +137,10 @@ def calc_score(static_params, params):
 
     ## save the iteration number, the value of each parameter tested and the score obtained ***
 
+
 def calibration(config, locator):
 
-    max_evals = 100
+    max_evals = 2
 
     #  define a search space
     SPACE = OrderedDict([('Es', hp.uniform('Es', 0.6, 1.0)),
@@ -130,6 +166,16 @@ def calibration(config, locator):
     print(best)
     print('Best Params: {}'.format(best))
     print (trials.losses())
+    for counter in range(0, max_evals):
+        results_it = [counter,trials.trials[counter]['misc']['vals']['Ea_Wm2'],trials.trials[counter]['misc']['vals']['El_Wm2'], trials.trials[counter]['misc']['vals']['Es'],trials.trials[counter]['misc']['vals']['Ns'],
+          trials.trials[counter]['misc']['vals']['Occ_m2pax'],trials.trials[counter]['misc']['vals']['Vww_lpdpax'],trials.losses()[counter]]
+        print (results_it)
+        results = pd.DataFrame(results_it)
+
+        results.to_csv('calibration_it.csv')
+        print (results)
+        # print(counter,trials.trials[counter]['misc']['vals']['Ea_Wm2'],trials.trials[counter]['misc']['vals']['El_Wm2'], trials.trials[counter]['misc']['vals']['Es'],trials.trials[counter]['misc']['vals']['Ns'],
+        #   trials.trials[counter]['misc']['vals']['Occ_m2pax'],trials.trials[counter]['misc']['vals']['Vww_lpdpax'],trials.losses()[counter])
 
     x=1
 def main(config):
@@ -146,6 +192,12 @@ def main(config):
     assert os.path.exists(config.scenario), 'Scenario not found: %s' % config.scenario
     locator = cea.inputlocator.InputLocator(config.scenario)
     calibration(config, locator)
+
+    # monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+    # measured_building_names = monthly_measured_data.Name.values
+    # measured_building_names = list(measured_building_names)
+    # building_names = measured_building_names
+    # return building_names
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
