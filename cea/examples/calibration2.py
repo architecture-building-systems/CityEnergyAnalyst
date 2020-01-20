572ef00fe1ad83575bf9efb4258152766506b5af
Luis <luis.santos@tum-create.edu.sg>
2020-01-20 17:23:33 +0800

    upates after Jimeno's review

--- cea/examples/calibration2.py
+++ cea/examples/calibration2.py
@@ -8,9 +8,11 @@ import os
 import cea.config
 import cea.inputlocator
 from cea.utilities.dbf import dbf_to_dataframe, dataframe_to_dbf
-from cea.demand import demand_main
+from cea.datamanagement import data_helper
+from cea.demand import demand_main, schedule_maker
 from cea.demand.schedule_maker import schedule_maker
 from cea.examples import validation
+from cea.utilities.schedule_reader import read_cea_schedule, save_cea_schedule
 import numpy as np
 from scipy.optimize import minimize
 from hyperopt import hp
@@ -39,18 +41,46 @@ __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
+def modify_monthly_multiplier (locator, config):
+
+    ##create building input schedules to set monthly multiplier
+    data_helper.data_helper(locator, config.data_helper.region, overwrite_technology_folder=False,
+                update_architecture_dbf=False, update_HVAC_systems_dbf=False, update_indoor_comfort_dbf=False,
+                update_internal_loads_dbf=False, update_supply_systems_dbf=False,
+                update_schedule_operation_cea=True, buildings=[])
+
+    ##calculate monthly multiplier based on buildings real consumption
+    monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+    measured_building_names = monthly_measured_data.Name.values
+    measured_building_names = list(measured_building_names)
+
+    for building_name in measured_building_names:
+        monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+        fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
+        monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
+        monthly_measured_demand = monthly_measured_demand.loc[building_name]
+        monthly_measured_demand = pd.DataFrame({'Month': monthly_measured_demand.index.values,
+                                                'measurements': monthly_measured_demand.values})
+        monthly_measured_load = monthly_measured_demand.measurements/max(monthly_measured_demand.measurements)
+
+        path_to_schedule = locator.get_building_weekly_schedules(building_name)
+        data_schedule, data_metadata = read_cea_schedule(path_to_schedule)
+        data_metadata["MONTHLY_MULTIPLIER"]=list(monthly_measured_load.round(2))
+
+        # save cea schedule format
+        save_cea_schedule(data_schedule, data_metadata, path_to_schedule)
+    return measured_building_names
 
 def calc_score(static_params, params):
     """
     This tool reduces the error between observed (real life measured data) and predicted (output of the model data) values by changing some of CEA inputs.
     Annual data is compared in terms of MBE and monthly data in terms of NMBE and CvRMSE (follwing ASHRAE Guideline 14-2002).
     A new input folder with measurements has to be created, with a csv each for monthly and annual data provided as input for this tool.
-    A new output csv is generated providing for each building (if measured data is available): building ID | ZIP Code | Measured data (observed) | Modelled data (predicted) | Model errors
+    A new output csv is generated providing the calibration results (iteration number, parameters tested and results(score metric))
     """
     locator = static_params['locator']
     config = static_params['config']
-
-    schedule_maker.schedule_maker_main(locator, config, building=None)
+    measured_building_names = static_params['measured_building_names']
 
     ## define set of CEA inputs to be calibrated and initial guess values
     Es = params['Es']
@@ -84,54 +114,14 @@ def calc_score(static_params, params):
     df_comfort.Tcs_setb_C = Tcs_setb_C
     df_comfort = dataframe_to_dbf(df_comfort, locator.get_building_comfort())
 
-    # Define monthly multiplier occupancy values in the schedules based on the monthly measured electricity consumption ***
-    monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
-    measured_building_names = monthly_measured_data.Name.values
-    measured_building_names = list(measured_building_names)
-    for building_name in measured_building_names:
-        monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
-        fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
-        monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
-        monthly_measured_demand = monthly_measured_demand.loc[building_name]
-        monthly_measured_demand = pd.DataFrame({'Month': monthly_measured_demand.index.values,
-                                                'measurements': monthly_measured_demand.values})
-        monthly_measured_load = monthly_measured_demand.measurements/max(monthly_measured_demand.measurements)
-
-        monthly_occupancy = pd.read_csv(locator.get_building_weekly_schedules(building_name))
-        # monthly_occupancy
-        # for row in monthly_occupancy:
-        #     if monthly_occupancy.line_num == 2:
-
-
-
     ## run building schedules and energy demand
-
-    # define which buildings will be simulated ***
-    # monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
-    # measured_building_names = monthly_measured_data.Name.values
-    # measured_building_names = list(measured_building_names)
-    building_names = measured_building_names
-    # return building_names
-
-    schedule_maker.schedule_maker_main(locator, config, building=None)
-
-    # Define monthly multiplier occupancy values in the schedules based on the monthly measured electricity consumption *** moved this section
-    # for building_name in measured_building_names:
-    #     monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
-    #     fields_to_extract = ['Name'] + MONTHS_IN_YEAR_NAMES
-    #     monthly_measured_demand = monthly_measured_data[fields_to_extract].set_index('Name')
-    #     monthly_measured_demand = monthly_measured_demand.loc[building_name]
-    #     monthly_measured_demand = pd.DataFrame({'Month': monthly_measured_demand.index.values,
-    #                                             'measurements': monthly_measured_demand.values})
-    #     monthly_measured_load = monthly_measured_demand.measurements/max(monthly_measured_demand.measurements)
-    #
-    #     monthly_occupancy = pd.read_csv(locator.get_building_weekly_schedules(building_name))
-
-
+    config.schedule_maker.buildings = measured_building_names
+    schedule_maker.schedule_maker_main(locator, config)
+    config.demand.buildings = measured_building_names
     demand_main.demand_calculation(locator, config)
 
     #calculate the score
-    score = validation.validation(locator)
+    score = validation.validation(locator, measured_building_names)
 
     return score
 
@@ -139,8 +129,8 @@ def calc_score(static_params, params):
 
 
 def calibration(config, locator):
-
-    max_evals = 2
+    measured_building_names = modify_monthly_multiplier(locator, config)
+    max_evals = 100
 
     #  define a search space
     SPACE = OrderedDict([('Es', hp.uniform('Es', 0.6, 1.0)),
@@ -150,7 +140,7 @@ def calibration(config, locator):
                          ('Ea_Wm2', hp.uniform('Ea_Wm2', 1.0, 5.0)),
                          ('El_Wm2', hp.uniform('El_Wm2', 1.0, 5.0))
                          ])
-    STATIC_PARAMS = {'locator': locator, 'config': config}
+    STATIC_PARAMS = {'locator': locator, 'config': config, 'measured_building_names':measured_building_names}
 
     #define the objective
     def objective(params):
@@ -166,14 +156,19 @@ def calibration(config, locator):
     print(best)
     print('Best Params: {}'.format(best))
     print (trials.losses())
+    results=[]
     for counter in range(0, max_evals):
-        results_it = [counter,trials.trials[counter]['misc']['vals']['Ea_Wm2'],trials.trials[counter]['misc']['vals']['El_Wm2'], trials.trials[counter]['misc']['vals']['Es'],trials.trials[counter]['misc']['vals']['Ns'],
-          trials.trials[counter]['misc']['vals']['Occ_m2pax'],trials.trials[counter]['misc']['vals']['Vww_lpdpax'],trials.losses()[counter]]
-        print (results_it)
-        results = pd.DataFrame(results_it)
 
-        results.to_csv('calibration_it.csv')
-        print (results)
+        results_it = [counter,trials.trials[counter]['misc']['vals']['Ea_Wm2'][0],trials.trials[counter]['misc']['vals']['El_Wm2'][0], trials.trials[counter]['misc']['vals']['Es'][0],trials.trials[counter]['misc']['vals']['Ns'][0],
+          trials.trials[counter]['misc']['vals']['Occ_m2pax'][0],trials.trials[counter]['misc']['vals']['Vww_lpdpax'][0],trials.losses()[counter]]
+        results.append(results_it)
+    results=pd.DataFrame(results)
+    path=locator.get_calibrationresults()
+    results.to_csv(path, index=False)
+        # results = pd.DataFrame(results_it)
+        #
+        # results.to_csv('calibration_it.csv')
+        # print (results)
         # print(counter,trials.trials[counter]['misc']['vals']['Ea_Wm2'],trials.trials[counter]['misc']['vals']['El_Wm2'], trials.trials[counter]['misc']['vals']['Es'],trials.trials[counter]['misc']['vals']['Ns'],
         #   trials.trials[counter]['misc']['vals']['Occ_m2pax'],trials.trials[counter]['misc']['vals']['Vww_lpdpax'],trials.losses()[counter])
 
