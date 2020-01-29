fc159f073eac6f47be43ca8f30f2a90000eadd0f
Reynold Mok <reyold@hotmail.com>
2020-01-29 20:18:30 +0800

    Copy databases when creating scenario

--- cea/interfaces/dashboard/api/project.py
+++ cea/interfaces/dashboard/api/project.py
@@ -115,6 +115,13 @@ class Scenarios(Resource):
 
         locator = cea.inputlocator.InputLocator(new_scenario_path)
 
+        # Copy selected database to scenario folder
+        database_path = locator.db_path
+        regions = [folder for folder in os.listdir(database_path) if folder != "weather"
+                   and os.path.isdir(os.path.join(database_path, folder))]
+        if payload['database'] in regions:
+            get_technology_related_databases(locator, payload['database'])
+
         if payload['input-data'] == 'import':
             files = payload['files']
 
@@ -207,6 +214,15 @@ def glob_shapefile_auxilaries(shapefile_path):
     """Returns a list of files in the same folder as ``shapefile_path``, but allows for varying extensions.
     This gets the .dbf, .shx, .prj, .shp and .cpg files"""
     return glob.glob('{basepath}.*'.format(basepath=os.path.splitext(shapefile_path)[0]))
+
+
+def get_technology_related_databases(locator, region):
+    technology_database_template = locator.get_technology_template_for_region(region)
+    print("Copying technology databases from {source}".format(source=technology_database_template))
+    output_directory = locator.get_databases_folder()
+
+    from distutils.dir_util import copy_tree
+    copy_tree(technology_database_template, output_directory)
 
 
 def check_scenario_exists(func):
