a229f7fb3b8a8bc731fd4259c7e89c0714f9c004
shanshanhsieh <hsieh@arch.ethz.ch>
2020-02-05 09:03:09 +0800

    Create plot_icc_locs.py

---
+++ cea/osmose/plots/plot_icc_locs.py
@@ -0,0 +1,77 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import rcParams
+from cea.osmose.plots.plot_curves_from_osmose_outputs import load_data_from_txt, set_plot_parameters
+from cea.osmose.plots.plot_carnot_from_icc import calc_T_from_carnot, calc_T_ref, calc_carnot_factor
+
+rcParams['mathtext.default'] = 'regular'
+COLOR_TABLE = {'base': sns.xkcd_rgb["pale red"], 'separated': sns.xkcd_rgb["sea blue"]}
+
+Y_CARNOT_RANGE = [0.0, -0.1]
+PLOT_SPECS = {'icc':{'ylabel':'Temperature [C]', 'xlabel':'Heat [kW]', 'ylim':(min(Y_CARNOT_RANGE),max(Y_CARNOT_RANGE)),
+                     'xlim':(-100,800), 'ylabel':'Carnot factor [-]', 'xlabel':'Heat [kW]'},
+              'carnot':{'ylabel':'Carnot factor [-]', 'xlabel':'Heat [kW]'}}
+
+
+def plot_loc2_to_loc3(path, t, plot_type, T_ref):
+    T_ref_t = T_ref[t-1]
+    # figure size
+    plt.figure(figsize=(8, 7))
+    ax1 = plt.subplot()
+    # plot the loc2
+    line_types = ['separated']
+    model_name = 'loc2'
+    for line_type in line_types:
+        x, y = load_data_from_txt(path, plot_type, line_type, model_name, t)
+        y_carnot = np.vectorize(calc_carnot_factor)(T_ref_t, y + 273.15)
+        ax1.plot(x*(-1), y_carnot, '-', color=COLOR_TABLE[line_type], label=line_type, linestyle='--')
+
+    # plot the loc3
+    line_types = ['separated']
+    model_name = 'loc3'
+    for line_type in line_types:
+        x, y = load_data_from_txt(path, plot_type, line_type, model_name, t)
+        y_carnot = np.vectorize(calc_carnot_factor)(T_ref_t, y + 273.15)
+        ax1.plot(x, y_carnot, '-', color=COLOR_TABLE[line_type], label=line_type)
+
+    # build second y-axis (Temperature)
+    ax2 = ax1.twinx()
+    ax2.set(ylim=calc_T_from_carnot(T_ref[t-1],Y_CARNOT_RANGE))
+    ax2.set_ylabel(ylabel='Temperature [C]' , fontsize=24, fontname = 'Times New Roman', fontweight='normal')
+
+    # save the figure
+    set_plot_parameters(ax1, PLOT_SPECS[plot_type])
+    ax1.set_title('t = ' + str(t), fontdict={'fontsize': 16, 'fontweight': 'medium'}, pad=20)
+    fig1 = plt.gcf()
+    os.chdir("..\\")
+    print('saving fig to...', os.path.abspath(os.curdir))
+    fig1.tight_layout()
+    fig1.savefig(plot_type + '_loc2_to_loc3_t' + str(t) + '_DefaultHeatCascade.png', transparent=True)
+    return
+
+
+
+
+def main():
+    plot_type = 'icc'
+    base_folder_path = 'E:\\OSMOSE_projects\\HCS_mk\\results\\HCS_base_locs'
+    run_folder_path = 'run_010_HOT_B005_1_168'
+
+    folder_layers = [base_folder_path, run_folder_path,'s_001\\plots\\' + plot_type, 'locations']
+    path_to_folder = os.path.join('', *folder_layers)
+
+    # load temperatures
+    T_ref = calc_T_ref(os.path.join('', *['C:\\Users\\Shanshan\\Documents\\WP1_results\\WP1_results_1130\\',
+                                          'HCS_base', 'run_008_HOT_B005_1_168']))
+
+    # plotting
+    # for t in np.arange(1,168,4):
+    for t in [89, 93, 97, 101, 105]:
+        plot_loc2_to_loc3(path_to_folder, t, plot_type, T_ref)
+
+
+
+if __name__ == '__main__':
+    main()
