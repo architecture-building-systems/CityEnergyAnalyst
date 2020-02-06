0ce41a4acaa186d33b4c7a56ca9e75e637341cb6
Reynold Mok <reyold@hotmail.com>
2020-02-06 14:41:45 +0800

    Only run data-initializer if databases-path is not 'create'

--- cea/interfaces/dashboard/api/project.py
+++ cea/interfaces/dashboard/api/project.py
@@ -199,11 +199,12 @@ class Scenarios(Resource):
                         return {'message': '{}_helper: {}'.format(tool, e.message), 'trace': trace}, 500
 
         # Run database_initializer to copy databases to input
-        try:
-            cea.api.data_initializer(config, scenario=new_scenario_path, databases_path=payload['databases-path'])
-        except Exception as e:
-            trace = traceback.format_exc()
-            return {'message': 'database_initializer: {}'.format(e.message), 'trace': trace}, 500
+        if payload['databases-path'] != 'create':
+            try:
+                cea.api.data_initializer(config, scenario=new_scenario_path, databases_path=payload['databases-path'])
+            except Exception as e:
+                trace = traceback.format_exc()
+                return {'message': 'data_initializer: {}'.format(e.message), 'trace': trace}, 500
 
         return {'scenarios': list_scenario_names_for_project(config)}
 
