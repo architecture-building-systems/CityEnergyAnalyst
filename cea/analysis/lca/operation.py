b04b6cf686cc12697a337d0c5c1ad0e1eec3d19a
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-02-07 18:38:01 +0800

    Update operation.py

--- cea/analysis/lca/operation.py
+++ cea/analysis/lca/operation.py
@@ -64,13 +64,13 @@ def lca_operation(locator):
     ## demand and the primary energy and emissions factors for each corresponding type of supply system
 
     heating_factors = factors_heating.merge(factors_resources, left_on='feedstock', right_on='code')[
-        ['code_x', 'feedstock', 'PEN', 'CO2']]
+        ['code_x', 'feedstock','CO2']]
     cooling_factors = factors_cooling.merge(factors_resources, left_on='feedstock', right_on='code')[
-        ['code_x', 'feedstock', 'PEN', 'CO2']]
+        ['code_x', 'feedstock', 'CO2']]
     dhw_factors = factors_dhw.merge(factors_resources, left_on='feedstock', right_on='code')[
-        ['code_x', 'feedstock', 'PEN', 'CO2']]
+        ['code_x', 'feedstock', 'CO2']]
     electricity_factors = factors_electricity.merge(factors_resources, left_on='feedstock', right_on='code')[
-        ['code_x', 'feedstock', 'PEN', 'CO2']]
+        ['code_x', 'feedstock', 'CO2']]
 
     heating = supply_systems.merge(demand, on='Name').merge(heating_factors, left_on='type_hs', right_on='code_x')
     dhw = supply_systems.merge(demand, on='Name').merge(dhw_factors, left_on='type_dhw', right_on='code_x')
@@ -86,11 +86,7 @@ def lca_operation(locator):
                         (Qhs_flag, 'OIL_hs_MWhyr', 'OIL_hs', 'Af_m2'),
                         (Qhs_flag, 'WOOD_hs_MWhyr', 'WOOD_hs', 'Af_m2')]
     for x in heating_services:
-        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2', x[2] + '_nre_pen_GJ',
-                          x[2] + '_nre_pen_MJm2']
-        # calculate the total (GJ) and specific (MJ/m2) operational non-renewable primary energy demand (O_nre_pen_)
-        heating[fields_to_plot[4]] = heating[x[1]] * heating['PEN'] * 3.6
-        heating[fields_to_plot[5]] = (heating[x[1]] * heating['PEN'] * 3600) / heating['GFA_m2']
+        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2']
         # calculate the total (t CO2-eq) and specific (kg CO2-eq/m2) operational greenhouse gas emissions (O_ghg_)
         heating[fields_to_plot[2]] = heating[x[1]] * heating['CO2'] * 3.6
         heating[fields_to_plot[3]] = (heating[x[1]] * heating['CO2'] * 3600) / heating['GFA_m2']
@@ -104,11 +100,7 @@ def lca_operation(locator):
                     (Qww_flag, 'WOOD_ww_MWhyr', 'WOOD_ww')]
 
     for x in dhw_services:
-        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2', x[2] + '_nre_pen_GJ',
-                          x[2] + '_nre_pen_MJm2']
-        # calculate the total (GJ) and specific (MJ/m2) operational non-renewable primary energy demand (O_nre_pen_)
-        dhw[fields_to_plot[4]] = dhw[x[1]] * dhw['PEN'] * 3.6
-        dhw[fields_to_plot[5]] = (dhw[x[1]] * dhw['PEN'] * 3600) / dhw['GFA_m2']
+        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2']
         # calculate the total (t CO2-eq) and specific (kg CO2-eq/m2) operational greenhouse gas emissions (O_ghg_)
         dhw[fields_to_plot[2]] = dhw[x[1]] * dhw['CO2'] * 3.6
         dhw[fields_to_plot[3]] = (dhw[x[1]] * dhw['CO2'] * 3600) / dhw['GFA_m2']
@@ -118,11 +110,7 @@ def lca_operation(locator):
                         (Qcs_flag, 'DC_cdata_MWhyr', 'DC_cdata'),
                         (Qcs_flag, 'DC_cre_MWhyr', 'DC_cre')]
     for x in cooling_services:
-        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2', x[2] + '_nre_pen_GJ',
-                          x[2] + '_nre_pen_MJm2']
-        # calculate the total (GJ) and specific (MJ/m2) operational non-renewable primary energy demand (O_nre_pen_)
-        cooling[fields_to_plot[4]] = cooling[x[1]] * cooling['PEN'] * 3.6
-        cooling[fields_to_plot[5]] = (cooling[x[1]] * cooling['PEN'] * 3600) / cooling['GFA_m2']
+        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2']
         # calculate the total (t CO2-eq) and specific (kg CO2-eq/m2) operational greenhouse gas emissions (O_ghg_)
         cooling[fields_to_plot[2]] = cooling[x[1]] * cooling['CO2'] * 3.6
         cooling[fields_to_plot[3]] = (cooling[x[1]] * cooling['CO2'] * 3600) / cooling['GFA_m2']
@@ -131,11 +119,7 @@ def lca_operation(locator):
     electrical_services = [(E_flag, 'GRID_MWhyr', 'GRID'),
                            (E_flag, 'PV_MWhyr', 'PV')]
     for x in electrical_services:
-        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2', x[2] + '_nre_pen_GJ',
-                          x[2] + '_nre_pen_MJm2']
-        # calculate the total (GJ) and specific (MJ/m2) operational non-renewable primary energy demand (O_nre_pen_)
-        electricity[fields_to_plot[4]] = electricity[x[1]] * electricity['PEN'] * 3.6
-        electricity[fields_to_plot[5]] = electricity[x[1]] * electricity['PEN'] * 3600 / electricity['GFA_m2']
+        fields_to_plot = ['Name', 'GFA_m2', x[2] + '_ghg_ton', x[2] + '_ghg_kgm2']
         # calculate the total (t CO2-eq) and specific (kg CO2-eq/m2) operational greenhouse gas emissions (O_ghg_)
         electricity[fields_to_plot[2]] = electricity[x[1]] * electricity['CO2'] * 3.6
         electricity[fields_to_plot[3]] = (electricity[x[1]] * electricity['CO2'] * 3600) / electricity['GFA_m2']
@@ -147,22 +131,16 @@ def lca_operation(locator):
 
     # calculate the total operational non-renewable primary energy demand and emissions as a sum of the results for each
     # energy service used in the building
-    result['O_nre_pen_GJ'] = 0.0
     result['O_ghg_kgm2'] = 0.0
-    result['O_nre_pen_MJm2'] = 0.0
     result['O_ghg_ton'] = 0.0
     all_services = electrical_services + cooling_services + heating_services + dhw_services
     for service in all_services:
-        fields_to_plot += [service[2] + '_nre_pen_GJ', service[2] + '_ghg_ton', service[2] + '_nre_pen_MJm2',
-                           service[2] + '_ghg_kgm2']
-        result['O_nre_pen_GJ'] += result[service[2] + '_nre_pen_GJ']
-        result['O_nre_pen_GJ'] += result[service[2] + '_nre_pen_GJ']
+        fields_to_plot += [service[2] + '_ghg_ton', service[2] + '_ghg_kgm2']
         result['O_ghg_ton'] += result[service[2] + '_ghg_ton']
-        result['O_nre_pen_MJm2'] += result[service[2] + '_nre_pen_MJm2']
         result['O_ghg_kgm2'] += result[service[2] + '_ghg_kgm2']
 
     # export the total operational non-renewable energy demand and emissions for each building
-    fields_to_plot += ['Name', 'GFA_m2', 'O_ghg_ton', 'O_ghg_kgm2', 'O_nre_pen_GJ', 'O_nre_pen_MJm2']
+    fields_to_plot += ['Name', 'GFA_m2', 'O_ghg_ton', 'O_ghg_kgm2']
     result[fields_to_plot].to_csv(locator.get_lca_operation(), index=False, float_format='%.2f')
 
 
