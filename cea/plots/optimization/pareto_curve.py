84757d5dd3e0b350717e8c067dd07290a04abcf2
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-02-10 15:15:05 +0800

    Update pareto_curve.py

--- cea/plots/optimization/pareto_curve.py
+++ cea/plots/optimization/pareto_curve.py
@@ -22,7 +22,7 @@ __status__ = "Production"
 
 class ParetoCurveForOneGenerationPlot(cea.plots.optimization.GenerationPlotBase):
     """Show a pareto curve for a single generation"""
-    name = "Pareto curve of costs, emissions and primary energy"
+    name = "Pareto curve"
     expected_parameters = {
         'generation': 'plots-optimization:generation',
         'normalization': 'plots-optimization:normalization',
@@ -35,10 +35,9 @@ class ParetoCurveForOneGenerationPlot(cea.plots.optimization.GenerationPlotBase)
         self.analysis_fields = ['individual_name',
                                 'TAC_sys_USD',
                                 'GHG_sys_tonCO2',
-                                'PEN_sys_MJoil',
                                 'Capex_total_sys_USD',
                                 'Opex_a_sys_USD']
-        self.objectives = ['TAC_sys_USD', 'GHG_sys_tonCO2', 'PEN_sys_MJoil']
+        self.objectives = ['TAC_sys_USD', 'GHG_sys_tonCO2', 'Capex_total_sys_USD']
         self.normalization = self.parameters['normalization']
         self.input_files = [(self.locator.get_optimization_generation_total_performance, [self.generation])]
         self.multi_criteria = self.parameters['multicriteria']
@@ -47,24 +46,24 @@ class ParetoCurveForOneGenerationPlot(cea.plots.optimization.GenerationPlotBase)
     def calc_titles(self):
         if self.normalization == "gross floor area":
             titlex = 'Total annualized costs [USD$(2015)/m2.yr]'
-            titley = 'GHG emissions [ton CO2-eq/m2.yr]'
-            titlez = 'Primary Energy <br>[MJ Oil-eq/m2.yr]'
+            titley = 'GHG emissions [kg CO2-eq/m2.yr]'
+            titlez = 'Investment costs <br>[USD$(2015)/m2]'
         elif self.normalization == "net floor area":
             titlex = 'Total annualized costs [USD$(2015)/m2.yr]'
-            titley = 'GHG emissions [ton CO2-eq/m2.yr]'
-            titlez = 'Primary Energy <br>[MJ Oil-eq/m2.yr]'
+            titley = 'GHG emissions [kg CO2-eq/m2.yr]'
+            titlez = 'Investment costs <br>[USD$(2015)/m2]'
         elif self.normalization == "air conditioned floor area":
             titlex = 'Total annualized costs [USD$(2015)/m2.yr]'
-            titley = 'GHG emissions [ton CO2-eq/m2.yr]'
-            titlez = 'Primary Energy <br>[MJ Oil-eq/m2.yr]'
+            titley = 'GHG emissions [kg CO2-eq/m2.yr]'
+            titlez = 'Investment costs <br>[USD$(2015)/m2.yr]'
         elif self.normalization == "building occupancy":
             titlex = 'Total annualized costs [USD$(2015)/pax.yr]'
-            titley = 'GHG emissions [ton CO2-eq/pax.yr]'
-            titlez = 'Primary Energy <br>[MJ Oil-eq/pax.yr]'
+            titley = 'GHG emissions [kg CO2-eq/pax.yr]'
+            titlez = 'Investment costs <br>[USD$(2015)/pax]'
         else:
             titlex = 'Total annualized costs [USD$(2015)/yr]'
             titley = 'GHG emissions [ton CO2-eq/yr]'
-            titlez = 'Primary Energy <br>[MJ Oil-eq/yr]'
+            titlez = 'Investment costs <br>[USD$(2015)]'
 
         return titlex, titley, titlez
 
@@ -73,7 +72,6 @@ class ParetoCurveForOneGenerationPlot(cea.plots.optimization.GenerationPlotBase)
         return go.Layout(legend=dict(orientation="v", x=0.8, y=0.95),
                          xaxis=dict(title=self.titlex),
                          yaxis=dict(title=self.titley))
-
 
     @property
     def title(self):
@@ -90,15 +88,6 @@ class ParetoCurveForOneGenerationPlot(cea.plots.optimization.GenerationPlotBase)
 
     def calc_graph(self):
         graph = []
-        # #PUT THE HALL OF FAME INSIDE
-        # data_HOF = self.process_generation_total_performance_halloffame()
-        # data_HOF = self.normalize_data(data_HOF, self.normalization, self.objectives)
-        # xs_HOF = data_HOF[self.objectives[0]].values
-        # ys_HOF = data_HOF[self.objectives[1]].values
-        # individual_names = data_HOF['individual_name'].values
-        # trace = go.Scattergl(x=xs_HOF, y=ys_HOF, mode='markers', name='Hall of fame', text=individual_names,
-        #                    marker=dict(size='12', color='grey_light'))
-        # graph.append(trace)
 
         # PUT THE PARETO CURVE INSIDE
         data = self.process_generation_total_performance_pareto()
@@ -129,32 +118,10 @@ class ParetoCurveForOneGenerationPlot(cea.plots.optimization.GenerationPlotBase)
             graph.append(trace)
         return graph
 
-    # def calc_table(self):
-    #     final_dataframe = calc_final_dataframe(self.process_generation_total_performance())
-    #
-    #     # transform data into currency
-    #     for column in final_dataframe.columns:
-    #         if '_USD' in column or '_MJoil' in column or '_tonCO2' in column:
-    #             final_dataframe[column] = final_dataframe[column].apply(lambda x: '{:20,.2f}'.format(x))
-    #
-    #     columns = ["Attribute"] + self.analysis_fields
-    #     values = []
-    #     for field in columns:
-    #         if field in ["Attribute", "individual_name"]:
-    #             values.append(final_dataframe[field].values)
-    #         else:
-    #             values.append(final_dataframe[field].values)
-    #
-    #     columns = ["Attribute"] + [NAMING[field] for field in self.analysis_fields]
-    #     table_df = pd.DataFrame({cn: cv for cn, cv in zip(columns, values)}, columns=columns)
-    #     return table_df
-
-
 def calc_final_dataframe(individual_data):
     least_annualized_cost = individual_data.loc[
         individual_data["TAC_rank"] < 2]  # less than two because in the case there are two individuals MCDA calculates 1.5
     least_emissions = individual_data.loc[individual_data["GHG_rank"] < 2]
-    least_primaryenergy = individual_data.loc[individual_data["PEN_rank"] < 2]
     user_defined_mcda = individual_data.loc[individual_data["user_MCDA_rank"] < 2]
     # do a check in the case more individuals had the same ranking.
     if least_annualized_cost.shape[0] > 1:
@@ -167,21 +134,17 @@ def calc_final_dataframe(individual_data):
         least_emissions = least_emissions.reset_index(drop=True)
         least_emissions = least_emissions.iloc[0].T
         least_emissions["System option"] = individual
-    if least_primaryenergy.shape[0] > 1:
-        individual = str(least_primaryenergy["individual_name"].values)
-        least_primaryenergy = least_primaryenergy.reset_index(drop=True)
-        least_primaryenergy = least_primaryenergy.iloc[0].T
-        least_primaryenergy["System option"] = individual
     if user_defined_mcda.shape[0] > 1:
         individual = str(user_defined_mcda["individual_name"].values)
         user_defined_mcda = user_defined_mcda.reset_index(drop=True)
         user_defined_mcda = user_defined_mcda.iloc[0].T
         user_defined_mcda["System option"] = individual
     # Now extend all dataframes
-    final_dataframe = least_annualized_cost.append(least_emissions).append(least_primaryenergy).append(
+    final_dataframe = least_annualized_cost.append(least_emissions).append(
         user_defined_mcda)
     final_dataframe.reset_index(drop=True, inplace=True)
-    final_dataframe["Attribute"] = ["least annualized costs", "least emissions", "least primary energy",
+    final_dataframe["Attribute"] = ["least annualized costs",
+                                    "least emissions",
                                     "user defined MCDA"]
     return final_dataframe
 
