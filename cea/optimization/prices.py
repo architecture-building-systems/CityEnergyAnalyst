9bc0920ecafe22828a52fa0b6a70374c656cb4a4
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-02-11 19:26:29 +0800

    Update prices.py

--- cea/optimization/prices.py
+++ cea/optimization/prices.py
@@ -4,6 +4,7 @@ This file imports the price details from the cost database as a class. This help
 of the corresponding values in individual files.
 """
 from __future__ import division
+import numpy as np
 
 __author__ = "Jimeno A. Fonseca"
 __copyright__ = "Copyright 2019, Architecture and Building Systems - ETH Zurich"
@@ -18,19 +19,19 @@ __status__ = "Production"
 class Prices(object):
     def __init__(self, supply_systems):
         pricing = supply_systems.FEEDSTOCKS
-        self.NG_PRICE = list(
-            pricing['NATURALGAS']['Opex_var_buy_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
-        self.BG_PRICE = list(
-            pricing['BIOGAS']['Opex_var_buy_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
-        self.WB_PRICE = list(
-            pricing['WETBIOMASS']['Opex_var_buy_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
-        self.DB_PRICE = list(
-            pricing['DRYBIOMASS']['Opex_var_buy_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
-        self.SOLAR_PRICE = list(
-            pricing['SOLAR']['Opex_var_buy_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
-        self.SOLAR_PRICE_EXPORT = list(
-            pricing['SOLAR']['Opex_var_sell_USD2015perkWh']) * 365  # in USD/Wh for every hour of a year
-        self.ELEC_PRICE = list(
-            pricing['GRID']['Opex_var_buy_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
-        self.ELEC_PRICE_EXPORT = list(
-            pricing['GRID']['Opex_var_sell_USD2015perkWh'] / 1000) * 365  # in USD/Wh for every hour of a year
+        self.NG_PRICE = np.tile(
+            pricing['NATURALGAS']['Opex_var_buy_USD2015perkWh'].values / 1000,  365)  # in USD/Wh for every hour of a year
+        self.BG_PRICE = np.tile(
+            pricing['BIOGAS']['Opex_var_buy_USD2015perkWh'].values / 1000,  365)   # in USD/Wh for every hour of a year
+        self.WB_PRICE = np.tile(
+            pricing['WETBIOMASS']['Opex_var_buy_USD2015perkWh'].values / 1000,  365)   # in USD/Wh for every hour of a year
+        self.DB_PRICE = np.tile(
+            pricing['DRYBIOMASS']['Opex_var_buy_USD2015perkWh'].values / 1000,  365)   # in USD/Wh for every hour of a year
+        self.SOLAR_PRICE = np.tile(
+            pricing['SOLAR']['Opex_var_buy_USD2015perkWh'].values / 1000,  365)  # in USD/Wh for every hour of a year
+        self.SOLAR_PRICE_EXPORT = np.tile(
+            pricing['SOLAR']['Opex_var_sell_USD2015perkWh'].values / 1000,  365)   # in USD/Wh for every hour of a year
+        self.ELEC_PRICE = np.tile(
+            pricing['GRID']['Opex_var_buy_USD2015perkWh'].values / 1000,  365)   # in USD/Wh for every hour of a year
+        self.ELEC_PRICE_EXPORT = np.tile(
+            pricing['GRID']['Opex_var_sell_USD2015perkWh'].values / 1000,  365)  # in USD/Wh for every hour of a year
