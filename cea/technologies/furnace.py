47132ce889d4d61cb426a246ffbf3bab46f901a0
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-02-11 18:59:30 +0800

    Update furnace.py

--- cea/technologies/furnace.py
+++ cea/technologies/furnace.py
@@ -191,7 +191,7 @@ def calc_Cinv_furnace(Q_design_W, locator, technology_type):
     :returns InvCa: annualized investment costs in [CHF] including O&M
         
     """
-    furnace_cost_data = pd.read_excel(locator.get_database_supply_systems(), sheet_name="Furnace")
+    furnace_cost_data = pd.read_excel(locator.get_database_conversion_systems(), sheet_name="Furnace")
     furnace_cost_data = furnace_cost_data[furnace_cost_data['code'] == technology_type]
     # if the Q_design is below the lowest capacity available for the technology, then it is replaced by the least
     # capacity for the corresponding technology from the database
