a0546e1ebb46813df1380fdab60ed8df2599d772
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-02-14 10:11:02 +0800

    Update dispatch_curve_cooling_plant.py

--- cea/plots/supply_system/dispatch_curve_cooling_plant.py
+++ cea/plots/supply_system/dispatch_curve_cooling_plant.py
@@ -47,14 +47,12 @@ class DispatchCurveDistrictCoolingPlot(cea.plots.supply_system.SupplySystemPlotB
 
     @property
     def title(self):
-        return "Dispatch curve for cooling plant in system #%s (%s)" % (self.individual, self.timeframe)
+        return "Dispatch curve for cooling plant in system %s (%s)" % (self.system, self.timeframe)
 
     @property
     def output_path(self):
         return self.locator.get_timeseries_plots_file(
-            'gen{generation}_ind{individual}dispatch_curve_cooling_plant'.format(individual=self.individual,
-                                                                                 generation=self.generation),
-            self.category_name)
+            '{system}_dispatch_curve_cooling_plant'.format(system=self.system), self.category_name)
 
     @property
     def layout(self):
