e4a1fb1da8349afdd189dcf4291c597eb289408f
Daren Thomas <thomas@arch.ethz.ch>
2020-03-04 16:59:13 +0100

    created database-migrator script

--- cea/utilities/dbf.py
+++ cea/utilities/dbf.py
@@ -28,6 +28,7 @@ __status__ = "Production"
 
 TYPE_MAPPING = {
     int: ('N', 20, 0),
+    long: ('N', 20, 0),
     np.int64: ('N', 20, 0),
     float: ('N', 36, 15),
     np.float64: ('N', 36, 15),
