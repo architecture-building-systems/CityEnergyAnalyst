d4eb8e079face6a3e0bf3ae7320815df63c9c817
Daren Thomas <thomas@arch.ethz.ch>
2020-03-24 13:42:46 +0100

    replaced read_glossary_df with schemas.yml equivalent output

--- cea/glossary.py
+++ cea/glossary.py
@@ -6,6 +6,7 @@ from __future__ import division
 
 import os
 import csv
+import cea.scripts
 import pandas as pd
 
 __author__ = "Daren Thomas"
@@ -19,12 +20,44 @@ __status__ = "Production"
 
 
 def read_glossary_df():
-    """Returns the glossary as a DataFrame"""
-    glossary_df = pd.read_csv(_path_to_glossary_csv(), sep=',')
+    """Returns the glossary as a DataFrame, created from the schemas.yml file."""
+    schemas = cea.scripts.schemas()
+    glossary_df = pd.DataFrame(columns=["SCRIPT", "LOCATOR_METHOD", "WORKSHEET", "VARIABLE",
+                                        "DESCRIPTION", "UNIT", "VALUES", "TYPE", "COLOR", "FILE_NAME"])
+    rows = []
+    for lm in schemas:
+        script = schemas[lm]["created_by"][0] if schemas[lm]["created_by"] else "-"
+        file_path = schemas[lm]["file_path"]
+        if schemas[lm]["file_type"] in {"xls", "xlsx"}:
+            for ws in schemas[lm]["schema"]:  # ws: worksheet
+                for col in schemas[lm]["schema"][ws]["columns"]:
+                    cd = schemas[lm]["schema"][ws]["columns"][col]
+                    rows.append(glossary_row(script, file_path, col, lm, cd, worksheet=ws))
+        else:
+            for col in schemas[lm]["schema"]["columns"]:
+                cd = schemas[lm]["schema"]["columns"][col]  # cd: column definition
+                rows.append(glossary_row(script, file_path, col, lm, cd, worksheet=""))
+
+    glossary_df = glossary_df.append(rows, ignore_index=True)
     glossary_df['key'] = glossary_df['FILE_NAME'] + '!!!' + glossary_df['VARIABLE']
     glossary_df = glossary_df.set_index(['key'])
     glossary_df = glossary_df.sort_values(by=['LOCATOR_METHOD', 'FILE_NAME', 'VARIABLE'])
     return glossary_df
+
+
+def glossary_row(script, file_path, col, lm, cd, worksheet):
+    return {
+        "SCRIPT": script,
+        "LOCATOR_METHOD": lm,
+        "WORKSHEET": worksheet,
+        "VARIABLE": col,
+        "DESCRIPTION": cd["description"],
+        "UNIT": cd["unit"],
+        "VALUES": cd["values"],
+        "TYPE": cd["type"],
+        "COLOR": "",
+        "FILE_NAME": ":".join((file_path, worksheet)) if worksheet else file_path
+    }
 
 
 def read_glossary_dicts():
@@ -37,3 +70,7 @@ def read_glossary_dicts():
 
 def _path_to_glossary_csv():
     return os.path.join(os.path.dirname(__file__), 'glossary.csv')
+
+
+if __name__ == "__main__":
+    print(read_glossary_df())
--- cea/tests/test_schemas.py
+++ cea/tests/test_schemas.py
@@ -25,8 +25,31 @@ class TestSchemas(unittest.TestCase):
         config = cea.config.Configuration()
         locator = cea.inputlocator.InputLocator(config.scenario)
 
-        for method in self.extract_locator_methods(locator):
+        for method in extract_locator_methods(locator):
             self.assertIn(method, schemas.keys())
+
+    def test_all_columns_have_description(self):
+        schemas = cea.scripts.schemas()
+        for lm in schemas:
+            if schemas[lm]["file_type"] in {"xls", "xlsx"}:
+                for ws in schemas[lm]["schema"]:
+                    for col in schemas[lm]["schema"][ws]["columns"]:
+                        self.assertIn("description", schemas[lm]["schema"][ws]["columns"][col],
+                                      "Missing description for {lm}/{ws}/{col}".format(lm=lm, ws=ws, col=col))
+            else:
+                for col in schemas[lm]["schema"]["columns"]:
+                    self.assertIn("description", schemas[lm]["schema"]["columns"][col],
+                                  "Missing description for {lm}/{col}".format(lm=lm, col=col))
+
+    def test_all_schemas_have_a_columns_entry(self):
+        schemas = cea.scripts.schemas()
+        for lm in schemas:
+            if schemas[lm]["file_type"] in {"xls", "xlsx"}:
+                for ws in schemas[lm]["schema"]:
+                    self.assertIn("columns", schemas[lm]["schema"][ws], "Missing columns for {lm}/{ws}".format(
+                        lm=lm, ws=ws))
+            else:
+                self.assertIn("columns", schemas[lm]["schema"], "Missing columns for {lm}".format(lm=lm))
 
     def fixme_test_all_schema_columns_documented(self):
         schemas = cea.scripts.schemas()
@@ -71,36 +94,37 @@ class TestSchemas(unittest.TestCase):
             self.assertIsInstance(schemas[lm]["used_by"], list,
                                   "used_by entry of {lm} must be a list".format(lm=lm))
 
-    def extract_locator_methods(self, locator):
-        """Return the list of locator methods that point to files"""
-        ignore = {
-            "ensure_parent_folder_exists",
-            "get_plant_nodes",
-            "get_temporary_file",
-            "get_weather_names",
-            "get_zone_building_names",
-            "verify_database_template",
-            "get_optimization_network_all_individuals_results_file",  # TODO: remove this when we know how
-            "get_optimization_network_generation_individuals_results_file",  # TODO: remove this when we know how
-            "get_optimization_network_individual_results_file",  # TODO: remove this when we know how
-            "get_optimization_network_layout_costs_file",  # TODO: remove this when we know how
-            "get_predefined_hourly_setpoints",  # TODO: remove this when we know how
-            "get_timeseries_plots_file",  # TODO: remove this when we know how
-        }
-        for m in dir(locator):
-            if not callable(getattr(locator, m)):
-                # normal attributes (fields) are not locator methods
-                continue
-            if m.startswith("_"):
-                # these are private methods, ignore
-                continue
-            if m in ignore:
-                # keep a list of special methods to ignore
-                continue
-            if m.endswith("_folder"):
-                # not interested in folders
-                continue
-            yield m
+
+def extract_locator_methods(locator):
+    """Return the list of locator methods that point to files"""
+    ignore = {
+        "ensure_parent_folder_exists",
+        "get_plant_nodes",
+        "get_temporary_file",
+        "get_weather_names",
+        "get_zone_building_names",
+        "verify_database_template",
+        "get_optimization_network_all_individuals_results_file",  # TODO: remove this when we know how
+        "get_optimization_network_generation_individuals_results_file",  # TODO: remove this when we know how
+        "get_optimization_network_individual_results_file",  # TODO: remove this when we know how
+        "get_optimization_network_layout_costs_file",  # TODO: remove this when we know how
+        "get_predefined_hourly_setpoints",  # TODO: remove this when we know how
+        "get_timeseries_plots_file",  # TODO: remove this when we know how
+    }
+    for m in dir(locator):
+        if not callable(getattr(locator, m)):
+            # normal attributes (fields) are not locator methods
+            continue
+        if m.startswith("_"):
+            # these are private methods, ignore
+            continue
+        if m in ignore:
+            # keep a list of special methods to ignore
+            continue
+        if m.endswith("_folder"):
+            # not interested in folders
+            continue
+        yield m
 
 
 if __name__ == '__main__':
