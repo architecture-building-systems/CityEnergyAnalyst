8e7783efe6cf2231b78488b93e8eeb1541ecfec0
Luis <luis.santos@tum-create.edu.sg>
2020-04-02 12:15:45 +0800

    debugged validation

--- cea/examples/validation.py
+++ cea/examples/validation.py
@@ -97,27 +97,12 @@ def validation(scenario_list,
                 monthly_data = monthly_modelled_data.merge(monthly_measured_demand, on='Month')
 
                 # calculate errors
-                # monthly_data.rename({'GRID_kWh': 'GRID_MWh'}, inplace=True)
-                biased_error = monthly_data['measurements'] - monthly_data[load + '_kWh']
-                normalized_mean_biased_error = ((biased_error.sum() / 12) / monthly_data[
-                    'measurements'].mean()) * 100  # %
-                print(normalized_mean_biased_error)
+                cv_root_mean_squared_error, normalized_mean_biased_error = calc_errors_per_building(load, monthly_data)
 
-                mean_squared_error = calc_mean_squared_error(monthly_data['measurements'], monthly_data[load + '_kWh'])
-                root_mean_squared_error = sqrt(mean_squared_error)  # root mean squared error
-                cv_root_mean_squared_error = root_mean_squared_error * 100 / monthly_data['measurements'].mean()
-                print(cv_root_mean_squared_error)
+                ind_calib_building, ind_score_building = calc_group_score(cv_root_mean_squared_error, monthly_data,
+                                                                          normalized_mean_biased_error)
 
-                # indicates if the building is calibrated or not
-                if abs(normalized_mean_biased_error) < 5 and cv_root_mean_squared_error < 15:
-                    ind_calib_building = 1
-                else:
-                    ind_calib_building = 0
-
-                # weights the calibration by building energy consumption
-                ind_score_building = ind_calib_building * sum(monthly_data['measurements'])
-
-
+                #appending list of variables for later use
                 number_of_calibrated.append(ind_calib_building)
                 list_of_scores.append(ind_score_building)
         n_calib = sum(number_of_calibrated)
@@ -126,6 +111,34 @@ def validation(scenario_list,
     print(score)
     return score
 
+
+def calc_errors_per_building(load, monthly_data):
+    biased_error = monthly_data['measurements'] - monthly_data[load + '_kWh']
+    normalized_mean_biased_error = ((biased_error.sum() / 12) / monthly_data[
+        'measurements'].mean()) * 100  # %
+    print(normalized_mean_biased_error)
+    mean_squared_error = calc_mean_squared_error(monthly_data['measurements'], monthly_data[load + '_kWh'])
+    root_mean_squared_error = sqrt(mean_squared_error)  # root mean squared error
+    cv_root_mean_squared_error = root_mean_squared_error * 100 / monthly_data['measurements'].mean()
+    print(cv_root_mean_squared_error)
+    return cv_root_mean_squared_error, normalized_mean_biased_error
+
+
+def calc_group_score(cv_root_mean_squared_error, monthly_data, normalized_mean_biased_error):
+    # indicates if the building is calibrated or not
+    if abs(normalized_mean_biased_error) < 5 and cv_root_mean_squared_error < 15:
+        ind_calib_building = 1
+    else:
+        ind_calib_building = 0
+    # weights the calibration by building energy consumption
+    ind_score_building = ind_calib_building * sum(monthly_data['measurements'])
+    return ind_calib_building, ind_score_building
+
+def get_measured_building_names(locator):
+    monthly_measured_data = pd.read_csv(locator.get_monthly_measurements())
+    measured_building_names = monthly_measured_data.Name.values
+    measured_building_names = list(measured_building_names)
+    return measured_building_names
 
 def main(config):
     """
@@ -140,9 +153,18 @@ def main(config):
     """
     assert os.path.exists(config.scenario), 'Scenario not found: %s' % config.scenario
     locator = cea.inputlocator.InputLocator(config.scenario)
+    measured_building_names = get_measured_building_names(locator)
+    scenario_list = [config.scenario]
+    locators_of_scenarios = [locator]
+    measured_building_names_of_scenarios =[measured_building_names]
 
-    validation(locator)
-
+    validation(scenario_list,
+               locators_of_scenarios,
+               measured_building_names_of_scenarios,
+               annual=False,
+               monthly=True,
+               load='GRID',
+               )
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
