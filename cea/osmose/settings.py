8f04973f9db1818a47e09ee11314d8b080ded48e
shanshanhsieh <hsieh@arch.ethz.ch>
2020-04-10 18:24:32 +0800

    Update settings.py

--- cea/osmose/settings.py
+++ cea/osmose/settings.py
@@ -5,7 +5,8 @@
 T_b_CDD = 25.0
 # TECHS = ['HCS_LD', 'HCS_coil', 'HCS_ER0', 'HCS_3for2', 'HCS_IEHX']
 # TECHS = ['HCS_base_LD', 'HCS_base_coil', 'HCS_base_3for2', 'HCS_base_ER0', 'HCS_base_IEHX',  'HCS_base']
-TECHS = ['HCS_base_LD', 'HCS_base_IEHX', 'HCS_base_ER0']
+# TECHS = ['HCS_base_LD', 'HCS_base_IEHX', 'HCS_base_ER0']
+TECHS = ['N_3for2'] # ONLY one tech if running moga via wp3
 # timesteps = [5136, 5144, 5145, 5147, 5148]  # 168 (week) [5389]
 # timesteps = [5145]  # 168 (week) [5389]
 timesteps = "typical hours"  # 168 (week) [5389]
