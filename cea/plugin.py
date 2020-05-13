3d28e6007203c570d594071981e29691d9b01174
Daren Thomas <thomas@arch.ethz.ch>
2020-05-13 13:49:23 +0200

    progress on the plugin plots...

--- cea/plugin.py
+++ cea/plugin.py
@@ -5,10 +5,13 @@ from __future__ import print_function
 from __future__ import division
 
 import os
+from typing import Generator
 import yaml
 import inspect
+import cea.plots.categories
+import cea.inputlocator
 from cea.utilities.yaml_ordered_dict import OrderedDictYAMLLoader
-
+from cea.utilities import identifier
 
 __author__ = "Daren Thomas"
 __copyright__ = "Copyright 2020, Architecture and Building Systems - ETH Zurich"
@@ -18,9 +21,6 @@ __version__ = "0.1"
 __maintainer__ = "Daren Thomas"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
-
-
-### NOTE / FIXME: have this class read in the scripts.yml and schemas.yml. plots need to be python classes.
 
 
 class CeaPlugin(object):
@@ -39,8 +39,17 @@ class CeaPlugin(object):
 
     @property
     def plots(self):
-        """Return a list of plots - each is a subclass of :py:class:`cea.plots.base`"""
-        return []
+        """
+        Return a list of :py:class`cea.plots.PlotCategory` instances to add to the GUI. The default implementation
+        uses the ``plots.yml`` file to create PluginPlotCategory instances that use PluginPlotBase to provide a
+        simplified plot mechanism using cufflinks_
+
+        .. _cufflinks: https://plotly.com/python/cufflinks/
+        """
+        plots_yml = os.path.join(os.path.dirname(inspect.getmodule(self).__file__), "plots.yml")
+        with open(plots_yml, "r") as plots_yml_fp:
+            plots = yaml.load(plots_yml_fp, OrderedDictYAMLLoader)
+        return [PluginPlotCategory(category_label, plots[category_label], self) for category_label in plots.keys()]
 
     @property
     def schemas(self):
@@ -57,3 +66,108 @@ class CeaPlugin(object):
     def __str__(self):
         """To enable encoding in cea.config.PluginListParameter, return the fqname of the class"""
         return "{module}.{name}".format(module=self.__class__.__module__, name=self.__class__.__name__)
+
+
+class PluginPlotCategory(cea.plots.categories.PlotCategory):
+    """
+    Normally, a PlotCategory reads it's plot classes by traversing a folder structure and importing all modules found
+    there. The PluginPlotCategory works just like a PlotCategory (i.e. compatible with the CEA GUI / Dashboard) but
+    the category information and plots are loaded from a ``plots.yml`` file. Plugin Plots are a bit restricted (so
+    you might want to implement your plots directly the way they are implemented in CEA) but instead they are much
+    easier to understand as they use the cufflinks library.
+    """
+
+    def __init__(self, category_label, plots_dict, plugin):
+        """Ignore calling super class' constructor as we use a totally different mechanism for building plots here
+        :param str category_label: The category label shown in the interface
+        :param dict plots_dict: A dictionary mapping plot labels to plot definitions
+        """
+        self.label = category_label
+        self.name = identifier(category_label)
+        self.plots_dict = plots_dict
+        self.plugin = plugin
+
+    @property
+    def plots(self):
+        """
+        Return a list of Plot classes to be used in the Dashboard.
+
+        :rtype: Generator[PluginPlotBase]
+        """
+        for plot_label, plot_config in self.plots_dict.items():
+            plugin = self.plugin
+            class Plot(PluginPlotBase):
+                name = plot_label
+                category_name = self.name
+                category_path = self.name
+                expected_parameters = plot_config.get("expected-parameters", {})
+
+                def __init__(self, project, parameters, cache):
+                    super(Plot, self).__init__(project, parameters, cache, plugin, plot_config)
+
+            Plot.__name__ = identifier(plot_label, sep="_")
+            yield Plot
+
+
+class PluginPlotBase(cea.plots.PlotBase):
+    """
+    A simplified version of cea.plots.PlotBase that is configured with the ``plots.yml`` entries.
+    """
+    def __init__(self, project, parameters, cache, plugin, plot_config):
+        super(PluginPlotBase, self).__init__(project, parameters, cache)
+        self.plugin = plugin
+        self.plot_config = plot_config
+        self.locator_method = getattr(self.locator, self.plot_config["data"]["location"])
+        self.locator_kwargs = {arg: self.parameters[arg] for arg in self.plot_config["data"].get("args", [])}
+        self.input_files = [(self.locator_method, self.locator_kwargs)]
+
+    def missing_input_files(self):
+        """
+        Return the list of missing input files for this plot - overriding cea.plots.PlotBase.missing_input_files
+        because we're now moving to kwargs for locator methods.
+
+        Also, PluginPlotBase only uses one input file.
+        """
+        result = []
+        if not os.path.exists(self.locator_method(**self.locator_kwargs)):
+            result.append((self.locator_method, self.locator_kwargs.values()))
+        return result
+
+    @property
+    def locator(self):
+        """
+        Make sure the plot's input-locator is aware of the plugin that defines it.
+
+        NOTE: We don't currently support depending on other plugins.
+
+        :rtype: cea.inputlocator.InputLocator
+        """
+        scenario = os.path.join(self.project, self.parameters['scenario-name'])
+        return cea.inputlocator.InputLocator(scenario=scenario, plugins=[self.plugin])
+
+    @property
+    def layout(self):
+        """The layout for plugin plots needs to conform to the input parameters to iplot (see cufflinks docs)"""
+        return self.plot_config.get("layout", {})
+
+    def _plot_div_producer(self):
+        """Use the plot_config to create a plot with cufflinks"""
+        import cufflinks
+        import plotly.offline
+
+        cufflinks.go_offline()
+
+        df = self.locator_method.read(**self.locator_kwargs)
+        fig = df.iplot(asFigure=True, **self.layout)
+        div = plotly.offline.plot(fig, output_type='div', include_plotlyjs=False, show_link=False)
+        return div
+
+    def table_div(self):
+        pass
+
+    def calc_graph(self):
+        raise AssertionError("cea.plots.PlotBase.calc_graph should not be part of the abstract interface")
+
+    def calc_table(self):
+        raise DeprecationWarning("cea.plots.PlotBase.calc_table is not used anymore and will be removed in future")
+
--- cea/utilities/__init__.py
+++ cea/utilities/__init__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import string
 
 
 def remap(x, in_min, in_max, out_min, out_max):
@@ -8,6 +9,17 @@ def remap(x, in_min, in_max, out_min, out_max):
     Based on this StackOverflow answer: https://stackoverflow.com/a/43567380/2260
     """
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def identifier(s, sep="-"):
+    """
+    First, all characters are lowercased, then, any character that is not in ascii_lowercase is replaced with ``sep``.
+
+    :param str s: the string to create an identifier of
+    :param str use_underscores: if set to true, underscores ("_") will be used instead of dashes ("-")
+    :rtype: str
+    """
+    return "".join(c if c in string.ascii_lowercase else "-" for c in s.lower())
 
 
 def simple_memoize(obj):
