1d87265a75c9b0ef1cbba722789cbeafc52b2dfa
Daren Thomas <thomas@arch.ethz.ch>
2020-06-24 15:34:41 +0200

    Update ceatestall.bat

--- bin/ceatestall.bat
+++ bin/ceatestall.bat
@@ -1,11 +1,9 @@
 REM script used to test the cea by the jenkins when merging to master
 SET CEA=C:\ProgramData\ceajenkins\ceatestall
-SET PATH=%CEA%\Python;%CEA%\Python\Scripts;%CEA%\Daysim;%PATH%
+CALL %CEA%\Python\Scripts\activate.bat
 SET PYTHONHOME=%CEA%\Python
-SET GDAL_DATA=%CEA%\Python\Library\share\gdal
-SET PROJ_LIB=%CEA%\Python\Library\share
 SET RAYPATH=%CEA%\Daysim
-"%CEA%\Python\python.exe" -u -m pip install -e .
+"%CEA%\Python\python.exe" -u -m pip install --no-deps -e .
 
 "%CEA%\Python\python.exe" -u -m cea.interfaces.cli.cli test --workflow slow
 
