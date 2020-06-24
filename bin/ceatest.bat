510f9dc4c3c3000575ad1036080e0f9f90ab9243
Daren Thomas <thomas@arch.ethz.ch>
2020-06-24 16:02:09 +0200

    remove *.pyc files

--- bin/ceatest.bat
+++ bin/ceatest.bat
@@ -5,6 +5,11 @@ SET PYTHONHOME=%CEA%\Python
 SET RAYPATH=%CEA%\Daysim
 "%CEA%\Python\python.exe" -u -m pip install --no-deps -e .
 
+REM: remove *.pyc files
+PUSHD cea
+DEL /S /Q *.pyc
+POPD
+
 "%CEA%\Python\python.exe" -u -m cea.interfaces.cli.cli test --workflow quick
 
 if %errorlevel% neq 0 exit /b %errorlevel%
