c2d8ba4c48bce43828e2fd2a05263caeb43232b4
Daren Thomas <thomas@arch.ethz.ch>
2020-07-06 17:13:31 +0200

    ResourceWarning: unclosed file

--- cea/scripts.py
+++ cea/scripts.py
@@ -96,7 +96,8 @@ def list_scripts(plugins):
     """List all scripts in scripts.yml and those defined in configured plugins
     :parameter List[CeaPlugin] plugins: the list of plugins to include in the search for scripts.
     """
-    scripts_by_category = yaml.load(open(SCRIPTS_YML), OrderedDictYAMLLoader)
+    with open(SCRIPTS_YML, "r") as fp:
+        scripts_by_category = yaml.load(fp, OrderedDictYAMLLoader)
     for plugin in plugins:
         scripts_by_category.update(plugin.scripts)
 
