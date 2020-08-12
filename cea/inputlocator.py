4d7e69e96abb354d6d0bc5807a9b9b71645a2b7d
martin-mosteiro <mosteiro@arch.ethz.ch>
2020-08-12 19:34:03 +0200

    add new inputlocator

--- cea/inputlocator.py
+++ cea/inputlocator.py
@@ -606,6 +606,11 @@ class InputLocator(object):
     def get_microclimate_folder(self):
         return self._ensure_folder(self.scenario, 'inputs', 'microclimate-data')
 
+    def get_microclimate_file(self, building_name):
+        file_path = os.path.join(self.get_microclimate_folder(), building_name + '.csv')
+        self.check_cpg(file_path)
+        return file_path
+
     def get_zone_geometry(self):
         """scenario/inputs/building-geometry/zone.shp"""
         shapefile_path =  os.path.join(self.get_building_geometry_folder(), 'zone.shp')
