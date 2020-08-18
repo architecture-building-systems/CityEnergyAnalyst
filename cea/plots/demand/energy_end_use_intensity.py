9cfc9781822f25100ee1105a26e13348225e7263
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-08-18 16:14:03 +0800

    adding percentages to graphs

--- cea/plots/demand/energy_end_use_intensity.py
+++ cea/plots/demand/energy_end_use_intensity.py
@@ -58,7 +58,9 @@ class EnergyUseIntensityPlot(cea.plots.demand.DemandPlotBase):
             for field in analysis_fields:
                 y = dataframe[field]
                 name = NAMING[field]
-                trace = go.Bar(x=dataframe["Name"], y=y, name=name, marker=dict(color=COLOR[field]))
+                total_percent = (y / dataframe['total'] * 100).round(2).values
+                total_percent_txt = ["(%.2f %%)" % x for x in total_percent]
+                trace = go.Bar(x=dataframe["Name"], y=y, name=name, text=total_percent_txt, marker=dict(color=COLOR[field]))
                 traces.append(trace)
             return traces
 
