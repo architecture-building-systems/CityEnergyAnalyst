89e76ecae1b4c37ef6a6370e9c792848ae58e737
Reynold Mok <reyold@hotmail.com>
2021-01-18 11:02:51 +0800

    Add some console outputs for info

--- cea/interfaces/dashboard/api/project.py
+++ cea/interfaces/dashboard/api/project.py
@@ -290,6 +290,7 @@ class ScenarioImage(Resource):
                     image_modified = os.path.getmtime(image_path)
 
                 if zone_modified > image_modified:
+                    print(f'Generating preview image for scenario: {scenario}')
                     # Make sure .cache folder exists
                     if not os.path.exists(cache_path):
                         os.makedirs(cache_path)
@@ -307,6 +308,9 @@ class ScenarioImage(Resource):
                                 out = Polygon(polygon, 'blue', 'black', False)
                                 m.add_polygon(out)
                         else:
+                            print(f'Number of buildings({len(polygons)}) exceed building limit({building_limit}): '
+                                  f'Generating simplified image')
+                            # Generate only the shape outline of the zone area
                             convex_hull = polygons.unary_union.convex_hull
                             polygon = convex_hull.exterior.coords
                             out = Polygon(polygon, None, 'black', False)
