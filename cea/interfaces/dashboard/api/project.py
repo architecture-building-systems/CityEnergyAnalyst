23efea1f7287450f3cdcc2d08a9e73758b8ec0c1
Reynold Mok <reyold@hotmail.com>
2021-01-18 10:05:35 +0800

    Add building limit to simplify preview image

--- cea/interfaces/dashboard/api/project.py
+++ cea/interfaces/dashboard/api/project.py
@@ -264,6 +264,8 @@ class Scenario(Resource):
 class ScenarioImage(Resource):
     @api.doc(params={'project': 'Path of Project (Leave blank to use path in config)'})
     def get(self, scenario):
+        building_limit = 500
+
         project = request.args.get('project')
         if project is None:
             config = current_app.cea_config
@@ -297,11 +299,17 @@ class ScenarioImage(Resource):
                         zone_df = zone_df.to_crs(get_geographic_coordinate_system())
                         polygons = zone_df['geometry']
 
-                        polygons = [list(polygons.geometry.exterior[row_id].coords) for row_id in range(polygons.shape[0])]
-
                         m = StaticMap(256, 160)
-                        for polygon in polygons:
-                            out = Polygon(polygon, 'blue', 'black', False)
+                        if len(polygons) <= building_limit:
+                            polygons = [list(polygons.geometry.exterior[row_id].coords) for row_id in
+                                        range(polygons.shape[0])]
+                            for polygon in polygons:
+                                out = Polygon(polygon, 'blue', 'black', False)
+                                m.add_polygon(out)
+                        else:
+                            convex_hull = polygons.unary_union.convex_hull
+                            polygon = convex_hull.exterior.coords
+                            out = Polygon(polygon, None, 'black', False)
                             m.add_polygon(out)
 
                         image = m.render()
