f8b2c711bf3c1e23c0249a3ba82db0d0589352f5
Jimeno Fonseca <fonseca.jimeno@gmail.com>
2021-02-22 08:02:14 +0100

    stacked und ordered

--- cea/plots/demand/peak_load.py
+++ cea/plots/demand/peak_load.py
@@ -29,7 +29,7 @@ class PeakLoadCurvePlot(cea.plots.demand.DemandPlotBase):
 
     @property
     def layout(self):
-        return go.Layout(barmode='group', yaxis=dict(title='Peak Demand [kW]'), showlegend=True,
+        return go.Layout(barmode='stack', yaxis=dict(title='Peak Demand [kW]'), showlegend=True,
                          xaxis=dict(title='Building Name'))
 
     def calc_graph(self):
@@ -49,53 +49,18 @@ class PeakLoadCurvePlot(cea.plots.demand.DemandPlotBase):
         else:
             traces = []
             dataframe = self.data
+            dataframe['total'] = dataframe[analysis_fields].sum(axis=1)
+            dataframe.sort_values(by='total', ascending=False, inplace=True)
+            dataframe.reset_index(inplace=True, drop=True)
             for field in analysis_fields:
                 y = dataframe[field]
                 name = NAMING[field]
-                trace = go.Bar(x=dataframe["Name"], y=y, name=name, marker=dict(color=COLOR[field]))
+                total_percent = (y / dataframe['total'] * 100).round(2).values
+                total_percent_txt = ["(%.2f %%)" % x for x in total_percent]
+                trace = go.Bar(x=dataframe["Name"], y=y, name=name, text=total_percent_txt, orientation='v',
+                               marker=dict(color=COLOR[field]))
                 traces.append(trace)
             return traces
-
-
-def peak_load_building(data_frame, analysis_fields, title, output_path):
-    # CREATE FIRST PAGE WITH TIMESERIES
-    traces = []
-    area = data_frame["GFA_m2"]
-    data_frame = data_frame[analysis_fields]
-    x = ["Absolute [kW] ", "Relative [W/m2]"]
-    for field in analysis_fields:
-        y = [data_frame[field], data_frame[field] / area * 1000]
-        name = NAMING[field]
-        trace = go.Bar(x=x, y=y, name=name,
-                       marker=dict(color=COLOR[field]))
-        traces.append(trace)
-
-    layout = go.Layout(images=LOGO, title=title, barmode='group', yaxis=dict(title='Peak Load'), showlegend=True)
-    fig = go.Figure(data=traces, layout=layout)
-    plot(fig, auto_open=False, filename=output_path)
-
-    return {'data': traces, 'layout': layout}
-
-
-def peak_load_district(data_frame_totals, analysis_fields, title, output_path):
-    traces = []
-    data_frame_totals['total'] = data_frame_totals[analysis_fields].sum(axis=1)
-    data_frame_totals = data_frame_totals.sort_values(by='total',
-                                                      ascending=False)  # this will get the maximum value to the left
-    for field in analysis_fields:
-        y = data_frame_totals[field]
-        total_perc = (y / data_frame_totals['total'] * 100).round(2).values
-        total_perc_txt = ["(" + str(x) + " %)" for x in total_perc]
-        name = NAMING[field]
-        trace = go.Bar(x=data_frame_totals["Name"], y=y, name=name,
-                       marker=dict(color=COLOR[field]))
-        traces.append(trace)
-
-    layout = go.Layout(title=title, barmode='group', yaxis=dict(title='Peak Load [kW]'), showlegend=True)
-    fig = go.Figure(data=traces, layout=layout)
-    plot(fig, auto_open=False, filename=output_path)
-
-    return {'data': traces, 'layout': layout}
 
 
 def diversity_factor(data_frame_timeseries, data_frame_totals, analysis_fields, title, output_path):
