b1e86974f7c27b62fb2346a8416ca8ccde50aacb
Shanshan Hsieh <hsieh@arch.ethz.ch>
2022-03-17 10:57:04 +0100

    remove hard-coded value 13 to number_of_configurations

--- cea/optimization/preprocessing/decentralized_buildings_heating.py
+++ cea/optimization/preprocessing/decentralized_buildings_heating.py
@@ -259,40 +259,42 @@ def disconnected_heating_for_building(building_name, supply_systems, T_ground_K,
         Opex_a_fixed_USD[3 + i][0] += Opex_a_fixed_GHP_USD
         Capex_opex_a_fixed_only_USD[3 + i][0] += Capex_a_GHP_USD + Opex_a_fixed_GHP_USD  # TODO:variable price?
     # Best configuration
-    Best = np.zeros((13, 1))
-    indexBest = 0
-    TAC_USD = np.zeros((13, 2))
-    TotalCO2 = np.zeros((13, 2))
-    for i in range(13):
+    number_of_configurations = len(GHG_tonCO2) # 13
+    Best = np.zeros((number_of_configurations, 1))
+    indexBest = None
+    TAC_USD = np.zeros((number_of_configurations, 2))
+    TotalCO2 = np.zeros((number_of_configurations, 2))
+    for i in range(number_of_configurations):
         TAC_USD[i][0] = TotalCO2[i][0]  = i
-        Opex_a_USD[i][1] = Opex_a_fixed_USD[i][0] + + Opex_a_var_USD[i][4]
+        Opex_a_USD[i][1] = Opex_a_fixed_USD[i][0] + Opex_a_var_USD[i][4]
         TAC_USD[i][1] = Capex_opex_a_fixed_only_USD[i][0] + Opex_a_var_USD[i][4]
         TotalCO2[i][1] = GHG_tonCO2[i][5]
+    # sort TAC_USD and TotalCO2
     CostsS = TAC_USD[np.argsort(TAC_USD[:, 1])]
     CO2S = TotalCO2[np.argsort(TotalCO2[:, 1])]
-    el = len(CostsS)
+    # rank results
     rank = 0
     Bestfound = False
-    optsearch = np.empty(el)
+    optsearch = np.empty(number_of_configurations)
     optsearch.fill(3)
     geothermal_potential = geothermal_potential_data.set_index('Name')
-    # Check the GHP area constraint
+    # Check the GHP area constraint for configuration 4-13
     for i in range(10):
         QGHP = (1 - i / 10.0) * Qnom_W
         areaAvail = geothermal_potential.loc[building_name, 'Area_geo']
         Qallowed = np.ceil(areaAvail / GHP_A) * GHP_HMAX_SIZE  # [W_th]
         if Qallowed < QGHP:
+            # disqualify the configuration if constraint not met
             optsearch[i + 3] += 1
             Best[i + 3][0] = - 1
-    while not Bestfound and rank < el:
-
+    while not Bestfound and rank < number_of_configurations:
         optsearch[int(CostsS[rank][0])] -= 1
         optsearch[int(CO2S[rank][0])] -= 1
-
-        if np.count_nonzero(optsearch) != el:
+        print(rank, optsearch)
+        if np.count_nonzero(optsearch) != number_of_configurations:
             Bestfound = True
-            indexBest = np.where(optsearch == 0)[0][0]
-
+            indexBest = np.where(optsearch == 0)[0][0] #FIXME: it is possible to have more than one configuration that reaches zero here
+            print(indexBest)
         rank += 1
     # get the best option according to the ranking.
     Best[indexBest][0] = 1
