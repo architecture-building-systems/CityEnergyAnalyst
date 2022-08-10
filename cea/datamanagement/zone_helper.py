05a213df2d305545454d63ad707cae00267f76ce
lguilhermers <50981039+lguilhermers@users.noreply.github.com>
2022-08-10 14:36:37 +0800

    Gets building height from OSM in zone helper

--- cea/datamanagement/zone_helper.py
+++ cea/datamanagement/zone_helper.py
@@ -88,7 +88,11 @@ def clean_attributes(shapefile, buildings_height, buildings_floors, buildings_he
             np.nanmedian(data_floors_sum_with_nan))  # median so we get close to the worse case
         shapefile["floors_ag"] = [int(x) if not np.isnan(x) else data_osm_floors_joined for x in
                                   data_floors_sum_with_nan]
-        shapefile["height_ag"] = shapefile["floors_ag"] * constants.H_F
+        if 'height' in list_of_columns:
+            shapefile["height_ag"] = shapefile['height'].fillna('0')
+            shapefile["height_ag"] = shapefile["height_ag"].where(shapefile["height_ag"]==0,shapefile["floors_ag"] * constants.H_F)
+        else:
+            shapefile["height_ag"] = shapefile["floors_ag"] * constants.H_F
     else:
         shapefile['REFERENCE'] = "User - assumption"
         if buildings_height is None and buildings_floors is not None:
