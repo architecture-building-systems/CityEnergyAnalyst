53eebdfc7e113b935b3a0b69af6b47fc511bbdfc
Reynold Mok <34395415+reyery@users.noreply.github.com>
2023-01-04 11:52:02 +0800

    Retrieve frontend build on startup

--- cea/interfaces/dashboard/frontend/__init__.py
+++ cea/interfaces/dashboard/frontend/__init__.py
@@ -1,3 +1,8 @@
+import os.path
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
 from flask import Blueprint
 
 blueprint = Blueprint('frontend', __name__, url_prefix='/', static_folder="build", static_url_path="/")
@@ -6,3 +11,18 @@ blueprint = Blueprint('frontend', __name__, url_prefix='/', static_folder="build
 @blueprint.route('/')
 def frontend():
     return blueprint.send_static_file('index.html')
+
+
+def get_build():
+    url = "https://github.com/architecture-building-systems/CityEnergyAnalyst-GUI/releases/download/v3.32.0-browser/build.zip"
+    output = os.path.dirname(os.path.abspath(__file__))
+
+    if os.path.exists(os.path.join(output, "build")):
+        return
+
+    with urlopen(url) as r:
+        with ZipFile(BytesIO(r.read())) as f:
+            f.extractall(output)
+
+
+get_build()
