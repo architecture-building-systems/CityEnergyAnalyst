127a79c45161e5f8c39bdf586c043f7a6f8d71a9
Reynold Mok <34395415+reyery@users.noreply.github.com>
2023-09-09 14:08:20 +0800

    Fix deprecated append method

--- cea/datamanagement/terrain_helper.py
+++ cea/datamanagement/terrain_helper.py
@@ -45,7 +45,7 @@ def calc_bounding_box_projected_coordinates(locator):
     # connect both files and avoid repetition
     data_zone, data_dis = get_zone_and_surr_in_projected_crs(locator)
     data_dis = data_dis.loc[~data_dis["Name"].isin(data_zone["Name"])]
-    data = data_zone.append(data_dis, ignore_index = True, sort=True)
+    data = pd.concat([data_zone, data_dis], ignore_index=True, sort=True)
     data = data.to_crs(get_geographic_coordinate_system())
     lon = data.geometry[0].centroid.coords.xy[0][0]
     lat = data.geometry[0].centroid.coords.xy[1][0]
