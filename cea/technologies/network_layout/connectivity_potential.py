3187967e0862bd3beb58527156b881a024babc05
Reynold Mok <34395415+reyery@users.noreply.github.com>
2023-09-09 17:23:29 +0800

    Store building geometry crs

--- cea/technologies/network_layout/connectivity_potential.py
+++ cea/technologies/network_layout/connectivity_potential.py
@@ -330,7 +330,7 @@ def create_terminals(building_centroids, crs, street_network):
     # get list of nearest points
     near_points = near_analysis(building_centroids, street_network, crs)
     # extend to the building centroids
-    all_points = pd.concat([near_points, building_centroids])
+    all_points = pd.concat([near_points.to_crs(crs), building_centroids.to_crs(crs)])
     all_points.crs = crs
     # Aggregate these points with the GroupBy
     lines_to_buildings = all_points.groupby(['Name'])['geometry'].apply(lambda x: LineString(x.tolist()))
