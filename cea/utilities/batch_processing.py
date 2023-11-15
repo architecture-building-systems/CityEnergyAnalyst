b7567137e3465bd4523b49c292c605c8b525a362
Zhongming Shi <dutszm@hotmail.com>
2023-11-15 16:02:09 +0100

    Update batch_processing.py

--- cea/utilities/batch_processing.py
+++ cea/utilities/batch_processing.py
@@ -1,13 +1,12 @@
 """
 Batch processing CEA commands over all scenarios in a project.
+This is a first exploration for the ETH MiBS IDP 2023.
 """
 
 import os
 import subprocess
-import cea
 
-
-__author__ = "Zhongming Shi"
+__author__ = "Mathias Niffeler, Zhongming Shi"
 __copyright__ = "Copyright 2023, Architecture and Building Systems - ETH Zurich"
 __credits__ = ["Zhongming Shi"]
 __license__ = "MIT"
@@ -17,11 +16,17 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
-#project directory
+#project directory, for example, /Users/zshi/Dropbox/CEA2/batch
 cea_project = r'/Users/zshi/Dropbox/CEA2/batch'
 
 #loop over all scenarios under the project
 for filename in os.listdir(cea_project)[1:]:
     cea_scenario = os.path.join(cea_project, '{scenario}'.format(scenario=filename))
     print('Executing CEA simulations on {cea_scenario}.'.format(cea_scenario=cea_scenario))
-    subprocess.run(['python','cea demand --scenario /Users/zshi/Dropbox/CEA2/batch/1'], shell=True, text=True)
+
+    # adding CEA to the environment
+    my_env = os.environ.copy()
+    my_env['PATH'] = f"/Users/zshi/micromamba/envs/cea/bin:{my_env['PATH']}"  #replace zshi with your name
+
+    # executing CEA commands
+    subprocess.run(['cea', 'demand', '--scenario', '{cea_scenario}'.format(cea_scenario=cea_scenario)], env=my_env)
