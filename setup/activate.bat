a5f770ae0e06e1ea68de2404e32af8d9f301bf32
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-02-06 14:57:35 +0100

    Fix micromamba activation

--- setup/activate.bat
+++ setup/activate.bat
@@ -1,5 +1,6 @@
-@SET MAMBA_ROOT_PREFIX="%~dp0dependencies\micromamba"
+@SET MAMBA_ROOT_PREFIX=%~dp0dependencies\micromamba
 @CALL "%~dp0dependencies\micromamba.exe" shell hook -s cmd.exe
+@CALL "%~dp0dependencies\micromamba\condabin\mamba_hook.bat"
 
 @REM run the command
 @CALL %*
