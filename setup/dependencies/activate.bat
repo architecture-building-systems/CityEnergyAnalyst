191e6e9326ff8ae46511aaef62f7a2f52273c71b
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-02-08 17:58:18 +0100

    Fix cea-env activation

--- setup/dependencies/activate.bat
+++
@@ -1,5 +0,0 @@
-@SET MAMBA_ROOT_PREFIX=%~dp0micromamba
-@CALL %windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& %~dp0micromamba.exe shell hook -s powershell | Out-String | Invoke-Expression"
-
-@REM run the command
-@CALL %*
