3027a3c2478a8de39d4f894b93574ca9c7e4084f
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-06-11 11:57:28 +0200

    Remove unused import

--- cea/datamanagement/terrain_helper.py
+++ cea/datamanagement/terrain_helper.py
@@ -7,7 +7,6 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 import rasterio
-import requests
 from pyproj import CRS
 from rasterio import MemoryFile
 from rasterio.mask import mask
