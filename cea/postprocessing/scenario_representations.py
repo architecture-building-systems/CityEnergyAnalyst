b757d71ac4a1b90ec49b1a20f1fd2c7eb0ca6500
peppenappi <gnappi@student.ethz.ch>
2024-07-02 12:47:52 +0800

    Added irradiation comparison per scenario

--- cea/postprocessing/scenario_representations.py
+++ cea/postprocessing/scenario_representations.py
@@ -6,14 +6,16 @@ import numpy as np
 
 # Define the main directory and the filename
 main_directory = "D:/CEATesting/THESIS_TEST_CASES_BASE/"
+main_directory_ren = "D:/CEATesting/THESIS_TEST_CASES_RENEWABLES/"
 directory_to_file = "inputs/building-geometry/zone.shp"
-save_directory = "D:/CEATesting/THESIS_TEST_CASES_PLOTS/"
+save_directory = "D:/CEATesting/THESIS_TEST_CASES_PLOTS/scenario_representations/"
 folder_combined = 'combined_analysis'
 selected_systems_combined = pd.DataFrame()
 selected_systems_structure = pd.DataFrame()
 percentage_variation = pd.DataFrame()
 current_DES = pd.DataFrame()
 singapore_shapefile_path = 'D:/CEATesting/THESIS_TEST_CASES_PLOTS/combined_analysis/Singapore_shapefile/SGP_adm0.shp'
+irradiation_plot = 'outputs/data/potentials/solar'
 
 def plot_zones_with_labels_and_image_background(main_dir, scenarios, end_path, singapore_shapefile_path, save_path):
     fig, ax = plt.subplots(figsize=(12, 12))
@@ -69,7 +71,59 @@ def plot_zones_with_labels_and_image_background(main_dir, scenarios, end_path, s
     ax.set_axis_off()
 
     # Save the combined plot
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     plot_file_path = os.path.join(save_path, "zones_with_labels_and_image_background.png")
+    plt.savefig(plot_file_path)
+    plt.close('all')
+
+def scenarios_irradiation(base_path, irradiation_directory, scenarios, output_path):
+
+    radiation_per_scenario = pd.DataFrame(index=[0], columns=scenarios)
+
+    for scenario in scenarios:
+        building_list = []
+        tot_radiation_per_building = []
+        file_path = os.path.join(base_path, scenario, irradiation_directory)
+
+        # Check if the path exists
+        if not os.path.exists(file_path):
+            print(f"Path {file_path} does not exist.")
+            return
+
+        buildings = os.listdir(file_path)[1:-1]
+        for building in buildings:
+            parts = building.split('_')
+            if 'B' in parts[0]:
+                building_list.append(parts[0])
+        building_names = list(set(building_list))
+        building_names.sort()
+
+        for name in building_names:
+            radiation_file_path = os.path.join(file_path, f'{name}_PV_sensors.csv')
+            radiation = pd.read_csv(radiation_file_path, index_col=0)
+            # Use radiation specific to area to compare scenarios with different amount of buildings and areas
+            tot_radiation = radiation['total_rad_Whm2']
+            tot_radiation = sum(tot_radiation) / 1e9 # Convert to GWh
+            tot_radiation_per_building.append(tot_radiation)
+
+        radiation_per_scenario[f'{scenario}'][0] = sum(tot_radiation_per_building)
+
+    # Plotting the radiation_per_scenario DataFrame
+    fig, ax = plt.subplots(figsize=(10, 8))
+    radiation_per_scenario = radiation_per_scenario.T  # Transpose for better plotting
+    radiation_per_scenario.plot(kind='bar', legend=False, color='orange', ax=ax, edgecolor= 'black')
+    ax.set_ylabel('Radiation (GWh/m2)', fontsize=12)
+    ax.set_title('Total Yearly Radiation per Scenario', fontsize=14)
+    ax.set_xticklabels(radiation_per_scenario.index, rotation=45) # Rotate x-labels
+
+    # Use tight layout to fit everything properly
+    plt.tight_layout()
+
+    # Save the plot
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    plot_file_path = os.path.join(output_path, 'Total_Radiation_per_Scenario.png')
     plt.savefig(plot_file_path)
     plt.close('all')
 
@@ -78,3 +132,4 @@ scenarios = os.listdir(main_directory)[1:]
 scenarios.remove('dashboard.yml')
 
 plot_zones_with_labels_and_image_background(main_directory, scenarios, directory_to_file, singapore_shapefile_path, save_directory)
+scenarios_irradiation(main_directory_ren, irradiation_plot, scenarios, save_directory)
