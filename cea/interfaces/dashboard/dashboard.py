b9ae40351c7a998e141b2e5091d3bd3e7a36e6f7
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-09-05 17:45:03 +0200

    Write missing config to env file

--- cea/interfaces/dashboard/dashboard.py
+++ cea/interfaces/dashboard/dashboard.py
@@ -1,4 +1,5 @@
 import sys
+import tempfile
 
 import uvicorn
 
@@ -8,6 +9,7 @@ from cea.interfaces.dashboard.settings import get_settings
 def main(config):
     # Try loading settings from env vars first
     settings = get_settings()
+    config_dict = dict()
 
     # Load from config if not found in env vars
     if settings.host is None:
@@ -18,15 +20,24 @@ def main(config):
 
     if settings.worker_url is None:
         settings.worker_url = config.worker.url
+        config_dict["worker_url"] = config.worker.url
 
     if settings.project_root is None:
         settings.project_root = config.server.project_root
+        config_dict["project_root"] = config.server.project_root
 
     print(f"Using settings: {settings}")
 
     try:
-        uvicorn.run("cea.interfaces.dashboard.app:app",
-                    host=settings.host, port=settings.port)
+        # Write missing settings to temp env file
+        with tempfile.NamedTemporaryFile(mode="w") as f:
+            for key, value in config_dict.items():
+                f.write(f"CEA_{key.upper()}={value}\n")
+                f.flush()
+
+            uvicorn.run("cea.interfaces.dashboard.app:app",
+                        env_file=f.name,
+                        host=settings.host, port=settings.port, reload=True)
     except KeyboardInterrupt:
         sys.exit(0)
 
