113097031d81da2c26a61929bf49eef8870418d3
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-11-15 11:34:12 +0100

    Change to static method

--- cea/demand/demand_writers.py
+++ cea/demand/demand_writers.py
@@ -4,10 +4,6 @@ that sums the values up monthly. See the `cea.analysis.sensitivity.sensitivity_d
 the `MonthlyDemandWriter`.
 """
 
-
-
-
-
 import numpy as np
 import pandas as pd
 
@@ -23,7 +19,6 @@ class DemandWriter(object):
     """
 
     def __init__(self, loads, massflows, temperatures):
-
         from cea.demand.thermal_loads import TSD_KEYS_ENERGY_BALANCE_DASHBOARD, TSD_KEYS_SOLAR
 
         self.load_vars = loads
@@ -61,7 +56,7 @@ class DemandWriter(object):
         # if printing total values is necessary
         # treating timeseries data from W to MWh
         data = dict((x + '_MWhyr', np.nan_to_num(tsd[x]).sum() / 1000000) for x in self.load_vars)
-        data.update(dict((x + '0_kW', np.nan_to_num(tsd[x]) .max() / 1000) for x in self.load_vars))
+        data.update(dict((x + '0_kW', np.nan_to_num(tsd[x]).max() / 1000) for x in self.load_vars))
         # get order of columns
         keys = data.keys()
         columns = self.OTHER_VARS
@@ -152,7 +147,6 @@ class MonthlyDemandWriter(DemandWriter):
         return monthly_data_new
 
 
-
 def aggregate_results(locator, building_names):
     aggregated_hourly_results_df = pd.DataFrame()
 
@@ -166,14 +160,12 @@ def aggregate_results(locator, building_names):
     return aggregated_hourly_results_df
 
 
-class YearlyDemandWriter(DemandWriter):
+class YearlyDemandWriter:
     """Write out the yearly demand results"""
 
-    def __init__(self, loads, massflows, temperatures):
-        super(YearlyDemandWriter, self).__init__(loads, massflows, temperatures)
-
-    def write_aggregate_buildingly(self, locator, building_names):
-        """read in the temporary results files and append them to the Total_demand_buildingly.csv file."""
+    @staticmethod
+    def write_aggregate_buildings(locator, building_names):
+        """read in the temporary results files and append them to the Total_demand_building.csv file."""
         df = None
         for building in building_names:
             temporary_file = locator.get_temporary_file('%(building)sT.csv' % locals())
@@ -183,7 +175,8 @@ class YearlyDemandWriter(DemandWriter):
                 df = pd.concat([df, pd.read_csv(temporary_file)], ignore_index=True)
         df.to_csv(locator.get_total_demand('csv'), index=False, float_format='%.3f', na_rep='nan')
 
-    def write_aggregate_hourly(config, locator, building_names):
+    @staticmethod
+    def write_aggregate_hourly(locator, building_names):
         """read in the building files and append them to the Total_demand_hourly.csv file."""
         aggregated_hourly_results_df = pd.DataFrame()
 
@@ -200,7 +193,8 @@ class YearlyDemandWriter(DemandWriter):
         aggregated_hourly_results_df.to_csv(locator.get_total_demand_hourly('csv'),
                                             index=True, float_format='%.3f', na_rep='nan')
 
-    def write_to_hdf5(self, list_buildings, locator):
+    @staticmethod
+    def write_to_hdf5(locator, list_buildings):
         """read in the temporary results files and append them to the Totals.csv file."""
         df = None
         for name in list_buildings:
