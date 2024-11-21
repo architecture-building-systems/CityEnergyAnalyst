c3dfcca10beb7d214a647b8ce77d34b24bf74784
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-11-21 17:20:36 +0100

    Update demand function

--- cea/interfaces/dashboard/map_layers/demand/layers.py
+++ cea/interfaces/dashboard/map_layers/demand/layers.py
@@ -1,10 +1,8 @@
-import itertools
 import os
-from concurrent.futures import ThreadPoolExecutor
 
-import fiona
 import pandas as pd
-from pyproj import CRS, Transformer
+import geopandas as gpd
+from pyproj import CRS
 
 from cea.inputlocator import InputLocator
 from cea.interfaces.dashboard.map_layers import day_range_to_hour_range
@@ -15,7 +13,7 @@ from cea.interfaces.dashboard.map_layers.demand import DemandCategory
 class DemandMapLayer(MapLayer):
     category = DemandCategory
     name = "demand"
-    label = "Demand [kWh]"
+    label = "Grid Electricity Consumption [kWh]"
     description = "Energy Demand of buildings"
 
     @property
@@ -26,7 +24,8 @@ class DemandMapLayer(MapLayer):
         # FIXME: Hardcoded to zone buildings for now
         buildings = locator.get_zone_building_names()
 
-        return [(locator.get_demand_results_file, [building]) for building in buildings]
+        return [(locator.get_demand_results_file, [building]) for building in buildings] + [
+            (locator.get_zone_geometry, [])]
 
     @classmethod
     def expected_parameters(cls):
@@ -52,6 +51,7 @@ class DemandMapLayer(MapLayer):
         buildings = locator.get_zone_building_names()
         period = self.parameters['period']
         start, end = day_range_to_hour_range(period[0], period[1])
+        data_column = "GRID_kWh"
 
         output = {
             "data": [],
@@ -62,58 +62,37 @@ class DemandMapLayer(MapLayer):
             }
         }
 
-        # Convert coordinates to WGS84
-        with fiona.open(locator.get_zone_geometry()) as src:
-            transformer = Transformer.from_crs(src.crs, CRS.from_epsg(4326), always_xy=True)
-
-        def get_building_sensors(building):
-            metadata = pd.read_csv(locator.get_radiation_metadata(building)).set_index('SURFACE')
-            building_sensors = pd.read_feather(locator.get_radiation_building_sensors(building)) * 1 / 1000 # Convert W/m2 to kWh/m2
-
-            # Get terrain elevation
-            if "terrain_elevation" in metadata.columns:
-                terrain_elevation = metadata["terrain_elevation"].values[0]
-            else:
-                terrain_elevation = 0
+        def get_building_demand(building, centroid):
+            demand = pd.read_csv(locator.get_demand_results_file(building), usecols=[data_column])[data_column]
 
             total_min = 0
-            total_max = building_sensors.sum(numeric_only=True).max()
+            total_max = demand.sum()
 
             if start < end:
-                period_sensor_values = building_sensors.iloc[start:end+1].sum(numeric_only=True)
+                period_value = demand.iloc[start:end + 1].sum()
             else:
-                period_sensor_values = building_sensors.iloc[start:].sum(numeric_only=True) + building_sensors.iloc[:end+1].sum(numeric_only=True)
-            period_min = period_sensor_values.min()
-            period_max = period_sensor_values.max()
+                period_value = demand.iloc[start:].sum() + demand.iloc[:end + 1].sum()
+            period_min = period_value
+            period_max = period_value
 
-            # Ensure metadata index matches sensor_values keys
-            metadata_subset = metadata.loc[list(period_sensor_values.keys())]
-
-            # Vectorized transformation
-            transformed_positions = transformer.transform(
-                metadata_subset['Xcoor'].values,
-                metadata_subset['Ycoor'].values,
-                metadata_subset['Zcoor'].values - terrain_elevation
-            )
-
-            # Create data efficiently using list comprehension
-            data = [
-                {
-                    "position": [float(x), float(y), float(z)],
-                    "value": float(period_sensor_values[sensor])
-                }
-                for sensor, x, y, z in zip(period_sensor_values.keys(),
-                                           transformed_positions[0], transformed_positions[1], transformed_positions[2])
-            ]
+            data = {"position": [centroid.x, centroid.y], "value": period_value}
 
             return total_min, total_max, period_min, period_max, data
 
-        with ThreadPoolExecutor() as executor:
-            values = executor.map(get_building_sensors, buildings)
+        df = gpd.read_file(locator.get_zone_geometry()).set_index("Name").loc[buildings]
+        building_centroids = df.geometry.centroid.to_crs(CRS.from_epsg(4326))
+
+        # with ThreadPoolExecutor() as executor:
+        #     values = executor.map(get_building_demand, buildings, building_centroids)
+        #
+        # total_min, total_max, period_min, period_max, data = zip(*values)
+
+        values = (get_building_demand(building, centroid)
+                  for building, centroid in zip(buildings, building_centroids))
 
         total_min, total_max, period_min, period_max, data = zip(*values)
 
-        output['data'] = list(itertools.chain(*data))
+        output['data'] = data
         output['properties']['range'] = {
             'total': {
                 'label': 'Total Range',
