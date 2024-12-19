7d86e515058af6fc5ab9d23ec4fbd5fad8cf1bb9
Reynold Mok <34395415+reyery@users.noreply.github.com>
2024-12-19 11:33:40 +0100

    Remove commented route

--- cea/interfaces/dashboard/api/contents.py
+++ cea/interfaces/dashboard/api/contents.py
@@ -81,7 +81,6 @@ def get_content_info(content_path: str, content_type: ContentType,
 
 
 @router.get('')
-# @router.get('/{content_path}')
 async def get_contents(project_root: CEAProjectRoot, content_type: ContentType,
                        content_path: str = "", show_hidden: bool = False):
     """
