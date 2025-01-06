d183b8d49fdd0d162f78a1b632e02b28696c3218
Zhongming Shi <dutszm@hotmail.com>
2025-01-06 09:22:42 +0100

    Update test_inputs_setup_workflow.py

--- cea/tests/test_inputs_setup_workflow.py
+++ cea/tests/test_inputs_setup_workflow.py
@@ -22,7 +22,7 @@ class TestInputSetupWorkflowCase(unittest.TestCase):
         self.config.create_polygon.coordinates = POLYGON_COORDINATES
         self.config.create_polygon.filename = 'site'
 
-        data_initializer.main(self.config)
+        database_helper.main(self.config)
         create_polygon.main(self.config)
         # TODO: Mock osmnx.footprints_from_polygon
         zone_helper.main(self.config)
