ee2963c27368c9ce073dd93a19cd843452576106
Zhongming Shi <dutszm@hotmail.com>
2025-01-10 15:40:59 +0100

    Update cea4_verify.py

--- cea/datamanagement/format_helper/cea4_verify.py
+++ cea/datamanagement/format_helper/cea4_verify.py
@@ -18,3 +18,42 @@ __version__ = "0.1"
 __maintainer__ = "Reynold Mok"
 __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
+
+
+## --------------------------------------------------------------------------------------------------------------------
+## The paths to the input files
+## --------------------------------------------------------------------------------------------------------------------
+
+# The paths are relatively hardcoded for now without using the inputlocator script.
+# This is because we want to iterate over all scenarios, which is currently not possible with the inputlocator script.
+def path_to_input_file_without_db(scenario, item):
+
+    if item == "zone":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-geometry", "zone.shp")
+    elif item == "surroundings":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-geometry", "surroundings.shp")
+    elif item == "air_conditioning":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-properties", "air_conditioning.csv")
+    elif item == "architecture":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-properties", "architecture.csv")
+    elif item == "indoor_comfort":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-properties", "indoor_comfort.csv")
+    elif item == "internal_loads":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-properties", "internal_loads.csv")
+    elif item == "supply_systems":
+        path_to_input_file = os.path.join(scenario, "inputs", "building-properties", "supply_systems.csv")
+    elif item == 'networks':
+        path_to_input_file = os.path.join(scenario, "inputs", "network", "networks.csv")
+    elif item == 'terrain':
+        path_to_input_file = os.path.join(scenario, "inputs", "topography", "terrain.tif")
+    elif item == 'weather':
+        path_to_input_file = os.path.join(scenario, "inputs", "weather", "weather.epw")
+
+    return path_to_input_file
+
+## --------------------------------------------------------------------------------------------------------------------
+## Unique traits for the CEA-4 format
+## --------------------------------------------------------------------------------------------------------------------
+
+#1. about zone.shp
+ZONE_SHP_COLUMNS = ['Name', 'height_ag', 'floors_ag', 'height_bg', 'floors_bg', ]
