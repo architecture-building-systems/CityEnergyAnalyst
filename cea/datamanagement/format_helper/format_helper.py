bd61d40d8488e48f8543a367c490037fd74d8136
Zhongming Shi <dutszm@hotmail.com>
2025-01-10 12:20:22 +0100

    config and scripts ready

---
+++ cea/datamanagement/format_helper/format_helper.py
@@ -0,0 +1,20 @@
+"""
+Main script of the formate helper that activates the verification and migration as needed.
+
+"""
+
+import cea.inputlocator
+import os
+import cea.config
+import time
+import geopandas as gpd
+
+
+__author__ = "Zhongming Shi"
+__copyright__ = "Copyright 2025, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Zhongming Shi"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Reynold Mok"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
