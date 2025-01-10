91f092ef4efa385f470011fe481139e65cadd532
Zhongming Shi <dutszm@hotmail.com>
2025-01-10 11:24:40 +0100

    file structure

---
+++ cea/datamanagement/format_helper/cea4_migrate.py
@@ -0,0 +1,20 @@
+"""
+Mirgate the format of the input data to CEA-4 format after verification.
+
+"""
+
+import cea.inputlocator
+import os
+import cea.config
+import time
+import geopandas as gpd
+
+
+__author__ = "Zhongming Shi"
+__copyright__ = "Copyright 2025, Architecture and Building Systems - ETH Zurich"
+__credits__ = ["Zhongming Shi"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Reynold Mok"
+__email__ = "cea@arch.ethz.ch"
+__status__ = "Production"
