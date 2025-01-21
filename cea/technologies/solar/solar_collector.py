08524f86a59042df7e100640c08a48d15dc806ee
Martín Mosteiro Romero <m.a.mosteiroromero@tudelft.nl>
2025-01-21 18:18:11 +0100

    clear unnecessary angle unit conversions

--- cea/technologies/solar/solar_collector.py
+++ cea/technologies/solar/solar_collector.py
@@ -767,16 +767,11 @@ def calc_IAM_beam_SC(solar_properties, teta_z_deg, tilt_angle_deg, type_SCpanel,
     # convert to radians
     Sz_rad = np.radians(solar_properties.Sz)  # solar zenith angle
     Az_rad = np.radians(Az)  # solar_properties.Az)  # solar azimuth angle [rad]
-    # g_rad = np.radians(solar_properties.g)  # declination [rad]
-    # ha_rad = np.radians(solar_properties.ha)  # hour angle [rad]
-    # lat_rad = radians(latitude_deg)
     teta_z_rad = radians(teta_z_deg)
     tilt_rad = radians(tilt_angle_deg)
 
-    # incidence_angle_rad = np.vectorize(solar_equations.calc_incident_angle_beam)(g_rad, lat_rad, ha_rad, tilt_rad,
-    #                                                                              teta_z_rad)  # incident angle in radians
     incidence_angle_deg = pvlib.irradiance.aoi(tilt_angle_deg, teta_z_deg, solar_properties.Sz, Az)
-    incidence_angle_rad = [radians(x) for x in incidence_angle_deg]
+    incidence_angle_rad = [radians(x) for x in incidence_angle_deg]  # incident angle in radians
 
     # calculate incident angles
     if type_SCpanel == 'FP':
