913c51783fac174ee82ca45ca079aaf0da4c42bc
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-03-11 15:32:00 +0100

    Remove unused imports

--- cea/technologies/solar/photovoltaic.py
+++ cea/technologies/solar/photovoltaic.py
@@ -5,7 +5,7 @@ Photovoltaic
 import os
 import time
 from itertools import repeat
-from math import radians, degrees, asin, sin, acos, cos, exp, tan, ceil, log
+from math import radians, degrees, sin, acos, cos, tan, ceil, log
 from multiprocessing.dummy import Pool
 
 import numpy as np
