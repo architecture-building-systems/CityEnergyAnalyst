cbe5dd0b57c148318eddefbe0dcd82302c793356
Zhongming Shi <dutszm@hotmail.com>
2025-03-24 17:48:45 +0100

    sorting works

--- cea/visualisation/b_data_processor.py
+++ cea/visualisation/b_data_processor.py
@@ -29,6 +29,8 @@ class data_processor:
         self.y_normalised_by = config_config.y_normalised_by
         self.x_to_plot = plot_instance.x
         self.x_facet = plot_instance.x_facet
+        self.x_sorted_by = config_config.x_sorted_by
+        self.x_sorted_reversed = config_config.x_sorted_reversed
         self.integer_year_start = config_config.filter_buildings_by_year_start
         self.integer_year_end = config_config.filter_buildings_by_year_end
         self.list_construction_type = config_config.filter_buildings_by_construction_type
@@ -53,6 +55,29 @@ class data_processor:
         normaliser_m2 = normaliser_m2[['normaliser_m2']]
 
         return normaliser_m2
+
+    def process_sorting_key(self):
+        if self.x_sorted_by == 'default':
+            sorting_key = self.df_architecture_data.set_index('name').loc[self.buildings].copy()
+            sorting_key['sorting_key'] = self.df_architecture_data.reset_index().index
+        elif self.x_sorted_by == 'construction_year':
+            sorting_key = self.df_architecture_data.set_index('name').loc[self.buildings, ['construction_year']].copy()
+            sorting_key = sorting_key.rename(columns={'construction_year': 'sorting_key'})
+        elif self.x_sorted_by == 'gross_floor_area':
+            sorting_key = self.df_architecture_data.set_index('name').loc[self.buildings, ['GFA_m2']].copy()
+            sorting_key = sorting_key.rename(columns={'GFA_m2': 'sorting_key'})
+        elif self.x_sorted_by == 'conditioned_floor_area':
+            sorting_key = self.df_architecture_data.set_index('name').loc[self.buildings, ['Af_m2']].copy()
+            sorting_key = sorting_key.rename(columns={'Af_m2': 'sorting_key'})
+        elif self.x_sorted_by == 'roof_area':
+            sorting_key = self.df_architecture_data.set_index('name').loc[self.buildings, ['Aroof_m2']].copy()
+            sorting_key = sorting_key.rename(columns={'Aroof_m2': 'sorting_key'})
+        else:
+            raise ValueError(f"Invalid x-sorted-by: {self.x_sorted_by}")
+
+        sorting_key = sorting_key[['sorting_key']]
+
+        return sorting_key
 
     def process_demand_data(self):
         y_cea_metric_map = {
@@ -231,7 +256,31 @@ def generate_dataframe_for_plotly(plot_instance, df_summary_data, df_architectur
     else:
         raise ValueError(f"Invalid y-normalised-by: {plot_instance.y_normalised_by}")
 
+    # Sort X-axis
+
     return df_to_plotly, list_y_columns
+
+
+def sort_df_by_sorting_key(df_1, df_2, descending=False):
+    # Copy to avoid mutating original data
+    df_1 = df_1.copy()
+    df_2 = df_2.copy()
+
+    # Map sorting_key to df_2 using df_2['X']
+    sorting_map = df_1['sorting_key']
+    df_2['_sorting_key'] = df_2['X'].map(sorting_map)
+
+    # Sort with stable sort to preserve original order for ties
+    df_2_sorted = df_2.sort_values(
+        by='_sorting_key',
+        ascending=not descending,
+        kind='mergesort'
+    )
+
+    # Drop helper column
+    df_2_sorted = df_2_sorted.drop(columns=['_sorting_key'])
+
+    return df_2_sorted
 
 
 # Main function
@@ -240,6 +289,9 @@ def calc_x_y_metric(config_config, plot_instance_a, plot_cea_feature, df_summary
 
     if plot_cea_feature == "demand":
         df_to_plotly, list_y_columns = generate_dataframe_for_plotly(plot_instance_b, df_summary_data, df_architecture_data)
+
+        if plot_instance_b.x_to_plot == 'building' or 'building_faceted_by_months' or 'building_faceted_by_seasons' or 'building_faceted_by_construction_type' or 'building_faceted_by_main_use_type':
+            df_to_plotly = sort_df_by_sorting_key(plot_instance_b.process_sorting_key(), df_to_plotly, descending=plot_instance_b.x_sorted_reversed)
 
     else:
         print("Error: Unsupported feature:", plot_cea_feature)
