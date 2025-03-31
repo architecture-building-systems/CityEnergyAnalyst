1c69485a958a1c5a5128f74de045a17f3ab0c26f
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-03-31 16:54:30 +0200

    Determine plot to create based on the parameters

--- cea/visualisation/plot_main.py
+++ cea/visualisation/plot_main.py
@@ -4,6 +4,7 @@ CEAFrontEnd – Combines everything
 """
 
 import cea.config
+from cea import CEAException
 from cea.visualisation.c_plotter import generate_fig
 from cea.visualisation.a_data_loader import plot_input_processor
 from cea.visualisation.b_data_processor import calc_x_y_metric
@@ -19,37 +20,54 @@ __email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
-def config_config_locator(config, plot_cea_feature):
-    if plot_cea_feature == 'demand':
-        config_config = config.plots_demand
-    else:
-        raise ValueError(f"Cannot locate the config section for {plot_cea_feature}")
-    return config_config
+def get_plot_cea_feature(config: cea.config.Configuration) -> str:
+    """
+    Tries to determine the cea feature to plot based on the provided config.
+    Currently, only works with the cli interface.
+
+    When running using the cli interface, the config parameters passed to the script are restricted based on the scripts.yml file.
+    Using the restricted_to property, we can determine the cea feature to use based on the section name of the parameters.
+    e.g. plots-demand for demand plots. There should only be one section of parameters for each cea feature plot.
+
+    When running the script using the main function, the config parameters passed to the script are not restricted.
+    In this case, we can't determine the cea feature to use. Therefore, we do not support this mode.
+    """
+    if config.restricted_to is None:
+        raise CEAException("Unable to determine feature to plot. "
+                           "This currently only works on the cli interface by calling the relevant plot script directly.")
+
+    sections = {p.split(":")[0] for p in config.restricted_to if p.startswith("plots-")}
+    if len(sections) != 1:
+        raise CEAException("Unable to determine feature to plot. "
+                           "Ensure that only one type of plot config is provided in scripts.yml in the correct format. "
+                           "e.g. plots-demand")
+
+    return sections.pop().split("-", 1)[1]
 
 
-def plot_all(config, scenario, plot_cea_feature, hour_start, hour_end):
-    # Find the config file
-    config_config = config_config_locator(config, plot_cea_feature)
+def plot_all(config, scenario, plot_cea_feature, hour_start=0, hour_end=8759):
+    # Find the plot config section for the cea feature
+    try:
+        plot_config = config.sections[f"plots-{plot_cea_feature}"]
+    except KeyError:
+        raise CEAException(f"Invalid plot_cea_feature: {plot_cea_feature}. Ensure that it exists in default.config.")
 
     # Activate a_data_loader
-    df_summary_data, df_architecture_data, plot_instance = plot_input_processor(config_config, scenario, plot_cea_feature, hour_start, hour_end)
+    df_summary_data, df_architecture_data, plot_instance = plot_input_processor(plot_config, scenario, plot_cea_feature, hour_start, hour_end)
 
     # Activate b_data_processor
-    df_to_plotly, list_y_columns = calc_x_y_metric(config_config, plot_instance, plot_cea_feature, df_summary_data, df_architecture_data)
+    df_to_plotly, list_y_columns = calc_x_y_metric(plot_config, plot_instance, plot_cea_feature, df_summary_data, df_architecture_data)
 
     # Activate c_plotter
-    fig = generate_fig(config_config, df_to_plotly, list_y_columns)
-
+    fig = generate_fig(plot_config, df_to_plotly, list_y_columns)
     return fig
 
 
 def main(config):
+    scenario = config.scenario
 
-    scenario = '/Users/zshi/Library/CloudStorage/Dropbox/CEA2/batch_gh1/_ZRH'
-    plot_cea_feature = 'demand'
-    hour_start = 0
-    hour_end = 8759
-    fig = plot_all(config, scenario, plot_cea_feature, hour_start, hour_end)
+    plot_cea_feature = get_plot_cea_feature(config)
+    fig = plot_all(config, scenario, plot_cea_feature)
     fig.show()
 
 
