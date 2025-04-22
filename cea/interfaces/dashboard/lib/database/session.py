3867995ae745e34e3922f053c6714cdc4984f634
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-04-22 11:37:21 +0800

    Use set instead of update to fully replace query dict

--- cea/interfaces/dashboard/lib/database/session.py
+++ cea/interfaces/dashboard/lib/database/session.py
@@ -63,7 +63,7 @@ def get_connection_props():
             url = url.set(drivername="postgresql+asyncpg")
 
             # strip out sslmode (asyncpg doesn’t accept it)
-            url = url.update_query_dict({k: v for k, v in url.query.items() if k.lower() != "sslmode"})
+            url = url.set(query={k: v for k, v in url.query.items() if k.lower() != "sslmode"})
             return url.render_as_string(hide_password=False), {}
 
         return settings.db_url, {}
