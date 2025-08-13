364b42378f8cd9c49c2f491d43bd3a5926ab35be
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-08-13 18:48:06 +0200

    Add get_bin_path function to fetch binary directory
    Introduces a get_bin_path() utility that returns the path to the external tools binary directory, aiding in locating compiled C++ tools like DAYSIM and CRAX.

--- external/cea_external_tools/__init__.py
+++ external/cea_external_tools/__init__.py
@@ -1,3 +1,14 @@
 # TODO: Add functions to help fetch external tools
 
 __version__ = "0.1.0"
+
+
+import os
+
+def get_bin_path() -> str:
+    """
+    Returns the path to the external tools binary directory.
+    This is typically where compiled C++ tools like DAYSIM and CRAX are located.
+    """
+
+    return os.path.join(os.path.dirname(__file__), "bin")
