3baf45de7cbf971f57dbaf63cfe0c77c6ad9453d
Yiqiao Wang <wangyiqiao97@gmail.com>
2025-08-20 16:31:24 +0200

    update material_mapping

--- cea/datamanagement/construction_helper/construction_helper.py
+++ cea/datamanagement/construction_helper/construction_helper.py
@@ -3,11 +3,14 @@ from cea.config import Configuration
 from cea.inputlocator import InputLocator
 
 # Use DataFrame for component parameters
-component_properties = ["material", "thickness", "k_value", "density", "specific_heat"]
+component_properties = ["material", "thickness", "lambda", "density", "specific_heat"]
+
+# the model consists of three layers: exterior, insulation and structure
 default_component_params = pd.DataFrame(
     [[None for _ in component_properties] for _ in range(3)],
     columns=component_properties
 )
+
 default_construction_params = {
     "prefix": None,
     "wall": default_component_params.copy(),
@@ -15,6 +18,8 @@ default_construction_params = {
     "floor": default_component_params.copy(),
     "u_value": None,
     "a_value": None,
+    "GHG": None,
+    "biogenic": None,
 }
 
 def construction_helper_switzerland(config: Configuration, locator: InputLocator):
@@ -25,14 +30,31 @@ def construction_helper_switzerland(config: Configuration, locator: InputLocator
     # 1. get structure type and name
     section = config.sections["construction_helper"].parameters
     structure_type = section["structure_type"].get()
+    insulation_type = section["insulation_type"].get()
+    insulation_thickness = section["insulation_thickness"].get()
+    cladding_type = section["cladding_type"].get()
 
     construct_swiss = default_component_params.copy()
 
-    if structure_type in ["masonry", "concrete", "wood", "steel"]:
-        values = find_db_swiss(locator, structure_type)
+    if cladding_type in ["plaster", "aluminium", "stainless steel", "glass", "stone", "masonry", "wood", "ceramic"]:
+        values = find_db_swiss(locator, cladding_type)
+        construct_swiss.loc[0, component_properties] = values
+    else:
+        raise ValueError(f"Unknown cladding type: {cladding_type}")
+
+    if insulation_type in ["XPS", "EPS", "PUR", "glass wool", "rock wool"]:
+        values = find_db_swiss(locator, insulation_type)
         construct_swiss.loc[1, component_properties] = values
     else:
+        raise ValueError(f"Unknown insulation type: {insulation_type}")
+
+    if structure_type in ["masonry", "concrete", "wood", "steel"]:
+        values = find_db_swiss(locator, structure_type)
+        construct_swiss.loc[2, component_properties] = values
+    else:
         raise ValueError(f"Unknown structure type: {structure_type}")
+    
+    # 2. get thickness of each layer
 
 
 def construction_helper(config: Configuration, locator: InputLocator):
