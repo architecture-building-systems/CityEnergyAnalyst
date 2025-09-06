d7b850181fed2df7f03db87e29f0677da86b8d36
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-09-06 12:15:45 +0200

    Ensure plots return html

--- cea/visualisation/special/pareto_front.py
+++ cea/visualisation/special/pareto_front.py
@@ -2,7 +2,9 @@ import cea.config
 from cea.plots.optimization_new.a_pareto_front import main as pareto_front_main
 
 def main(config):
-    return pareto_front_main(config)
+    plot_2d, plot_3d =  pareto_front_main(config)
+
+    return plot_2d.to_html(), plot_3d.to_html()
 
 if __name__ == '__main__':
     main(cea.config.Configuration())
