Fix void_deck reference in RC model calculation
Replaced areas_df['void_deck'] with geometry['void_deck'] to correctly determine floating buildings in the BuildingRCModel class.

cea/demand/building_properties/building_rc_model.py
@@ -142,7 +142,7 @@ class BuildingRCModel:
         df['Htr_w'] = envelope['Awin_ag'] * envelope['U_win'] * np.sqrt(areas_df['Hs_ag'])
 
         # check if buildings are completely above ground
-        is_floating = (areas_df["void_deck"] > 0).astype(int)
+        is_floating = (geometry["void_deck"] > 0).astype(int)
 
         # direct thermal transmission coefficient to the external environment in [W/K]
         # Weigh area of with fraction of air-conditioned space, relationship of area and perimeter is squared