88ea55d3b1f5f83ad07e24b934a81df7f8be4617
Reynold Mok <34395415+reyery@users.noreply.github.com>
2025-09-22 16:31:33 +0200

    Fix type check for EnergyCarrier assignment
    Replaces the type check in EnergyFlow to use isinstance with the EnergyCarrier class directly, ensuring correct assignment and error handling for energy carrier objects.

--- cea/optimization_new/containerclasses/energyFlow.py
+++ cea/optimization_new/containerclasses/energyFlow.py
@@ -82,7 +82,7 @@ class EnergyFlow(object):
                 self._energy_carrier = EnergyCarrier(new_energy_carrier)
             except ValueError:
                 raise ValueError('The indicated energy carrier code is invalid.')
-        elif isinstance(new_energy_carrier, type(EnergyCarrier())):
+        elif isinstance(new_energy_carrier, EnergyCarrier):
             self._energy_carrier = new_energy_carrier
         else:
             raise ValueError('Please indicate a valid energy carrier.')
