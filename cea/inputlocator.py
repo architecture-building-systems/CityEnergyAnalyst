4ca4a7376c73a5c58e575784ffc248121541b64f
Yiqiao Wang <wangyiqiao97@gmail.com>
2025-11-20 23:37:20 +0100

    remove primary energy related changes from this branch

--- cea/inputlocator.py
+++ cea/inputlocator.py
@@ -1464,31 +1464,6 @@ class InputLocator(object):
         """scenario/outputs/data/emissions/operational/{building}_operational_hourly.csv"""
         return os.path.join(self.get_lca_operational_folder(), f"{building}_operational_hourly.csv")
 
-    # PRIMARY ENERGY
-    def get_primary_energy_folder(self):
-        """scenario/outputs/data/primary-energy"""
-        return os.path.join(self.scenario, 'outputs', 'data', 'primary-energy')
-
-    def get_primary_energy_hourly_folder(self):
-        """scenario/outputs/data/primary-energy/hourly"""
-        return os.path.join(self.get_primary_energy_folder(), 'hourly')
-
-    def get_primary_energy_annual(self):
-        """scenario/outputs/data/primary-energy/Total_primary_energy_buildings.csv"""
-        return os.path.join(self.get_primary_energy_folder(), 'Total_primary_energy_buildings.csv')
-
-    def get_primary_energy_building(self, building_name):
-        """scenario/outputs/data/primary-energy/{building_name}_primary_energy.csv"""
-        return os.path.join(self.get_primary_energy_folder(), f'{building_name}_primary_energy.csv')
-
-    def get_primary_energy_hourly_building(self, building_name):
-        """scenario/outputs/data/primary-energy/hourly/{building_name}_primary_energy_hourly.csv"""
-        return os.path.join(self.get_primary_energy_hourly_folder(), f'{building_name}_primary_energy_hourly.csv')
-
-    def get_primary_energy_hourly_district(self):
-        """scenario/outputs/data/primary-energy/Total_primary_energy_hourly.csv"""
-        return os.path.join(self.get_primary_energy_folder(), 'Total_primary_energy_hourly.csv')
-
     # COSTS
     def get_costs_folder(self):
         """scenario/outputs/data/costs"""
